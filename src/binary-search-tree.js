0d634371309cfa8f203cdc706d609bcd1caa1a5e
Sean Chen <skypemaster007@gmail.com>
2018-05-03 10:56:44 -0700

    Initial commit

---
+++ src/binary-search-tree.js
@@ -0,0 +1,45 @@
+class BinarySearchTree {
+  /* Do not modify the constructor */
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+
+  /* Inserts the given value
+  Make sure the rules of a binary search
+  tree are being adhered to */
+  insert(value) {
+
+  }
+
+  /* Traverses the tree until either the
+  target value has been found in the true
+  or the entire tree has been searched.
+  Returns true or false accordingly */
+  contains(target) {
+
+  }
+
+  /* Returns the maximum value in the tree 
+  Should not remove the max value from the tree */
+  getMax() {
+
+  }
+
+  /* Traverses the tree in a 'vertical' fashion,
+  from parent to child. Executes the given callback
+  on each visited tree node */
+  depthFirstForEach(cb) {
+
+  }
+
+  /* Traverses the tree in a 'horizontal' fashion,
+  from sibling to sibling. Executes the given callback
+  on each visited tree node */
+  breadthFirstForEach(cb) {
+
+  }
+}
+
+module.exports = BinarySearchTree;
---
+++ src/doubly-linked-list.js
@@ -0,0 +1,80 @@
+class ListNode {
+  /* Do not modify the constructor */
+  constructor(value, prev = null, next = null) {
+    this.value = value;
+    this.prev = prev;
+    this.next = next;
+  }
+
+  /* Insert the given value as this node's
+  `next` node */
+  insertAfter(value) {
+
+  }
+
+  /* Insert the given value as the this node's
+  `prev` node */
+  insertBefore(value) {
+
+  }
+
+  /* Delete this node */
+  delete() {
+
+  }
+}
+
+class DoublyLinkedList {
+  /* Do not modify the constructor */
+  constructor() {
+    this.head = null;
+    this.tail = null;
+  }
+
+  /* Adds the given value as the new head
+  node of the list */
+  addToHead(value) {
+
+  }
+
+  /* Remove the list's current head. The list's
+  `head` pointer should point to the removed node's
+  `next` node */
+  removeFromHead() {
+
+  }
+
+  /* Adds the given value as the new tail
+  node of the list */
+  addToTail(value) {
+
+  }
+
+  /* Remove the list's current tail. The list's
+  `tail` pointer should point to the removed node's
+  `prev` node */
+  removeFromTail() {
+
+  }
+
+  /* Move the given node to the front of the
+  list. Update the list's `head` pointer
+  accordingly */
+  moveToFront(node) {
+
+  }
+
+  /* Move the given node to the back of the
+  list. Update the list's `tail` pointer 
+  accordingly */
+  moveToBack(node) {
+
+  }
+
+  /* Delete the given node from the list */
+  delete(node) {
+
+  }
+}
+
+module.exports = DoublyLinkedList;

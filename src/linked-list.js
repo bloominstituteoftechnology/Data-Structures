e6730279d8d16f5d3d726259608110001add3f3f
charletta <charlettabullard12@outlook.com>
2018-05-07 16:55:00 -0400

    working on linked list last function--WIP

--- src/linked-list.js
+++ src/linked-list.js
@@ -9,26 +9,58 @@ class LinkedList {
   of the list. The `tail` pointer
   should be updated accordingly */
   addToTail(value) {
-
+    const newNode = {
+      value: value,
+      next: null,
+    };
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+      return;
+    }
+    this.tail.next = newNode;
+    this.tail = newNode;
   }
 
   /* Remove the list's `head` value 
   The `head` pointer should be updated
   accordingly */
   removeHead() {
+    if (this.length === 0) {
+      return null;
+    }
 
+    const value = this.head.value;
+    this.head = this.head.next;
+    this.length--;
+
+    return value;
   }
 
   /* Searches the list for the given value
   Returns true or false accordingly */
   contains(value) {
-
+    if (this.head === value) {
+      return true;
+    }
+    if (this.head !== null) {
+      return false;
+    }
+    return value;
   }
 
   /* Finds and returns the maximal value
   of all the values in the list */
   getMax() {
-    
+    const currentNode = this.head;
+    if (currentNode === null) {
+      return 0;
+    }
+    let max = currentNode + value;
+    if (currentNode + value > max) {
+      return max;
+    }
+    return null;
   }
 }
 

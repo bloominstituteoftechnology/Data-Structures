39ca47c648ec1b1ffc6b8e3f08061112514b68c2
Justin Kaseman <justinkaseman@live.com>
2018-05-07 11:55:02 -0700

    Pass linked-list

--- src/linked-list.js
+++ src/linked-list.js
@@ -9,26 +9,49 @@ class LinkedList {
   of the list. The `tail` pointer
   should be updated accordingly */
   addToTail(value) {
-
+    const newNode = {
+      value,
+      next: null,
+    };
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+      return;
+    }
+    this.tail.next = newNode;
+    this.tail = newNode;
   }
 
   /* Remove the list's `head` value 
   The `head` pointer should be updated
   accordingly */
   removeHead() {
-
+    const currentHead = this.head;
+    this.head = currentHead.next;
+    return currentHead.value;
   }
 
   /* Searches the list for the given value
   Returns true or false accordingly */
   contains(value) {
-
+    let currentNode = this.head;
+    while (currentNode) {
+      if (currentNode.value === value) return true;
+      else currentNode = currentNode.next;
+    }
+    return false;
   }
 
   /* Finds and returns the maximal value
   of all the values in the list */
   getMax() {
-    
+    let max = null;
+    let currentNode = this.head;
+    while (currentNode) {
+      if (currentNode.value > max) max = currentNode.value;
+      currentNode = currentNode.next;
+    }
+    return max;
   }
 }
 

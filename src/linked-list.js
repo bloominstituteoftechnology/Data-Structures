1a4050a68be3ad99015d8228caec53628e78b239
Frank Faustino <nawzyah@gmail.com>
2018-05-07 12:57:29 -0700

    Linked List donezo

--- src/linked-list.js
+++ src/linked-list.js
@@ -1,35 +1,58 @@
 class LinkedList {
   /* Do not modify the constructor */
   constructor() {
-    this.head = null;
-    this.tail = null;
+    this.head = null
+    this.tail = null
   }
 
   /* Add the given value to the tail
   of the list. The `tail` pointer
   should be updated accordingly */
   addToTail(value) {
+    const node = { value, next: null }
 
+    if (!this.head) {
+      this.head = node
+      this.tail = node
+      return
+    }
+
+    this.tail.next = node
+    this.tail = node
   }
 
-  /* Remove the list's `head` value 
+  /* Remove the list's `head` value
   The `head` pointer should be updated
   accordingly */
   removeHead() {
-
+    let removed = this.head
+    this.head = this.head.next
+    return removed.value
   }
 
   /* Searches the list for the given value
   Returns true or false accordingly */
   contains(value) {
-
+    let tmp = this.head
+    while (tmp) {
+      if (tmp.value === value) return true
+      tmp = tmp.next
+    }
+    return false
   }
 
   /* Finds and returns the maximal value
   of all the values in the list */
   getMax() {
-    
+    if (!this.head) return null
+    let maximal = this.head.value
+    let tmp = this.head
+    while (tmp) {
+      if (maximal < tmp.value) maximal = tmp.value
+      tmp = tmp.next
+    }
+    return maximal
   }
 }
 
-module.exports = LinkedList;
+module.exports = LinkedList

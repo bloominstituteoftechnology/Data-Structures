cee89810937d1db162526a79338743fd982c01f4
mjd02 <mostdarryl@gmail.com>
2018-05-07 15:52:19 -0400

    passed all tests

--- src/linked-list.js
+++ src/linked-list.js
@@ -34,20 +34,42 @@ class LinkedList {
   The `head` pointer should be updated
   accordingly */
   removeHead() {
-
+    let val = this.head.value;
+    this.head = this.head.next;
+    return val;
   }
 
   /* Searches the list for the given value
   Returns true or false accordingly */
   contains(value) {
-
-
+    let current = this.head;
+    let found = false;
+    while(current){
+      if(current.value === value){
+        return true;
+      }
+      current = current.next;
+    }
+    return false;
   }
 
   /* Finds and returns the maximal value
   of all the values in the list */
   getMax() {
-    
+    if(this.head){
+      let current = this.head;
+      let max = current.value;
+      while(current){
+        if(current.value > max){
+          max = current.value;
+        }
+        current = current.next;
+      }
+      return max;
+    }
+    else{
+      return null;
+    }
   }
 }
 

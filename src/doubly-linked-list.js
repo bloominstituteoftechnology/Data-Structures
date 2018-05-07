0c3773305e27c32ca32ab3efd82e5d8ab39c9e8f
zackhitch <zackhitch@users.noreply.github.com>
2018-05-07 15:56:41 -0700

    Changed syntax, removed last line in helper methods

--- src/doubly-linked-list.js
+++ src/doubly-linked-list.js
@@ -9,25 +9,36 @@ class ListNode {
   /* Insert the given value as this node's
   `next` node */
   insertAfter(value) {
-    const newNode = new ListNode(value);
-    newNode.next = this.next;
-    newNode.prev = this;
-    this.next = newNode;
-    newNode.next.prev = newNode;
+    // const newNode = new ListNode(value);
+    // newNode.next = this.next;
+    // newNode.prev = this;
+    // this.next = newNode;
+    // newNode.next.prev = newNode;
+    this.next = new ListNode(value, this, this.next);
   }
 
   /* Insert the given value as the this node's
   `prev` node */
   insertBefore(value) {
-    const newNode = new ListNode(value);
-    newNode.next = this;
-    newNode.prev = this.prev;
-    this.prev = newNode;
-    newNode.prev.next = newNode;
+    // const newNode = new ListNode(value);
+    // newNode.next = this;
+    // newNode.prev = this.prev;
+    // this.prev = newNode;
+    // newNode.prev.next = newNode;
+    this.prev = new ListNode(value, this.prev, this);
+    // newNode.prev.next = newNode;
   }
 
   /* Delete this node */
-  delete() {}
+  delete() {
+    if (this.head === this.tail) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.next.prev = this.prev;
+      this.prev.next = this.next;
+    }
+  }
 }
 
 class DoublyLinkedList {
@@ -42,21 +53,11 @@ class DoublyLinkedList {
   addToHead(value) {
     if (this.head !== null) {
       this.head.insertBefore(value);
+      this.head = this.head.prev;
     } else {
       this.head = new ListNode(value);
+      this.tail = this.head;
     }
-    // const newNode = {
-    //   value: value,
-    //   next: null,
-    // };
-    // if (this.head === null) {
-    //   this.head = newNode;
-    //   this.tail = newNode;
-    // } else {
-    //   newNode.next = this.head;
-    //   this.head.prev = newNode;
-    //   this.head = newNode;
-    // }
   }
 
   /* Remove the list's current head. The list's
@@ -72,17 +73,13 @@ class DoublyLinkedList {
   /* Adds the given value as the new tail
   node of the list */
   addToTail(value) {
-    const newNode = {
-      value: value,
-      next: null,
-    };
-    if (!this.head) {
-      this.head = newNode;
-      this.tail = newNode;
-      return;
+    if (this.tail !== null) {
+      this.tail.insertAfter(value);
+      this.tail = this.tail.next;
+    } else {
+      this.head = new ListNode(value);
+      this.tail = this.head;
     }
-    this.tail.next = newNode;
-    this.tail = newNode;
   }
 
   /* Remove the list's current tail. The list's
@@ -107,7 +104,9 @@ class DoublyLinkedList {
   moveToBack(node) {}
 
   /* Delete the given node from the list */
-  delete(node) {}
+  delete(node) {
+    node.delete();
+  }
 }
 
 module.exports = DoublyLinkedList;

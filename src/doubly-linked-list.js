05d74fccd2c5e572e57cc25e44b73a547321293b
kait-schorr <kaitlynschorr@gmail.com>
2018-05-07 17:45:12 -0500

    Finished linked list and half of doubly linked list

--- src/doubly-linked-list.js
+++ src/doubly-linked-list.js
@@ -9,19 +9,28 @@ class ListNode {
   /* Insert the given value as this node's
   `next` node */
   insertAfter(value) {
+    let newNode = {
+      value: value,
+      next: null,
+      prev: this
+    };
 
+    this.next = newNode;
   }
 
   /* Insert the given value as the this node's
   `prev` node */
   insertBefore(value) {
-
+    let newNode = {
+      value: value,
+      next: this,
+      prev: null
+    };
+    this.prev = newNode;
   }
 
   /* Delete this node */
-  delete() {
-
-  }
+  delete() {}
 }
 
 class DoublyLinkedList {
@@ -34,47 +43,69 @@ class DoublyLinkedList {
   /* Adds the given value as the new head
   node of the list */
   addToHead(value) {
-
+    let newNode = new ListNode(value);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+      return;
+    }
+    newNode.next = this.head;
+    this.head.prev = newNode;
+    this.head = newNode;
   }
 
   /* Remove the list's current head. The list's
   `head` pointer should point to the removed node's
   `next` node */
   removeFromHead() {
-
+    if (!this.head) return null;
+    const removedNode = this.head;
+    this.head = this.head.next;
+    return removedNode.value;
   }
 
   /* Adds the given value as the new tail
   node of the list */
   addToTail(value) {
-
+    let newNode = new ListNode(value);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+      return;
+    }
+    newNode.prev = this.tail;
+    this.tail.next = newNode;
+    this.tail = newNode;
   }
 
   /* Remove the list's current tail. The list's
   `tail` pointer should point to the removed node's
   `prev` node */
   removeFromTail() {
-
+    if (!this.head) return null;
+    if (!this.tail.prev) {
+      let removedNode = this.tail;
+      this.head = null;
+      this.tail = null;
+      return removedNode.value;
+    }
+    const removedNode = this.tail;
+    this.tail = this.tail.prev;
+    return removedNode.value;
   }
 
   /* Move the given node to the front of the
   list. Update the list's `head` pointer
   accordingly */
-  moveToFront(node) {
-
-  }
+  moveToFront(node) {}
 
   /* Move the given node to the back of the
   list. Update the list's `tail` pointer 
   accordingly */
-  moveToBack(node) {
-
-  }
+  moveToBack(node) {}
 
   /* Delete the given node from the list */
-  delete(node) {
-
-  }
+  delete(node) {}
 }
 
 module.exports = DoublyLinkedList;
--- src/linked-list.js
+++ src/linked-list.js
@@ -9,26 +9,54 @@ class LinkedList {
   of the list. The `tail` pointer
   should be updated accordingly */
   addToTail(value) {
-
+    const newNode = {
+      value: value,
+      next: null
+    };
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+      return;
+    }
+    this.tail.next = newNode;
+    this.tail = newNode;
   }
 
   /* Remove the list's `head` value 
   The `head` pointer should be updated
   accordingly */
   removeHead() {
-
+    const removedHead = this.head;
+    this.head = this.head.next;
+    return removedHead.value;
   }
 
   /* Searches the list for the given value
   Returns true or false accordingly */
   contains(value) {
-
+    let nextNode = this.head;
+    while (nextNode) {
+      if (nextNode.value === value) {
+        return true;
+      }
+      nextNode = nextNode.next;
+    }
+    return false;
   }
 
   /* Finds and returns the maximal value
   of all the values in the list */
   getMax() {
-    
+    if (!this.head) return null;
+    let max = this.head.value;
+    let nextNode = this.head;
+    while (nextNode) {
+      if (nextNode.value > max) {
+        max = nextNode.value;
+      }
+      nextNode = nextNode.next;
+    }
+    return max;
   }
 }
 

ec311e7fdb45995ef6ebf4b8afe9c307aa898ee4
mjd02 <mostdarryl@gmail.com>
2018-05-07 16:34:23 -0400

    almost done

--- src/doubly-linked-list.js
+++ src/doubly-linked-list.js
@@ -9,18 +9,18 @@ class ListNode {
   /* Insert the given value as this node's
   `next` node */
   insertAfter(value) {
-
+    this.next = new ListNode(value,this,this.next);
   }
 
   /* Insert the given value as the this node's
   `prev` node */
   insertBefore(value) {
-
+    this.prev = new ListNode(value,this.prev,this);
   }
 
   /* Delete this node */
   delete() {
-
+    this.prev.next = this.next;
   }
 }
 
@@ -34,34 +34,69 @@ class DoublyLinkedList {
   /* Adds the given value as the new head
   node of the list */
   addToHead(value) {
-
+    if(!this.head || !this.tail){
+      this.head = this.tail = new ListNode(value);
+    }
+    else{
+      this.head.insertBefore(value);
+      this.head = this.head.prev;
+    }
   }
 
   /* Remove the list's current head. The list's
   `head` pointer should point to the removed node's
   `next` node */
   removeFromHead() {
-
+    if(this.head){
+      let value = this.head.value;
+      this.head = this.head.next;
+      return value;
+    }
+    else{
+      return null;
+    }
   }
 
   /* Adds the given value as the new tail
   node of the list */
   addToTail(value) {
-
+    if(!this.tail || !this.head){
+      this.head = this.tail = new ListNode(value);
+    }
+    else{
+      this.tail.insertAfter(value);
+      this.tail = this.tail.next;
+    }
   }
 
   /* Remove the list's current tail. The list's
   `tail` pointer should point to the removed node's
   `prev` node */
   removeFromTail() {
-
+    if(this.tail){
+      let value = this.tail.value;
+      this.tail = this.tail.prev;
+      return value;
+    }
+    else{
+      return null;
+    }
   }
 
   /* Move the given node to the front of the
   list. Update the list's `head` pointer
   accordingly */
   moveToFront(node) {
-
+    if(node.next){
+      node.next.prev = node.prev;
+    }
+    else{
+      this.tail = node.prev;
+    }
+    node.prev.next = node.next;
+    node.prev = null;
+    node.next = this.head;
+    this.head = node;
   }
 
   /* Move the given node to the back of the
@@ -76,5 +111,4 @@ class DoublyLinkedList {
 
   }
 }
-
 module.exports = DoublyLinkedList;

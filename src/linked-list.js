356cbbf12b02cc7b19cce36e602bdcaf63fa7c77
William Pelton <williamrpelton@gmail.com>
2018-05-07 18:21:30 -0500

    William Pelton Charletta Bullard

--- src/linked-list.js
+++ src/linked-list.js
@@ -9,26 +9,50 @@ class LinkedList {
   of the list. The `tail` pointer
   should be updated accordingly */
   addToTail(value) {
-
+    const newNode = {
+      value: value,
+      next: null,
+    };
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+      return;
+    }
+    this.tail.next = newNode;
+    this.tail = newNode;
   }
 
   /* Remove the list's `head` value 
   The `head` pointer should be updated
   accordingly */
   removeHead() {
-
+    const head = this.head.value;
+    this.head = this.head.next;
+    return head;
   }
 
   /* Searches the list for the given value
   Returns true or false accordingly */
   contains(value) {
-
+    let current = this.head;
+    while (current !== null) {
+      if (current.value === value) {
+        return true;
+      } current = current.next;
+    } return false;
   }
 
   /* Finds and returns the maximal value
   of all the values in the list */
   getMax() {
-    
+    if (!this.head) return null;
+    let max = null;
+    let current = this.head;
+    while (current) {
+      if (current.value > max) {
+        max = current.value;
+      } current = current.next;
+    } return max;
   }
 }
 

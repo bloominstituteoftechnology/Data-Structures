ba21d82608ba7a8bdef8c5bfa14b146f2d90ff8a
AJ Genung <DragonRydrz@me.com>
2018-05-07 17:21:44 -0400

    3/5 completed

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -10,7 +10,25 @@ class BinarySearchTree {
   Make sure the rules of a binary search
   tree are being adhered to */
   insert(value) {
+    const newNode = {
+      value: value,
+      right: null,
+      left: null,
+    };
+    // console.log(this.value, 'this.value');
 
+    const check = node => {
+      if (value >= node.value) {
+        node.right ? check(node.right) : (node.right = newNode);
+      } else if (value < node.value) {
+        node.left ? check(node.left) : (node.left = newNode);
+      }
+    };
+    if (!this.value) {
+      this.value = value;
+    } else {
+      check(this);
+    }
   }
 
   /* Traverses the tree until either the
@@ -18,28 +36,36 @@ class BinarySearchTree {
   or the entire tree has been searched.
   Returns true or false accordingly */
   contains(target) {
-
+    const check = node => {
+      if (node.value === target) return true;
+      if (target > node.value && node.right) return check(node.right);
+      if (target < node.value && node.left) return check(node.left);
+      return false;
+    };
+    return check(this);
   }
 
   /* Returns the maximum value in the tree 
   Should not remove the max value from the tree */
   getMax() {
-
+    let max = this.value;
+    let node = this;
+    while (node.right) {
+      node = node.right;
+      max = node.value;
+    }
+    return max;
   }
 
   /* Traverses the tree in a 'vertical' fashion,
   from parent to child. Executes the given callback
   on each visited tree node */
-  depthFirstForEach(cb) {
-
-  }
+  depthFirstForEach(cb) {}
 
   /* Traverses the tree in a 'horizontal' fashion,
   from sibling to sibling. Executes the given callback
   on each visited tree node */
-  breadthFirstForEach(cb) {
-
-  }
+  breadthFirstForEach(cb) {}
 }
 
 module.exports = BinarySearchTree;

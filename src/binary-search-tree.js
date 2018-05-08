dfd154267f49002f005a2fa2508c5531f5eb0763
csstone12 <36163530+csstone12@users.noreply.github.com>
2018-05-08 15:24:59 -0400

    Stone Chris | Data Structures

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -10,20 +10,56 @@ class BinarySearchTree {
   Make sure the rules of a binary search
   tree are being adhered to */
   insert(value) {
-
+let currentNode = this;
+  while (currentNode) {
+    if (value < currentNode.value) {
+      if (!currentNode.left) {
+        currentNode.left = { value, left: null, right: null };
+        return;
+      } else {
+        currentNode = this.left;
+      }
+    } else {
+      if (!currentNode.right) {
+        currentNode.right = { value, left: null, right: null };
+        return;
+      } else {
+        currentNode = this.right;
+      }
+    }
   }
+}
 
   /* Traverses the tree until either the
   target value has been found in the true
   or the entire tree has been searched.
   Returns true or false accordingly */
   contains(target) {
+    let currentNode = this;
+    while (currentNode) {
+      if (currentNode.value === target) return true;
+      if (target < currentNode.value) {
+        currentNode = currentNode.left;
+      } else {
+        currentNode = currentNode.right;
+      }
+    }
+    return false;
 
   }
 
   /* Returns the maximum value in the tree 
   Should not remove the max value from the tree */
   getMax() {
+    let max = this.value;
+    let currentNode = this;
+    while (currentNode) {
+      if (currentNode.value > max) max = currentNode.value;
+      else {
+        currentNode = currentNode.right;
+      }
+    }
+    return max;
 
   }
 
@@ -31,14 +67,31 @@ class BinarySearchTree {
   from parent to child. Executes the given callback
   on each visited tree node */
   depthFirstForEach(cb) {
-
+    let currentNode = this;
+ 	 
+    function depthFirst(node) {
+      if (!node) return null;
+      cb(node.value);
+      depthFirst(node.left);
+      depthFirst(node.right);
+    }
+    depthFirst(currentNode);
   }
 
   /* Traverses the tree in a 'horizontal' fashion,
   from sibling to sibling. Executes the given callback
   on each visited tree node */
   breadthFirstForEach(cb) {
-
+    var current = [this];
+    while (current.length > 0) {
+      let next = [];
+      current.forEach(node => {
+        cb(node.value);
+        if (node.left) next.push(node.left);
+        if (node.right) next.push(node.right);
+      });
+      current = next;
+    }
   }
 }
 
--- src/doubly-linked-list.js
+++ src/doubly-linked-list.js
@@ -9,13 +9,24 @@ class ListNode {
   /* Insert the given value as this node's
   `next` node */
   insertAfter(value) {
-
+    let newNode = {
+      value: value,
+      next: null,
+      prev: this
+    };
+          
+    this.next = newNode;
   }
 
   /* Insert the given value as the this node's
   `prev` node */
   insertBefore(value) {
-
+    let newNode = {
+      value: value,
+      next: this,
+      prev: null
+    };
+    this.prev = newNode;
   }
 
   /* Delete this node */
@@ -34,46 +45,99 @@ class DoublyLinkedList {
   /* Adds the given value as the new head
   node of the list */
   addToHead(value) {
-
+    let newNode = new ListNode(value);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+      return;
+    }
+    newNode.next = this.head;
+    this.head.prev = newNode;
+    this.head = newNode;
   }
 
   /* Remove the list's current head. The list's
   `head` pointer should point to the removed node's
   `next` node */
   removeFromHead() {
-
+    if (!this.head) return null;
+    const removedNode = this.head;
+    this.head = this.head.next;
+    return removedNode.value;
   }
 
   /* Adds the given value as the new tail
   node of the list */
   addToTail(value) {
-
+    let newNode = new ListNode(value);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+      return;
+    }
+    newNode.prev = this.tail;
+    this.tail.next = newNode;
+    this.tail = newNode;
   }
 
   /* Remove the list's current tail. The list's
   `tail` pointer should point to the removed node's
   `prev` node */
   removeFromTail() {
-
+    if (!this.head) return null;
+    if (!this.tail.prev) {
+      let removedNode = this.tail;
+      this.head = null;
+      this.tail = null;
+      return removedNode.value;
+    }
+    const removedNode = this.tail;
+    this.tail = this.tail.prev;
+    return removedNode.value;
   }
 
   /* Move the given node to the front of the
   list. Update the list's `head` pointer
   accordingly */
   moveToFront(node) {
-
+    if (this.head === this.tail) return;
+    let leftNode = node.prev || null;
+    let rightNode = node.next || null;
+    this.head.prev = node;
+    node.next = this.head;
+    node.prev = null;
+    this.head = node;
+    leftNode.next = rightNode;
+    if (rightNode) {
+      rightNode.prev = leftNode;
+    } else {
+      this.tail = leftNode;
+    }
   }
 
   /* Move the given node to the back of the
   list. Update the list's `tail` pointer 
   accordingly */
   moveToBack(node) {
-
+    if (this.head === this.tail) return;
+    let leftNode = node.prev || null;
+    let rightNode = node.next || null;
+    node.prev = this.tail;
+    this.tail.next = node;
+    this.tail = node;
+    node.next = null;
+    rightNode.prev = leftNode;
+    if (leftNode) {
+      leftNode.next = rightNode;
+    } else {
+      this.head = rightNode;
+    }
   }
 
   /* Delete the given node from the list */
   delete(node) {
-
+    node.next.prev = node.prev;
+    node.prev.next = node.next;
   }
 }
 

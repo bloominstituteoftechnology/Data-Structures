04c1748e9dc34980a049a766f5d8bcd22e9de825
zackhitch <zackhitch@users.noreply.github.com>
2018-05-07 22:45:55 -0700

    wip on BST, 4/6 passing, stuck on depthFirstForEach - ZH

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -9,17 +9,58 @@ class BinarySearchTree {
   /* Inserts the given value
   Make sure the rules of a binary search
   tree are being adhered to */
-  insert(value) {}
+  insert(value) {
+    let currentNode = this;
+    while (currentNode) {
+      if (value < currentNode.value) {
+        if (!currentNode.left) {
+          currentNode.left = { value };
+          return;
+        } else {
+          currentNode = this.left;
+        }
+      } else {
+        if (!currentNode.right) {
+          currentNode.right = { value };
+          return;
+        } else {
+          currentNode = this.right;
+        }
+      }
+    }
+  }
 
   /* Traverses the tree until either the
   target value has been found in the true
   or the entire tree has been searched.
   Returns true or false accordingly */
-  contains(target) {}
+  contains(target) {
+    let currentNode = this;
+    while (currentNode) {
+      if (target === currentNode.value) return true;
+      if (target < currentNode.value) {
+        currentNode = currentNode.left;
+      } else {
+        currentNode = currentNode.right;
+      }
+    }
+    return false;
+  }
 
   /* Returns the maximum value in the tree 
   Should not remove the max value from the tree */
-  getMax() {}
+  getMax() {
+    let currentNode = this;
+    let currentMax = currentNode.value;
+    while (currentNode) {
+      if (currentNode.value > currentMax) {
+        currentMax = currentNode.value;
+      } else {
+        currentNode = currentNode.right;
+      }
+    }
+    return currentMax;
+  }
 
   /* Traverses the tree in a 'vertical' fashion,
   from parent to child. Executes the given callback

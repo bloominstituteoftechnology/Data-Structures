0d4eff5ea506c598bc27b3a54f51722da28c9eee
noxasaxon <saxon.h@outlook.com>
2018-05-08 15:59:44 -0700

    binary search done

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -31,31 +31,52 @@ class BinarySearchTree {
   Returns true or false accordingly */
   contains(target) {
     //check main node
-    console.log(this.value);
     if (target === this.value) return true;
-    else if (target > this.value) {
-      if (this.right) this.right.contains(target);
-      else return false;
+    if (target > this.value) {
+      if (this.right) if (this.right.contains(target)) return true;
     } else {
-      if (this.left) this.left.contains(target);
-      else return false;
+      if (this.left) if (this.left.contains(target)) return true;
     }
     return false;
   }
 
   /* Returns the maximum value in the tree 
   Should not remove the max value from the tree */
-  getMax() {}
+  getMax() {
+    if (this.value) {
+      if (this.right) return this.right.getMax();
+      else return this.value;
+    }
+  }
 
   /* Traverses the tree in a 'vertical' fashion,
   from parent to child. Executes the given callback
   on each visited tree node */
-  depthFirstForEach(cb) {}
+  depthFirstForEach(cb) {
+    // first invoke on current node
+    cb(this.value);
+    if (this.left) this.left.depthFirstForEach(cb);
+    if (this.right) this.right.depthFirstForEach(cb);
+  }
 
   /* Traverses the tree in a 'horizontal' fashion,
   from sibling to sibling. Executes the given callback
   on each visited tree node */
-  breadthFirstForEach(cb) {}
+  breadthFirstForEach(cb) {
+    //use a queue to get order of elements
+    const q = [];
+    //push root node into the queue
+    q.push(this);
+    //iterate through queue til empty
+    while (q.length > 0) {
+      //remove item from queue and work on it
+      const node = q.shift();
+      //add node children to node
+      if (node.left) q.push(node.left);
+      if (node.right) q.push(node.right);
+      cb(node.value);
+    }
+  }
 }
 
 module.exports = BinarySearchTree;

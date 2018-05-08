f4807669d1b187fb6fd15dbb9d715f7690b3dd85
noxasaxon <34722463+noxasaxon@users.noreply.github.com>
2018-05-07 23:43:54 -0700

    linked-list done

--- src/linked-list.js
+++ src/linked-list.js
@@ -9,11 +9,15 @@ class LinkedList {
   of the list. The `tail` pointer
   should be updated accordingly */
   addToTail(value) {
-    if (this.head) {
-      const newNode = { value: value, prevNode: this.tail };
+    let newNode = {value : value};
+    if (this.head ) {
+      if(!this.tail){
+        this.head.nextNode = newNode;
+        this.tail = newNode;
+      }else
+      this.tail.nextNode = newNode;
       this.tail = newNode;
     } else {
-      const newNode = { value: value, prevNode: null };
       this.head = newNode;
       this.tail = newNode;
     }
@@ -22,15 +26,45 @@ class LinkedList {
   /* Remove the list's `head` value 
   The `head` pointer should be updated
   accordingly */
-  removeHead() {}
+  removeHead() {
+    let retObj = this.head;
+    // this.head = this.head.nextNode;
+    if(this.head.nextNode) this.head = this.head.nextNode;
+    else this.head = null;
+    return retObj.value;
+  }
 
   /* Searches the list for the given value
   Returns true or false accordingly */
-  contains(value) {}
+  contains(value) {
+    if(this.head.value === value) return true;
+    else {
+      let curObj = this.head;
+      while(1){
+        if(curObj.value === value) return true;
+        else if(curObj.nextNode){
+          curObj = curObj.nextNode;
+        }else return false;
+      }
+    }
+  }
 
   /* Finds and returns the maximal value
   of all the values in the list */
-  getMax() {}
+  getMax() {
+    let max = 0;
+    if(this.head) max = this.head.value;
+    else return null;
+    let curObj = null;
+    if(this.head.nextNode) curObj = this.head.nextNode;
+    else return max;
+    while(1){
+      console.log(curObj.value)
+      if(curObj.value > max )max = curObj.value;
+      if(curObj.nextNode) curObj = curObj.nextNode;
+      else return max;
+    }
+  }
 }
 
 module.exports = LinkedList;

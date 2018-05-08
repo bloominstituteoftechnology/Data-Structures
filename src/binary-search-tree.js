355b0d57d6040a5f6a66d05067fa8640e4bbb2cd
William Pelton <williamrpelton@gmail.com>
2018-05-08 18:51:05 -0500

    finished doubly linked list

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -11,24 +11,20 @@ class BinarySearchTree {
   tree are being adhered to */
   insert(value) {
     const node = new BinarySearchTree(value);
-    if (!this.root) {
-      this.root = node;
-    } else {
-      let current = this.root;
-      while (current !== null) {
-        if (node.value < current.value) {
-          if (!current.left) {
-            current.left = node;
-            break;
-          } current = current.left;
-        } else if (node.value > current.value) {
-          if (!current.right) {
-            current.right = node;
-            break;
-          } current = current.right;
-        } else {
+    let current = this;
+    while (current !== null) {
+      if (node.value < current.value) {
+        if (!current.left) {
+          current.left = node;
           break;
-        }
+        } current = current.left;
+      } else if (node.value > current.value) {
+        if (!current.right) {
+          current.right = node;
+          break;
+        } current = current.right;
+      } else {
+        break;
       }
     }
   };
@@ -38,27 +34,62 @@ class BinarySearchTree {
   or the entire tree has been searched.
   Returns true or false accordingly */
   contains(target) {
-
+    const checkCurrent = (current) => {
+      if (target === current.value) {
+        return true;
+      } if (target > current.value) {
+        if (!current.right) return false;
+        else return checkCurrent(current.right);
+      } if (target < current.value) {
+        if (!current.left) return false;
+        else return checkCurrent(current.left);
+      }
+    }; return checkCurrent(this);
   }
 
   /* Returns the maximum value in the tree 
   Should not remove the max value from the tree */
-  getMax() {
-
+  getMax(current=this) {
+    if (!current.right) return current.value;
+    return this.getMax(current.right);
   }
-
   /* Traverses the tree in a 'vertical' fashion,
   from parent to child. Executes the given callback
   on each visited tree node */
   depthFirstForEach(cb) {
-
+    let queue = [];
+    const traverseAndCb = (current) => {
+      cb(current.value);
+      if (current.right) {
+        queue.push(current.right);
+      } if (current.left) {
+        return traverseAndCb(current.left)
+      } if (!queue.length) return;
+      else {
+        traverseAndCb(queue.pop());
+      }
+    }; traverseAndCb(this);
   }
 
   /* Traverses the tree in a 'horizontal' fashion,
   from sibling to sibling. Executes the given callback
   on each visited tree node */
   breadthFirstForEach(cb) {
-
+    let queue = [];
+    //push left and right of root to queue
+    const traverseAndCb = (current) => {
+      cb(current.value);
+      if (current.left) {
+        queue.push(current.left);
+      } if (current.right) {
+        queue.push(current.right);
+      } if (!queue.length) return;
+      else {
+        return traverseAndCb(queue.shift());
+      }
+    }; traverseAndCb(this);
+    //traverse via shift
+    //push left and right of each in queue
   }
 }
 
--- src/doubly-linked-list.js
+++ src/doubly-linked-list.js
@@ -9,18 +9,18 @@ class ListNode {
   /* Insert the given value as this node's
   `next` node */
   insertAfter(value) {
-
+    this.next = value;
   }
 
   /* Insert the given value as the this node's
   `prev` node */
   insertBefore(value) {
-
+    this.prev = value;
   }
 
   /* Delete this node */
   delete() {
-
+    this.node = null;
   }
 }
 
@@ -34,46 +34,126 @@ class DoublyLinkedList {
   /* Adds the given value as the new head
   node of the list */
   addToHead(value) {
-
+    if (!this.head) {
+      this.head = new ListNode(value);
+      this.tail = this.head;
+    } else {
+      const oldHead = this.head;
+      const newHead = new ListNode(value, null, oldHead);
+      this.head = newHead;
+      oldHead.prev = this.head;
+    }
   }
 
   /* Remove the list's current head. The list's
   `head` pointer should point to the removed node's
   `next` node */
   removeFromHead() {
-
+    if (!this.head) return null;
+    const oldHead = { ...this.head };
+    if (!this.head.next) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.head = this.head.next;
+      this.head.prev = null;
+    }
+    return oldHead.value;
   }
 
   /* Adds the given value as the new tail
   node of the list */
   addToTail(value) {
-
+    if (!this.tail) {
+      this.tail = new ListNode(value);
+      this.head = this.tail;
+    } else {
+      const oldTail = this.tail;
+      const newTail = new ListNode(value, oldTail, null);
+      this.tail = newTail;
+      oldTail.next = this.tail;
+    }
   }
 
   /* Remove the list's current tail. The list's
   `tail` pointer should point to the removed node's
   `prev` node */
   removeFromTail() {
-
+    if (!this.tail) return null;
+    const oldTail = { ...this.tail };
+    if (!this.tail.prev) {
+      this.tail = null;
+      this.head = null;
+    } else {
+      this.tail = this.tail.prev;
+      this.tail.next = null;
+    }
+    return oldTail.value;
   }
 
   /* Move the given node to the front of the
   list. Update the list's `head` pointer
   accordingly */
   moveToFront(node) {
-
+    if (this.head === node) return;
+    if (node.next) {
+      node.next.prev = node.prev;
+      node.prev.next = node.next;
+    } else {
+      this.tail = node.prev;
+      this.tail.next = null;
+    }
+    this.head.prev = node;
+    node.prev = null;
+    node.next = this.head;
+    this.head = node;
   }
 
   /* Move the given node to the back of the
   list. Update the list's `tail` pointer 
   accordingly */
   moveToBack(node) {
-
+    //redundancy check
+    if (this.tail === node) return;
+    if (node.prev) {
+      //step out
+      node.next.prev = node.prev;
+      node.prev.next = node.next;
+    } else {
+      //step off the end
+      this.head = node.next;
+      this.head.prev = null;
+    }
+    //update tail, node, and list
+    this.tail.next = node;
+    node.next = null;
+    node.prev = this.tail;
+    this.tail = node;
   }
 
   /* Delete the given node from the list */
   delete(node) {
-
+    if (node.next) {
+      if (node.prev) {
+        //prev and next - update adjacent node references only
+        node.prev.next = node.next;
+        node.next.prev = node.prev;
+      } else {
+        //next but no prev (head) - update next and list head
+        node.next.prev = null;
+        this.head = node.next;
+      }
+    } else {
+      if (node.prev) {
+        //prev but no next (tail) - update previous and list tail
+        node.prev.next = null;
+        this.tail = node.prev;
+      } else {
+        //no prev no next (head/tail) - update list head and tail
+        this.head = null;
+        this.tail = null;
+      }
+    }
   }
 }
 

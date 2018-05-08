c3ec566963373904be8cb7b82d69ff4024f63e7b
crypto-jones <jeremyjones@Macintosh-58b035f2f989.local>
2018-05-08 14:36:07 -0700

    doubly linked list complete

--- src/doubly-linked-list.js
+++ src/doubly-linked-list.js
@@ -34,34 +34,90 @@ class DoublyLinkedList {
 
   /* Adds the given value as the new head
   node of the list */
-  addToHead(value) {}
+  addToHead(value) {
+    if (!this.head) {
+      const newNode = new ListNode(value, null, null);
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      const newNode = new ListNode(value, null, this.head);
+      this.head.insertBefore(newNode);
+      this.head = newNode;
+    }
+  }
 
   /* Remove the list's current head. The list's
   `head` pointer should point to the removed node's
   `next` node */
-  removeFromHead() {}
+  removeFromHead() {
+    if (!this.head) return null;
+    else {
+      const current = this.head;
+      this.head = current.next;
+      if (this.head) this.head.insertBefore(null);
+      current.delete();
+      return current.value;
+    }
+  }
 
   /* Adds the given value as the new tail
   node of the list */
-  addToTail(value) {}
+  addToTail(value) {
+    const newNode = new ListNode(value, this.tail, null);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.insertAfter(newNode);
+      this.tail = newNode;
+    }
+  }
 
   /* Remove the list's current tail. The list's
   `tail` pointer should point to the removed node's
   `prev` node */
-  removeFromTail() {}
+  removeFromTail() {
+    if (!this.tail) return null;
+    const value = this.tail.value;
+    if (this.head === this.tail) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      const current = this.tail;
+      this.tail = current.prev;
+      if (this.tail) this.tail.insertAfter(null);
+      current.delete();
+    }
+    return value;
+  }
 
   /* Move the given node to the front of the
   list. Update the list's `head` pointer
   accordingly */
-  moveToFront(node) {}
+  moveToFront(node) {
+    if (node === this.tail) this.tail = node.prev;
+    if (node.next) node.next.insertBefore(node.prev);
+    if (node.prev) node.prev.insertAfter(node.next);
+    this.addToHead(node.value);
+  }
 
   /* Move the given node to the back of the
   list. Update the list's `tail` pointer 
   accordingly */
-  moveToBack(node) {}
+  moveToBack(node) {
+    if (node === this.head) this.head = node.next;
+    if (node.next) node.next.insertBefore(node.prev);
+    if (node.prev) node.prev.insertAfter(node.next);
+    this.addToTail(node.value);
+  }
 
   /* Delete the given node from the list */
-  delete(node) {}
+  delete(node) {
+    if (node === this.head) this.head = node.next;
+    if (node === this.tail) this.tail = node.prev;
+    if (node.next) node.next.insertBefore(node.prev);
+    if (node.prev) node.prev.insertAfter(node.next);
+  }
 }
 
 module.exports = DoublyLinkedList;

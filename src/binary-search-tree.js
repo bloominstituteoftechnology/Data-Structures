8325361e858ba7cae2b56402cef63832c1685d9c
charletta <charlettabullard12@outlook.com>
2018-05-07 22:53:28 -0400

    got two binary search tree test to pass WIP

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -10,6 +10,26 @@ class BinarySearchTree {
   Make sure the rules of a binary search
   tree are being adhered to */
   insert(value) {
+    const node = new BinarySearchTree(value);
+    let current = this;
+    while (current !== null) {
+      console.log(this);
+      if (node.value < current.value) {
+        if (!current.left) {
+          current.left = node;
+          break;
+        }
+        current = current.left;
+      } else if (node.value > current.value) {
+        if (!current.right) {
+          current.right = node;
+          break;
+        }
+        current = current.right;
+      } else {
+        break;
+      }
+    }
 
   }
 
@@ -18,12 +38,25 @@ class BinarySearchTree {
   or the entire tree has been searched.
   Returns true or false accordingly */
   contains(target) {
+    const current = this.root;
+    while (current) {
+      if (target === current.target) {
+        return true;
+      }
+      if (target < current.target) {
+        current = current.left;
+      } else {
+        current = current.right;
+      }
+    }
+    return false;
 
   }
 
   /* Returns the maximum value in the tree 
   Should not remove the max value from the tree */
   getMax() {
+
 
   }
 

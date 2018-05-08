5c597b3a03f6ae16cff34d3187f27f8dc097f940
noxasaxon <saxon.h@outlook.com>
2018-05-07 20:12:17 -0700

    linked list

--- src/linked-list.js
+++ src/linked-list.js
@@ -9,27 +9,28 @@ class LinkedList {
   of the list. The `tail` pointer
   should be updated accordingly */
   addToTail(value) {
-
+    if (this.head) {
+      const newNode = { value: value, prevNode: this.tail };
+      this.tail = newNode;
+    } else {
+      const newNode = { value: value, prevNode: null };
+      this.head = newNode;
+      this.tail = newNode;
+    }
   }
 
   /* Remove the list's `head` value 
   The `head` pointer should be updated
   accordingly */
-  removeHead() {
-
-  }
+  removeHead() {}
 
   /* Searches the list for the given value
   Returns true or false accordingly */
-  contains(value) {
-
-  }
+  contains(value) {}
 
   /* Finds and returns the maximal value
   of all the values in the list */
-  getMax() {
-    
-  }
+  getMax() {}
 }
 
 module.exports = LinkedList;

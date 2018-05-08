386108e98d93b84635ccd6ad7192d20d704ce1c0
Cesar Mejia Leiva <cmejia@gmail.com>
2018-05-08 15:44:22 -0700

    working on binary-search-tree

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,45 +1,67 @@
 class BinarySearchTree {
   /* Do not modify the constructor */
-  constructor(value) {
-    this.value = value;
-    this.left = null;
-    this.right = null;
+  constructor (value) {
+    this.value = value
+    this.left = null
+    this.right = null
   }
 
   /* Inserts the given value
   Make sure the rules of a binary search
   tree are being adhered to */
-  insert(value) {
+  createRoot () {
+    this.root = null
+  }
+  insert (value) {
+    var root = this.root
 
+    if (!root) {
+      this.root = new BinarySearchTree(value)
+      this.value = this.root.value
+      return
+    }
+
+    let currentNode = root
+    let newNode = new BinarySearchTree(value)
+
+    while (currentNode) {
+      if (value < currentNode.value) {
+        if (!currentNode.left) {
+          currentNode.left = newNode
+          break
+        } else {
+          currentNode = currentNode.left
+        }
+      } else {
+        if (!currentNode.right) {
+          currentNode.right = newNode
+          break
+        } else {
+          currentNode = currentNode.right
+        }
+      }
+    }
   }
 
   /* Traverses the tree until either the
   target value has been found in the true
   or the entire tree has been searched.
   Returns true or false accordingly */
-  contains(target) {
+  contains (target) {}
 
-  }
-
-  /* Returns the maximum value in the tree 
+  /* Returns the maximum value in the tree
   Should not remove the max value from the tree */
-  getMax() {
-
-  }
+  getMax () {}
 
   /* Traverses the tree in a 'vertical' fashion,
   from parent to child. Executes the given callback
   on each visited tree node */
-  depthFirstForEach(cb) {
-
-  }
+  depthFirstForEach (cb) {}
 
   /* Traverses the tree in a 'horizontal' fashion,
   from sibling to sibling. Executes the given callback
   on each visited tree node */
-  breadthFirstForEach(cb) {
-
-  }
+  breadthFirstForEach (cb) {}
 }
 
-module.exports = BinarySearchTree;
+module.exports = BinarySearchTree

d2cfb7a515241c9e3d29eb7b6f4c3463acc46f64
nickbrenn <nbren@tutanota.com>
2018-06-04 17:59:08 -0400

    Finished binary-search-tree.js

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -10,7 +10,25 @@ class BinarySearchTree {
   Make sure the rules of a binary search
   tree are being adhered to */
   insert(value) {
-
+    const newNode = {
+      value: value,
+      left: null,
+      right: null
+    };
+    let currentNode = this;
+    while (currentNode) {
+      if (value < currentNode.value && currentNode.left !== null) {
+        currentNode = currentNode.left;
+      } else if (value < currentNode.value) {
+        currentNode.left = newNode;
+        break;
+      } else if (value > currentNode.value && currentNode.right !== null) {
+        currentNode = currentNode.right;
+      } else if (value > currentNode.value) {
+        currentNode.right = newNode;
+        break;
+      } else break;
+    }
   }
 
   /* Traverses the tree until either the
@@ -18,13 +36,28 @@ class BinarySearchTree {
   or the entire tree has been searched.
   Returns true or false accordingly */
   contains(target) {
+    let currentNode = this;
+    while (currentNode) {
+      if (target < currentNode.value && currentNode.left !== null) {
+        currentNode = currentNode.left;
+      } else if (target > currentNode.value && currentNode.right !== null) {
+        currentNode = currentNode.right;
+      }
 
+      if (currentNode.value === target) return true;
+
+      if (!currentNode.right && !currentNode.left) return false;
+    }
   }
 
   /* Returns the maximum value in the tree 
   Should not remove the max value from the tree */
   getMax() {
-
+    let currentNode = this;
+    while (currentNode.right) {
+      currentNode = currentNode.right;
+    }
+    return currentNode.value;
   }
 }
 

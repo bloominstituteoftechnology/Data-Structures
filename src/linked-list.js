8637424b73201556512d187787a5e3d949f9fc50
Justin Rigler <justinmrigler@gmail.com>
2018-06-04 16:29:04 -0400

    linkedlist addToTail complete

--- src/linked-list.js
+++ src/linked-list.js
@@ -9,27 +9,32 @@ class LinkedList {
   of the list. The `tail` pointer
   should be updated accordingly */
   addToTail(value) {
+    const newNode = {
+      value,
+      next: null
+    };
 
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+      return;
+    }
+    this.tail.next = newNode;
+    this.tail = newNode;
   }
 
   /* Remove the list's `head` value 
   The `head` pointer should be updated
   accordingly */
-  removeHead() {
-
-  }
+  removeHead() {}
 
   /* Searches the list for the given value
   Returns true or false accordingly */
-  contains(value) {
-
-  }
+  contains(value) {}
 
   /* Finds and returns the maximal value
   of all the values in the list */
-  getMax() {
-    
-  }
+  getMax() {}
 }
 
 module.exports = LinkedList;

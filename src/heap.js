60ab42eb8d6563f297aea66e9e9f6f5f59662096
Nedu Robert <nedumrobert@gmail.com>
2018-06-04 23:49:15 +0100

    Implemented deleting the maximal value from the heap

--- src/heap.js
+++ src/heap.js
@@ -8,14 +8,20 @@ class Heap {
   The heap should maintain the heap property 
   after insertion */
   insert(value) {
-    this.storage.push(value);
+    // if(value > this.getMax()) {
+    //   this.storage.unshift(value);
+    // } else {
+    //   this.storage.push(value);
+    // }
   }
 
   /* Remove the maximal value from the heap and
   return it. The heap should maintain the heap
   property after removing the maximal value */
   delete() {
-
+    const maxValue = this.storage.shift();
+    this.siftDown(0);
+    return maxValue;
   }
 
   /* Return the maximal value in the heap
@@ -42,7 +48,22 @@ class Heap {
   child's value is greater than the value located at
   the input index */
   siftDown(index) {
-    
+    const leftChildIndex = index * 2 + 1;
+    const rightChildIndex = index * 2 + 2;
+    let maxChildIndex;
+
+    if(this.storage[leftChildIndex] && this.storage[rightChildIndex]) {
+      maxChildIndex = this.storage[leftChildIndex] > this.storage[rightChildIndex] ? leftChildIndex : rightChildIndex;
+    } else if(this.storage[leftChildIndex]) {
+      maxChildIndex = leftChildIndex;
+    } else if(this.storage[rightChildIndex]) {
+      maxChildIndex = rightChildIndex;
+    }
+
+    if(this.storage[index] < this.storage[maxChildIndex]) {
+      [this.storage[maxChildIndex], this.storage[index]] = [this.storage[index], this.storage[maxChildIndex]];
+      this.siftDown(maxChildIndex);
+    }
   }
 }
 

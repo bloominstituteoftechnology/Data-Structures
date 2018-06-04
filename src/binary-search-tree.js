3fff324ce216ec4a581e872f98d1dff0a802e45b
Michal Chmielniak <michal@Michals-MacBook-Pro.local>
2018-06-04 15:40:31 -0400

    added in binary search tree.js

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -10,7 +10,13 @@ class BinarySearchTree {
   Make sure the rules of a binary search
   tree are being adhered to */
   insert(value) {
-
+    if (value > this.value) {
+      if(!this.right) this.right = new BinarySearchTree(value);
+      else this.right.insert(value);
+    } else {
+      if (!this.left) this.left = new BinarySearchTree(value);
+      else this.left.insert(value);
+    }
   }
 
   /* Traverses the tree until either the
@@ -18,13 +24,25 @@ class BinarySearchTree {
   or the entire tree has been searched.
   Returns true or false accordingly */
   contains(target) {
-
+    if (target > this.value) {
+      if (this.right.value === target) return true;
+      if (this.right.right === null && this.right.left === null) return false;
+      else return this.right.contains(target);
+    } else {
+      if (this.left.value === target) return true;
+      if (this.left.right === null && this.left.left === null) return false;
+      else return this.left.contains(target);
+    }
   }
 
   /* Returns the maximum value in the tree 
   Should not remove the max value from the tree */
   getMax() {
-
+    if (this.right  === null && this.left === null) return this.value;
+    else if (this.right) 
+    return this.right.getMax();
+    else if (this.left.value > this.value) 
+    return this.left.getMax();
   }
 }
 

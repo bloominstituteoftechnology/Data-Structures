5b8eefb7e51f0a850da417bbc5e1d99301904168
nickbrenn <nbren@tutanota.com>
2018-06-04 16:27:29 -0400

    Finished queue.js

--- src/queue.js
+++ src/queue.js
@@ -1,4 +1,4 @@
-const List = require('./linked-list');
+const List = require("./linked-list");
 
 class Queue {
   /* Do not modify the constructor */
@@ -9,24 +9,30 @@ class Queue {
 
   /* Adds the given item to the queue */
   enqueue(item) {
-    
+    this.size += 1;
+    this.storage.addToTail(item);
+    return this.storage.tail.value;
   }
 
   /* Removes and returns the least recently
   added item from the queue */
   dequeue() {
-
+    if (!this.size > 0) return null;
+    this.size -= 1;
+    const head = this.storage.head;
+    this.storage.removeHead();
+    return head.value;
   }
 
   /* Returns true if the queue contains no
   elements, false otherwise */
   isEmpty() {
-
+    return !this.storage.head;
   }
 
   /* A getter method for the length of the queue */
   get length() {
-
+    return this.size;
   }
 }
 

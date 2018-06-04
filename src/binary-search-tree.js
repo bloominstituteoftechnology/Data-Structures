0fa3999630fa35aa0221d34ebf2972ac0d65741e
Kaitlyn Flynn <kaitlynflynn@gmail.com>
2018-06-04 14:28:34 -0700

    Solved for binary-search-tree.js; insert, contains, & getMax

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -10,7 +10,19 @@ class BinarySearchTree {
   Make sure the rules of a binary search
   tree are being adhered to */
   insert(value) {
-
+    if (value > this.value) {
+      if(!this.right) {
+        this.right = new BinarySearchTree(value);
+      } else {
+        this.right.insert(value);
+      }
+    } else {
+      if(!this.left) {
+        this.left = new BinarySearchTree(value);
+      } else {
+        this.left.insert(value);
+      }
+    }
   }
 
   /* Traverses the tree until either the
@@ -18,12 +30,33 @@ class BinarySearchTree {
   or the entire tree has been searched.
   Returns true or false accordingly */
   contains(target) {
+   let pug = this
+    while (pug){
+     if(pug.value === target) return true;
 
+    if(pug.value > target) {
+      pug = pug.left
+    }
+    else {
+      pug = pug.right
+    }}
+    return false;
   }
 
   /* Returns the maximum value in the tree 
   Should not remove the max value from the tree */
   getMax() {
+    if(this.left === null && this.right === null) return this.value;
+
+    else if (this.right) return this.right.getMax();
+
+    else return this.left.getMax();
+
+  }
+  breadthFirstForEach() {
+
+  }
+  depthFirstForEach() {
 
   }
 }

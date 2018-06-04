f13bac5d8e8c82c6735acdd992f4f8b4902493b9
Michal Chmielniak <michal@Michals-MacBook-Pro.local>
2018-06-04 15:32:45 -0400

    added in heap.js answers

--- src/heap.js
+++ src/heap.js
@@ -8,25 +8,34 @@ class Heap {
   The heap should maintain the heap property 
   after insertion */
   insert(value) {
-
+    if (value > this.getMax()) this.storage.unshift.value;
+    else {
+      this.storage.push(value);
+      let index = this.getSize();
+      this.bubbleUp(index);
+    }
   }
 
   /* Remove the maximal value from the heap and
   return it. The heap should maintain the heap
   property after removing the maximal value */
   delete() {
+    const max = this.getMax();
+    this.storage.shift();
+    this.siftDown(0);
+    return max;
 
   }
 
   /* Return the maximal value in the heap
   without removing it */
   getMax() {
-
+    return this.storage[0];
   }
 
   /* Return the size of the heap */
   getSize() {
-
+    return this.storage.length;
   }
 
   /* Moves the element at the specified index "up"
@@ -34,7 +43,13 @@ class Heap {
   parent value is less than the value located at
   the input index */
   bubbleUp(index) {
-
+    const pelement = this.storage[Math.floor((index - 2) / 2)];
+    const celement = this.storage[index -1];
+    if (celement > pelement) {
+      this.storage[Math.floor((index - 2) / 2)] = celement;
+      this.storage[index - 1] = pelement;
+      return this.bubbleUp(Math.floor((index - 2) / 2));
+    }
   }
 
   /* Move the element at the specified index "down"
@@ -42,7 +57,19 @@ class Heap {
   child's value is greater than the value located at
   the input index */
   siftDown(index) {
-    
+    const pelement = this.storage[index];
+    const cleft = this.storage[index * 2 + 1];
+    const cright = this.storage[index * 2 + 2];
+    const largest = cleft >= cright ? cleft : cright;
+    if (largest > pelement) {
+      if (cleft === largest) {
+        this.storage[index * 2 + 1] = pelement;
+        this.storage[index] = clefft;
+      } else {
+        this.storage[index * 2 + 2] = pelement;
+        this.storage[index] = cright;
+      }
+    }
   }
 }
 

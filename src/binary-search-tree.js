de527a67b8384eb5ea4abf6eea5b8ea44d10e01d
Robert Hogan <roberthogan@Roberts-MacBook-Pro.local>
2018-06-04 19:16:43 -0400

    Robert Hogan. Completed linked list. very hard to understand this.

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -10,7 +10,7 @@ class BinarySearchTree {
   Make sure the rules of a binary search
   tree are being adhered to */
   insert(value) {
-
+    
   }
 
   /* Traverses the tree until either the
@@ -18,7 +18,6 @@ class BinarySearchTree {
   or the entire tree has been searched.
   Returns true or false accordingly */
   contains(target) {
-
   }
 
   /* Returns the maximum value in the tree 

757cc1ecebe82d79ea853713baae82690b7b6613
Shawn Stewart <s.stewart0220@gmail.com>
2018-06-04 17:27:13 -0400

    Completed linked-list.js

--- src/linked-list.js
+++ src/linked-list.js
@@ -16,24 +16,54 @@ class LinkedList {
 		if (!this.head) {
 			this.head = newNode;
 			this.tail = newNode;
-			return;
+		} else if (this.head === this.tail) {
+			this.head.next = newNode;
+			this.tail = newNode;
+		} else {
+			this.tail.next = newNode;
+			this.tail = newNode;
 		}
-		this.tail.next = newNode;
-		this.tail = newNode;
 	}
 
 	/* Remove the list's `head` value 
   The `head` pointer should be updated
   accordingly */
-	removeHead() {}
+	removeHead() {
+		const removed = this.head.value;
+		this.head = this.head.next;
+		return removed;
+	}
 
 	/* Searches the list for the given value
   Returns true or false accordingly */
-	contains(value) {}
+	contains(value) {
+		// function checkVal(node, val) {
+		// 	if (node.value === val) {
+		// 		return true;
+		// 	} else {
+		// 		checkVal(node.next, val);
+		// 	}
+		// }
+		// checkVal(this.head, value);
+		let current = this.head;
+		while (current) {
+			if (current.value === value) return true;
+			else current = current.next
+		}
+		return false
+	}
 
 	/* Finds and returns the maximal value
   of all the values in the list */
-	getMax() {}
+	getMax() {
+		let result = null;
+		let current = this.head;
+		while (current) {
+			if (current.value > result) result = current.value;
+			else current = current.next;
+		}
+		return result;
+	}
 }
 
 module.exports = LinkedList;

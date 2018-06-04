dcf0058673ce7e8eb9b649067e578b6a3b56689b
Jacob Leon Lyerla <umasour@yahoo.com>
2018-06-04 15:23:53 -0700

    search tree almost done just not passing insert

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -10,26 +10,51 @@ class BinarySearchTree {
   Make sure the rules of a binary search
   tree are being adhered to */
   insert(value) {
-    const bst = new BinarySearchTree(value)
-    if (value > this.value){
-      if(!this.left){
-        
-      }
-    }
+    const bst = new BinarySearchTree(value);
+    if (value > this.value) {
+      if (!this.right) {
+        this.right = bst;
 
+      }else{
+        this.right.insert(value)
+      }
+      if (value < this.value) {
+        if (!this.left) {
+          this.left = bst;
+  
+        }else{
+          this.left.insert(value)
+        }
+    }
   }
+}
 
   /* Traverses the tree until either the
   target value has been found in the true
   or the entire tree has been searched.
   Returns true or false accordingly */
   contains(target) {
-
+    if(this.value ===target)return true;
+    if(this.left && this.left.contains(target))return true;
+    if(this.right && this.right.contains(target))return true;
+  return false;
   }
+
 
   /* Returns the maximum value in the tree 
   Should not remove the max value from the tree */
   getMax() {
+ let max = this.value
+ let node = this;
+ while(node.right){
+   node = node.right;
+   max = node.value;
+   
+ }
+ return max;
+ 
+    // if(this.left === null && this.right === null)return this.value;
+    // else if(this.right) return this.right.getMax()
 
   }
 }

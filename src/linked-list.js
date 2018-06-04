c028898f3d1b2e27a724840b1ff1e3ff6a78ad89
Jacob Leon Lyerla <umasour@yahoo.com>
2018-06-04 13:53:27 -0700

    first two link list

--- src/linked-list.js
+++ src/linked-list.js
@@ -8,28 +8,39 @@ class LinkedList {
   /* Add the given value to the tail
   of the list. The `tail` pointer
   should be updated accordingly */
-  addToTail(value) {
 
+  addToTail(value) {
+    newNode = {
+      value: value,
+      next: null
+    };
+
+    if (!this.head) {
+      this.head = this.newNode;
+      this.tail = this.newNode;
+      return;
+    }
+
+    this.tail.next = newNode;
+    this.tail = newNode;
   }
 
   /* Remove the list's `head` value 
   The `head` pointer should be updated
   accordingly */
   removeHead() {
-
+    let currentNode = this.head;
+    this.head = currentNode.next
+    return currentNode.value
   }
 
   /* Searches the list for the given value
   Returns true or false accordingly */
-  contains(value) {
-
-  }
+  contains(value) {}
 
   /* Finds and returns the maximal value
   of all the values in the list */
-  getMax() {
-    
-  }
+  getMax() {}
 }
 
 module.exports = LinkedList;

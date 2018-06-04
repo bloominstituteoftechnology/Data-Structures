fcb320da521cd152853c06d2605c9ae3d0d59237
Velsu <riftmage50@gmail.com>
2018-06-04 22:07:32 +0200

    Linked Lists complete

--- src/linked-list.js
+++ src/linked-list.js
@@ -9,26 +9,57 @@ class LinkedList {
   of the list. The `tail` pointer
   should be updated accordingly */
   addToTail(value) {
-
+    const newNode = {
+      value,
+      next: null
+    };
+    // check if tail exists
+    if (this.tail) {
+      //give previous tail pointer to new tail
+      this.tail.next = newNode;
+    } else {
+      this.head = newNode;
+    }
+    // set the tail
+    this.tail = newNode;
   }
 
   /* Remove the list's `head` value 
   The `head` pointer should be updated
   accordingly */
   removeHead() {
-
+    const head = this.head.value;
+    this.head = this.head.next;
+    return head;
   }
 
   /* Searches the list for the given value
   Returns true or false accordingly */
   contains(value) {
-
+    let node = this.head;
+    while (node) {
+      if (node.value === value) {
+        return true;
+      } else {
+        node = node.next;
+      }
+    }
+    return false;
   }
 
   /* Finds and returns the maximal value
   of all the values in the list */
   getMax() {
-    
+    if (!this.head) return null;
+    let node = this.head;
+    let max = this.head.value;
+    while (node) {
+      if (node.value > max) {
+        max = node.value;
+      }
+      node = node.next;
+    }
+    return max;
   }
 }
 

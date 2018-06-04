67b7b91a86f7e94344df4dc0ebda7047ffc1577e
Cruise Brantley <cruise.brantley@gmail.com>
2018-06-04 16:57:01 -0500

    Binary Search Tree done

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -10,7 +10,26 @@ class BinarySearchTree {
   Make sure the rules of a binary search
   tree are being adhered to */
   insert(value) {
+    let temp = this;
+    const newLeaf = { value: value, left: null, right: null };
 
+    while (temp) {
+      if (value < temp.value) {
+        if (temp.left === null) {
+          temp.left = newLeaf;
+          return;
+        }
+        temp = temp.left;
+      } else if (value > temp.value) {
+        if (temp.right === null) {
+          temp.right = newLeaf;
+          return;
+        }
+        temp = temp.right;
+      } else {
+        return;
+      }
+    }
   }
 
   /* Traverses the tree until either the
@@ -18,13 +37,32 @@ class BinarySearchTree {
   or the entire tree has been searched.
   Returns true or false accordingly */
   contains(target) {
+    let temp = this;
 
+    while (temp) {
+      if (target < temp.value) {
+        if (temp.left === null) {
+          return false;
+        }
+        temp = temp.left;
+      } else if (target > temp.value) {
+        if (temp.right === null) {
+          return false;
+        }
+        temp = temp.right;
+      } else {
+        return true;
+      }
+    }
   }
 
   /* Returns the maximum value in the tree 
   Should not remove the max value from the tree */
   getMax() {
+    let temp = this;
 
+    while (temp.right) temp = temp.right;
+    return temp.value;
   }
 }
 

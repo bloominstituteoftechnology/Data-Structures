b7182a4f23f19bfad2238b3d9d1134b2b565fa1d
Kaitlyn Flynn <kaitlynflynn@gmail.com>
2018-06-04 13:33:02 -0700

    Solved for linked-list.js; addToTail, removeHead, contains & getMax

--- src/linked-list.js
+++ src/linked-list.js
@@ -9,26 +9,53 @@ class LinkedList {
   of the list. The `tail` pointer
   should be updated accordingly */
   addToTail(value) {
+    const newNode = {
+      value,
+      next: null
+    };
 
+    if(!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+      this.max = value;
+      return;
+    }
+
+    this.tail.next = newNode;
+    this.tail = newNode;
   }
 
   /* Remove the list's `head` value 
   The `head` pointer should be updated
   accordingly */
   removeHead() {
-
+    const pugHead = this.head;
+    this.head = pugHead.next;
+    return pugHead.value;
   }
 
   /* Searches the list for the given value
   Returns true or false accordingly */
   contains(value) {
+    let catNode = this.head;
 
+    while(catNode) {
+      if(catNode.value === value) return true;
+      catNode = catNode.next;
+    }
+    return false;
   }
 
   /* Finds and returns the maximal value
   of all the values in the list */
   getMax() {
-    
+    let max = null;
+    let catNode = this.head;
+    while(catNode) {
+      if(catNode.value > max) max = catNode.value;
+      catNode = catNode.next;
+    }
+    return max;
   }
 }
 

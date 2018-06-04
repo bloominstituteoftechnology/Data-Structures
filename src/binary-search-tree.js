52aa35fe1a46edacabaa46d6afac909cbdff4ff7
Devon Bacon <devon.bacon@gmail.com>
2018-06-04 13:58:31 -0700

    binary-search-tree.js - created insert method

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -10,7 +10,15 @@ class BinarySearchTree {
   Make sure the rules of a binary search
   tree are being adhered to */
   insert(value) {
+    const node = new BinarySearchTree(value);
 
+    if (value < this.value) {
+      if (this.left === null ) this.left = node;
+      else this.left.insert(value);
+    } else {
+      if (this.right === null) this.right = node;
+      else this.right.insert(value);
+    }
   }
 
   /* Traverses the tree until either the

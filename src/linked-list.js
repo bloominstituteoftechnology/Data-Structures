40d809e32a793fd5a65bc8148e2a3f1ccab6ea70
Mark Wong <fmarkwong@gmail.com>
2018-06-04 15:28:33 -0700

    linked list

--- src/linked-list.js
+++ src/linked-list.js
@@ -3,32 +3,57 @@ class LinkedList {
   constructor() {
     this.head = null;
     this.tail = null;
-  }
+
+ }
 
   /* Add the given value to the tail
   of the list. The `tail` pointer
   should be updated accordingly */
   addToTail(value) {
+    const newNode = {
+      value: value,
+      next: null
+    };
 
+    if (this.tail) {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail = this.head = newNode;
+    }
   }
 
   /* Remove the list's `head` value 
   The `head` pointer should be updated
   accordingly */
   removeHead() {
-
+    const head = this.head
+    this.head = this.head.next;
+    return head.value;
   }
 
   /* Searches the list for the given value
   Returns true or false accordingly */
   contains(value) {
-
+    let current = this.head;
+    while(current) {
+      if (current.value == value) return true
+      current = current.next;
+    }
+    return false;
   }
 
   /* Finds and returns the maximal value
   of all the values in the list */
   getMax() {
-    
+    if (this.head === null) return null;
+    let current = this.head;
+    let max = current.value;
+    while(current) {
+      if (current.value > max) max = current.value 
+      current = current.next;
+    }
+    return max;
   }
 }
 

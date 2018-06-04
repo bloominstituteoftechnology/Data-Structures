b324839b007c71d3bc45b495339f9f073bfc47b5
harrisonbrock <harrison@harrisonbrock.com>
2018-06-04 13:51:34 -0400

    Added queue methods

--- src/queue.js
+++ src/queue.js
@@ -1,33 +1,41 @@
 const List = require('./linked-list');
 
 class Queue {
-  /* Do not modify the constructor */
-  constructor() {
-    this.size = 0;
-    this.storage = new List();
-  }
+    /* Do not modify the constructor */
+    constructor() {
+        this.size = 0;
+        this.storage = new List();
+    }
 
-  /* Adds the given item to the queue */
-  enqueue(item) {
-    
-  }
+    /* Adds the given item to the queue */
+    enqueue(item) {
+        this.storage.addToTail(item);
+        this.size++;
+    }
 
-  /* Removes and returns the least recently
-  added item from the queue */
-  dequeue() {
+    /* Removes and returns the least recently
+    added item from the queue */
+    dequeue() {
 
-  }
+        if (this.isEmpty()) {
+            return null;
+        }
+        else {
+            this.size--;
+            return this.storage.removeHead();
+        }
+    }
 
-  /* Returns true if the queue contains no
-  elements, false otherwise */
-  isEmpty() {
+    /* Returns true if the queue contains no
+    elements, false otherwise */
+    isEmpty() {
+        return this.size === 0;
+    }
 
-  }
-
-  /* A getter method for the length of the queue */
-  get length() {
-
-  }
+    /* A getter method for the length of the queue */
+    get length() {
+        return this.size;
+    }
 }
 
 module.exports = Queue;

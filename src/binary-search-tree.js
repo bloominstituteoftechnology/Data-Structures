c97bbc47fff313dbc40f8ed7fff381e63b792205
Tylar Pierson <tylarpiersonbusiness@gmail.com>
2018-06-04 13:48:58 -0700

    filled out binary functions/removed old tests

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -10,7 +10,24 @@ class BinarySearchTree {
   Make sure the rules of a binary search
   tree are being adhered to */
   insert(value) {
-
+    let currentNode = this;
+    while (currentNode) {
+      if (value < currentNode.value) {
+        if (!currentNode.left) {
+          currentNode.left = { value };
+          return;
+        } else {
+          currentNode = this.left;
+        }
+      } else {
+        if (!currentNode.right) {
+          currentNode.right = { value };
+          return;
+        } else {
+          currentNode = this.right;
+        }
+      }
+    }
   }
 
   /* Traverses the tree until either the
@@ -18,13 +35,31 @@ class BinarySearchTree {
   or the entire tree has been searched.
   Returns true or false accordingly */
   contains(target) {
-
+    let currentNode = this;
+    while (currentNode) {
+      if (target === currentNode.value) return true;
+      if (target < currentNode.value) {
+        currentNode = currentNode.left;
+      } else {
+        currentNode = currentNode.right;
+      }
+    }
+    return false;
   }
 
   /* Returns the maximum value in the tree 
   Should not remove the max value from the tree */
   getMax() {
-
+    let currentNode = this;
+    let currentMax = currentNode.value;
+    while (currentNode) {
+      if (currentNode.value > currentMax) {
+        currentMax = currentNode.value;
+      } else {
+        currentNode = currentNode.right;
+      }
+    }
+    return currentMax;
   }
 }
 

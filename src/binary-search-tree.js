2134ffb90f3700bdc6f038a3fb7540d4f8a6188f
Nedu Robert <nedumrobert@gmail.com>
2018-06-04 22:24:18 +0100

    Implemented inserting values to a binary search tree

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -10,7 +10,20 @@ class BinarySearchTree {
   Make sure the rules of a binary search
   tree are being adhered to */
   insert(value) {
-
+    const rootNode = new BinarySearchTree(value);
+    if(value >= this.value){
+      if(!this.right){
+        this.right = rootNode;
+      } else {
+        this.right.insert(value);
+      }
+    } else {
+      if(!this.left){
+        this.left = rootNode;
+      } else {
+        this.left.insert(value);
+      }
+    }
   }
 
   /* Traverses the tree until either the

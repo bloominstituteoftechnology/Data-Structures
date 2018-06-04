9cef7de13ab368142a2ca53dbdf45ae0ca79bfd9
Jared Cuffe <jcuffe@gmail.com>
2018-06-04 14:22:32 -0700

    BST implemented

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -10,21 +10,58 @@ class BinarySearchTree {
   Make sure the rules of a binary search
   tree are being adhered to */
   insert(value) {
+    let leaf = this
+    let newLeaf = { value, left: null, right: null }
 
+    while (leaf) {
+      if (value < leaf.value) {
+        if (!leaf.left) {
+          leaf.left = newLeaf
+          return
+        }
+        leaf = leaf.left
+      } else if (value > leaf.value) {
+        if (!leaf.right) {
+          leaf.right = newLeaf
+          return
+        }
+        leaf = leaf.right
+      } else return
+    }
   }
 
   /* Traverses the tree until either the
   target value has been found in the true
   or the entire tree has been searched.
   Returns true or false accordingly */
-  contains(target) {
+  contains(value) {
+    let leaf = this
 
+    while (leaf) {
+      if (value < leaf.value) {
+        if (!leaf.left) {
+          return false
+        }
+        leaf = leaf.left
+      } else if (value > leaf.value) {
+        if (!leaf.right) {
+          return false
+        }
+        leaf = leaf.right
+      } else return true
+    }
   }
 
   /* Returns the maximum value in the tree 
   Should not remove the max value from the tree */
   getMax() {
+    let leaf = this
 
+    while (leaf) {
+      if (leaf.right) {
+        leaf = leaf.right
+      } else return leaf.value
+    }
   }
 }
 

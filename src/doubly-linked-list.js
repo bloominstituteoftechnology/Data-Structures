2032d4454bd150361eaa5aaaf5a1712e1a395e1f
Velsu <riftmage50@gmail.com>
2018-06-05 23:58:06 +0200

    Doubly Linked completed

--- src/doubly-linked-list.js
+++ src/doubly-linked-list.js
@@ -9,18 +9,19 @@ class ListNode {
   /* Insert the given value as this node's
   `next` node */
   insertAfter(value) {
-
+    this.next = value;
   }
 
   /* Insert the given value as the this node's
   `prev` node */
   insertBefore(value) {
-
+    this.prev = value;
   }
 
   /* Delete this node */
   delete() {
-
+    this.next = null;
+    this.prev = null;
   }
 }
 
@@ -34,46 +35,98 @@ class DoublyLinkedList {
   /* Adds the given value as the new head
   node of the list */
   addToHead(value) {
-
+    if (!this.head) {
+      const node = new ListNode(value, null, null);
+      this.head = node;
+      this.tail = node;
+    } else {
+      const node = new ListNode(value, null, this.head);
+      this.head.insertBefore(node);
+      this.head = node;
+    }
   }
 
   /* Remove the list's current head. The list's
   `head` pointer should point to the removed node's
   `next` node */
   removeFromHead() {
+    if (!this.head) {
+      return null;
+    } else {
+      const currentHead = this.head;
+      this.head = currentHead.next;
 
+      if (this.head) {
+        this.head.insertBefore(null);
+      }
+
+      currentHead.delete();
+      return currentHead.value;
+    }
   }
 
   /* Adds the given value as the new tail
   node of the list */
   addToTail(value) {
-
+    const newTail = new ListNode(value, this.tail, null);
+    if (!this.head) {
+      this.head = newTail;
+      this.tail = newTail;
+    } else {
+      this.tail.insertAfter(newTail);
+      this.tail = newTail;
+    }
   }
 
   /* Remove the list's current tail. The list's
   `tail` pointer should point to the removed node's
   `prev` node */
   removeFromTail() {
-
+    if (!this.tail) {
+      return null;
+    }
+    if (!this.tail.prev) {
+      const tehTail = this.tail.value;
+      this.tail = null;
+      this.head = null;
+      return tehTail;
+    } else {
+      const currentTail = this.tail;
+      this.tail = currentTail.prev;
+      if (this.tail) {
+        this.tail.insertAfter(null);
+      }
+      currentTail.delete();
+      return currentTail.value;
+    }
   }
 
   /* Move the given node to the front of the
   list. Update the list's `head` pointer
   accordingly */
   moveToFront(node) {
-
+    if (node === this.tail) this.tail = node.prev;
+    if (node.prev) node.prev.insertAfter(node.next);
+    if (node.next) node.next.insertBefore(node.prev);
+    this.addToHead(node.value);
   }
 
   /* Move the given node to the back of the
   list. Update the list's `tail` pointer 
   accordingly */
   moveToBack(node) {
-
+    if (node === this.head) this.head = node.next;
+    if (node.prev) node.prev.insertAfter(node.next);
+    if (node.next) node.next.insertBefore(node.prev);
+    this.addToTail(node.value);
   }
 
   /* Delete the given node from the list */
   delete(node) {
-
+    if (node === this.tail) this.tail = node.prev;
+    if (node === this.head) this.head = node.next;
+    if (node.prev) node.prev.insertAfter(node.next);
+    if (node.next) node.next.insertBefore(node.prev);
   }
 }
 

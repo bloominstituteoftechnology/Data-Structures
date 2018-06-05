6e042efa6195c558415cb233c7b0ba543af3b00f
Yasin Shuman <email@yasin.io>
2018-06-04 18:13:59 -0700

    completed contains function on binary search tree

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -23,7 +23,17 @@ class BinarySearchTree {
   target value has been found in the true
   or the entire tree has been searched.
   Returns true or false accordingly */
-  contains(target) {}
+  contains(target) {
+    if (target > this.value) {
+      if (this.right.value === target) return true;
+      if (this.right.right === null && this.right.left === null) return false;
+      else return this.right.contains(target);
+    } else {
+      if (this.left.value === target) return true;
+      if (this.left.right === null && this.left.left === null) return false;
+      else return this.left.contains(target);
+    }
+  }
 
   /* Returns the maximum value in the tree 
   Should not remove the max value from the tree */

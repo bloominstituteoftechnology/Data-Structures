bc3e18d91ef7bf36c67f39317a9aaa2844a6b99e
Chris <cjmarsh725@gmail.com>
2018-06-05 10:08:54 -0700

    Partially completed heap

--- src/heap.js
+++ src/heap.js
@@ -8,25 +8,33 @@ class Heap {
   The heap should maintain the heap property 
   after insertion */
   insert(value) {
-
+    if (value > this.getMax()) this.storage.unshift(value);
+    else {
+      this.storage.push(value);
+      let index = this.getSize();
+      this.bubbleUp(index);
+    }
   }
 
   /* Remove the maximal value from the heap and
   return it. The heap should maintain the heap
   property after removing the maximal value */
   delete() {
-
+    const max = this.getMax();
+    this.storage.shift();
+    this.siftDown(0);
+    return max;
   }
 
   /* Return the maximal value in the heap
   without removing it */
   getMax() {
-
+    return this.storage[0];
   }
 
   /* Return the size of the heap */
   getSize() {
-
+    return this.storage.length;
   }
 
   /* Moves the element at the specified index "up"
@@ -34,7 +42,14 @@ class Heap {
   parent value is less than the value located at
   the input index */
   bubbleUp(index) {
-
+    const parentIndex = Math.floor((index - 2) / 2);
+    const parent = this.storage[parentIndex];
+    const child = this.storage[index - 1];
+    if (child > parent) {
+      this.storage[parentIndex] = child;
+      this.storage[index - 1] = parent;
+      return this.bubbleUp(parentIndex);
+    }
   }
 
   /* Move the element at the specified index "down"
@@ -42,7 +57,19 @@ class Heap {
   child's value is greater than the value located at
   the input index */
   siftDown(index) {
-    
+    const parent = this.storage[index];
+    const leftChild = this.storage[index * 2 + 1];
+    const rightChild = this.storage[index * 2 + 2];
+    const largestChild = leftChild >= rightChild ? leftChild : rightChild;
+    if (largestChild > parent) {
+      if (leftChild === largestChild) {
+        this.storage[index * 2 + 1] = parent;
+        this.storage[index] = leftChild;
+      } else {
+        this.storage[index * 2 + 2] = parent;
+        this.storage[index] = rightChild;
+      }
+    }
   }
 }
 

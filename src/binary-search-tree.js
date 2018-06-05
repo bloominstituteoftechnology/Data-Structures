1bca613ba4cd25ab669e7d667fa0d4ce7c746c3d
Henry K <mykim1891@hotmail.com>
2018-06-05 13:20:28 -0700

    fixed some tests for binary search tree

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -10,22 +10,27 @@ class BinarySearchTree {
   Make sure the rules of a binary search
   tree are being adhered to */
   insert(value) {
-
+    let node = {
+      value: value,
+      left: null,
+      right: null
+    };
+    if (value < this.value) {
+      this.left = node;
+    } else if (value > this.value) {
+      this.right = node;
+    }
   }
 
   /* Traverses the tree until either the
   target value has been found in the true
   or the entire tree has been searched.
   Returns true or false accordingly */
-  contains(target) {
-
-  }
+  contains(target) {}
 
   /* Returns the maximum value in the tree 
   Should not remove the max value from the tree */
-  getMax() {
-
-  }
+  getMax() {}
 }
 
 module.exports = BinarySearchTree;

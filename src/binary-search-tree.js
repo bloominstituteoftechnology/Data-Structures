63fd76621d50307e7185edde129168a44666b9ed
Velsu <riftmage50@gmail.com>
2018-06-05 22:16:06 +0200

    Heap complete

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -10,7 +10,19 @@ class BinarySearchTree {
   Make sure the rules of a binary search
   tree are being adhered to */
   insert(value) {
-
+    if (value > this.value) {
+      if (!this.right) {
+        this.right = new BinarySearchTree(value);
+      } else {
+        this.right.insert(value);
+      }
+    } else {
+      if (!this.left) {
+        this.left = new BinarySearchTree(value);
+      } else {
+        this.left.insert(value);
+      }
+    }
   }
 
   /* Traverses the tree until either the
@@ -18,13 +30,25 @@ class BinarySearchTree {
   or the entire tree has been searched.
   Returns true or false accordingly */
   contains(target) {
-
+    if (this.value === target) return true;
+    if (this.value > target) {
+      if (this.left) {
+        return this.left.contains(target);
+      }
+    } else {
+      if (this.right) {
+        return this.right.contains(target);
+      }
+    }
+    return false;
   }
 
   /* Returns the maximum value in the tree 
   Should not remove the max value from the tree */
   getMax() {
-
+    if (!this.right && !this.left) return this.value;
+    else if (this.right) return this.right.getMax();
+    else if (this.left.value > this.value) return this.left.getMax();
   }
 }
 
--- src/heap.js
+++ src/heap.js
@@ -8,25 +8,33 @@ class Heap {
   The heap should maintain the heap property 
   after insertion */
   insert(value) {
-
+    if (value > this.getMax()) {
+      this.storage.unshift(value);
+    } else {
+      this.storage.push(value);
+      this.bubbleUp(this.storage.length);
+    }
   }
 
   /* Remove the maximal value from the heap and
   return it. The heap should maintain the heap
   property after removing the maximal value */
   delete() {
-
+    const max = this.getMax();
+    this.storage.shift();
+    this.siftDown(0);
+    return max;
   }
 
   /* Return the maximal value in the heap
   without removing it */
   getMax() {
-
+    return this.storage[0];
   }
 
   /* Return the size of the heap */
   getSize() {
-
+    return this.storage.length;
   }
 
   /* Moves the element at the specified index "up"
@@ -34,7 +42,14 @@ class Heap {
   parent value is less than the value located at
   the input index */
   bubbleUp(index) {
-
+    let parentIndex = Math.floor((index - 1) / 2);
+    if (this.storage[index] > this.storage[parentIndex]) {
+      [this.storage[parentIndex], this.storage[index]] = [
+        this.storage[index],
+        this.storage[parentIndex]
+      ];
+      this.bubbleUp(parentIndex);
+    }
   }
 
   /* Move the element at the specified index "down"
@@ -42,7 +57,27 @@ class Heap {
   child's value is greater than the value located at
   the input index */
   siftDown(index) {
-    
+    const leftChildIndex = index * 2 + 1;
+    const rightChildIndex = index * 2 + 2;
+    let maxChildIndex;
+
+    if (this.storage[leftChildIndex] && this.storage[rightChildIndex]) {
+      maxChildIndex =
+        this.storage[leftChildIndex] > this.storage[rightChildIndex]
+          ? leftChildIndex
+          : rightChildIndex;
+    } else if (this.storage[leftChildIndex]) {
+      maxChildIndex = leftChildIndex;
+    } else if (this.storage[rightChildIndex]) {
+      maxChildIndex = rightChildIndex;
+    }
+    if (this.storage[index] < this.storage[maxChildIndex]) {
+      [this.storage[maxChildIndex], this.storage[index]] = [
+        this.storage[index],
+        this.storage[maxChildIndex]
+      ];
+      this.siftDown(maxChildIndex);
+    }
   }
 }
 

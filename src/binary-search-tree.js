2a03e317df22075bf262192e2a5b9161ff3499e2
Yasin Shuman <email@yasin.io>
2018-06-04 18:09:07 -0700

    passing insert function on Binary-search-tree

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -10,22 +10,24 @@ class BinarySearchTree {
   Make sure the rules of a binary search
   tree are being adhered to */
   insert(value) {
-
+    if (value > this.value) {
+      if (!this.right) this.right = new BinarySearchTree(value);
+      else this.right.insert(value);
+    } else {
+      if (!this.left) this.left = new BinarySearchTree(value);
+      else this.left.insert(value);
+    }
   }
 
   /* Traverses the tree until either the
   target value has been found in the true
   or the entire tree has been searched.
   Returns true or false accordingly */
-  contains(target) {
-
-  }
+  contains(target) {}
 
   /* Returns the maximum value in the tree 
   Should not remove the max value from the tree */
-  getMax() {
-
-  }
+  getMax() {}
 }
 
 module.exports = BinarySearchTree;

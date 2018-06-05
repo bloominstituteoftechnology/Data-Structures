6fd6b4bcc42971df8f36c446456f9ec018141689
kyle <kauzlarich.kyle@gmail.com>
2018-06-04 18:28:55 -0700

    i dont know

--- src/linked-list.js
+++ src/linked-list.js
@@ -9,6 +9,15 @@ class LinkedList {
   of the list. The `tail` pointer
   should be updated accordingly */
   addToTail(value) {
+    let node = {
+      value: value,
+      next: null
+    }
+    if (this.head === null) {
+      this.head = node;
+      this.head.next = this.tail;
+    }
+    this.tail = node;
 
   }
 
@@ -16,18 +25,52 @@ class LinkedList {
   The `head` pointer should be updated
   accordingly */
   removeHead() {
+    let removed = this.head.value;
+    this.head = this.head.next;
+    return removed;
 
   }
 
   /* Searches the list for the given value
   Returns true or false accordingly */
   contains(value) {
+    // if (this.head.value === value) {
+    //   return true;
+    // } else if (this.tail.value === value) {
+    //   return true;
+    // } else {
+    //   return false;
+    // }
+    let current = this.head;
+    while (current) {
+      if (current.value === value) {
+        return true;
+      } else {
+        current = current.next;
+      }
+    }
+    return false;
+    
+    // if (this.head.value === value) {
+    //   return true;
+    // } if (this.head.next === null) {
+    //   return false;
+    // } if (this.head.next !== null) {
+    //   this.head = this.head.next;
+    //   return this.contains(value);
+    // } 
 
   }
 
   /* Finds and returns the maximal value
   of all the values in the list */
   getMax() {
+    let max = this.tail;
+    while (this.head.next != null) {
+      this.head = this.head.next;
+      if (this.tail.value > max) max = this.tail.value;
+    }
+    return max;
     
   }
 }

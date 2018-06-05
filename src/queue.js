1f4116adb7925c07c887b3118711e1f92a42fd72
cgoez <goez2001@gmail.com>
2018-06-05 13:31:30 -0700

    queue enqueue, dequeue

--- src/queue.js
+++ src/queue.js
@@ -14,13 +14,16 @@ class Queue {
 
   /* Adds the given item to the queue */
   enqueue(item) {
-    
+    this.storage.addToTail(item)
+    this.size++
   }
 
   /* Removes and returns the least recently
   added item from the queue */
   dequeue() {
-
+    if (this.isEmpty()) return null;
+    this.size--;
+    return this.storage.removeHead()
   }
 
   /* Returns true if the queue contains no

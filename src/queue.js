59a8d98843f2f84006df7ef94b2f91e50ddf685b
Axhon <axhonruiz@gmail.com>
2018-06-05 06:21:39 -1000

    queue implemented

--- src/queue.js
+++ src/queue.js
@@ -3,36 +3,38 @@
   your Linked List implementation to construct
   the Queue.
 */
-const List = require('./linked-list');
+const List = require('./linked-list')
 
 class Queue {
   /* Do not modify the constructor */
   constructor() {
-    this.size = 0;
-    this.storage = new List();
+    this.size = 0
+    this.storage = new List()
   }
 
   /* Adds the given item to the queue */
   enqueue(item) {
-    
+    this.storage.addToTail(item)
+    this.size++
   }
 
   /* Removes and returns the least recently
   added item from the queue */
   dequeue() {
-
+    if (this.storage.head === null) return null
+    return this.storage.removeHead()
   }
 
   /* Returns true if the queue contains no
   elements, false otherwise */
   isEmpty() {
-
+    return Boolean(this.size)
   }
 
   /* A getter method for the length of the queue */
   get length() {
-
+    return this.size
   }
 }
 
-module.exports = Queue;
+module.exports = Queue

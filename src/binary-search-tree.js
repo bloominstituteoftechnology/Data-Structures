f9210bc7d4a7ebbcf88b7e90894f9f689312ee7c
Justin Hong <jhong2009@gmail.com>
2018-06-05 15:19:23 -0700

    in binary-search-tree.js: set up getMax() helper method

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -53,11 +53,46 @@ class BinarySearchTree {
   target value has been found in the true
   or the entire tree has been searched.
   Returns true or false accordingly */
-  contains(target) {}
+  contains(target) {
+    let isFound = false;
+    let current = this;
+
+    // traverse unless target is found or there is no node to search
+    while (!isFound && current) {
+      // target is less than current value - go left
+      if (target < current.value) {
+        current = current.left;
+      }
+      // target is greater than current value - go right
+      else if (target > current.value) {
+        current = current.right;
+      }
+      // target is equal to the current value - target found
+      else {
+        isFound = true; // change search status
+      }
+    }
+    return isFound; // return the search status
+  }
 
   /* Returns the maximum value in the tree 
   Should not remove the max value from the tree */
-  getMax() {}
+  getMax() {
+    let current = this;
+    let max;
+    // traverse the tree and start at the root
+    while (current) {
+      // ignore left half of the tree
+      // set max equal to the root if no other right-side values exist
+      if (current.right === null) {
+        max = current.value;
+      }
+      // jump to the next right-side node
+        // current will be null if no other right-side values exist
+      current = current.right;
+    }
+    return max;
+  }
 }
 
 module.exports = BinarySearchTree;

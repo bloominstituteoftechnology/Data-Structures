65c21223eff9cc1a6dd822c0fefcdb0b663c1c73
Finlie <dfinley2917@gmail.com>
2018-06-05 17:21:16 -0400

    working on the heap

--- src/heap.js
+++ src/heap.js
@@ -1,49 +1,55 @@
 class Heap {
-  /* Do not modify the constructor */
-  constructor() {
-    this.storage = [];
-  }
+	/* Do not modify the constructor */
+	constructor() {
+		this.storage = [];
+	}
 
-  /* Insert the given value into the heap.
+	/* Insert the given value into the heap.
   The heap should maintain the heap property 
   after insertion */
-  insert(value) {
+	insert(value) {
+		this.storage.push(value);
+		this.bubbleUp(this.storage.length - 1);
+	}
 
-  }
-
-  /* Remove the maximal value from the heap and
+	/* Remove the maximal value from the heap and
   return it. The heap should maintain the heap
   property after removing the maximal value */
-  delete() {
+	delete() {
+		this.storage.pop();
+		if (this.storage.length > 0) {
+			let end = this.storage[0];
+			this.siftDown(0);
+		}
+		return end;
+	}
 
-  }
-
-  /* Return the maximal value in the heap
+	/* Return the maximal value in the heap
   without removing it */
-  getMax() {
+	getMax() {
+		let max = 0;
+		for (let i = 0; i < this.storage.length; i++) {
+			if (this.storage[i] >= this.storage[max]) max = i;
+		}
+		return this.storage[max];
+	}
 
-  }
+	/* Return the size of the heap */
+	getSize() {
+		return this.storage.length;
+	}
 
-  /* Return the size of the heap */
-  getSize() {
-
-  }
-
-  /* Moves the element at the specified index "up"
+	/* Moves the element at the specified index "up"
   the heap by swapping it with its parent if its
   parent value is less than the value located at
   the input index */
-  bubbleUp(index) {
+	bubbleUp(index) {}
 
-  }
-
-  /* Move the element at the specified index "down"
+	/* Move the element at the specified index "down"
   the heap by swapping it with its larger child if its
   child's value is greater than the value located at
   the input index */
-  siftDown(index) {
-    
-  }
+	siftDown(index) {}
 }
 
 module.exports = Heap;

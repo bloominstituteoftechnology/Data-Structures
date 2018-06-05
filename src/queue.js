669b06e329ca649054fe8109efbf5cb78693c754
cgoez <goez2001@gmail.com>
2018-06-05 13:32:25 -0700

    queue isEmpty

--- src/queue.js
+++ src/queue.js
@@ -29,7 +29,7 @@ class Queue {
   /* Returns true if the queue contains no
   elements, false otherwise */
   isEmpty() {
-
+    return this.size === 0;
   }
 
   /* A getter method for the length of the queue */

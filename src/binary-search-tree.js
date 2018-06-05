12200ebf91530be50194a0b8aca4836cf0ab66d1
Justin Hong <jhong2009@gmail.com>
2018-06-05 15:03:43 -0700

    in binary-search-tree.js: set up insert() helper method

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,3 +1,11 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
 class BinarySearchTree {
   /* Do not modify the constructor */
   constructor(value) {
@@ -10,22 +18,46 @@ class BinarySearchTree {
   Make sure the rules of a binary search
   tree are being adhered to */
   insert(value) {
+    const newNode = new Node(value);
+    let current = this;
 
+    while (true) {
+      // new value is less than node's value - move left
+      if (value < current.value) {
+        // no left - new node goes left
+        if (current.left === null) {
+          current.left = newNode;
+          break;
+        } else {
+          current = current.left;
+        }
+      }
+      // new value is greater than node's value - move right
+      else if (value > current.value) {
+        // no right - move right
+        if (current.right === null) {
+          current.right = newNode;
+          break;
+        } else {
+          current = current.right;
+        }
+      }
+      // ignore new value if it is equal to the current one
+      else {
+        break;
+      }
+    }
   }
 
   /* Traverses the tree until either the
   target value has been found in the true
   or the entire tree has been searched.
   Returns true or false accordingly */
-  contains(target) {
-
-  }
+  contains(target) {}
 
   /* Returns the maximum value in the tree 
   Should not remove the max value from the tree */
-  getMax() {
-
-  }
+  getMax() {}
 }
 
 module.exports = BinarySearchTree;

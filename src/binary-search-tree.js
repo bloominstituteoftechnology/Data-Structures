bc973dd9f68fa86128517bb1819ef26b6d746b12
Axhon <axhonruiz@gmail.com>
2018-06-05 20:36:09 -1000

    binary working

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,16 +1,36 @@
 class BinarySearchTree {
   /* Do not modify the constructor */
   constructor(value) {
-    this.value = value;
-    this.left = null;
-    this.right = null;
+    this.value = value
+    this.left = null
+    this.right = null
   }
 
   /* Inserts the given value
   Make sure the rules of a binary search
   tree are being adhered to */
   insert(value) {
-
+    let cur = this
+    const node = {
+      value,
+      left: null,
+      right: null
+    }
+    while (cur) {
+      if (value < cur.value) {
+        if (!cur.left) {
+          cur.left = node
+          return
+        }
+        cur = cur.left
+      } else if (value > cur.value) {
+        if (!cur.right) {
+          cur.right = node
+          return
+        }
+        cur = cur.right
+      } else return
+    }
   }
 
   /* Traverses the tree until either the
@@ -18,14 +38,36 @@ class BinarySearchTree {
   or the entire tree has been searched.
   Returns true or false accordingly */
   contains(target) {
-
+    if (this.value === target) return true
+    let cur = this
+    while (cur) {
+      if (target < cur.value) {
+        if (!cur.left) {
+          return false
+        }
+        cur = cur.left
+      } else if (target > cur.value) {
+        if (!cur.right) {
+          return false
+        }
+        cur = cur.right
+      } else return true
+    }
+    return false
   }
 
   /* Returns the maximum value in the tree 
   Should not remove the max value from the tree */
   getMax() {
+    if (!this) return null
 
+    let max = this.value,
+      cur = this
+    while (cur) {
+      !cur.right ? (max = cur.value) : (cur = cur.right)
+    }
+    return max
   }
 }
 
-module.exports = BinarySearchTree;
+module.exports = BinarySearchTree

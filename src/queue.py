e4194cdf29dc520d16b38abe6744f80cfd673899
Sean Chen <skypemaster007@gmail.com>
2018-06-27 16:05:40 -0500

    Add skeleton py files for each data structure

---
+++ src/queue.py
@@ -0,0 +1,16 @@
+class Queue:
+  def __init__(self):
+    self.size = 0
+    self.storage = []
+
+  def enqueue(self, item):
+    pass
+  
+  def dequeue(self):
+    pass
+
+  def isEmpty(self):
+    pass
+
+  def len(self):
+    pass

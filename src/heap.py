20e6c975583f9813aa329f74d04dc566da5b3170
Sean Chen <skypemaster007@gmail.com>
2018-06-27 16:05:40 -0500

    Add skeleton py files for each data structure

---
+++ src/heap.py
@@ -0,0 +1,21 @@
+class Heap:
+  def __init__(self):
+    self.storage = []
+
+  def insert(self, value):
+    pass
+
+  def delete(self):
+    pass
+
+  def getMax(self):
+    pass
+
+  def getSize(self):
+    pass
+
+  def bubbleUp(self, index):
+    pass
+
+  def siftDown(self, index):
+    pass
---
+++ src/linked-list.py
@@ -0,0 +1,16 @@
+class LinkedList:
+  def __init__(self):
+    self.head = None
+    self.tail = None
+
+  def addToTail(self, value):
+    pass
+
+  def removeHead(self):
+    pass
+
+  def contains(self):
+    pass
+
+  def getMax(self):
+    pass

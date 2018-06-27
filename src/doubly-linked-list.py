aeb9b047e1bd65f7ae18720d722d7e0ade62d514
Sean Chen <skypemaster007@gmail.com>
2018-06-27 16:09:57 -0500

    Add dll py file as well

---
+++ src/doubly-linked-list.py
@@ -0,0 +1,40 @@
+class ListNode:
+  def __init__(self, value, prev=None, next=None):
+    self.value = value
+    self.prev = prev
+    self.next = next
+
+  def insertAfter(self, value):
+    pass
+
+  def insertBefore(self, value):
+    pass
+
+  def delete(self):
+    pass
+
+class DoublyLinkedList:
+  def __init__(self):
+    self.head = None
+    self.tail = None
+
+  def addToHead(self, value):
+    pass
+
+  def removeFromHead(self):
+    pass
+
+  def addToTail(self, value):
+    pass
+
+  def removeFromTail(self):
+    pass
+
+  def moveToFront(self, node):
+    pass
+
+  def moveToBack(self, node):
+    pass
+
+  def delete(self, node):
+    pass

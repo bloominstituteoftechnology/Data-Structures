2301c64c1fa58d781e53f48e668d83df286f9837
Zach Campbell <beazlebubbles@gmail.com>
2018-07-16 15:14:05 -0600

    Completed linked_list.py

--- linked_list/linked_list.py
+++ linked_list/linked_list.py
@@ -10,15 +10,6 @@ class Node:
         self.value = value
         self.next_node = next_node
 
-    def get_value(self):
-        return self.value
-
-    def get_next(self):
-        return self.next_node
-
-    def set_next(self, new_next):
-        self.next_node = new_next
-
 
 class LinkedList:
     def __init__(self):
@@ -26,13 +17,37 @@ class LinkedList:
         self.tail = None
 
     def add_to_tail(self, value):
-        pass
+        if self.tail is not None:
+            self.tail.next_node = Node(value)
+            self.tail = self.tail.next_node
+        else:
+            self.tail = Node(value)
+            self.head = self.tail
 
     def remove_head(self):
-        pass
+        if self.head is not None:
+            return_value = self.head.value
+            self.head = self.head.next_node
+            return return_value
+        else:
+            return None
 
-    def contains(self):
-        pass
+    def contains(self, value):
+        current_node = self.head
+        while current_node is not None:
+            if current_node.value == value:
+                return True
+            else:
+                current_node = current_node.next_node
+        return False
 
     def get_max(self):
-        pass
+        if self.head is None:
+            return None
+        current_node = self.head
+        biggest_value = float("-inf")
+        while current_node is not None:
+            if current_node.value > biggest_value:
+                biggest_value = current_node.value
+            current_node = current_node.next_node
+        return biggest_value

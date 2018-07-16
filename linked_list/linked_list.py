f212aabc3cf34f28af705495b43690bc4323bdb8
Edward Manda <lilkups@yahoo.com>
2018-07-16 15:33:42 -0500

    add to tail implemented

--- linked_list/linked_list.py
+++ linked_list/linked_list.py
@@ -3,33 +3,47 @@ Class that represents a single linked
 list node that holds a single value
 and a reference to the next node in the list
 """
+
+
 class Node:
-  def __init__(self, value=None, next_node=None):
-    self.value = value
-    self.next_node = next_node
+    def __init__(self, value=None, next_node=None):
+        self.value = value
+        self.next_node = next_node
 
-  def get_value(self):
-    return self.value
+    def get_value(self):
+        return self.value
 
-  def get_next(self):
-    return self.next_node
+    def get_next(self):
+        return self.next_node
 
-  def set_next(self, new_next):
-    self.next_node = new_next
+    def set_next(self, new_next):
+        self.next_node = new_next
+
 
 class LinkedList:
-  def __init__(self):
-    self.head = None
-    self.tail = None
+    def __init__(self):
+        self.head = None
+        self.tail = None
 
-  def add_to_tail(self, value):
-    pass
+    def add_to_tail(self, value):
+        newNode = Node(value, self.tail)
+        # check if there is a head
+        if(self.head == None):
+            # no head, so also no tail, make them both equal to new node
+            self.head = newNode
+            self.tail = newNode
+            return
+        # otherwise,
+        # update the next pointer on the current tail,
+        self.tail.next = newNode
+        # make the new node the tail
+        self.tail = newNode
 
-  def remove_head(self):
-    pass
+    def remove_head(self):
+        pass
 
-  def contains(self):
-    pass
+    def contains(self):
+        pass
 
-  def get_max(self):
-    pass
+    def get_max(self):
+        pass

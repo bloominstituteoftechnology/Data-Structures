30764c453b143f50c920c6fb6b84bed87859423a
jesuarva <jesuarva.developer@gmail.com>
2018-07-16 22:32:08 +0200

    IMPLEMENT LinkedList -> add_to_tail

--- linked_list/linked_list.py
+++ linked_list/linked_list.py
@@ -3,33 +3,51 @@ Class that represents a single linked
 list node that holds a single value
 and a reference to the next node in the list
 """
+
+
 class Node:
-  def __init__(self, value=None, next_node=None):
-    self.value = value
-    self.next_node = next_node
+    def __init__(self, value=None, next_node=None):
+        self.value = value
+        self.next_node = next_node
 
-  def get_value(self):
-    return self.value
+    def get_value(self):
+        return self.value
 
-  def get_next(self):
-    return self.next_node
+    def get_next(self):
+        return self.next_node
 
-  def set_next(self, new_next):
-    self.next_node = new_next
+    def set_next(self, new_next):
+        self.next_node = new_next
+
 
 class LinkedList:
-  def __init__(self):
-    self.head = None
-    self.tail = None
+    def __init__(self):
+        self.head = None
+        self.tail = None
 
-  def add_to_tail(self, value):
-    pass
+    def add_to_tail(self, value):
+        newNode = Node(value)
+        if self.head == None and self.tail == None:
+            self.head = newNode
+            self.tail = newNode
+        else:
+            self.tail.set_next(newNode)
+            self.tail = newNode
 
-  def remove_head(self):
-    pass
+    def remove_head(self):
+        removed = self.head
+        self.head = self.head.get_next()
+        return removed
 
-  def contains(self):
-    pass
+    def contains(self, value):
+        nextValue = self.head
+        while (next != None):
+            if next.get_value == value:
+                return True
+            else:
+                nextValue = nextValue.get_next
 
-  def get_max(self):
-    pass
+        return False
+
+    def get_max(self):
+        pass

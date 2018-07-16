e14c6c29183a51ccf0707186f09109d0957c4bad
kelly <kelfro@fastmail.com>
2018-07-16 15:44:12 -0700

    I'm redoing my queue stuff from scratch because I think I'm going about it the wrong way.

--- queue/queue.py
+++ queue/queue.py
@@ -8,15 +8,7 @@ class Queue:
     self.storage = LinkedList()
 
   def enqueue(self, item):
-    if item not in self.queue:
-      self.queue.insert(0, item)
-      return True
-    return False
 
   def dequeue(self):
-    if len(self.queue)>0:
-      return self.queue.pop()
-    return False
 
   def len(self):
-    return len(self.queue)

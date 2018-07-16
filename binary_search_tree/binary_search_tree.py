ca5bbbe4610a3175bbef94d1f27c3f75cce3f8d2
Jason fleischer <jrjflei@gmail.com>
2018-07-16 16:38:11 -0600

    completed linked_list, binary_search_tree, and queue

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -5,10 +5,42 @@ class BinarySearchTree:
     self.right = None
 
   def insert(self, value):
-    pass
+    if self.value:
 
+      if value < self.value:
+        if self.left is None:
+            self.left = BinarySearchTree(value)
+        else:
+            self.left.insert(value)
+
+      elif value > self.value:
+        if self.right is None:
+            self.right = BinarySearchTree(value)
+        else:
+            self.right.insert(value)
+
+    else:
+        self.value = value
+    
   def contains(self, target):
-    pass
+    if target == self.value:
+      return True
+    elif target < self.value:
+      if self.left == None:
+        return None
+      else:
+        return self.left.contains(target)
+    else:
+      if self.right == None:
+        return None
+      else:
+        return self.right.contains(target)
 
   def get_max(self):
-    pass
+    max_num = self.value
+    if self.right == None:
+      return max_num
+    elif self.right.value < max_num:
+      return max_num
+    else:
+      return self.right.get_max()
--- linked_list/linked_list.py
+++ linked_list/linked_list.py
@@ -23,13 +23,45 @@ class LinkedList:
     self.tail = None
 
   def add_to_tail(self, value):
-    pass
+    node = Node(value)
+    if self.head == None:
+      self.head = node
+    elif self.head == self.tail and self.head != None:
+      self.head.set_next(node)  
+    else:
+      self.tail.set_next(node)
+    self.tail = node
 
   def remove_head(self):
-    pass
+    if self.head != None:
+        head_value = self.head.get_value()
+        self.head = self.head.get_next()
+        return head_value
+    if self.head == None:
+        return None
 
-  def contains(self):
-    pass
+  def contains(self, value):
+    if self.head != None:
+      i = self.head
+      while i.get_next() != None:
+        if i.get_value() == value:
+          return True
+        else:
+          i = i.get_next()
+      if self.tail.get_value() == value:
+        return True
+      return False
+    if self.head == None:
+      return False
 
   def get_max(self):
-    pass
+    if self.head != None:
+      i = self.head
+      max_num = i.get_value()
+      while i.get_next() != None:
+        i = i.get_next()
+        if i.get_value() > max_num:
+          max_num = i.get_value()
+      return max_num
+    if self.head == None:
+      return None

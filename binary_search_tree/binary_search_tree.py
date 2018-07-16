cced6732d93c17560f1f0ed38664948689fe70e4
David <AquilaVirtual>
2018-07-16 14:20:12 -0700

    implemented insert method for BinarySearchTree

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -5,7 +5,20 @@ class BinarySearchTree:
     self.right = None
 
   def insert(self, value):
-    pass
+    if self == None:
+      self = value
+      return
+    else:
+      if self.value < value:
+        if self.right == None:
+          self.right = value
+      else:
+        insert(self.right, value)
+    
+      if self.left == None:
+        self.left = value
+      else:
+        insert(self.left, value)
 
   def contains(self, target):
     pass

aa5d9caf2295947aacff00e8340efb9f722e5641
AmyShackles <amyshackles@gmail.com>
2018-07-16 12:46:34 -0700

    Made changes to appease flake8

--- doubly_linked_list/doubly_linked_list.py
+++ doubly_linked_list/doubly_linked_list.py
@@ -1,40 +1,41 @@
 class ListNode:
-  def __init__(self, value, prev=None, next=None):
-    self.value = value
-    self.prev = prev
-    self.next = next
+    def __init__(self, value, prev=None, next=None):
+        self.value = value
+        self.prev = prev
+        self.next = next
 
-  def insert_after(self, value):
-    pass
+    def insert_after(self, value):
+        pass
 
-  def insert_before(self, value):
-    pass
+    def insert_before(self, value):
+        pass
 
-  def delete(self):
-    pass
+    def delete(self):
+        pass
+
 
 class DoublyLinkedList:
-  def __init__(self):
-    self.head = None
-    self.tail = None
+    def __init__(self):
+        self.head = None
+        self.tail = None
 
-  def add_to_head(self, value):
-    pass
+    def add_to_head(self, value):
+        pass
 
-  def remove_from_head(self):
-    pass
+    def remove_from_head(self):
+        pass
 
-  def add_to_tail(self, value):
-    pass
+    def add_to_tail(self, value):
+        pass
 
-  def remove_from_tail(self):
-    pass
+    def remove_from_tail(self):
+        pass
 
-  def move_to_front(self, node):
-    pass
+    def move_to_front(self, node):
+        pass
 
-  def move_to_end(self, node):
-    pass
+    def move_to_end(self, node):
+        pass
 
-  def delete(self, node):
-    pass
+    def delete(self, node):
+        pass

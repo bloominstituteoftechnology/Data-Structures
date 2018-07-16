304962d7a43ce0c86551fd0bfe7e11897ba0395d
Edward Manda <lilkups@yahoo.com>
2018-07-16 17:48:05 -0500

    working on linked list

--- linked_list/linked_list.py
+++ linked_list/linked_list.py
@@ -48,17 +48,38 @@ class LinkedList:
 
         # check if the nex node is None
         if(head.next == None):
-            this.head = None
-            this.tail = None
+            self.head = None
+            self.tail = None
         else:
             # assign the next node as the new head
-            this.head = head.next
+            self.head = head.next
 
         # return the value if you want
         return head.value
 
-    def contains(self):
-        pass
+    def contains(self, target):
+        current = self.head
+
+        while current is not None:
+            if current.value == target:
+                return True
+            current = current.next_node
+        return False
 
     def get_max(self):
-        pass
+        current = self.head
+
+        # check if there is a value
+        if(current is None):
+            return None
+
+        # assign the first value as max
+        max = current.value
+
+        while(current is not None):
+            print(current.value)
+            if(current.value > max):
+                max = current.value
+            current = current.next_node
+
+        return max

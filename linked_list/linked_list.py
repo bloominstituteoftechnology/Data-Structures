1b3f114f6ed416e9f36ba63f25a10581cb736fe7
maxwashington <mnwashington18@gmail.com>
2018-07-16 16:18:50 -0500

    Linked-List Done

--- linked_list/linked_list.py
+++ linked_list/linked_list.py
@@ -4,7 +4,7 @@ list node that holds a single value
 and a reference to the next node in the list
 """
 class Node:
-  def __init__(self, value=None, next_node=None):
+      def __init__(self, value=None, next_node=None):
     self.value = value
     self.next_node = next_node
 
@@ -23,13 +23,38 @@ class LinkedList:
     self.tail = None
 
   def add_to_tail(self, value):
-    pass
+    new_node = Node(value)
+    if self.tail == None:
+      if self.head == None:
+        self.head = new_node
+      else:
+        self.head.next_node = new_node
+    else:
+      self.tail.next_node = new_node
+    self.tail = new_node
 
   def remove_head(self):
-    pass
+    if self.head != None:
+      original_head = self.head
+      self.head = self.head.next_node
+      return original_head.value
+    return None
 
-  def contains(self):
-    pass
+  def contains(self, value):
+    current_head = self.head
+    while current_head != None:
+      if current_head.value == value:
+        return True
+      current_head = current_head.next_node
+    return False
 
   def get_max(self):
-    pass
+    if self.head == None:
+      return None
+    max_node = self.head.value
+    current_node = self.head
+    while current_node != None:
+      if current_node.value > max_node:
+        max_node = current_node.value
+      current_node = current_node.next_node
+    return max_node

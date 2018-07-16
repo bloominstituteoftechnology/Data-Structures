08c69c324e5ca247f00fdcf3cc0367b568e3273c
bbenefield89 <bsquared18@gmail.com>
2018-07-16 17:17:43 -0400

    feat(linked_list.py): `add_to_tail` method adds correct node to lists tail feat(linked_list.py): `remove_head` method removes head from list and returns the removed head

--- linked_list/linked_list.py
+++ linked_list/linked_list.py
@@ -3,33 +3,57 @@ Class that represents a single linked
 list node that holds a single value
 and a reference to the next node in the list
 """
+
+
 class Node:
-  def __init__(self, value=None, next_node=None):
-    self.value = value
-    self.next_node = next_node
+    def __init__(self, value=None, next_node=None):
+        self.value = value
+        self.next_node = next_node
 
-  def get_value(self):
-    return self.value
+    def get_value(self):
+        return self.value
 
-  def get_next(self):
-    return self.next_node
+    def get_next(self):
+        return self.next_node
 
-  def set_next(self, new_next):
-    self.next_node = new_next
+    def set_next(self, new_next):
+        self.next_node = new_next
+
 
 class LinkedList:
-  def __init__(self):
-    self.head = None
-    self.tail = None
+    def __init__(self):
+        self.head = None
+        self.tail = None
 
-  def add_to_tail(self, value):
-    pass
+    def add_to_tail(self, value):
+        self.tail = Node(value)
 
-  def remove_head(self):
-    pass
+    def remove_head(self):
+        head = self.head
+        self.head = None
 
-  def contains(self):
-    pass
+        return head
 
-  def get_max(self):
-    pass
+    def contains(self, value):
+        pass
+
+    def get_max(self):
+        pass
+
+
+nodeA = Node('a') # current head
+nodeB = Node('b')
+nodeC = Node('c') # current tail
+
+nodeA.next = nodeB
+nodeB.next = nodeC
+
+list = LinkedList()
+
+list.head = nodeA
+list.add_to_tail(nodeC)
+
+print(list.head) # prints 'a'
+print(list.tail.value) # prints '<__main__.Node object at 0x7f84ce7cba20>'
+
+print(list.remove_head().value)

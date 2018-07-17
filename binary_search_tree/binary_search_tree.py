5794106acc4f3a3706be59bc94cb9763787ef952
Danny Vail <danny_vail@outlook.com>
2018-07-17 01:20:54 -0400

    Added insert to BinarySearchTree

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -5,7 +5,21 @@ class BinarySearchTree:
     self.right = None
 
   def insert(self, value):
-    pass
+    bst_node = BinarySearchTree(value)
+    parent = self
+    while True:
+        if value < parent.value:
+            if parent.left:
+                parent = parent.left
+            else:
+                parent.left = bst_node
+                return
+        else:
+            if parent.right:
+                parent = parent.right
+            else:
+                parent.right = bst_node
+                return
 
   def contains(self, target):
     pass

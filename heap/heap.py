3da39580a9b4ddc4bbcc6dd72d2c910d9256cebe
udykas <udykas@gmail.com>
2018-07-17 17:05:02 -0400

    1 test passing in heap, have yet to build delete and sift down

--- heap/heap.py
+++ heap/heap.py
@@ -8,19 +8,24 @@ class Heap:
   def insert(self, value):
     self.storage.append(value)
     self.size += 1
-    
+    self._bubble_up(self.size)
 
   def delete(self):
     pass
 
   def get_max(self):
-    pass
+    return self.storage[1]
 
   def get_size(self):
-    pass
+    return self.size
 
   def _bubble_up(self, index):
-    pass
+    parent = index // 2
+    if self.storage[parent] > 0 and self.storage[index] > self.storage.parent:
+      temp = self.storage[index]
+      self.storage[index] = self.storage[parent]
+      self.storage[parent] = temp
+      self._bubble_up(parent)
 
   def _sift_down(self, index):
     pass

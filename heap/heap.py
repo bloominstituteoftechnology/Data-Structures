142544a849044f08b3e5bd4410821225342c53b5
Danny Vail <danny_vail@outlook.com>
2018-07-17 16:27:24 -0400

    Added delete method to Heap - Fixed loop conditions in _bubble_up - Fixed _sift_down - Added additional helper methods to increase readability

--- heap/heap.py
+++ heap/heap.py
@@ -1,61 +1,69 @@
 import math
 
 class Heap:
-  def __init__(self):
-    self.storage = []
-    self.size = 0
+    def __init__(self):
+        self.storage = []
+        self.size = 0
 
-  def insert(self, value):
-    self.storage.append(value)
-    index = 0 if self.get_size() == 0 else self.get_size() - 1
-    self._bubble_up(index)
+    def insert(self, value):
+        self.storage.append(value)
+        index = 0 if self.get_size() == 0 else self.get_size() - 1
+        self._bubble_up(index)
 
-  def delete(self):
-    # swap root with last node
-    root = self.storage[0]
-    self.storage[0] = self.storage[self.get_size() - 1]
-    self.storage[self.get_size() - 1] = root
+    def delete(self):
+        # swap root with last node
+        root = self.storage[0]
+        self.storage[0] = self.storage[self.get_size() - 1]
+        self.storage[self.get_size() - 1] = root
 
-    # pop off last node
-    max = self.storage.pop(self.get_size() - 1)
+        # pop off last node
+        max = self.storage.pop(self.get_size() - 1)
 
-    # sift down
-    if self.get_size() > 1:
-        index = 0
-        while index:
-            index = self._sift_down(index)
+        # sift down
+        self._sift_down(0)
+        return max
+
+    def get_max(self):
+        return self.storage[0]
+
+    def get_size(self):
+        return len(self.storage)
+
+    def _bubble_up(self, index):
+        parent_index = math.floor((index-1)/2)
+
+        while index != 0 and self.storage[parent_index] < self.storage[index]:
+            parent = self.storage[parent_index]
+            self.storage[parent_index] = self.storage[index]
+            self.storage[index] = parent
+            index = parent_index
+            parent_index = math.floor((index-1)/2)
+
+    def _sift_down(self, index):
+        left_index = self._get_left_index(index)
+        right_index = self._get_right_index(index)
+
+        while left_index and self.storage[index] < self.storage[left_index]:
+            parent = self.storage[index]
             
-    return max
+            if self.storage[left_index] > self.storage[right_index]:
+                self.storage[index] = self.storage[left_index]
+                self.storage[left_index] = parent
+                left_index = self._get_left_index(left_index)
+            else:
+                self.storage[index] = self.storage[right_index]
+                self.storage[right_index] = parent
+                left_index = self._get_left_index(left_index)
+                
+            right_index = left_index + 1 if left_index else None
 
-  def get_max(self):
-    return self.storage[0]
+    def _get_parent_index(self, index):
+        pass
 
-  def get_size(self):
-    return len(self.storage)
+    def _get_left_index(self, index):
+        left_index = (2*index) + 1
+        return left_index if left_index < self.get_size() else None
 
-  def _bubble_up(self, index):
-    parent_index = math.floor((index-1)/2)
-
-    while self.storage[parent_index] < self.storage[index]:
-        parent = self.storage[parent_index]
-        self.storage[parent_index] = self.storage[index]
-        self.storage[index] = parent
-        parent_index = math.floor((parent_index-1)/2)
-
-  def _sift_down(self, index):
-    left_child_index = (2*index) + 1
-    left_child = self.storage[left_child_index]
-    right_child_index = (2*index) + 2
-    right_child = self.storage[right_child_index]
-    parent = self.storage[index]
-
-    if left_child > parent and left_child > right_child:
-        self.storage[left_child_index] = parent
-        self.storage[index] = left_child
-        return left_child_index
-    elif right_child > parent and right_child > left_child:
-        self.storage[right_child_index] = parent
-        self.storage[index] = right_child
-        return right_child_index
-    
-    return None
+    def _get_right_index(self, index):
+        right_index = (2*index) + 2
+        return right_index if right_index < self.get_size() else None

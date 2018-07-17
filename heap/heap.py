513ff9d194de3e28393580c2e713a188a55d30e0
NandoTheessen <nando.theessen@gmail.com>
2018-07-18 01:24:43 +0200

    refactor of sift down

--- heap/heap.py
+++ heap/heap.py
@@ -38,32 +38,22 @@ class Heap:
       else:
         # break out if element has reached the correct spot
         break
-      index = index //2
-
+      index = index // 2
 
   def _sift_down(self, index):
-    left_child = index * 2
-    right_child = index * 2 + 1
-    if self.size == 2:
-      if self.storage[1] < self.storage[2]:
-        temp = self.storage[1]
-        self.storage[1] = self.storage[2]
-        self.storage[2] = temp
-    if left_child > self.size - 1 and right_child > self.size - 1:
-      return
-    root = self.storage[index]
-    if root < self.storage[left_child] and self.storage[right_child] >= self.storage[left_child]:
-      temp = root
-      self.storage[index] = self.storage[right_child]
-      self.storage[right_child] = temp
-      self._sift_down(right_child)
-    elif root < self.storage[left_child] and self.storage[right_child] < self.storage[left_child]:
-      temp = root
-      self.storage[index] = self.storage[left_child]
-      self.storage[left_child] = temp
-      self._sift_down(left_child)
-    if right_child > self.size and root < self.storage[left_child]:
-      temp = root
-      self.storage[index] = self.storage[left_child]
-      self.storage[left_child] = temp
-      self._sift_down(left_child)
+    
+    while (index * 2) <= self.size:
+      mc = self._max_child(index)
+      
+      if self.storage[index] < self.storage[mc]:
+        self.storage[index], self.storage[mc] = self.storage[mc], self.storage[index]
+      else:
+        break
+
+      index = mc
+
+  def _max_child(self, index):
+    if index * 2 + 1 > self.size:
+      return index * 2
+    else:
+      return index * 2 if self.storage[index * 2] > self.storage[2 * index + 1] else index * 2 + 1

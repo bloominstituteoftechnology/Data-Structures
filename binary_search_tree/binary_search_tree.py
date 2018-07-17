8febbaa5703d742bd95e451cd0cb57f4a31251f0
Bee C <b.choeung@gmail.com>
2018-07-17 19:49:30 -0400

    coded along to Sean for Heap, Binary Search Tree and Queue and Data Structure

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -5,25 +5,48 @@ class BinarySearchTree:
     self.right = None
 
   def insert(self, value):
-    if self.value:
-      if value < self.value:
-        if self.left is None:
-          self.left = Node(value)
-        else:
-          self.left.insert(value)
-        elif value > self.value:
-          if self.right is None:
-            self.right = Node(value)
-          else:
-            self.right.insert(value)
+    # wrap the value in a BinarySearthTree instance
+    new_tree = BinarySearchTree(value)
+    #check and see if the new node's value is less than our current node's value
+    if value < self.value:
+      # check to see if there is no left child here already, place the new node here
+      if not self.left:
+        self.left = new_tree
+      #there is a node there
+      else:
+        # repeat the whole process with recursion
+        self.left.insert(value)
+    #the value is >= the current value
     else:
-      self.value = value
+      if not self.right:
+        self.right = new_tree
+      else:
+        self.right.insert(value)
           
 
   def contains(self, target):
-    pass
-
-
+    #if the value of the current node matches the target, we're done
+    if self.value == target:
+      return True
+      #if value < the current node's value, call contains on the left subtree
+    if target < self.value:
+      #check if self.left exists
+      if self.left:
+        if self.left.contains(target):
+          return True
+    else:
+      if self.right:
+        if self.right.contains(target):
+          return True
+    # our tree doesn't contain the target
+    return False
 
   def get_max(self):
-    pass
+    #no point in doing anything if our tree is empty
+    if not self:
+      return None
+    #check to see if we have a right side
+    if not self.right:
+      return self.value
+    # otherwise we still have more children to the right
+    return self.right.get_max()
--- queue/queue.py
+++ queue/queue.py
@@ -9,19 +9,14 @@ class Queue:
 
   def enqueue(self, item):
     #enqueue should add an item to the back of the queue.
-      #check to see if size is 0
-      if not self.size:
-      #add 1 to self
-        self.size += 1
-      #return the outcome of adding to back
-      return self.storage.add_to_tail(item)
+    self.storage.add_to_tail(item)
+    self.size += 1
   
   def dequeue(self):
     #dequeue should remove and return an item from the front of the queue.
-    #
-    if self.size != 0:
-      return self.size.remove_head
-    
+    if self.size > 0:
+      self.size -= 1
+    return self.storage.remove_head()
 
   def len(self):
     #len returns the number of items in the queue.

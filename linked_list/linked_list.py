3e548e592a106554727dd857d0c4624e389507c3
kammagik <kammagik@yahoo.com>
2018-07-16 22:01:29 -0700

    add to tail passes

--- linked_list/linked_list.py
+++ linked_list/linked_list.py
@@ -23,7 +23,15 @@ class LinkedList:
     self.tail = None
 
   def add_to_tail(self, value):
-    pass
+    new_node = Node(value)
+    if self.head == None:
+      self.head = new_node
+      self.tail = new_node
+    
+    else:
+      self.tail.next_node = new_node
+      self.tail = new_node
+
 
   def remove_head(self):
     pass

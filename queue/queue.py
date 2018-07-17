26adaeee79631213c6cc4dd774d4d1f239211969
maribelcuales <maribelcuales@gmail.com>
2018-07-18 04:54:37 +0800

    add codes for queue functions

--- queue/queue.py
+++ queue/queue.py
@@ -5,13 +5,16 @@ from linked_list import LinkedList
 class Queue:
   def __init__(self):
     self.size = 0
-    self.storage = LinkedList()
+    self.storage = LinkedList()    
 
   def enqueue(self, item):
-    pass
+    self.storage.add_to_tail(item)
+    self.size += 1
   
   def dequeue(self):
-    pass
+    if self.storage.head != None:
+      self.size -= 1
+    return self.storage.remove_head()   
 
   def len(self):
-    pass
+    return self.size

8028baa82c80dd7936b897e0f8dd6ee77b2dd300
astrebeck <alexstrebeck@gmail.com>
2018-07-16 21:50:17 -0600

    initial trial of linked list

--- linked_list/linked_list.py
+++ linked_list/linked_list.py
@@ -3,6 +3,8 @@ Class that represents a single linked
 list node that holds a single value
 and a reference to the next node in the list
 """
+from numpy import inf
+
 class Node:
   def __init__(self, value=None, next_node=None):
     self.value = value
@@ -23,13 +25,42 @@ class LinkedList:
     self.tail = None
 
   def add_to_tail(self, value):
-    pass
+    if(self.head == None): 
+      self.tail = Node(value)
+      self.head = self.tail
+    elif(self.head == self.tail):
+      self.head.set_next(Node(value))
+      self.tail = self.head.get_next()
+    else:
+      cur_node = self.head.get_next()
+
+      while(cur_node.getnext() != None):
+
+        cur_node = cur_node.get_next()
+
+      cur_node.set_next(Node(value))
+      self.tail = cur_node.get_next()
+
 
   def remove_head(self):
-    pass
+    self.head = self.head.get_next()
 
   def contains(self, value):
-    pass
+    cur_node = self.head
+
+    while(cur_node.get_next() != None):
+      if(cur_node.get_value == value):
+        return True
+      else:
+        cur_node = cur_node.get_next()
 
   def get_max(self):
-    pass
+    max = inf
+    cur_node = self.head
+
+    while(cur_node.get_next() != None):
+      if(cur_node.get_value > max):
+        max = cur_node.get_value()
+        cur_node = cur_node.get_next()
+      else:
+        cur_node = cur_node.get_next()

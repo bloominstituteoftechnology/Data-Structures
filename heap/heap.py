2cca4d4d2893222c95a7eaf493dfde36cd8b07b5
Zach Campbell <beazlebubbles@gmail.com>
2018-07-16 18:54:18 -0600

    Completed heap.py

--- heap/heap.py
+++ heap/heap.py
@@ -6,19 +6,49 @@ class Heap:
         self.size = 0
 
     def insert(self, value):
-        pass
+        self.storage.append(value)
+        self.size += 1
+        self._bubble_up(self.size)
 
     def delete(self):
-        pass
+        if self.size == 0:
+            return None
+        elif self.size == 1:
+            self.size -= 1
+            return self.storage.pop()
+        new_root = self.storage.pop()
+        old_root = self.storage[1]
+        self.storage[1] = new_root
+        self.size -= 1
+        self._sift_down(1)
+        return old_root
 
     def get_max(self):
-        pass
+        return self.storage[1]
 
     def get_size(self):
-        pass
+        return self.size
 
     def _bubble_up(self, index):
-        pass
+        parent_index = index // 2
+        if self.storage[index] > self.storage[parent_index] and parent_index > 0:
+            stored_value = self.storage[index]
+            self.storage[index] = self.storage[parent_index]
+            self.storage[parent_index] = stored_value
+            self._bubble_up(parent_index)
 
     def _sift_down(self, index):
-        pass
+        if (
+            2 * index + 1 <= self.size
+            and self.storage[2 * index + 1] >= self.storage[2 * index]
+        ):
+            child_index = 2 * index + 1
+        elif 2 * index <= self.size:
+            child_index = 2 * index
+        else:
+            return
+        if self.storage[child_index] > self.storage[index]:
+            stored_value = self.storage[index]
+            self.storage[index] = self.storage[child_index]
+            self.storage[child_index] = stored_value
+            self._sift_down(child_index)

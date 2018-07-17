c634a8615bb66d4dab5046d9ac1910df4b6df4ee
april <martinezapril56@yahoo.com>
2018-07-17 16:29:17 -0400

    trying to understand the logic

--- queue/queue.py
+++ queue/queue.py
@@ -1,13 +1,15 @@
 import sys
 sys.path.append('../linked_list')
 from linked_list import LinkedList
-
+#queue-first in out
 class Queue:
   def __init__(self):
     self.size = 0
     self.storage = LinkedList()
 
   def enqueue(self, item):
+        #add
+        
   #this the start size and adds 1
    self.size += 1
  #this returns the last node to storage
@@ -15,7 +17,8 @@ class Queue:
     
   
   def dequeue(self):
-
+#to delete
+#your removing what was you add in enque
     remove_result = self.storage.remove_head()
     if remove_result != None:
       self.size -= 1

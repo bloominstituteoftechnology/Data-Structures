042102b7447d80b0127af9a10e98308eea135311
maribelcuales <maribelcuales@gmail.com>
2018-07-18 04:12:56 +0800

    added complete codes for linked_list functions

--- linked_list/linked_list.py
+++ linked_list/linked_list.py
@@ -23,25 +23,69 @@ class LinkedList:
     self.tail = None
 
   def add_to_tail(self, value):
-    new_node = Node(value)    
+    new_node = Node(value)     # constant time 
 
-    if self.head is None:
+    # check to see if the list has a head 
+    if not self.head:
       self.head = new_node
-      return
-
-    last = self.head
-
-    while (last.next):
-      last = last.next 
+    # don't forget the tail 
+      self.tail = new_node
     
-    last.next = new_node
+    else: 
+      # we have a non-empty list
+      # setting the last node's 'next' to the new node 
+      self.tail.next_node = new_node
+
+      # update the 'tail' reference 
+      self.tail = new_node 
 
 
   def remove_head(self):
-    pass
+    # first check if there is no head
+    if not self.head:
+      return None
+    
+    #if head has no 'next', then we have a single element
+    if not self.head.next_node:
+      # take a reference to the current head 
+      head = self.head
+      # delete the list's head reference
+      self.head = None
+      # also make sure the tail points to None
+      self.tail = None
+      return head.value
+    else:
+      # we have multiple elements in our list
+      value = self.head.value
+      self.head = self.head.next_node
+      return value
 
-  def contains(self):
-    pass
+  def contains(self, value):
+    # check to see if our list is empty
+    if not self.head:
+      return None
+    # assign the current node to a variable
+    current = self.head
+    # iterate through the list
+    while current:
+      if current.value == value:
+        return True
+      # move on to the next list node
+      # by updating 'current'
+      current = current.get_next()
+    # if we've gotten here, then the node we're looking for
+    return False
+
 
   def get_max(self):
-    pass
+    if not self.head:
+      return None
+    max_value = self.head.value
+    # set current to head's 'next'
+    current = self.head.get_next()
+    while current:
+      if current.value > max_value:
+        # update max_value
+        max_value = current.value
+      current = current.next_node
+    return max_value

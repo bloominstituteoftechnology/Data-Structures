d517c2dae26d67a0a7b2afbfa4acd2a7b3653c69
unknown <sam.park.email@gmail.com>
2018-07-17 18:06:22 -0400

    insert and bubbleup prob working

--- heap/heap.py
+++ heap/heap.py
@@ -6,7 +6,9 @@ class Heap:
     self.size = 0
 
   def insert(self, value):
-    pass
+    self.storage.append(value)
+    self.size = self.size + 1
+    self._bubble_up(self.size)
 
   def delete(self):
     pass
@@ -15,10 +17,15 @@ class Heap:
     pass
 
   def get_size(self):
-    pass
+    return self.size
 
   def _bubble_up(self, index):
-    pass
+    index = index-1
+    if self.storage[index] > self.storage[index/2]:
+        tmp = self.storage[index]
+        self.storage[index] = self.storage[index/2]
+        self.storage[index/2] = tmp
+        self._bubble_up(index/2)
 
   def _sift_down(self, index):
     pass

f095e633dae1d322bd4aac8ac6b169b56cbeab32
maxwashington <mnwashington18@gmail.com>
2018-07-17 13:55:47 -0500

    Binary Tree Done Passing 3 of 4 test

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -5,10 +5,44 @@ class BinarySearchTree:
     self.right = None
 
   def insert(self, value):
-   pass
-
+    if self.value:
+      if value < self.value:
+        if self.left is None:
+          self.left = BinarySearchTree(value)
+        else:
+          self.left.insert(value)
+      elif value > self.value:
+        if self.right is None:
+          self.right = BinarySearchTree(value)
+        else:
+          self.right.insert(value)
+    else:
+      self.value = value
+              
   def contains(self, target):
-    pass
+      if target < self.value:
+            if self.left is None:
+                  return None
+            return self.left.contains(value)
+      elif value > self.value:
+            if self.right is None:
+                  return None
+            return self.right.contains(value)
+      else:
+            return self
 
   def get_max(self):
-    pass
+    if self.right:
+          x = self.right
+    else:
+          return self.value
+    max = self.value
+    while True:
+          if x.value > max:
+                max = x.value
+          if x.right is None:
+                return max
+          else:
+                x = x.right
+          
+

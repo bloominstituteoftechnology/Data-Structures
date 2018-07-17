b29db1a87c582017d234211ec11b80bf6d2fd531
Thuy <tpham045@hotmail.com>
2018-07-17 04:31:45 +0000

    feat: heap passed

--- heap/heap.py
+++ heap/heap.py
@@ -6,19 +6,60 @@ class Heap:
     self.size = 0
 
   def insert(self, value):
-    pass
+    # add to last index
+    self.storage.append(value)
+    self.size += 1
+    
+    # bubble up the added value
+    self._bubble_up(self.size)
 
   def delete(self):
-    pass
+    # switch top and last item
+    self.swap(self.size, 0)
+    
+    # remove the last item
+    delItem = self.storage.pop()
+    self.size -= 1
+    
+    # sift down the top item
+    self._sift_down(0)
+    return delItem
 
   def get_max(self):
-    pass
+    return self.storage[0]
 
   def get_size(self):
-    pass
+    return self.size
 
   def _bubble_up(self, index):
-    pass
+    # find parent index
+    pI = int(index/2)
+    
+    if self.storage[index] > self.storage[pI]:
+      self.swap(pI, index)
+      self._bubble_up(pI)
 
   def _sift_down(self, index):
-    pass
+    left = 2*index+1
+    right = 2*index+2
+    
+    if left > self.size and right > self.size:
+        return
+    
+    if right > self.size:
+      self.swap(left, index)
+      self._sift_down(left)
+      return
+      
+    # detect sift to left or right
+    if self.storage[left] <= self.storage[right]:
+        self.swap(right,index)
+        self._sift_down(right)
+    else:
+        self.swap(left, index)
+        self._sift_down(left)
+  
+  def swap(self, first, second):
+    temp = self.storage[first]
+    self.storage[first] = self.storage[second]
+    self.storage[second] = temp
--- heap/test_heap.py
+++ heap/test_heap.py
@@ -38,12 +38,10 @@ class HeapTests(unittest.TestCase):
     self.heap.insert(1)
     self.heap.insert(2)
     self.heap.insert(5)
-
     descending_order = []
-
     while self.heap.get_size() > 0:
       descending_order.append(self.heap.delete())
-
+      
     self.assertEqual(descending_order, [10, 8, 7, 6, 5, 5, 2, 1])
 
   def test_bubble_up_was_called(self):

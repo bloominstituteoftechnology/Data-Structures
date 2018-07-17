e517d0a292c9bf8b98c9fc7e6286be0e8a930b28
jesuarva <jesuarva.developer@gmail.com>
2018-07-17 02:30:07 +0200

    IMPLEMENT Heap -> insert AND _bubble_up

--- heap/heap.py
+++ heap/heap.py
@@ -1,24 +1,45 @@
 class Heap:
-  def __init__(self):
-    # storage starts with an unused 0 to make 
-    # integer division simpler later on
-    self.storage = [0]
-    self.size = 0
+    def __init__(self):
+        # storage starts with an unused 0 to make
+        # integer division simpler later on
+        self.storage = [0]
+        self.size = 0
 
-  def insert(self, value):
-    pass
+    def insert(self, value):
+        self.storage.append(value)
+        # print('\n', self.storage)
+        self.size += 1
+        valueIndex = len(self.storage) - 1
+        # print('len', valueIndex + 1)
+        # parentIndex = valueIndex // 2
+        # parent = self.storage[parentIndex]
+        self._bubble_up(valueIndex)
 
-  def delete(self):
-    pass
+        print('\n', self.storage, '\n\n\n\n')
 
-  def get_max(self):
-    pass
+    def delete(self):
+        pass
 
-  def get_size(self):
-    pass
+    def get_max(self):
+        pass
 
-  def _bubble_up(self, index):
-    pass
+    def get_size(self):
+        pass
 
-  def _sift_down(self, index):
-    pass
+    def _bubble_up(self, index):
+        currentvalue = self.storage[index]
+        parentIndex = index // 2
+        parent = self.storage[parentIndex]
+        while currentvalue > parent and parentIndex != 0:
+            self.storage[index // 2] = currentvalue
+            self.storage[index] = parent
+            index = parentIndex
+            currentvalue = self.storage[index]
+            parentIndex = index // 2
+            parent = self.storage[parentIndex]
+
+    def _sift_down(self, index):
+        pass
+
+    def get_parent(self, currentPosition):
+        return currentPosition // 2

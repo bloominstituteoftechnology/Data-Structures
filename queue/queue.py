56ae6912f21b085fc01223fc424040985802bbc0
bbenefield89 <bsquared18@gmail.com>
2018-07-17 17:28:33 -0400

    FINISH queue CLASS FUNCTIONALITY
    - in queue.py
    `enqueue: sets new tail to linked list and increase `self.size` by 1
    `dequeue`: remove head from linked list and decrease `self.size` by 1
    `len`: return amount of lists that are queued

--- queue/queue.py
+++ queue/queue.py
@@ -2,16 +2,24 @@ import sys
 sys.path.append('../linked_list')
 from linked_list import LinkedList
 
+list = LinkedList()
+
 class Queue:
   def __init__(self):
     self.size = 0
     self.storage = LinkedList()
 
   def enqueue(self, item):
-    pass
+    list.add_to_tail(item)
+    self.size += 1
   
   def dequeue(self):
-    pass
+    if not self.size:
+        return None
+      
+    self.size -= 1
+    
+    return list.remove_head()
 
   def len(self):
     return self.size

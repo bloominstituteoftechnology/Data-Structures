21301ec6a88eaf7126ee94293aa6446b3b9b688f
Matthias Bakken <matthias.bakken@gmail.com>
2018-07-17 12:03:24 -0700

    finished insert and contains for binary_search_tree

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -5,10 +5,33 @@ class BinarySearchTree:
     self.right = None
 
   def insert(self, value):
-    pass
+    if value < self.value and not self.left:
+      self.left = BinarySearchTree(value)
+      return self.left
+    elif value >= self.value and not self.right:
+      self.right = BinarySearchTree(value)
+      return self.right
+    
+    if value < self.value:
+      node_val = self.left
+    else:
+      node_val = self.right
+    
+    node_val.insert(value)
 
   def contains(self, target):
-    pass
+    if target == self.value:
+      return True
+    
+    if target < self.value:
+      node_val = self.left
+    else:
+      node_val = self.right
+    
+    if not node_val:
+      return False
+    
+    return node_val.contains(target)
 
   def get_max(self):
     pass

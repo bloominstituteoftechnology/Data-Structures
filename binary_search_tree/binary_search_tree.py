51833f149c3613b4aaa3e604b6331ceb2c66a9b9
maribelcuales <maribelcuales@gmail.com>
2018-07-18 06:24:27 +0800

    add codes for insert and contains functions in binary search tree

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -5,10 +5,44 @@ class BinarySearchTree:
     self.right = None
 
   def insert(self, value):
-    pass
+    
+    if self.value:
+      if value < self.value:
+        if self.left is None:
+          self.left = BinarySearchTree(value)
+        
+        else:
+          self.left.insert(value)
+      
+      elif value > self.value:
+        if self.right is None:
+          self.right = BinarySearchTree(value)
+        else:
+          self.right.insert(value)
+    
+    else:
+      self.value = value
 
-  def contains(self, target):
-    pass
+
+  def contains(self, target):    
+
+    if target == self.value:
+      return True
+    
+    if target < self.value:
+      if self.left == None:
+        return False
+      return self.left.contains(target)
+    
+    else:
+      if self.right == None:
+        return False
+      return self.right.contains(target)
+
 
   def get_max(self):
-    pass
+    max_value = self.value    
+
+    while self.right is not None:
+      max_value = self.right.value
+    return max_value

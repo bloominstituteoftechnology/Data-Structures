241a0bdb43533da7315f05baa9dc8803cac55719
NandoTheessen <nando.theessen@gmail.com>
2018-07-17 23:23:14 +0200

    Added solution to doubly linked list, added faster traversal for the doubly linked list (head and tail pointer

--- doubly_linked_list/doubly_linked_list.py
+++ doubly_linked_list/doubly_linked_list.py
@@ -5,36 +5,122 @@ class ListNode:
     self.next = next
 
   def insert_after(self, value):
-    pass
+    new_node = ListNode(value, self, self.next)
+    self.next = new_node
 
   def insert_before(self, value):
-    pass
+    new_node = ListNode(value, self.prev, self)
+    self.prev = new_node
 
   def delete(self):
-    pass
+    if not self.next:
+      self.prev.next = None
+    elif not self.prev:
+      self.next.prev = None
+    else:
+      next_node = self.next
+      prev_node = self.prev
+
+      next_node.prev = prev_node
+      prev_node.next = next_node
+
 
 class DoublyLinkedList:
-  def __init__(self):
-    self.head = None
-    self.tail = None
+  def __init__(self, node = None):
+    self.head = node
+    self.tail = node
 
   def add_to_head(self, value):
-    pass
+    if not self.head:
+      new_node = ListNode(value, None, None)
+      self.head = new_node
+      self.tail = new_node
+    else:  
+      self.head.insert_before(value)
+      self.head = self.head.prev
+    
 
   def remove_from_head(self):
-    pass
+    value = self.head.value
+    self.head = self.head.next
+    self.head.prev = None
+    #self.head.insert_before(None)
+    return value
 
   def add_to_tail(self, value):
-    pass
+    if not self.tail:
+      new_node = ListNode(value, None, None)
+      self.head = new_node
+      self.tail = new_node
+    else:
+      self.tail.insert_after(value)
+      self.tail = self.tail.next
 
   def remove_from_tail(self):
-    pass
+    value = self.tail.value
+    self.tail = self.tail.prev
+    self.tail.next = None
+    #self.tail.insert_after(None)
+    return value
+
+  # def move_to_front(self, node):
+  #   current = self.head
+  #   while current:
+  #     if current.value == node.value:
+  #       self.head.insert_before(node.value)
+  #       current.delete()
+  #       self.head = self.head.prev
+  #       return self.head.value
+  #     else:
+  #         current = current.next
+  #   return False
 
   def move_to_front(self, node):
-    pass
+    head = self.head
+    headValue = head.value
+    tail = self.tail
+    tailValue = tail.value
+    while head.value != tailValue and tail.value != headValue:
+      if headValue == node.value:
+        self.head.insert_before(node.value)
+        head.delete()
+        self.head = self.head.prev
+        return self.head.value
+      if tailValue == node.value:
+        self.head.insert_before(node.value)
+        tail.delete()
+        self.head = self.head.prev
+        return self.head.value
+      else:
+        head = head.next
+        tail = tail.prev
+    return False
 
   def move_to_end(self, node):
-    pass
+    pointer_end = self.tail
+    pointer_front = self.head
+
+    while pointer_end and pointer_front:
+      if pointer_end == pointer_front:
+        print('fire')
+        break
+      if pointer_end.value == node.value:
+        self.tail.insert_after(node.value)
+        pointer_end.delete()
+        self.tail = self.tail.next
+        return self.tail.value
+      elif pointer_front.value == node.value:
+        self.tail.insert_after(node.value)
+        pointer_front.delete()
+        self.tail = self.tail.next
+        return self.tail.value
+      else:
+        pointer_end = pointer_end.prev
+        pointer_front = pointer_front.next
+
+    return False
+
 
   def delete(self, node):
-    pass
+    node.delete()
+    return node

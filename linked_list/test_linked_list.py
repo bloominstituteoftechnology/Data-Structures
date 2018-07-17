03e4d590403654f6d3ce070d8ce9e775f73dd163
Navjot Singh <navjotts@gmail.com>
2018-07-17 11:56:24 -0700

    fix typo

--- linked_list/test_linked_list.py
+++ linked_list/test_linked_list.py
@@ -32,7 +32,7 @@ class LinkedListTests(unittest.TestCase):
 
     # at this point list should be empty, as we have removed the 2 elements we added
     self.list.add_to_tail(10) # new list should have 1 element (with both head and tail pointing to it)
-    self.assertEqual(self.list.remove_head(), 10) # remove the newly added element again (resulsting in an empty list again)
+    self.assertEqual(self.list.remove_head(), 10) # remove the newly added element again (resulting in an empty list again)
     self.assertIsNone(self.list.head) # head should be None
     self.assertIsNone(self.list.tail) # tail should be None
     self.assertIsNone(self.list.remove_head())

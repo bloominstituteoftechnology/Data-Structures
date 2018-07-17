a45f051ca7a1a9ecb47a6457f25e6b6b28fbe1a5
bbenefield89 <bsquared18@gmail.com>
2018-07-17 19:34:08 -0400

    COMPLETE `contains & `get_max` METHODS
    - in binary_search_tree.py
    `contains`: return true or false depending on whether or not tree contains value passed
    `get_max`: return the highest number in binary tree

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -9,7 +9,7 @@ class BinarySearchTree:
         current = self
 
         while not done:
-            if value > current.value:
+            if value >= current.value:
                 if not current.right:
                     current.right = BinarySearchTree(value)
                     break
@@ -23,7 +23,36 @@ class BinarySearchTree:
                     current = current.left
 
     def contains(self, target):
-        pass
+        done = False
+        current = self
+
+        while not done:
+            if target == current.value:
+                return True
+
+            if target >= current.value:
+                if not current.right:
+                    return False
+
+                current = current.right
+            else:
+                if not current.left:
+                    return False
+
+                current = current.left
 
     def get_max(self):
-        pass
+        max_value = self.value
+        current = self
+        next_node = self.right
+
+        while current:
+            if not current.right:
+                print(current.value)
+                return max_value
+
+            if current.value > max_value:
+                print(f"{current.value} is greater than {max_value}")
+                return current.value
+            
+            current = next_node

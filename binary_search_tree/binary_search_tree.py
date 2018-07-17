f587ed93b0cd5b5ad2286eec952b7f3e2061b525
kelly <kelfro@fastmail.com>
2018-07-17 13:16:42 -0700

    completed binary search tree hope you like reading lots of commentary. I kind of wrote my way through it.

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -15,45 +15,66 @@ class BinarySearchTree:
         if self.value:
             if value < self.value:
                 if self.left is None:
-                    self.left = Node(value)
+                    self.left = BinarySearchTree(value)
                 else:
                     self.left.insert(value)
             elif value > self.value:
                 if self.right is None:
-                    self.right = Node(value)
+                    self.right = BinarySearchTree(value)
                 else:
                     self.right.insert(value)
         else:
             self.value = value
 
 # First, compare value passed in with the node - > or <?
-# If <, check left node. If nothing is there, congratulations - you have a new node.
+# If <, check left node. If nothing is there, congratulations - you have a
+# new node.
 # If something is there, insert the new value
 # Same with the right.
 # Ignore new value if equal to current node value
 
     def contains(self, target):
         if target == self.value:
-            return self
+            return True
         if target < self.value:
             if self.left is None:
-                print('doh!')
+                return False
             else:
-                return self.find(target, node.left)
+                return self.left.contains(target)
         elif target > self.value:
             if self.right is None:
-                print('doh!')
+                return False 
             else:
-                return self.find(target, node.right)
-        else:
-            print(int(self.value) + 'is present')
-# This is similar to above where we compare the target with the node value and then look down each subtree
+                return self.right.contains(target)
+        return False
+
+# If the target = the current value, we're done.
+# If target > current value, look at left branch. If empty, return false.
+# If not, call this function recursively on the branch
+# If target < current value, do the same with the right branch
+# If you don't find it anywhere, return false.
+# NOTE:
+# I got some help from a classmate who suggested the recursive function
+# call to search. I was trying the find method and it wasn't working. The
+# recursion works because the first 2 lines check on the current value and
+# if it's true, then it returns True. It doesn't run forever because if
+# there is nothing left to search, it returns False.
+
 
     def get_max(self):
-        if self is none or self.left is None:
-            return self
-        current = self
-        while current:
-            if not current.right:
-                return current
-            current = current.right
+        if self.right is None:
+            return self.value
+        else:
+            return self.right.get_max()
+			
+# This works a little differently than linked lists because I'm pretty sure 
+# the left branch, if it exists, is never going to contain the maximum, by
+# definition. So you only need to check the right branch.
+# I was looking at my linked list get_max as a model but I don't think 
+# it'll work because we don't have a next_node here. So I think using 
+# recursion again here makes sense.
+# If there is nothing on the right branch, the current value must be the
+# maximum
+# If there is something there, then run the function recursively on itself.
+# It will stop when there is nothing left on the right branch and return
+# whatever value it currently holds as the maximum.

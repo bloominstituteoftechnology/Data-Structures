1de50c5b9ed8af2f9279d2cb1a9341091aa0d671
bbenefield89 <bsquared18@gmail.com>
2018-07-17 18:14:00 -0400

    COMPLETE `insert` METHOD
    - in binary_search_tree.py
    Check if value passed is greater than or equal to current node and create new node depending on the result

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -1,14 +1,29 @@
 class BinarySearchTree:
-  def __init__(self, value):
-    self.value = value
-    self.left = None
-    self.right = None
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
 
-  def insert(self, value):
-    pass
+    def insert(self, value):
+        done = False
+        current = self
 
-  def contains(self, target):
-    pass
+        while not done:
+            if value > current.value:
+                if not current.right:
+                    current.right = BinarySearchTree(value)
+                    break
+                else:
+                    current = current.right
+            else:
+                if not current.left:
+                    current.left = BinarySearchTree(value)
+                    break
+                else:
+                    current = current.left
 
-  def get_max(self):
-    pass
+    def contains(self, target):
+        pass
+
+    def get_max(self):
+        pass

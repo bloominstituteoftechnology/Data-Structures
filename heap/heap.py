96cc02ec6552af2b209460c7ba659951bb380c3d
kelly <kelfro@fastmail.com>
2018-07-17 16:14:36 -0700

    still working on heap

--- heap/heap.py
+++ heap/heap.py
@@ -16,42 +16,34 @@ class Heap:
 
     def delete(self):
         max = self.get_max()
-        self.storage.shift()
+        self.storage.pop(0)
         self._sift_down(0)
         return max
 
     def get_max(self):
-        if self.size > 0:
-            self.storage[0] = newroot
-            index = 0
-            self._sift_down(index)
+        return self.storage[1]
 
     def get_size(self):
         return self.size
 
     def _bubble_up(self, index):
         while index // 2 > 0:
-            if self.storage[index] > self.storage[index // 2]:
+            if self.storage[index] < self.storage[index // 2]:
                 temp = self.storage[index // 2]
                 self.storage[index // 2] = self.storage[index]
                 self.storage[index] = temp
-                index = index // 2
+            index = index // 2
 # Compare index value with its parent. If parent is < index value, then
 # move index value up the heap. 
 # Resource: https://interactivepython.org/courselib/static/pythonds/Trees/BinaryHeapImplementation.html
     
     def _sift_down(self, index):
-        if (index * 2 + 1 <= self.size and self.storage[index * 2 + 1] >= self.storage[2 * index]):
-            child_index = 2 * index + 1j
-        child_index1 = index * 2
-        child_index2 = index * 2 + 1
-        if self.storage[child_index1] > self.storage[index]:
-            temp = self.storage[index]
-            self.storage[index] = self.storage[child_index1]
-            self.storage[child_index1] = temp
-            self._sift_down(child_index)
-        if self.storage[child_index2] > self.storage[index]:
-            temp = self.storage[index]
-            self.storage[index] = self.storage[child_index2]
-            self.storage[child_index2] = temp
-            self._sift_down(child_index)
+        child_index1 = index * 2 + 1
+        child_index2 = index * 2 + 2
+        newindex = index
+        if child_index1 <= self.size - 1 and self.storage[child_index1] > self.storage[child_index2]:
+            newindex = child_index1
+        if child_index2 <= self.size - 1 and self.storage[child_index2] > self.storage[newindex]:
+            newindex = child_index2
+        if newindex is not index:
+           self.storage[index], self.storage[newindex] = self.storage[newindex], self.storage[index]

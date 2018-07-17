3bfb9f147525f20a2b0d1ecb733db684a5005144
07sebascode <sebastiannorena@gmail.com>
2018-07-17 16:25:40 -0400

    linked list complete passed all

--- linked_list/linked_list.py
+++ linked_list/linked_list.py
@@ -51,10 +51,38 @@ class LinkedList:
       value=self.head.value
       self.head=self.head.next_node
       return value
-      
 
-  def contains(self):
-    pass
+
+  def contains(self, value):
+    #check if empty
+    if not self.head:
+      return None
+    #assign the current node to a variable
+    current=self.head
+    #iterate through the list
+    while current:
+      if current.get_value() == value:
+        return True
+      #move to next node
+      current = current.get_next()
+    #if we don't find the value
+    return False
+
+
+
 
   def get_max(self):
-    pass
+    if not self.head:
+      return None
+    max_value = self.head.get_value()
+    #set current to head's next
+    current = self.head.get_next()
+    while current:
+      if current.get_value() > max_value:
+        max_value = current.get_value()
+      current = current.get_next()
+    return max_value
+
+
+
+

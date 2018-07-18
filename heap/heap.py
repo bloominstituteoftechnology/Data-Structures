e3f01a1a6ac1c1e3ba57be40823e25506f4a19d8
07sebascode <sebastiannorena@gmail.com>
2018-07-17 23:26:48 -0400

    Heap tests ok

--- heap/heap.py
+++ heap/heap.py
@@ -6,19 +6,50 @@ class Heap:
     self.size = 0
 
   def insert(self, value):
-    pass
+    self.storage.append(value)
+    self.size += 1
+    self._bubble_up(self.size)
 
   def delete(self):
-    pass
+    if self.size == 0:
+      return None
+
+    top = self.storage[1]
+    last = self.storage.pop()
+    self.size -= 1
+    if self.size == 0:
+      return top
+
+    self.storage[1] = last
+    self._sift_down(1)
+    return top
 
   def get_max(self):
-    pass
+    return self.storage[1]
 
   def get_size(self):
-    pass
+    return self.size
 
   def _bubble_up(self, index):
-    pass
+    parent_index = index//2
+    if parent_index == 0:
+      return
+
+    if self.storage[parent_index] < self.storage[index]:
+      temp = self.storage[parent_index]
+      self.storage[parent_index] = self.storage[index]
+      self.storage[index] = temp
+      self._bubble_up(parent_index)
 
   def _sift_down(self, index):
-    pass
+    left_index = index if 2*index > self.size else 2*index
+    right_index = index if 2*index+1 > self.size else 2*index+1
+    child_index = left_index if self.storage[left_index] > self.storage[right_index] else right_index
+    if child_index == index:
+      return
+
+    if self.storage[child_index] > self.storage[index]:
+      temp = self.storage[index]
+      self.storage[index] = self.storage[child_index]
+      self.storage[child_index] = temp
+      self._sift_down(child_index)

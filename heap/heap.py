feb0470d5f4f9c7c83afdc6eae1a6f2cb65229e3
Mason Morrow <A99@99s-macbook-pro.home>
2018-07-18 23:34:12 +0200

    Add heap_sort:
    - insert
    - delete
    - get_max
    - get_size
    - _bubble_up
    - _sift_down

--- heap/heap.py
+++ heap/heap.py
@@ -1,3 +1,5 @@
+from math import floor
+
 class Heap:
   def __init__(self):
     # storage starts with an unused 0 to make 
@@ -6,19 +8,93 @@ class Heap:
     self.size = 0
 
   def insert(self, value):
-    pass
+    self.size += 1
+    self.storage.append(value)
+    self._bubble_up(self.size)
 
   def delete(self):
-    pass
+    if self.size == 0:
+      return
+    elif self.size == 1:
+      self.size -= 1
+      deleted = self.storage[1]
+      self.storage.pop()
+      return deleted
+    elif self.size == 2:
+      self.size -= 1
+      last_num = self.storage[-1]
+      deleted = self.storage[1]
+      self.storage = [0, last_num]
+      self._sift_down(1)
+      return deleted
+    else:
+      self.size -= 1
+      last_num = self.storage[-1]
+      deleted = self.storage[1]
+      self.storage = self.storage[:1] + [last_num] + self.storage[2:-1]
+      self._sift_down(1)
+      return deleted
 
   def get_max(self):
-    pass
+    if self.size > 0:
+      return self.storage[1]
+    else:
+      return 0
 
   def get_size(self):
-    pass
+    return self.size
 
   def _bubble_up(self, index):
-    pass
+    parent_index = floor(index/2)
+    parent = self.storage[parent_index]
+    child_index = index
+    child = self.storage[child_index]
+
+    if parent == 0:
+      return
+    if parent > child:
+      return
+    else:
+      self.storage[parent_index] = child
+      self.storage[child_index] = parent
+      return self._bubble_up(parent_index)
 
   def _sift_down(self, index):
-    pass
+    parent_index = index
+    parent = self.storage[parent_index]
+    child_left_index = 2*index
+    child_right_index = 2*index + 1
+
+    if child_left_index > self.size:
+      #no children to test = end of heap
+      return
+    elif child_right_index > self.size:
+      #left child to test only
+      child_left = self.storage[child_left_index]
+      if parent < child_left:
+        self.storage[parent_index] = child_left
+        self.storage[child_left_index] = parent
+        return
+      else:
+        return
+    else:
+      #test both children
+      child_left = self.storage[child_left_index]
+      child_right = self.storage[child_right_index]
+      if parent > child_left and parent > child_right:
+        return
+      else:
+        if child_left > child_right:
+          if parent < child_left:
+            self.storage[parent_index] = child_left
+            self.storage[child_left_index] = parent
+            return self._sift_down(child_left_index)
+          else:
+            return
+        else:
+          if parent < child_right:
+            self.storage[parent_index] = child_right
+            self.storage[child_right_index] = parent
+            return self._sift_down(child_right_index)
+          else:
+            return

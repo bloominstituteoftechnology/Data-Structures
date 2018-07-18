860bd8c639e09db7938bafbf058348f9439466a4
sagardesai90 <saagardesai@gmail.com>
2018-07-18 13:35:16 -0500

    heap & data structures questions

--- heap/heap.py
+++ heap/heap.py
@@ -6,19 +6,46 @@ class Heap:
     self.size = 0
 
   def insert(self, value):
-    pass
+    self.storage.append(value)
+    self.size += 1
+    self._bubble_up(self.size)
 
   def delete(self):
-    pass
+    max = self.storage[1] #store max so we can return it
+    #replace first storage el w last el in the heap
+    if len(self.storage) == 1:
+      return None
+    
+    self.storage[1] = self.storage[self.size]
+    self.size -= 1
+    self.storage.pop()
+    self._sift_down(1)
+    return max
 
   def get_max(self):
-    pass
+    return self.storage[1]
 
   def get_size(self):
-    pass
+    return self.size
 
   def _bubble_up(self, index):
-    pass
+    while index // 2 > 0:
+      if self.storage[index // 2] < self.storage[index]:
+        self.storage[index // 2], self.storage[index] = self.storage[index], self.storage[index // 2]
+      else:
+        break
+      index = index / 2
 
   def _sift_down(self, index):
-    pass
+    while (index * 2) <= self.size:
+      maxChild = self._max_child(index)
+      if self.storage[index] < self.storage[maxChild]:
+        self.storage[index], self.storage[maxChild] = self.storage[maxChild], self.storage[index]
+      else:
+        break
+      index = maxChild
+  def _max_child(self, index):
+    if index * 2 + 1 > self.size:
+      return index * 2
+    else:
+      return index * 2 if self.storage[index * 2] > self.storage[index * 2 + 1] else index * 2 + 1

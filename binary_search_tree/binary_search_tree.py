2d1311c733de4a616af33df885b967a44beeed66
Matthias Bakken <matthias.bakken@gmail.com>
2018-08-14 04:03:15 -0700

    adding psuedo code to binary_search_tree.py

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -5,16 +5,27 @@ class BinarySearchTree:
     self.right = None
 
   def insert(self, value):
+    # checking to see if value inserted is less than current value
+    # and that there is no left value
     if value < self.value and not self.left:
+      # sets left value to new value
       self.left = BinarySearchTree(value)
+      # returns new set value
       return self.left
+    # checks to see if value is greater than or equal to current value
+    # and that there is no right value
     elif value >= self.value and not self.right:
+      # sets right value to new value
       self.right = BinarySearchTree(value)
+      # returns new set value
       return self.right
     
+    # if there is a current value and it is greater than new value
     if value < self.value:
+      # insert new value left
       self.left.insert(value)
     else:
+      # else enter it right
       self.right.insert(value)
 
 

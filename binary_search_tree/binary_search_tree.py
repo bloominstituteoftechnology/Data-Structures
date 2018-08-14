378af1edbd651322f4c5036f702f9dcb22930e59
Matthias Bakken <matthias.bakken@gmail.com>
2018-08-14 06:28:41 -0700

    completed binary_search_tree.py

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -30,23 +30,31 @@ class BinarySearchTree:
 
 
   def contains(self, target):
+    # if target value is equal to current value
     if target == self.value:
+      # return true
       return True
-    
+    # if target value is less than current value
     if target < self.value:
+      # set node value to left value
       node_val = self.left
     else:
+      # if target value is more than current value
+      # set it to right value
       node_val = self.right
     
     if not node_val:
       return False
-    
+      
     return node_val.contains(target)
 
   def get_max(self):
+    # if there is no right value
     if not self.right:
+      # right equals current value
       right = self.value
     else:
+      # if there is right value set right to right.get_max to run until max value found
       right = self.right.get_max()
-    
+    # return max value
     return max(self.value, right)

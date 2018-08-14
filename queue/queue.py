5829a4a61c5eb63e96f777bd2ef07782798c98ce
Matthias Bakken <matthias.bakken@gmail.com>
2018-08-13 22:56:41 -0700

    completed queue.py

--- queue/queue.py
+++ queue/queue.py
@@ -4,14 +4,28 @@ from linked_list import LinkedList
 
 class Queue:
   def __init__(self):
+    # initializes size of queue
     self.size = 0
+    # initializes items in storage
     self.storage = LinkedList()
 
   def enqueue(self, item):
-    pass
+    # adds another value to the tail of storage
+    self.storage.add_to_tail(item)
+    # when adding the new item to storage it increases storage size by one
+    self.size += 1
   
   def dequeue(self):
-    pass
+    # sets remove_value to the value of remove_head()
+    remove_value = self.storage.remove_head()
+    if remove_value == None:
+      # if there is no value to remove then return None
+      return None
+    # if there is a value and it is removed, subtract 1 from storage size
+    self.size -= 1
+    # return the value of removed item
+    return remove_value
 
   def len(self):
-    pass
+    # returns the value of size
+    return self.size

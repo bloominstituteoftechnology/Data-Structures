3eadedb10083b410a13de9bf713b044f9920d368
darthMinimus <irishseanvaldivia@gmail.com>
2018-08-13 21:29:19 -0500

    Still working on linked_list; made display_values() method for troubleshooting.

--- linked_list/linked_list.py
+++ linked_list/linked_list.py
@@ -22,17 +22,46 @@ class LinkedList:
     self.head = None
     self.tail = None
 
-  def add_to_tail(self, value):
-    if not self.head and not self.tail:
-      self.head = value
-      self.tail = value
-      
+  def display_values(self):
+    current_node = self.head
+    values = []
+    while current_node:
+      values.append(current_node.value)
+      current_node = current_node.next_node
+    return values
 
+  def add_to_tail(self, value):
+    new_node = Node(value)
+    if not self.head and not self.tail:
+        self.head = new_node
+        self.tail = new_node
+    else:
+      self.tail.next_node = new_node
+      self.tail = new_node
+      
   def remove_head(self):
-    pass
+    node_to_delete = self.head
+    if not self.head:
+        return None
+    elif self.tail == self.head:
+        self.tail = None
+        self.head = None
+        return node_to_delete
+    else:
+      node_to_delete.next = None
+      self.head = self.head.next_node
+    
+      
 
   def contains(self, value):
     pass
 
   def get_max(self):
     pass
+
+linked_list = LinkedList()
+linked_list.add_to_tail(10)
+linked_list.add_to_tail(20)
+
+values = linked_list.display_values()
+print(values)

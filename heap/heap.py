c92eecd80464d8c06061a6b061bd71c7b80c9185
Matthias Bakken <matthias.bakken@gmail.com>
2018-08-16 05:48:17 -0700

    completed heap.py

--- heap/heap.py
+++ heap/heap.py
@@ -6,19 +6,54 @@ class Heap:
     self.size = 0
 
   def insert(self, value):
-    pass
+    self.storage.append(value)
+    self.size += 1
+    self._bubble_up(self.size)
 
   def delete(self):
-    pass
+    if self.size == 0:
+      return None
+    first_val = self.storage[1]
+    last_val = self.storage.pop()
+    self.size -= 1
+    if self.size == 0:
+      return first_val
+
+    self.storage[1] = last_val
+    self._sift_down(1)
+    return first_val
 
   def get_max(self):
-    pass
+    return self.storage[1]
 
   def get_size(self):
-    pass
+    return self.size
 
   def _bubble_up(self, index):
-    pass
+    parent_index = index // 2
+    if parent_index >= 1 and self.storage[index] > self.storage[parent_index]:
+      stored_value = self.storage[index]
+      self.storage[index] = self.storage[parent_index]
+      self.storage[parent_index] = stored_value
+      self._bubble_up(parent_index)
 
   def _sift_down(self, index):
-    pass
+    if 2*index > self.size: 
+      left_index = index
+    else:
+      left_index = 2*index
+    if 2*index+1 > self.size:
+      right_index = index
+    else:
+      right_index = 2*index+1
+    if self.storage[left_index] > self.storage[right_index]:
+      child_index = left_index
+    else:
+      child_index = right_index
+    if child_index == index:
+      return
+    if self.storage[child_index] > self.storage[index]:
+      stored_value = self.storage[index]
+      self.storage[index] = self.storage[child_index]
+      self.storage[child_index] = stored_value
+      self._sift_down(child_index)

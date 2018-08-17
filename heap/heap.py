b75bb3c4e9f89b17156ff9766aa6a4cc896ea407
darthMinimus <irishseanvaldivia@gmail.com>
2018-08-17 11:16:11 -0500

    Trying to get it to work still...

--- heap/heap.py
+++ heap/heap.py
@@ -9,38 +9,28 @@ class Heap:
 
   def insert(self, value):
     self.storage.append(value)
-    self._bubble_up(self.size+1)
     self.size += 1
+    print('size', self.size)
+    self._bubble_up(self.size)
+    pass
     
-  def get_parent_index(self, child_index):
-     return self.storage[int((floor(child_index - 1) / 2))]
-
-  def left_child_index(self, parent_index):
-    return self.storage[int(parent_index * 2)]
-
-  def right_child_index(self, parent_index):
-    return self.storage[int((parent_index * 2) + 1)]
-
   def delete(self):
     pass
 
   def get_max(self):
+    print(self.storage)
     return self.storage[1]
+    pass
 
   def get_size(self):
-    return self.size
+    pass
 
   def _bubble_up(self, index):
-      current_value = self.storage[index]
-      parent_value = self.get_parent_index(index)
-      if parent_value == 0:
-        return 
-      while parent_value < current_value:
-          if current_value > parent_value:
-              current_value, parent_value = parent_value, current_value
-      return self.storage[index]
-
-
-  def _sift_down(self, index):
-    
+    current = self.storage[index]
+    parent = self.storage[index // 2]
+    if index == 1 or parent >= current:
+      return None
+    else:
+      self.storage[index], self.storage[index // 2] = self.storage[index // 2], self.storage[index]
+      self._bubble_up(index // 2)
     pass

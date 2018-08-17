119434f5fb294ca084d90a64be104c2bc8e61058
darthMinimus <irishseanvaldivia@gmail.com>
2018-08-16 22:04:09 -0500

    Welcome to the Inifinite Loop. Press enter to do NOTHING!

--- heap/heap.py
+++ heap/heap.py
@@ -9,31 +9,36 @@ class Heap:
 
   def insert(self, value):
     self.storage.append(value)
-    self.__bubble_up(self.size+1)
+    self._bubble_up(self.size+1)
     self.size += 1
     
-  def left_child_index(parent_i):
-    return current_i * 2
+  def get_parent_index(self, child_index):
+     return self.storage[int((floor(child_index - 1) / 2))]
 
-  def right_child_index(parent_i):
-    return (current_i * 2) + 1
+  def left_child_index(self, parent_index):
+    return self.storage[int(parent_index * 2)]
 
-  def parent_index(child_index):
-     return floor(child_index / 2)
+  def right_child_index(self, parent_index):
+    return self.storage[int((parent_index * 2) + 1)]
 
   def delete(self):
     pass
 
   def get_max(self):
-    pass
+    return self.storage[1]
 
   def get_size(self):
-    pass
+    return self.size
 
   def _bubble_up(self, index):
-      if self.storate[index] < self.storage[(cieling(index-1)/2)]:
-        return
-      else:
+      current_value = self.storage[index]
+      parent_value = self.get_parent_index(index)
+      if parent_value == 0:
+        return 
+      while parent_value < current_value:
+          if current_value > parent_value:
+              current_value, parent_value = parent_value, current_value
+      return self.storage[index]
 
 
   def _sift_down(self, index):

c8ccbe80f13cea0268bbcb50f1d93764b0a4d10f
darthMinimus <irishseanvaldivia@gmail.com>
2018-08-19 21:15:31 -0500

    Got heap working??? Also added more pseudocode

--- heap/heap.py
+++ heap/heap.py
@@ -1,4 +1,4 @@
-from math import floor
+import math
 
 class Heap:
   def __init__(self):
@@ -7,42 +7,95 @@ class Heap:
     self.storage = [0]
     self.size = 0
 
-
   def insert(self, value):
     self.storage.append(value)
     self.size += 1
     self._bubble_up(self.size)
     
+    
   def delete(self):
-    pass
+    self.storage[1], self.storage[-1] = self.storage[-1], self.storage[1]
+    deleted_value = self.storage.pop(-1)
+    self.size -= 1
+    self._sift_down_(1)
+    return deleted_value
 
   def get_max(self):
     return self.storage[1]
+
+  def get_min(self):
+    return self.storage[-1]
 
   def get_size(self):
     return self.size
 
   def _bubble_up(self, index):
-      current_index = index
-      
-      
-      while current_index != 1:
-        if self.storage[current_index] > self.storage[current_index // 2]:
-          self.storage[current_index], self.storage[current_index // 2] = self.storage[current_index // 2], self.storage[current_index]
-        current_index = current_index // 2
-      return self.storage[current_index]
+      # my two part base case for recursion: 
+      # if parent is greater than child or if index less than or equal to one
+      if self.storage[index] < self.storage[index//2] or index <= 1:
+          return
+      else:
+          # swap the parent/child values
+          self.storage[index], self.storage[index//2] = self.storage[index//2], self.storage[index]
+          # recursive call
+          return self._bubble_up(index//2)
+
 
   def _sift_down_(self, index):
-    pass
+    left_child = index * 2
+    right_child = (index * 2) + 1
+    if left_child > self.size:
+      return
+    if right_child <= self.size:
+        if self.storage[index] < self.storage[left_child] or self.storage[index] < self.storage[right_child]:
+            largest_child = right_child if (self.storage[right_child] > self.storage[left_child]) else left_child
+            self.storage[index], self.storage[largest_child] = self.storage[largest_child], self.storage[index]
+            return self._sift_down_(largest_child)
+        else:
+          return              
+    else:
+      if self.storage[index] < self.storage[left_child]:
+        self.storage[index], self.storage[left_child] = self.storage[left_child], self.storage[index]
+        return self._sift_down_(left_child)
+      else:
+        return
+      
+    
+
+# heap = Heap()
+
+# heap.insert(20)
+# heap.insert(13)
+# heap.insert(9)
+# heap.insert(8)
+# heap.insert(5)
+# heap.insert(3)
+# heap.insert(7)
+# heap.insert(6)
+# heap.insert(2)
+# heap.insert(1)
 
 
-my_heap = Heap()
-entries  = [20, 13, 9, 8, 5, 3, 7, 6, 2, 1]
-for i in range(len(entries)):
-  my_heap.insert(entries[i])
-print(my_heap.storage)
-heap = Heap()
-more_entries = [6, 7, 5, 8, 10, 1, 2, 5]
-for i in range(len(more_entries)):
-  heap.insert(more_entries[i])
-print(heap.storage)
+
+# print(heap.storage)
+
+heap2 = Heap()
+
+heap2.insert(40)
+heap2.insert(18)
+heap2.insert(20)
+heap2.insert(15)
+heap2.insert(13)
+heap2.insert(9)
+heap2.insert(19)
+heap2.insert(1)
+heap2.insert(3)
+heap2.insert(8)
+
+print(heap2.storage)
+
+heap2.delete()
+print("heap2 after delete(): ", heap2.storage)
+
+heap2.insert(40)
+print("After 40 resinserted: ", heap2.storage)

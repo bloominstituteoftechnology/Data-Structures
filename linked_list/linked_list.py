1c8ab2b12ea1a7adaea0ca99965680641740125c
snowcoding <vimalshah77@gmail.com>
2018-09-05 14:05:28 -0700

    Basic get_max() for LL

--- linked_list/linked_list.py
+++ linked_list/linked_list.py
@@ -3,6 +3,8 @@ Class that represents a single linked
 list node that holds a single value
 and a reference to the next node in the list
 """
+import math
+
 class Node:
 	def __init__(self, value=None, next_node=None):
 		self.value = value
@@ -90,26 +92,49 @@ class LinkedList:
 			return contains
 
 	def get_max(self):
-		pass
+		if self.head == None or self.tail == None:
+			return None
+
+		#set max to -inf
+		max = -math.inf
+
+		#Reference to the head as a starting point
+		node = self.head
+		
+		#While we're not at the tail:
+		while node.next_node != None:
+
+			# Check if any value is greater than the curr max	
+			if node.get_value() > max:
+				max = node.get_value()
+			
+			#Set the next node (note making this an else, makes it an infinite loop!!!!!)
+			node = node.get_next()
+
+		#Check the last node if it's a max
+		if self.tail.get_value() > max:
+			max = self.tail.get_value()
+		
+		return max
 
 
 
 #Testing consecutive additions to tail and printing them out:
-list = LinkedList()
-print('\n append a 1')
-list.add_to_tail(1)
-print(list.head.get_value())
+# list = LinkedList()
+# print('\n append a 1')
+# list.add_to_tail(1)
+# print(list.head.get_value())
 
-print('\n append a 9')
-list.add_to_tail(9)
-print(list.head.get_value())
-print(list.head.get_next().get_value())
+# print('\n append a 9')
+# list.add_to_tail(9)
+# print(list.head.get_value())
+# print(list.head.get_next().get_value())
 
-print('\n append a -34, and print them all out')
-list.add_to_tail(-34)
-print(list.head.get_value())
-print(list.head.get_next().get_value())
-print(list.head.get_next().get_next().get_value())
+# print('\n append a -34, and print them all out')
+# list.add_to_tail(-34)
+# print(list.head.get_value())
+# print(list.head.get_next().get_value())
+# print(list.head.get_next().get_next().get_value())
 
-print('\n check if it has 10')
-print(list.contains(9))
+# print('\n check if it has 10')
+# print(list.contains(9))

7c61d026839c18b2056b5e560e8a01b86af7216c
Austin Merando <ajmerando1@yahoo.com>
2018-09-05 15:33:34 -0700

    binary search done

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -1,14 +1,49 @@
 class BinarySearchTree:
-  def __init__(self, value):
-    self.value = value
-    self.left = None
-    self.right = None
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
 
-  def insert(self, value):
-    pass
+    def insert(self, value):
+        new_bst = BinarySearchTree(value)
+        current = self
+        put = False
 
-  def contains(self, target):
-    pass
+        while put is False:
+            if new_bst.value > current.value:
+                if current.right is None:
+                    current.right = new_bst
+                    put = True
+                else:
+                    current = current.right
+            elif new_bst.value < current.value:
+                if current.left is None:
+                    current.left = new_bst
+                    put = True
+                else:
+                    current = current.left
+            elif current.left is None and current.left is None:
+                current = new_bst
+                put = True
 
-  def get_max(self):
-    pass
+    def contains(self, target):
+        current = self
+        found = False
+        while found is False:
+            if current is None:
+                break
+            if target == current.value:
+                found = True
+            elif target > current.value:
+                current = current.right
+            elif target < current.value:
+                current = current.left
+        return found
+
+    def get_max(self):
+        current = self
+        while current is not None:
+            if current.right is None:
+                return current.value
+            else:
+              current = current.right

55022dbe467810f1dbbb6266be4700395fe1d434
Thomas Greenhalgh <thomasgreenhalgh@gmail.com>
2018-09-05 16:13:39 -0700

    finishes contains

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -18,14 +18,27 @@ class BinarySearchTree:
                 self.left.insert(value)
 
     def contains(self, target):
-        pass
+        if target == self.value:
+            return True
+        if target > self.value:
+            if self.right:
+                return self.right.contains(target)
+            else:
+                return False
+        if target < self.value:
+            if self.left:
+                return self.left.contains(target)
+            else:
+                return False
 
     def get_max(self):
         pass
 
 
-# bst = BinarySearchTree(5)
-# bst.insert(7)
+bst = BinarySearchTree(5)
+# print(bst.contains(5))
+bst.insert(7)
+print(bst.contains(7))
 # print(bst.right.value)
 # bst.insert(8)
 # print(bst.right.right.value)

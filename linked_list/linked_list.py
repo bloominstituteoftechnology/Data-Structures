131beb4dd0f160e577975ea33d9e7503d51017f7
Erin Meredith <ekmeredith@att.net>
2018-09-05 17:36:34 -0400

    working on linked_list; all tests failing

--- linked_list/linked_list.py
+++ linked_list/linked_list.py
@@ -23,13 +23,45 @@ class LinkedList:
     self.tail = None
 
   def add_to_tail(self, value):
-    pass
+    new_node = Node(data)
+    new_node.set_next(None)
+    self.tail = new_node
 
   def remove_head(self):
-    pass
+    current = self.head
+    previous = None
+    found = False
+    while current and found is False:
+      if current.get_value == value:
+        found = True
+      else:
+        previous = current
+        current = current.get_next()
+    if previous is None:
+      self.head = current.get_next()
+    else: previous.set_next(current.get_next())
+    return self.head
 
   def contains(self, value):
-    pass
+    current = self.head
+    found = False
+    while current and found is False:
+      if current.get_value() == value:
+        found = True
+      else:
+        current = current.get_next()
+    if current is None:
+      raise ValueError('Data not in list')
+    return current
 
   def get_max(self):
-    pass
+    current = self.head
+    found = False
+    while current and found is False:
+      if current.get_value() == max(LinkedList):
+        found = True
+      else:
+        current = current.get_next()
+    if current is None:
+      raise ValueError('Data not in list')
+    return current

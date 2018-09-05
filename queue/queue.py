17b55ce9eff2df694ac8a17c39b7bd7c4154bc3b
Austin Merando <ajmerando1@yahoo.com>
2018-09-05 14:28:16 -0700

    queues done

--- queue/queue.py
+++ queue/queue.py
@@ -2,16 +2,20 @@ import sys
 sys.path.append('../linked_list')
 from linked_list import LinkedList
 
+
 class Queue:
-  def __init__(self):
-    self.size = 0
-    self.storage = LinkedList()
+    def __init__(self):
+        self.size = 0
+        self.storage = LinkedList()
 
-  def enqueue(self, item):
-    self.size = self.size + 1
-  
-  def dequeue(self):
-    self.size = self.size - 1
+    def enqueue(self, item):
+        self.storage.add_to_tail(item)
+        self.size = self.size + 1
 
-  def len(self):
-    return self.size
+    def dequeue(self):
+        if self.size is not 0:
+            self.size = self.size - 1
+        return self.storage.remove_head()
+
+    def len(self):
+        return self.size

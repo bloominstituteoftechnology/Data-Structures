ea1b161c75c566c27841e455d6441145b4d6ec08
M.Real <mreal126@protonmail.com>
2018-09-05 18:40:26 -0400

    queue

--- queue/queue.py
+++ queue/queue.py
@@ -8,10 +8,14 @@ class Queue:
     self.storage = LinkedList()
 
   def enqueue(self, item):
-    pass
+    self.size += 1
+    self.storage.add_to_tail(item)
   
   def dequeue(self):
-    pass
+    if self.size > 0:
+        self.size -= 1
+        self.storage.remove_head
+    return self.storage.remove_head()    
 
   def len(self):
-    pass
+     return self.size

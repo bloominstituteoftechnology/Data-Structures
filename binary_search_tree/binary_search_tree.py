458c0691bbf5de30b912b84a14bc0a1f3161d8ba
cmok4290 <cmok4290@gmail.com>
2018-09-05 19:44:06 -0400

    edited insert method for BinarySearchTree
    - refactored LinkedList

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -5,10 +5,22 @@ class BinarySearchTree:
     self.right = None
 
   def insert(self, value):
-    pass
+    # `insert` adds the input value to the binary search tree, adhering to the rules of the ordering of elements in a binary search tree.
+    if value < self.value:
+      if self.left is None:
+        self.left = BinarySearchTree(value)
+      else:
+        self.left.insert(value)
+    elif value >= self.value:
+      if self.right is None:
+        self.right = BinarySearchTree(value)
+      else:
+        self.right.insert(value)
 
   def contains(self, target):
+    # `contains` searches the binary search tree for the input value, returning a boolean indicating whether the value exists in the tree or not.
     pass
 
   def get_max(self):
+    # `get_max` returns the maximum value in the binary search tree.
     pass
--- linked_list/linked_list.py
+++ linked_list/linked_list.py
@@ -34,31 +34,42 @@ class LinkedList:
   def remove_head(self):
     if self.head is None:
       return None
+
     current = self.head
+
+    if current == self.tail:
+      self.tail = None
+
     self.head = self.head.get_next()
-    self.tail = None
+
     return current.value
 
   def contains(self, value):
     if self.head is None:
       return None
+
     current = self.head
     found = False
+
     while current and found is False:
       if current.get_value() == value:
         found = True
       else:
         current = current.get_next()
+
     return found
 
   def get_max(self):
     if self.head is None:
       return None
+
     current = self.head
-    current_max = self.head.get_value()
+    current_max = current.get_value()
+
     while current is not None:
       if current.get_value() > current_max:
         current_max = current.get_value()
       else:
         current = current.get_next()
+
     return current_max

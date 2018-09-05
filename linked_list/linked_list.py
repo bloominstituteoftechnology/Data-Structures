3febcee76cad19fab87f515194a831e9214bf3d9
Alec <afroenygam@gmail.com>
2018-09-05 16:12:53 -0400

    Linked list updated

--- linked_list/linked_list.py
+++ linked_list/linked_list.py
@@ -23,15 +23,40 @@ class LinkedList:
     self.tail = None
 
   def add_to_tail(self, value):
-    values = Node(value, self.tail)
-    self.tail = values
-    return self.tail
+    new_node = Node(value)
+    if not self.head:
+        self.head = new_node
+        self.tail = new_node
+    else:
+        self.tail.set_next(new_node)
+        self.tail = new_node
 
   def remove_head(self):
-    pass
+    if self.head:
+        removed_head = self.head
+        self.head = self.head.get_next()
+        self.tail = None
+        return removed_head.value
 
   def contains(self, value):
-    pass
+    node_being_searched = self.head #We start by checking the head value
+    while node_being_searched:
+        if node_being_searched.value == value:
+            return True
+        node_being_searched = node_being_searched.get_next() #node to be searched updated to next node
+    return False
 
   def get_max(self):
-    pass
+    current_node = self.head
+    if not current_node:
+        return None
+
+    max_value = current_node.value
+
+    while current_node:
+        if current_node.value > max_value:
+            max_value = current_node.value
+
+        current_node = current_node.get_next()
+
+    return max_value

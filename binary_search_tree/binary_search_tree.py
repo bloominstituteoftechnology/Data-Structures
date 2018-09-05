a4f7c5a56abd51d4e0887e60c61dc4710919adb5
Simon <Simon_Freelance@hotmail.com>
2018-09-05 18:55:22 -0400

    Implemented binary search tree insert method

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -5,10 +5,33 @@ class BinarySearchTree:
     self.right = None
 
   def insert(self, value):
-    pass
-
+    newTreeNode = BinarySearchTree(value)
+    # From the root go to the left side of the tree
+    while True:
+      if self.left == None and self.value >= newTreeNode.value:
+        self.left = newTreeNode
+        return
+      
+      if self.right == None and self.value < newTreeNode.value:
+        self.right = newTreeNode
+        return
+      if self.value >= newTreeNode.value:
+        self = self.left
+      else:
+        self = self.right
+  
   def contains(self, target):
     pass
 
   def get_max(self):
     pass
+if __name__ == '__main__':
+  tree = BinarySearchTree(4)
+  tree.insert(3)
+  tree.insert(2)
+  tree.insert(7)
+  tree.insert(8)
+  tree.insert(5)
+  print(tree.right.left.value)
+
+  

978e87324a6af08f8a09bbc27c5231789ef2f370
TangledTessellations <mcevoya@oregonstate.edu>
2018-09-05 12:19:38 -0700

    Complete linked_list.py

--- linked_list/linked_list.py
+++ linked_list/linked_list.py
@@ -23,13 +23,43 @@ class LinkedList:
     self.tail = None
 
   def add_to_tail(self, value):
-    pass
+    # Check if first node
+    if not self.head:
+      self.head = self.tail = Node(value, None)
+    else:
+      newNode = Node(value, None)
+      self.tail.set_next(newNode)
+      self.tail = newNode
 
   def remove_head(self):
-    pass
+    # Check if no items
+    if not self.head:
+      return None
+    else:
+      # Check if only one item in list, and unlink tail
+      if self.head == self.tail:
+        self.tail = None
+      kill_node = self.head
+      self.head = self.head.get_next()
+      return kill_node.get_value()
 
   def contains(self, value):
-    pass
+    node = self.head
+    while node is not None:
+      if node.get_value() == value:
+        return True
+      else:
+        node = node.get_next()
+    return False
 
   def get_max(self):
-    pass
+    # Check if no nodes
+    if not self.head:
+      return None
+    max_val = self.head.get_value()
+    node = self.head
+    while node is not None:
+      if node.get_value() > max_val:
+        max_val = node.get_value()
+      node = node.get_next()
+    return max_val

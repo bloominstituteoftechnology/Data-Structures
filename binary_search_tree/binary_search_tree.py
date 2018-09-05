8019520145e6842aa17685b4ec23922b5c446968
Mark Hong <m4rkh0ng@gmail.com>
2018-09-05 19:33:24 -0400

    adds day 1 progress

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -5,10 +5,23 @@ class BinarySearchTree:
     self.right = None
 
   def insert(self, value):
-    pass
+    # adds the input value to the binary search tree, adhering to the rules of the ordering of elements in a binary search tree.
+    current = self
+    parent = None
+    while current:
+      parent = current
+      if value < current.value:
+        current = current.left
+      else:
+        current = current.right
+      # alternatively could be
+      # elif value > current.value:
+      #   current = current.right
 
   def contains(self, target):
+    # searches the binary search tree for the input value, returning a boolean indicating whether the value exists in the tree or not
     pass
 
   def get_max(self):
+    # returns the maximum value in the binary search tree
     pass

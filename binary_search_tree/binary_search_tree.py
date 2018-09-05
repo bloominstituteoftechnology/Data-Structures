2f2819c99426f6e960dbcb75d948c2427991ca8b
markdavidwilliams <markwilliams.aus@gmail.com>
2018-09-05 16:56:47 -0500

    completed binary search tree

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -5,10 +5,29 @@ class BinarySearchTree:
     self.right = None
 
   def insert(self, value):
-    pass
+    tree = BinarySearchTree(value)
+    if self.value > value:
+      if not self.left:
+        self.left = tree
+      else:
+        self.left.insert(value)
+    else:
+      if not self.right:
+        self.right = tree
+      else:
+        self.right.insert(value)
 
   def contains(self, target):
-    pass
+    if self.value == target:
+      return True
+    if self.value > target:
+      if self.left and self.left.contains(target):
+        return True
+    else:
+      if self.right and self.right.contains(target):
+        return True
 
   def get_max(self):
-    pass
+    if self.right:
+      return self.right.get_max()
+    return self.value

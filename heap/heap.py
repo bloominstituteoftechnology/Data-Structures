d917119bbf69abb5b392d648319b30004fa7d6a3
Thomas Greenhalgh <thomasgreenhalgh@gmail.com>
2018-09-06 16:55:58 -0700

    answers the questions

--- heap/heap.py
+++ heap/heap.py
@@ -31,17 +31,30 @@ class Heap:
                 self._bubble_up(parent)
             return
 
-    def _sift_down(self, index):
-        lChild = (2 * index) + 1
-        if lChild > self.get_size() - 1:
-            return
-        rChild = lChild + 1
-        if rChild > self.get_size() - 1:
-            return
+    # def _sift_down(self, index):
+    #     lChild = (2 * index) + 1
+    #     if lChild > self.get_size() - 1:
+    #         return
+    #     rChild = lChild + 1
+    #     if rChild > self.get_size() - 1:
+    #         return
 
-        if self.storage[index] < self.storage[lChild]:
-            self.storage[index], self.storage[lChild] = self.storage[lChild], self.storage[index]
-            self._sift_down(lChild)
-        if self.storage[index] < self.storage[rChild]:
-            self.storage[index], self.storage[rChild] = self.storage[rChild], self.storage[index]
-            self._sift_down(rChild)
+    #     if self.storage[index] < self.storage[lChild]:
+    #         self.storage[index], self.storage[lChild] = self.storage[lChild], self.storage[index]
+    #         self._sift_down(lChild)
+    #     if self.storage[index] < self.storage[rChild]:
+    #         self.storage[index], self.storage[rChild] = self.storage[rChild], self.storage[index]
+    #         self._sift_down(rChild)
+
+    def _sift_down(self, index):
+        while index * 2 + 1 <= len(self.storage) - 1:
+            mc = self._max_child(index)
+            if self.storage[index] < self.storage[mc]:
+                self.storage[index], self.storage[mc] = self.storage[mc], self.storage[index]
+            index = mc
+
+    def _max_child(self, index):
+        if index * 2 + 2 > len(self.storage) - 1:
+            return index * 2 + 1
+        else:
+            return index * 2 + 1 if self.storage[index * 2 + 1] > self.storage[index * 2 + 2] else index * 2 + 2

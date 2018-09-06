893b416bc52895fa985b43266197ee5b5cc53923
noxasaxon <34722463+noxasaxon@users.noreply.github.com>
2018-09-05 23:54:51 -0700

    doubly linked, need to fix lines 11 and 18

--- doubly_linked_list/doubly_linked_list.py
+++ doubly_linked_list/doubly_linked_list.py
@@ -4,37 +4,96 @@ class ListNode:
     self.prev = prev
     self.next = next
 
+# Insert the given value as this node's
+#   `next` node
   def insert_after(self, value):
-    pass
+    if self.next: self.next.prev = value
+    value.next = self.next
+    self.next = value
 
+# Insert the given value as the this node's
+#   `prev` node
   def insert_before(self, value):
-    pass
+    if self.prev: self.prev.next = value
+    value.prev = self.prev
+    self.prev = value
 
+# Delete this node (returns this node)
   def delete(self):
-    pass
+    if self.prev: self.prev.next = self.next
+    if self.next: self.next.prev = self.prev
+    return self.value
 
 class DoublyLinkedList:
   def __init__(self, node=None):
     self.head = node
     self.tail = node
 
+# Adds the given value as the new head
+#   node of the list
   def add_to_head(self, value):
-    pass
+    if not self.head:
+      newNode = ListNode(value)
+      self.head = newNode
+      self.tail = newNode
+    else:
+      newNode = ListNode(value, None, self.head)
+      if not self.tail: self.tail = newNode
+      self.head.insert_before(newNode)
+      self.head = newNode
 
+# Remove the list's current head. The list's
+#   `head` pointer should point to the removed node's
+#   `next` node
   def remove_from_head(self):
-    pass
+    if not self.head: return None
+    retVal = self.head.value
+    if not self.head.next:
+      self.head = self.head.next
+    else:
+      self.head.delete()
+      self.head = None
+    return retVal
 
+# Adds the given value as the new tail
+#   node of the list
   def add_to_tail(self, value):
-    pass
+    if not self.tail:
+      newNode = ListNode(value)
+      self.tail = newNode
+      self.head = newNode
+    else:
+      newNode = ListNode(value, self.tail, None)
+      if not self.head: self.head = newNode
+      self.tail.insert_after(newNode)
+      self.tail = newNode
 
+# Remove the list's current tail. The list's
+#   `tail` pointer should point to the removed node's
+#   `prev` node
   def remove_from_tail(self):
-    pass
+    if not self.tail: return None
+    retVal = self.tail.value
+    if self.tail.prev: self.tail = self.tail.prev
+    else:
+      self.tail.delete()
+      self.tail = None
+    return retVal
 
+# Move the given node to the front of the
+#   list. Update the list's `head` pointer
+#   accordingly
   def move_to_front(self, node):
-    pass
+    if node == self.tail: self.remove_from_tail()
+    if node == self.head: self.remove_from_head()
+    node.delete()
+    self.add_to_head(node.value)
 
   def move_to_end(self, node):
-    pass
+    if node == self.tail: self.remove_from_tail()
+    if node == self.head: self.remove_from_head()
+    node.delete()
+    self.add_to_tail(node.value)
 
   def delete(self, node):
-    pass
+    node.delete()

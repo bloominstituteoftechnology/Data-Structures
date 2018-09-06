3eb23add0b3e48279d91acd0b2210a388aca2b82
Dil Sombayeva <dilsombayeva@KevinsMacBook2.attlocal.net>
2018-09-06 15:17:48 -0700

    still working on heap

--- heap/heap.py
+++ heap/heap.py
@@ -5,10 +5,12 @@ class Heap:
   def insert(self, value):
     self.storage.append(value)
     index = len(self.storage) - 1
+    # or  index = self.get_size() - 1
     self._bubble_up(index)
 
   def delete(self):
     self.storage[0]= self.storage[len(self.storage) - 1]
+    # self.storage[0]= self.storage[self.get_size() - 1]
     self._sift_down(0)
 
 
@@ -23,14 +25,16 @@ class Heap:
         if self.storage[(index - 1) // 2] < self.storage[index]:
             self.storage[(index - 1) // 2] = self.storage[index]
             self.storage[index] = self.storage[(index - 1) // 2]
-        
-            index = (index - 1) // 2
+        else:
+            break
+        index = (index - 1) // 2
 
   def _sift_down(self, index):
-    #   while((index*2)+1, 2) <= len(self.storage) - 1:
-    #     if self.storage[(index*2)+1, 2)] > self.storage[index]:
-    #         self.storage[(index*2)+1, 2)] = self.storage[index]
-    #         self.storage[index] = self.storage[(index*2)+1, 2)]
-
-    #         index = (index*2) + 1
-    pass
+      while((index*2)+1) <= len(self.storage) - 1:
+        if self.storage[(index*2)+1] > self.storage[index]:
+            self.storage[(index*2)+1] = self.storage[index]
+            self.storage[index] = self.storage[(index*2)+1]
+        else:
+            break
+        index = (index*2) + 1
+    

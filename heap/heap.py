2737e528a85b40448aa8a13666e79256f57be9cc
markdavidwilliams <markwilliams.aus@gmail.com>
2018-09-06 17:33:47 -0500

    completed heap

--- heap/heap.py
+++ heap/heap.py
@@ -1,6 +1,6 @@
 class Heap:
   def __init__(self):
-    self.storage = []
+    self.storage = [0]
     self.size = 0
 
   def insert(self, value):
@@ -9,36 +9,31 @@ class Heap:
     self._bubble_up(self.size)
 
   def delete(self):
-    pass
+    item = self.storage[1]
+    self.storage[1] = self.storage[self.size]
+    self.size -= 1
+    self.storage.pop()
+    self._sift_down(1)
+    return item
 
   def get_max(self):
-    pass
+    return self.storage[1]
 
   def get_size(self):
-    pass
+    return self.size
 
   def _bubble_up(self, index):
-    while index // 2 > 0:
-      if self.storage[index] < self.storage[index // 2]:
+    while (index // 2) > 0:
+      if self.storage[index // 2] < self.storage[index]:
         tmp = self.storage[index // 2]
         self.storage[index // 2] = self.storage[index]
         self.storage[index] = tmp
       index = index // 2
 
   def _sift_down(self, index):
-    while (index * 2) <= self.size:
-      mc = self.min_child(index)
-      if self.storage[index] > self.storage[mc]:
-        tmp = self.storage[index]
-        self.storage[index] = self.storage[mc]
-        self.storage[mc] = tmp
-      index = mc
-  
-  def min_child(self, index):
-    if index * 2 + 1 > self.size:
-      return index * 2
-    else:
-      if self.storage[index * 2] < self.storage[index * 2 + 1]:
-        return index * 2
-      else:
-        return index * 2 + 1
+    if self.size > 1:
+      for i, val in enumerate(self.storage):
+        if self.storage[index] < val:
+          item = self.storage[index]
+          self.storage[index] = val
+          self.storage[i] = item

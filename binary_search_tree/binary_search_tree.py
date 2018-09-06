1fb75f99a707015e04ed813ee066666de8625d15
Kevin Tena <kevten09@gmail.com>
2018-09-06 16:59:47 -0500

    Binary search tree solution and tests passed

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -18,7 +18,21 @@ class BinarySearchTree:
         self.right.insert(value)
 
   def contains(self, target):
-    pass
+    if self.value == target:
+      return True
+    
+    elif self.right == None and self.left == None:
+      return False
+
+    elif self.value > target:
+       return self.left.contains(target)
+      
+    else:
+       return self.right.contains(target)
+       
 
   def get_max(self):
-    pass
+    if self.right:
+      return self.right.get_max()
+    else:
+      return self.value

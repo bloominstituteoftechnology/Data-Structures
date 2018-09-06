5aa7439459f777f45819cdfd148bee4644b052b1
TangledTessellations <mcevoya@oregonstate.edu>
2018-09-06 15:15:06 -0700

    Complete heap

--- heap/heap.py
+++ heap/heap.py
@@ -3,19 +3,45 @@ class Heap:
     self.storage = []
 
   def insert(self, value):
-    pass
+    self.storage.append(value)
+    self._bubble_up(len(self.storage) - 1)
 
   def delete(self):
-    pass
+    first = self.storage.pop(0)
+    self.storage = self.storage[-1:] + self.storage[:-1]
+    self._sift_down(0)
+    return first
 
   def get_max(self):
-    pass
+    return self.storage[0]
 
   def get_size(self):
-    pass
+    return len(self.storage)
 
   def _bubble_up(self, index):
-    pass
+    parent_index = (index - 1) // 2
+    if index <= 0:
+      return True
+    elif self.storage[parent_index] < self.storage[index]:
+      self.storage[index], self.storage[parent_index] = self.storage[parent_index], self.storage[index]
+      self._bubble_up(parent_index)
 
   def _sift_down(self, index):
-    pass
+    leftchild = index * 2 + 1
+    rightchild = index * 2 + 2
+    newindex = index
+
+    if leftchild <= len(self.storage) - 1:
+      left_child_val = self.storage[leftchild]
+      if left_child_val > self.storage[newindex]:
+        newindex = leftchild
+    if rightchild <= len(self.storage) - 1:
+      right_child_val = self.storage[rightchild]
+      if right_child_val > self.storage[newindex]:
+        newindex = rightchild
+    if newindex is not index:
+      self.storage[index], self.storage[newindex] = self.storage[newindex], self.storage[index]
+      self._sift_down(newindex)
+  
+
+

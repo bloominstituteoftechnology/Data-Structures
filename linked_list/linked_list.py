a04bcfae70e61cf89dd8e214e9c9322d957eb8ec
Adrian Adames <adrianadames@gmail.com>
2018-09-06 01:26:49 -0400

    Went back to linked_list.py in order to fix the code so that it passes the tests, as queue.py depends on the correctness of the LinkedList class. A few tests still don't work. The get_max method I wrote makes the test_linked_list.py program hang up.

--- linked_list/linked_list.py
+++ linked_list/linked_list.py
@@ -17,6 +17,147 @@ class Node:
   def set_next(self, new_next):
     self.next_node = new_next
 
+
+#ATTEMPT #2
+class LinkedList:
+  def __init__(self):
+    self.head = None
+    self.tail = None
+
+  def add_to_tail(self, value):
+    newNode = Node(value)
+    if self.head == None:
+      self.head = newNode
+      self.tail = newNode
+    elif self.head:
+      newNode.set_next(None)
+      self.tail.set_next(newNode)
+      self.tail = newNode
+
+  def remove_head(self):
+    if self.head == None:
+      # print('There is no head to remove.')
+      return False
+    if self.head.get_next() == None:
+      self.head = None
+    elif self.head:
+      self.head = self.head.get_next()
+
+  
+  def contains(self,value):
+    currentNode = self.head
+    foundNode = False
+    while currentNode and foundNode is False:
+      if currentNode.get_value() == value:
+        foundNode = True
+        # print(f'Linked list contains value {value}')
+        return True
+      else:
+        currentNode = currentNode.get_next()
+    if currentNode == None:
+      # print(f'Linked list does not contain a value of {value}')
+      return False
+
+  def get_max(self):
+    currentNode = self.head
+    if currentNode == None:
+      return None
+    if currentNode.get_next() == None:
+      # maximum = currentNode.get_value()
+      maximum = currentNode.value
+      return maximum
+    else:
+      while currentNode.get_next():
+        # if currentNode.get_value() > currentNode.get_next().get_value():
+        if currentNode.value > currentNode.get_next().value:
+          # maximum = currentNode.getValue()
+          maximum = currentNode.value
+        else:
+          # maximum = currentNode.get_next().get_value()
+          maximum = currentNode.get_next().value
+          currentNode = currentNode.get_next()
+    return maximum
+
+
+
+
+# ######ATTEMPT 2 tests
+# list1 = LinkedList()
+
+# list1.add_to_tail(10)
+# list1.add_to_tail(20)
+# list1.add_to_tail(30)
+# list1.add_to_tail(40)
+# print(list1.get_max())
+# print(list1.contains(20))
+# print(list1.head.value)
+# list1.remove_head()
+# print(list1.head.value)
+
+
+
+
+
+
+
+
+
+#To add an element to the head of a list, we create a new node, set its element
+#to the new element, set its next link to refer to the current head, and then set 
+#the list's head to point to the new node. 
+
+
+
+
+
+# #ATTEMPT #1
+# class LinkedList:
+#   def __init__(self):
+#     self.head = None
+#     self.tail = None
+
+#   def add_to_tail(self, value):
+#     # newNode = Node(value)
+#     newNode = value
+#     if self.head == None:
+#       self.head = newNode
+#       self.tail = newNode
+#     elif self.head:
+#       newNode.set_next(None)
+#       self.tail.set_next(newNode)
+#       self.tail = newNode
+
+#   def remove_head(self):
+#     if self.head == None:
+#       print('There is no head to remove.')
+#     elif self.head:
+#       self.head = self.head.get_next()
+#     pass
+  
+#   def contains(self,value):
+#     currentNode = self.head
+#     foundNode = False
+#     while currentNode and foundNode is False:
+#       if currentNode.get_value() == value:
+#         foundNode = True
+#         return f'Linked list contains value {value}'
+#       else:
+#         currentNode = currentNode.get_next()
+#     if currentNode == None:
+#       return 'Value not in list.'
+
+#   def get_max(self):
+#     currentNode = self.head
+#     while currentNode.get_next():
+#       if currentNode.get_value() > currentNode.get_next().get_value():
+#         maximum = currentNode.getValue()
+#       else:
+#         maximum = currentNode.get_next().get_value()
+#         currentNode = currentNode.get_next()
+#     return maximum
+#
+#
+#
 # node4 = Node(798)
 # node3 = Node(757)
 # node2 = Node(364)
@@ -29,85 +170,3 @@ class Node:
 
 # node3.set_next(node4)
 # print(Node.get_value(Node.get_next(node3)))
-
-
-
-class LinkedList:
-  def __init__(self):
-    self.head = None
-    self.tail = None
-
-  def add_to_tail(self, value):
-    # newNode = Node(value)
-    newNode = value
-    if self.head == None:
-      self.head = newNode
-      self.tail = newNode
-    elif self.head:
-      newNode.set_next(None)
-      self.tail.set_next(newNode)
-      self.tail = newNode
-
-  def remove_head(self):
-    if self.head == None:
-      print('There is no head to remove.')
-    elif self.head:
-      self.head = self.head.get_next()
-    pass
-  
-  def contains(self,value):
-    currentNode = self.head
-    foundNode = False
-    while currentNode and foundNode is False:
-      if currentNode.get_value() == value:
-        foundNode = True
-        return f'Linked list contains value {value}'
-      else:
-        currentNode = currentNode.get_next()
-    if currentNode == None:
-      return 'Value not in list.'
-
-  def get_max(self):
-    currentNode = self.head
-    while currentNode.get_next():
-      if currentNode.get_value() > currentNode.get_next().get_value():
-        maximum = currentNode.getValue()
-      else:
-        maximum = currentNode.get_next().get_value()
-        currentNode = currentNode.get_next()
-    return maximum
-
-
-# node4 = Node(798)
-# node3 = Node(757)
-# node2 = Node(364)
-# node1 = Node(133)
-
-
-# list1 = LinkedList()
-# list1.add_to_tail(node1)
-# # print(list1.tail.value)
-# list1.add_to_tail(node2)
-# # print(list1.tail.value)
-# list1.add_to_tail(node3)
-# # print(list1.tail.value)
-
-# # list1.remove_head()
-# # print(list1.head.value)
-# # list1.remove_head()
-# # print(list1.head.value)
-
-# print(list1.get_max())
-# print(list1.contains(133))
-
-
-# # print(list1.contains(134))
-# # print(list1.contains(133))
-
-# print(list1.get_max())
-
-
-#To add an element to the head of a list, we create a new node, set its element
-#to the new element, set its next link to refer to the current head, and then set 
-#the list's head to point to the new node. 
-

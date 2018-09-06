d432947abfca4a251bd6171c11231a464901f8cf
noxasaxon <34722463+noxasaxon@users.noreply.github.com>
2018-09-06 16:42:13 -0700

    heap done

--- heap/heap.py
+++ heap/heap.py
@@ -29,8 +29,26 @@ class Heap:
       #check if element's parent's value is less than current value
       if self.storage[(index - 1) // 2] < self.storage[index]:
         #if true swap both values
-        self.storage[index], self.storage[(index - 1) // 2] = self.storage[(index - 1) //2]
+        self.storage[index], self.storage[(index - 1) // 2] = self.storage[(index - 1) //2], self.storage[index]
       index = (index - 1) // 2
 
   def _sift_down(self, index):
-    pass
+    # in while loop, keep sifting the element at the given index down the heap until correct
+    while (index * 2 + 1) <= len(self.storage) -1:
+      #figure out wich of the cur node's two children has larger value
+      mc = self._max_child(index)
+      #if the cur node's value is less than the larger child node's value
+      if self.storage[index] < self.storage[mc]:
+        #swap them
+        self.storage[index], self.storage[mc] = self.storage[mc], self.storage[index]
+      #update index
+      index = mc
+  
+  def _max_child(self, index):
+    #if the right child index is out of bounds of storage array
+    if index * 2 + 2 > len(self.storage) -1:
+      #return index of left child
+      return index * 2 + 1
+    #else, return index of child node with larger value
+    else:
+      return index * 2 + 1 if self.storage[index*2+1] > self.storage[index*2 + 2] : return index * 2 _ 2

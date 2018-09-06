f95b0ae0108f353aa10b9ebaac59d8b2c42871db
cmok4290 <cmok4290@gmail.com>
2018-09-06 16:03:53 -0400

    edited methods for Heap
    - edited insert, getmax, getsize, and bubbleup

--- heap/heap.py
+++ heap/heap.py
@@ -3,19 +3,25 @@ class Heap:
     self.storage = []
 
   def insert(self, value):
-    pass
+    self.storage.append(value)
+    self._bubble_up(self.get_size() - 1)
 
   def delete(self):
     pass
 
   def get_max(self):
-    pass
+    return self.storage[self.get_size() - 1]
 
   def get_size(self):
-    pass
+    return len(self.storage)
 
   def _bubble_up(self, index):
-    pass
+    while index // 2 > 0:
+      if self.storage[index] < self.storage[index // 2]:
+        temp = self.storage[index // 2]
+        self.storage[index // 2] = self.storage[index]
+        self.storage[index] = temp
+      index = index // 2
 
   def _sift_down(self, index):
     pass

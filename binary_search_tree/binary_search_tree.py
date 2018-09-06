ca2e0dd66d3beef1c097a12f7b087b3a73d8fd16
Nicky Chen <nicky.chen0@gmail.com>
2018-09-06 15:48:25 -0700

    binary & heap done

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -1,14 +1,34 @@
 class BinarySearchTree:
-  def __init__(self, value):
-    self.value = value
-    self.left = None
-    self.right = None
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
 
-  def insert(self, value):
-    pass
+    def insert(self, value):
+        newTree = BinarySearchTree(value)
+        if self.value > value:
+            if not self.left:
+                self.left = newTree
+            else:
+                self.left.insert(newTree)
+        else:
+            if not self.right:
+                self.right = newTree
+            else:
+                self.right.insert(newTree)
 
-  def contains(self, target):
-    pass
+    def contains(self, target):
+        if target == self.value:
+            return True
+        if target > self.value:
+            if self.right and self.right.contains(target):
+                return True
+        else:
+            if self.left and self.left.contains(target):
+                return True
 
-  def get_max(self):
-    pass
+    def get_max(self):
+        if self.right:
+            return self.right.get_max()
+        else:
+            return self.value
--- heap/heap.py
+++ heap/heap.py
@@ -1,21 +1,47 @@
+import math
+
+
 class Heap:
-  def __init__(self):
-    self.storage = []
+    def __init__(self):
+        self.storage = []
 
-  def insert(self, value):
-    pass
+    def insert(self, value):
+        self.storage.append(value)
+        value_index = self.get_size() - 1
+        self._bubble_up(value_index)
 
-  def delete(self):
-    pass
+    def delete(self):
+        to_delete = self.storage[0]
+        popped = self.storage.pop()
+        if self.get_size() > 0:
+            self.storage[0] = popped
+            self._sift_down(0)
+        return to_delete
 
-  def get_max(self):
-    pass
+    def get_max(self):
+        return self.storage[0]
 
-  def get_size(self):
-    pass
+    def get_size(self):
+        return len(self.storage)
 
-  def _bubble_up(self, index):
-    pass
+    def _bubble_up(self, index):
+        parent = (index - 1) // 2
+        if self.storage[parent] < self.storage[index]:
+            self.storage[parent], self.storage[index] = self.storage[index], self.storage[parent]
+            if parent != 0:
+                self._bubble_up(parent)
+            return
 
-  def _sift_down(self, index):
-    pass
+    def _sift_down(self, index):
+        left_child = 2 * index + 1
+        right_child = 2 * index + 2
+        if left_child > self.get_size() - 1:
+            return
+        if right_child > self.get_size() - 1:
+            return
+        if self.storage[left_child] > self.storage[index]:
+            self.storage[index], self.storage[left_child] = self.storage[left_child], self.storage[index]
+            self._sift_down(left_child)
+        if self.storage[right_child] > self.storage[index]:
+            self.storage[index], self.storage[right_child] = self.storage[right_child], self.storage[index]
+            self._sift_down(right_child)

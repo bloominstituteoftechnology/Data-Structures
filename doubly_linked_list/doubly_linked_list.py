0bc9b6d006d2c7a5eea675fe9426e5e0c56bdbe7
emaquino44 <prexy44@yahoo.com>
2018-09-06 15:32:35 -0700

    DataStructures_questions.md - Completed doubly linked_list questions

--- doubly_linked_list/doubly_linked_list.py
+++ doubly_linked_list/doubly_linked_list.py
@@ -5,13 +5,16 @@ class ListNode:
     self.next = next
 
   def insert_after(self, value):
-    pass
+      self.next = ListNode(value, self, self.next)
 
   def insert_before(self, value):
-    pass
+      self.prev = ListNode(value, self.prev, self)
 
   def delete(self):
-    pass
+    if self.prev != None:
+      self.prev.next = self.next
+    if self.next != None:
+      self.next.prev = self.prev
 
 class DoublyLinkedList:
   def __init__(self, node=None):
@@ -19,22 +22,50 @@ class DoublyLinkedList:
     self.tail = node
 
   def add_to_head(self, value):
-    pass
+    if self.head != None:
+      self.head.insert_before(value)
+      self.head = self.head.prev
+    else:
+      self.head = ListNode(value)
 
   def remove_from_head(self):
-    pass
+    if self.head != None:
+        currentHead = self.head
+        self.head = self.head.next
+        if currentHead == self.tail:
+          self.tail = None
+        return currentHead.value
+    else:
+        return None
 
   def add_to_tail(self, value):
-    pass
+    if self.tail != None:
+      self.tail.insert_after(value)
+      self.tail = self.tail.next
+    else:
+      self.tail = ListNode(value)
 
   def remove_from_tail(self):
-    pass
+    if self.tail != None:
+        currentTail = self.tail
+        self.tail = self.tail.prev
+        if currentTail == self.head:
+          self.head = None
+        return currentTail.value
+    else:
+        return None
 
-  def move_to_front(self, node):
-    pass
+  def move_to_front(self, node): #1 -> 2 -> 3 -> 4 -> 5
+    if self.tail == node:
+      value = self.tail.value
+      self.tail.delete()
+      self.add_to_head(value)
 
   def move_to_end(self, node):
-    pass
+    if self.head == node:
+      value = self.head.value
+      self.head.delete()
+      self.add_to_tail(value)
 
   def delete(self, node):
     pass

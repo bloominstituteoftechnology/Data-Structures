dce0e190f83917b5ac372645f348691950e882cb
M.Real <mreal126@protonmail.com>
2018-09-06 16:53:08 -0400

    queue no longer working starting at test_queue.py line 34 105 != 100. Will fix later

--- queue/queue.py
+++ queue/queue.py
@@ -1,20 +1,19 @@
 import sys
-from linked_list import LinkedList
 sys.path.append('../linked_list')
+from linked_list import LinkedList
 class Queue:
   def __init__(self):
     self.size = 0
     self.storage = LinkedList()
 
   def enqueue(self, item):
-    self.size += 1
     self.storage.add_to_tail(item)
+    self.size += 1
   
   def dequeue(self):
-    if self.size > 0:
+    if self.size is not 0:
       self.size -= 1
-      self.storage.remove_head
-    return self.storage.remove_head()    
+      return self.storage.remove_head()    
 
   def len(self):
     return self.size

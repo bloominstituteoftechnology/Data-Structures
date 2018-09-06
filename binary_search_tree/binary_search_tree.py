87e245d92cc386846c46abf391396d521212ed25
sean chavez <seanchavez@seanchavezsMBP.home>
2018-09-06 13:21:50 -0700

    Implement insert method in BinarySearchTree

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -1,14 +1,26 @@
 class BinarySearchTree:
-  def __init__(self, value):
-    self.value = value
-    self.left = None
-    self.right = None
-
-  def insert(self, value):
-    pass
-
-  def contains(self, target):
-    pass
-
-  def get_max(self):
-    pass
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+  
+    def insert(self, value):
+        if self.value is None:
+            self.value = value
+        else:
+            if self.value < value:
+                if self.right is None:
+                    self.right = BinarySearchTree(value)
+                else:
+                    insert(self.right, value)
+            else:
+                if self.left is None:
+                    self.left = BinarySearchTree(value)
+                else:
+                    insert(self.left, value) 
+  
+    def contains(self, target):
+        pass
+  
+    def get_max(self):
+        pass

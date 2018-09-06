d03aa49290bfa8d05c0a19160bb5b188a8dc3225
Adrian Adames <adrianadames@gmail.com>
2018-09-06 00:07:32 -0400

    queue.py in progress.

--- queue/queue.py
+++ queue/queue.py
@@ -1,17 +1,33 @@
 import sys
 sys.path.append('../linked_list')
 from linked_list import LinkedList
+from linked_list import Node
 
 class Queue:
   def __init__(self):
     self.size = 0
     self.storage = LinkedList()
 
-  def enqueue(self, item):
+  def enqueue(self, item): #INSERT operation
+    self.storage.add_to_tail(item)
     pass
   
-  def dequeue(self):
+  def dequeue(self): #DELETE operation
+    self.storage.remove_head()
     pass
 
   def len(self):
     pass
+
+
+
+queue1 = Queue()
+
+queue1.enqueue(Node(1))
+queue1.enqueue(Node(5))
+
+print(queue1.storage.head.value)
+print(queue1.storage.tail.value)
+queue1.dequeue()
+
+print()

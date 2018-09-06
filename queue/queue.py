63807df7a2c0f1bcb48cd3c77012a511f56d0946
Adrian Adames <adrianadames@gmail.com>
2018-09-06 01:52:57 -0400

    Done with queue.py. Passed all the tests. Working on fixing linked_list as I think I figured out my issue in working on the queue data structure.

--- queue/queue.py
+++ queue/queue.py
@@ -13,21 +13,51 @@ class Queue:
     pass
   
   def dequeue(self): #DELETE operation
-    self.storage.remove_head()
-    pass
+    if self.storage.head == None:
+      return None
+    else:
+      oldHead = self.storage.head.value
+      self.storage.remove_head()
+    return oldHead
 
   def len(self):
-    pass
+    currentNode = self.storage.head
+    if currentNode == None:
+      self.size = 0
+      return self.size
+    if currentNode.get_next() == None:
+      self.size = 1
+      return self.size
+    else:
+      counter = 1
+      while currentNode.get_next() != None:
+        counter += 1
+        currentNode = currentNode.get_next()
+      self.size = counter
+      return self.size
 
 
 
-queue1 = Queue()
 
-queue1.enqueue(Node(1))
-queue1.enqueue(Node(5))
 
-print(queue1.storage.head.value)
-print(queue1.storage.tail.value)
-queue1.dequeue()
+# queue1 = Queue()
 
-print()
+# queue1.enqueue(1)
+# queue1.enqueue(5)
+# queue1.enqueue(15)
+# queue1.enqueue(54)
+
+# print(queue1.storage.head.value)
+# print(queue1.storage.tail.value)
+# queue1.dequeue()
+
+# print(queue1.storage.head.value)
+
+# print(queue1.len())
+
+
+# queue1.enqueue(100)
+# queue1.enqueue(101)
+# queue1.enqueue(105)
+# print(queue1.dequeue())
+# assertEqual(queue1.dequeue(), 100)

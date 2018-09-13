09368ca3f7416d068678fbe5f112279b0d3e5830
Daniel Chamorro <dan@dcitllc.com>
2018-09-13 12:06:43 -0400

    Binary Search Tree: insert

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -1,14 +1,23 @@
 class BinarySearchTree:
-  def __init__(self, value):
-    self.value = value
-    self.left = None
-    self.right = None
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
 
-  def insert(self, value):
-    pass
+    def insert(self, value):
+        if value >= self.value:
+            if (not self.right):
+                self.right = BinarySearchTree(value)
+            else:
+                self.right.insert(value)
+        else:
+            if (not self.left):
+                self.left = BinarySearchTree(value)
+            else:
+                self.left.insert(value)
 
-  def contains(self, target):
-    pass
+    def contains(self, target):
+        pass
 
-  def get_max(self):
-    pass
+    def get_max(self):
+        pass

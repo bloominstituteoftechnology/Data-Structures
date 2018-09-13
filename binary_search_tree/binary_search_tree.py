bede5e0f896e63bd4e96b09666445380002e92b2
Adrian Adames <adrianadames@gmail.com>
2018-09-13 01:34:52 -0400

    Done with binary_search_tree. Passed all tests.

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -5,10 +5,50 @@ class BinarySearchTree:
     self.right = None
 
   def insert(self, value):
-    pass
+    if self.value:
+      if value < self.value:
+        if self.left is None:
+          self.left = BinarySearchTree(value)
+        else:
+          self.left.insert(value)
+      elif value > self.value:
+        if self.right is None:
+          self.right = BinarySearchTree(value)
+        else:
+          self.right.insert(value)
+    else:
+       self.value = value
 
   def contains(self, target):
-    pass
+    if target == self.value:
+      return True
+    elif target < self.value and self.left:
+      return self.left.contains(target)
+    elif target > self.value and self.right:
+      return self.right.contains(target)
+    return False
 
   def get_max(self):
-    pass
+    if self.value:
+      if self.right is None:
+        return self.value
+      else:
+        return self.right.get_max()
+    else:
+      return False
+
+
+
+# root = BinarySearchTree(5)
+
+
+# root.insert(10)
+# root.insert(4)
+# root.insert(3)
+# root.insert(39)
+# root.insert(30)
+# root.insert(60)
+# root.insert(47)
+
+# print(root.contains(47))
+# print(root.get_max())

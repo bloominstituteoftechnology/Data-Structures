c9abe4930f0b8104b1dc06147baa4a1aeecfc08b
Randy Calderon <randycweb@gmail.com>
2018-09-12 20:26:16 -0700

    Added remove_head

--- linked_list/linked_list.py
+++ linked_list/linked_list.py
@@ -32,11 +32,31 @@ class LinkedList:
       self.tail = new_tail
 
   def remove_head(self):
-    
+      value = self.head.get_value()
+      
+      if self.head:
+        next_node = self.head.get_next()
+        self.head = next_node
+        self.tail = next_node
+        self.head.set_next(next_node)
+      else:
+        return value
+
+  def print_values(self):
+    if self.head == None:
+      return
+    node = self.head
+    while True:
+      print(node.get_value())
+      if node.next_node == None:
+        break
+      node = node.get_next()
+
+list = LinkedList()
+print(list.print_values())
+  
+def contains(self, value):
     pass
 
-  def contains(self, value):
-    pass
-
-  def get_max(self):
+def get_max(self):
     pass

d84ed02ef903916a0044c2065ae396a17e8222a3
Adrian Adames <adrianadames@gmail.com>
2018-09-26 23:04:35 -0400

    Attempted linked_list data structure with minimum resources. Almost got it. Need to address problems that come up in the tests.

--- linked_list/linked_list.py
+++ linked_list/linked_list.py
@@ -1,81 +1,83 @@
-"""
-Class that represents a single linked
-list node that holds a single value
-and a reference to the next node in the list
-"""
-class Node:
-  def __init__(self, value=None, next_node=None):
-    self.value = value
-    self.next_node = next_node
+# """
+# Class that represents a single linked
+# list node that holds a single value
+# and a reference to the next node in the list
+# """
+# class Node:
+#   def __init__(self, value=None, next_node=None):
+#     self.value = value
+#     self.next_node = next_node
 
-  def get_value(self):
-    return self.value
+#   def get_value(self):
+#     return self.value
 
-  def get_next(self):
-    return self.next_node
+#   def get_next(self):
+#     return self.next_node
 
-  def set_next(self, new_next):
-    self.next_node = new_next
+#   def set_next(self, new_next):
+#     self.next_node = new_next
 
 
-#ATTEMPT #2
-class LinkedList:
-  def __init__(self):
-    self.head = None
-    self.tail = None
+# #ATTEMPT #2
+# class LinkedList:
+#   def __init__(self):
+#     self.head = None
+#     self.tail = None
 
-  def add_to_tail(self, value):
-    newNode = Node(value)
-    if self.head == None:
-      self.head = newNode
-      self.tail = newNode
-    elif self.head:
-      newNode.set_next(None)
-      self.tail.set_next(newNode)
-      self.tail = newNode
+#   def add_to_tail(self, value):
+#     newNode = Node(value)
+#     if self.head == None:
+#       self.head = newNode
+#       self.tail = newNode
+#     elif self.head:
+#       newNode.set_next(None)
+#       self.tail.set_next(newNode)
+#       self.tail = newNode
 
-  def remove_head(self):
-    if self.head == None:
-      print('There is no head to remove.')
-      self.tail = None
-      return None
-    elif self.head.get_next() == None:
-      oldHead = self.head
-      self.head = None
-      self.tail = None
-      return oldHead.value
-    elif self.head:
-      oldHead = self.head
-      self.head = self.head.get_next()
-      return oldHead.value
+#   def remove_head(self):
+#     if self.head == None:
+#       print('There is no head to remove.')
+#       self.tail = None
+#       return None
+#     elif self.head.get_next() == None:
+#       oldHead = self.head
+#       self.head = None
+#       self.tail = None
+#       return oldHead.value
+#     elif self.head:
+#       oldHead = self.head
+#       self.head = self.head.get_next()
+#       return oldHead.value
 
-  def contains(self,value):
-    currentNode = self.head
-    foundNode = False
-    while currentNode and foundNode is False:
-      if currentNode.get_value() == value:
-        foundNode = True
-        print(f'Linked list contains value {value}')
-        return True
-      else:
-        currentNode = currentNode.get_next()
-    if currentNode == None:
-      print(f'Linked list does not contain a value of {value}')
-      return False
+#   def contains(self,value):
+#     currentNode = self.head
+#     foundNode = False
+#     while currentNode and foundNode is False:
+#       if currentNode.get_value() == value:
+#         foundNode = True
+#         print(f'Linked list contains value {value}')
+#         return True
+#       else:
+#         currentNode = currentNode.get_next()
+#     if currentNode == None:
+#       print(f'Linked list does not contain a value of {value}')
+#       return False
       
-  def get_max(self):
-      currentNode = self.head
-      maximum = currentNode
-      if currentNode == None:
-        return None
-      if currentNode.get_next() == None:
-        maximum = currentNode.get_value()
-      while currentNode.get_value() is not None:
-        if currentNode.get_value() > maximum:
-          maximum = currentNode.get_value()
-        else:
-          currentNode = currentNode.get_next()
-      return maximum
+#   def get_max(self):
+#       currentNode = self.head
+#       maximum = currentNode
+#       if currentNode == None:
+#         return None
+#       if currentNode.get_next() == None:
+#         maximum = currentNode.get_value()
+#       while currentNode.get_value() is not None:
+#         if currentNode.get_value() > maximum:
+#           maximum = currentNode.get_value()
+#         else:
+#           currentNode = currentNode.get_next()
+#       return maximum
+
+
 
 # ######ATTEMPT 2 tests
 # list1 = LinkedList()
@@ -169,3 +171,92 @@ class LinkedList:
 
 # node3.set_next(node4)
 # print(Node.get_value(Node.get_next(node3)))
+
+
+
+###### ATTEMPT 3 ########
+
+"""
+Class that represents a single linked
+list node that holds a single value
+and a reference to the next node in the list
+"""
+class Node:
+  def __init__(self, value=None, next_node=None):
+    self.value = value
+    self.next_node = next_node
+
+  def get_value(self):
+    return self.value
+
+  def get_next(self):
+    return self.next_node
+
+  def set_next(self, new_next):
+    self.next_node = new_next
+
+class LinkedList:
+  def __init__(self):
+    self.head = None
+    self.tail = None
+
+  def add_to_tail(self, value):
+    #By default, the next_value argument in Node class is set to None. 
+    newNode = Node(value) 
+    if self.head == None or self.tail == None:
+      self.head = newNode
+      self.tail = newNode
+    else:
+      self.tail.set_next(newNode)
+      self.tail = newNode
+
+  def remove_head(self):
+    if self.head is None:
+      return None
+    else:
+      oldHead= self.head
+      self.head = oldHead.get_next()
+      return oldHead.value
+
+  def contains(self,value):
+    currentNode = self.head
+    foundNode = False
+    while currentNode and foundNode is False:
+      if currentNode.get_value() == value:
+        foundNode = True
+        return True
+      else: 
+        currentNode = currentNode.get_next()
+    if currentNode == None:
+      return False
+
+  def get_max(self):
+    currentNode = self.head
+    maxValue = currentNode.value
+    if currentNode == None:
+      return None
+    if currentNode.get_next() == None:
+      maxValue = currentNode.value
+    while currentNode is not None:
+      if currentNode.value > maxValue:
+        maxValue = currentNode.value
+      else:
+        currentNode = currentNode.get_next()
+    return maxValue
+    
+
+
+        
+
+
+list1 = LinkedList()
+list1.add_to_tail(5)
+list1.add_to_tail(7)
+list1.add_to_tail(8)
+list1.add_to_tail(17)
+
+print(list1.head.value)
+
+
+print(list1.contains(117))
+print(list1.get_max())

45a4c831f49729a3f91384179d0b5d67dea35871
keegn <keegan.t.burkett@gmail.com>
2018-10-10 17:27:49 -0500

    working on search tree

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -5,7 +5,18 @@ class BinarySearchTree:
     self.right = None
 
   def insert(self, value):
-    pass
+    insertedTreeNode = BinarySearchTree(value)
+    while True:
+      if self.left == None and self.value >= insertedTreeNode.value:
+        self.left = insertedTreeNode
+        return True
+      if self.right == None and self.value < insertedTreeNode.value:
+        self.right = insertedTreeNode
+        return True
+      if self.value >= insertedTreeNode.value:
+        self = self.left
+      else:
+        self = self.right
 
   def contains(self, target):
     pass

46bf59b6b3ae84edffb88f21e945411dcd63ae8b
mister-corn <code@ronaldlibago.com>
2018-10-10 10:27:03 -0700

    Revert "`doubly_linked_list.py` is passing. O.M.G."
    This reverts commit 555a19fb98fd444cbc5bb5a94ec5531012d83f22.

--- doubly_linked_list/doubly_linked_list.py
+++ doubly_linked_list/doubly_linked_list.py
@@ -6,69 +6,37 @@ class ListNode:
 
   def insert_after(self, value):
     self.next = ListNode(value, self, self.next)
-    return self.next
 
   def insert_before(self, value):
     self.prev = ListNode(value, self.prev, self)
-    return self.prev
 
   def delete(self):
-    if self.prev:
-      self.prev.next = self.next
-    if self.next:
-      self.next.prev = self.prev
-    return self.value
+    self.prev.next = self.next
+    self.next.prev = self.prev
 
 
 class DoublyLinkedList:
-  def __init__(self, node=None):
-    self.head = node
-    self.tail = node
+  def __init__(self):
+    self.head = None
+    self.tail = None
 
   def add_to_head(self, value):
-    new_node = ListNode(value, None, self.head)
+    pass
 
-    if not self.head:
-      self.tail = new_node
-    else:
-      self.head.prev = new_node
-    
-    self.head = new_node
-    
   def remove_from_head(self):
-    if not self.head: return None
-
-    next_node = self.head.next
-    retval = self.head.delete()
-    self.head = next_node
-      
-    return retval
+    pass
 
   def add_to_tail(self, value):
-    if not self.tail:
-      new_node = ListNode(value)
-      self.head = new_node
-      self.tail = new_node
-    else:
-      self.tail = self.tail.insert_after(value)
-
+    pass
 
   def remove_from_tail(self):
-    if not self.tail:
-      return None
-    else:
-      prev_node = self.tail.prev
-      if prev_node:
-        prev_node.next = None
-      return self.tail.delete()
+    pass
 
   def move_to_front(self, node):
-    self.add_to_head(node.value)
-    return node.delete()
+    pass
 
   def move_to_end(self, node):
-    self.add_to_tail(node.value)
-    return node.delete()
+    pass
 
   def delete(self, node):
-    return node.delete()
+    pass

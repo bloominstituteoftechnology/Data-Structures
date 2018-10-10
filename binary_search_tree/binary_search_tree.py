3d87b461914f8b58864305f285bb71ee4550664a
“Kennedykid1995” <34872885+Kennedykid1995@users.noreply.github.com>
2018-10-10 17:04:20 -0500

    Added Notes to BST

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -5,18 +5,28 @@ class BinarySearchTree:
     self.right = None
 
   def insert(self, value):
+    #"""if the value is greater than or equal to
+    # insert it into the right side of the tree."""
     if value >= self.value:
       if self.right is None:
         self.right = BinarySearchTree(value)
       else:
         self.right.insert(value)
     else:
+      #"""if the value is less than
+    # insert it into the left side of the tree."""
       if self.left is None:
         self.left = BinarySearchTree(value)
       else:
         self.left.insert(value)
 
   def contains(self, target):
+    #""" if the target is equal to the self.value
+    # return true. if not move on, target is greater than
+    # value then access the right side then find if the right
+    # side contains the value if so then end. If the value
+    # is less than the self.value access the left side and
+    # see if the left side contains the value. """
     if target == self.value:
       return True
     if target > self.value:
@@ -31,7 +41,18 @@ class BinarySearchTree:
         return self.left.contains(target)
 
   def get_max(self):
+    #""" since the left side is filled with lower values 
+    # just forget about it. go straight for the right side.
+    # if the rigth side is None then just return the value
+    # else find the max of value on the right side since
+    # all the values on the right side are bigger than 
+    # the core value of the tree."""
     if self.right is None:
       return self.value
     else:
       return self.right.get_max()
+
+
+      # """    5
+      #     4     6
+      #    2 4   5 7   """

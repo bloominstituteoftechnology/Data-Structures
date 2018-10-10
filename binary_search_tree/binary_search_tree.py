59f188972f082e2e6a358bf1c5f6f10441ce9012
mister-corn <code@ronaldlibago.com>
2018-10-10 10:28:55 -0700

    Revert "`binary_search_tree.py` is passing. Noice."
    This reverts commit ad15591ba0ea47ddd90f446748b62e451fc43789.

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -5,47 +5,10 @@ class BinarySearchTree:
     self.right = None
 
   def insert(self, value):
-    if value < self.value:
-      if self.left == None:
-        self.left = BinarySearchTree(value)
-      else:
-        self.left.insert(value)
-    elif value >= self.value:
-      if self.right == None:
-        self.right = BinarySearchTree(value)
-      else:
-        self.right.insert(value)
+    pass
 
   def contains(self, target):
-    if target == self.value:
-      return True
-    elif target < self.value:
-      if self.left == None:
-        return False
-      else:
-        return self.left.contains(target)
-    elif target >= self.value:
-      if self.right == None:
-        return False
-      else:
-        return self.right.contains(target)
+    pass
 
   def get_max(self):
-    if self.right == None: return self.value
-    return self.right.get_max()
-
-def main():
-  # My Janky Test Program
-  my_bst = BinarySearchTree(5)
-  my_bst.insert(2)
-  my_bst.insert(3)
-  my_bst.insert(7)
-  print('Contains 7? ',my_bst.contains(7))
-  """
-  This is where I found out that if you want 
-  your to give you a result, you have to tell
-  it to give it to you.
-  """
-  
-if __name__ == '__main__':
-  main()
+    pass

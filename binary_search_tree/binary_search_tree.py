ede83bb143db5f6b7357c7e22ddba5127483feb8
codejoncode <jonathanjholloway@gmail.com>
2018-10-10 17:55:57 -0500

    started the binary_search_tree   complted the get_max added some functions for the insert and contains function I plan to build

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -1,18 +1,51 @@
 
 
 class BinarySearchTree:
-  def __init__(self, value):
+  def __init__(self, value, parent=None):
     self.value = value
     self.left = None
     self.right = None
+    self.parent = parent
 
   def insert(self, value):
+    # need to check if there is a root, if there isn't set the first item as the root. 
     pass
 
   def contains(self, target):
     pass
 
   def get_max(self):
-    pass
+    current = self
+    while current.hasRight():
+      current = current.right
+    return current
+    
+  def hasLeft(self):
+    return self.left 
+  
+  def hasRight(self):
+    return self.right
+  
+  def isLeft(self):
+    return self.parent and self.parent.left == self 
+  
+  def isRight(self):
+    return self.parent and self.parent.right == self 
+  
+  def isRoot(self):
+    return not self.parent
+  
+  def isLeaf(self):
+    return not (self.right or self.left)
+  
+  def has_a_child(self):
+    return self.right or self.left 
+  
+  def has_max_children(self):
+    return self.right and self.left 
+  
 
+
+
+  
   

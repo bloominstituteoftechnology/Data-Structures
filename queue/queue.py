b1a22295b895375bf4879b502535d040ea2dd72b
Jun Hyuk Kim <junhyukkeem@gmail.com>
2018-10-10 12:07:36 -0700

    implement queue, pass tests

--- queue/queue.py
+++ queue/queue.py
@@ -8,10 +8,15 @@ class Queue:
     self.storage = LinkedList()
 
   def enqueue(self, item):
+    self.size += 1
     self.storage.add_to_tail(item)
   
   def dequeue(self):
-    return self.storage.remove_head()
+    if self.size > 0:
+      self.size -= 1
+      return self.storage.remove_head()
+    else:
+      return None
 
   def len(self):
-    pass
+    return self.size

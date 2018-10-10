ebe35bf8edf88751af9a2fc1f34456ac5bee5c0e
AI2EJAY <JHammett1314@gmail.com>
2018-10-10 17:14:54 -0500

    Pull Request

--- linked_list/linked_list.py
+++ linked_list/linked_list.py
@@ -2,6 +2,8 @@
 Class that represents a single linked
 list node that holds a single value
 and a reference to the next node in the list
+
+Change for Pull Request
 """
 class Node:
   def __init__(self, value=None, next_node=None):

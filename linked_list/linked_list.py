4bf66609ab8456c581e305085f73522a16a81b5b
JoCatherine Griggs <jocafrin@gmail.com>
2018-10-10 16:35:13 -0400

    Initial commit, linked list

--- linked_list/linked_list.py
+++ linked_list/linked_list.py
@@ -3,6 +3,8 @@ Class that represents a single linked
 list node that holds a single value
 and a reference to the next node in the list
 """
+
+
 class Node:
   def __init__(self, value=None, next_node=None):
     self.value = value
@@ -17,19 +19,53 @@ class Node:
   def set_next(self, new_next):
     self.next_node = new_next
 
+
 class LinkedList:
   def __init__(self):
     self.head = None
     self.tail = None
 
   def add_to_tail(self, value):
-    pass
+    node = Node(value)
+    if self.head == None:
+      self.head = node
+      self.tail = node
+    else:
+      self.tail.set_next(node)
+      self.tail = node
+      self.tail.set_next(None)
 
   def remove_head(self):
-    pass
+    if self.head == None:
+      return None
+    else:
+      current_head = self.head.get_value()
+    if self.head.get_next() != None:
+        self.head = self.head.get_next()
+    else:
+        self.head = None
+        self.tail = None
+    return current_head
 
   def contains(self, value):
-    pass
+    if self.head == None:
+      return None
+    current = self.head
+    if current.get_value() == value:
+      return True
+    while current.get_next() != None:
+      current = current.get_next()
+      if current.get_value() == value:
+        return True
+    return False
 
   def get_max(self):
-    pass
+    if self.head == None:
+      return None
+    current = self.head
+    maximum = current.get_value()
+    while current.get_next() != None:
+      current = current.get_next()
+      if current.get_value() > maximum:
+        maximum = current.get_value()
+    return maximum

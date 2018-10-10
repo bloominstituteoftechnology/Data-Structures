e2cc461ed375bfa70891c1c3f38291e754146d27
Arthur Bates <aabatesj@gmail.com>
2018-10-10 17:07:43 -0400

    DLL add to head
    add to head function implemented

--- doubly_linked_list/doubly_linked_list.py
+++ doubly_linked_list/doubly_linked_list.py
@@ -17,9 +17,21 @@ class DoublyLinkedList:
   def __init__(self, node=None):
     self.head = node
     self.tail = node
+    self.count = 0
 
   def add_to_head(self, value):
-    pass
+    if self.count == 0:
+            self.head = self.ListNode( self, value, prev=None, next=None )
+            self.tail = self.head
+        elif self.count > 0:
+            # create a new node pointing to self.head
+            node = self.Node( self, value, prev=None, self.head )
+            # have self.head point back to the new node
+            self.head.prev = node
+            # finally point to the new node as the self.head
+            self.head = node
+        self.current = self.head
+        self.count += 1
 
   def remove_from_head(self):
     pass

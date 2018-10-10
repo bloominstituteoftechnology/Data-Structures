2a96074256b0fcacc9ca3bb973d5a7c0a4a72ca1
mkerbleski <mkerbles@gmail.com>
2018-10-10 14:19:24 -0600

    work on 'add_to_tail'

--- linked_list/linked_list.py
+++ linked_list/linked_list.py
@@ -23,6 +23,21 @@ class LinkedList:
     self.tail = None
 
   def add_to_tail(self, value):
+   
+    # print(self.head.value, '-- self.head After')
+
+    if self.head == None:
+      print('no head')
+      # self.tail.next_node = None
+    else:  
+      print('no tail')
+      print(self.tail, '-- self.tail OG')
+      print(self.head, '-- self.tail OG')
+      new_node = Node(value)
+      new_node.set_next(self.tail)
+      print(new_node.get_value(), '-- new_node value')
+      self.tail = new_node
+      print(self.tail.value, '-- self.tail After')
     pass
 
   def remove_head(self):

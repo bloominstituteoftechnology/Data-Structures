c158da6563a059ed293597abe32e3e8956a7bd44
“Kennedykid1995” <34872885+Kennedykid1995@users.noreply.github.com>
2018-10-10 15:50:20 -0500

    Linked

--- linked_list/linked_list.py
+++ linked_list/linked_list.py
@@ -23,20 +23,49 @@ class LinkedList:
     self.tail = None
 
   def add_to_tail(self, value):
-    if self.head is None:
-      self.head = new
+    if self.tail is None:
+      node_one = Node(value)
+      self.head = node_one
+      self.tail = node_one
     else:
-      self.tail.next = item
-    
-    self.tail = item
-    
-    return
+      new_node = Node(value)
+      self.tail.next_node = new_node
+      self.tail = new_node
+      self.tail.next_node = None
 
   def remove_head(self):
-    pass
+    if not self.head:
+      return None
+    if not self.head.next_node:
+      head = self.head
+      self.head = None
+      self.tail = None
+      return head.value
+    else:
+      value = self.head.value
+      self.head = self.head.next_node
+      return value
+      
+
 
   def contains(self, value):
-    pass
+    if not self.head:
+      return None
+    current_node = self.head
+    while current_node:
+      if current_node.value == value:
+        return True
+      current_node = current_node.get_next()
+    return False
 
   def get_max(self):
-    pass
+    if not self.head:
+      return None
+    max_value = self.head.value
+    current_head = self.head.get_next()
+    while current_head:
+      if current_head.value > max_value:
+        max_value = current_head.value
+      current_head = current_head.next_node
+    return max_value
+  

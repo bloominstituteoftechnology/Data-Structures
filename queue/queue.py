66cb3816c9a25016ff29d4670a7145e9c348ab68
“Kennedykid1995” <34872885+Kennedykid1995@users.noreply.github.com>
2018-10-10 17:12:30 -0500

    Added notes to queue

--- queue/queue.py
+++ queue/queue.py
@@ -8,13 +8,20 @@ class Queue:
     self.storage = LinkedList()
 
   def enqueue(self, item):
+    #just add the item to the storage using 
+    #add to tail, then add 1 to size
     self.storage.add_to_tail(item)
     self.size += 1
   
   def dequeue(self):
+    #if the size is greater than 0 then 
+    # self.size -= 1 is removing 1 item from the list
+    #remove the head of the list using the 
+    #remove head function
     if self.size > 0:
       self.size -= 1
     return self.storage.remove_head()
 
   def len(self):
+    #just return the size of the list
     return self.size

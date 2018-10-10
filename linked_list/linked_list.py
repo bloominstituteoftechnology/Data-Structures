9415f0f6633dfe413ad8e2ef85b918116799e8bd
Joseph Stossmeister <joseph.stossmeister@gmail.com>
2018-10-10 15:50:48 -0400

    linked_list

--- linked_list/linked_list.py
+++ linked_list/linked_list.py
@@ -26,13 +26,52 @@ class LinkedList:
         self.tail = None
 
     def add_to_tail(self, value):
-        pass
+        new_node = Node(value)
+        if self.tail is not None:
+            self.tail.set_next(new_node)
+        if self.head is None:
+            self.head = new_node
+        self.tail = new_node
 
     def remove_head(self):
-        pass
+        old_head = self.head
+        return_value = None
+        if self.head is not None:
+            if self.head == self.tail:
+                return_value = old_head.get_value()
+                self.head = None
+                self.tail = None
+            else:
+                new_head = old_head.get_next()
+                self.head = new_head
+                return_value = old_head.get_value()
+        return return_value
 
     def contains(self, value):
-        pass
+        found = False
+        if self.head is not None:
+            current = self.head
+            if self.head == self.tail:
+                if self.head.get_value() == value:
+                    found = True
+            else:
+                while current and found is False:
+                    if current.get_value() == value:
+                        found = True
+                    else:
+                        current = current.get_next()
+        return found
 
     def get_max(self):
-        pass
+        max = None
+        if self.head is not None:
+            current = self.head
+            if self.head is not None and self.head == self.tail:
+                max = self.head.get_value()
+            else:
+                while current:
+                    if max is None or current.get_value() > max:
+                        max = current.get_value()
+                    else:
+                        current = current.get_next()
+        return max

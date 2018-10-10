5817d893f138ffe284a30d58cb7a63ad2ec4c880
jordan-massingill <jordan_massingill@hotmail.com>
2018-10-10 17:16:07 -0500

    Jordan Massingill
    Initial PR
    Working through linked_list

--- linked_list/linked_list.py
+++ linked_list/linked_list.py
@@ -23,13 +23,46 @@ class LinkedList:
     self.tail = None
 
   def add_to_tail(self, value):
-    pass
+    if self.head is None:
+      self.head = Node(value)
+    elif self.tail and self.head:
+      self.tail.set_next(Node(value))
+    self.tail = Node(value)
 
   def remove_head(self):
-    pass
+    if self.head:
+      prev = self.head.value
+      self.head = self.head.next_node
+      return prev
+    return None
+
 
   def contains(self, value):
-    pass
+    result = False
+    if self.head:
+        def node_contains(node):
+          nonlocal result
+          if node.value == value:
+            result = True
+          elif result == True:
+            return result
+          elif node.next_node:
+            result = node_contains(node.next_node)
+        node_contains(self.head)
+        return result
+    return result
 
   def get_max(self):
-    pass
+    maxx = None
+
+    def maxx_func(node):
+      if node.next_node is not None and node.next_node.value > maxx.value:
+        maxx = node.next_node
+      elif node.next_node is not None:
+        maxx_func(node.next_node)
+      else:
+        return None
+    if self.head:
+      maxx = self.head
+      maxx_func(maxx)
+    return maxx.value or maxx

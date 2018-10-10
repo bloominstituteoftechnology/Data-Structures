9f07dfbfb766438830d2f842e1868e25416ccb41
Chris Honda <christopher.honda92@gmail.com>
2018-10-10 13:25:20 -0700

    Finished doubly linked list

--- doubly_linked_list/doubly_linked_list.py
+++ doubly_linked_list/doubly_linked_list.py
@@ -5,13 +5,21 @@ class ListNode:
     self.next = next
 
   def insert_after(self, value):
-    pass
+    new = ListNode(value, self.next, self)
+    self.next = new
+    return new
 
   def insert_before(self, value):
-    pass
+    new = ListNode(value, self.prev, self)
+    self.prev = new
+    return new
 
   def delete(self):
-    pass
+    if self.prev != None:
+      self.prev.next = self.next
+    if self.next != None:
+      self.next.prev = self.prev
+    return self
 
 class DoublyLinkedList:
   def __init__(self):
@@ -19,22 +27,48 @@ class DoublyLinkedList:
     self.tail = None
 
   def add_to_head(self, value):
-    pass
+    if self.head.next == None:
+      self.head.next = self.tail
+    self.head = self.head.insert_before(value)
 
   def remove_from_head(self):
-    pass
+    old_head = self.head
+    if old_head != None:
+      if old_head.next == None:
+        self.tail = None
+      old_head.delete()
+    return old_head.value
 
   def add_to_tail(self, value):
-    pass
+    if self.tail.prev == None:
+      self.tail.prev = self.head
+    self.tail = self.tail.insert_after(value)
 
   def remove_from_tail(self):
-    pass
+    old_tail = self.tail
+    if old_tail != None:
+      if old_tail.prev == None:
+        self.head = None
+      old_tail.delete()
+    return old_tail.value
 
   def move_to_front(self, node):
-    pass
+    current = self.head
+    while current != None:
+      if current == node:
+        current.delete()
+        self.add_to_head(node.value)
+        break
+      current = current.next
 
   def move_to_end(self, node):
-    pass
+    current = self.head
+    while current != None:
+      if current == node:
+        current.delete()
+        self.add_to_tail(node.value)
+        break
+      current = current.next
 
   def delete(self, node):
-    pass
+    node.delete()

fce1570c78ed3791e9e023d4f3fe8f08da309dc4
bri79319 <bri79319@gmail.com>
2018-10-11 19:36:30 -0400

    code modified

--- heap/heap.py
+++ heap/heap.py
@@ -15,7 +15,20 @@ class Heap:
     pass
 
   def _bubble_up(self, index):
-    pass
+    while (index - 1) // 2 >= 0:
+      if self.storage[(index - 1) // 2] < self.storage[index]:
+        self.storage[index], self.storage[(index - 1) // 2] = self.storage
+      index = (index - 1) // 2
 
   def _sift_down(self, index):
-    pass
+    while index * 2 + 1 <= len(self.storage) - 1:
+      mc = self._max_child(index)
+      if self.storage[index] < self.storage[mc]:
+        self.storage[index], self.storage[mc] = self.storage[mc], self.storage[index]
+      index = mc
+  
+  def _max_child(self, index):
+    if index * 2 + 2 > len(self.storage) - 1:
+      return index * 2 + 1
+    else:
+      return index * 2 + 1 if self.storage[index * 2 + 1] > self.storage[index * 2 + 2] else index * 2 + 2

63d797b707b67ca96d0863657d65105ab0203677
mkerbleski <mkerbles@gmail.com>
2018-10-11 15:53:19 -0600

    complete insert in 'heap.py'

--- heap/heap.py
+++ heap/heap.py
@@ -3,7 +3,13 @@ class Heap:
     self.storage = []
 
   def insert(self, value):
-    pass
+    # if len(self.storage) <= 1:
+    #   self.storage.append(value)
+    # else: 
+      self.storage.append(value)
+      self._bubble_up(len(self.storage)-1)
+      print(self.storage)
+    
 
   def delete(self):
     pass
@@ -15,7 +21,14 @@ class Heap:
     pass
 
   def _bubble_up(self, index):
-    pass
+    #index is the index of the element that will bemoving up the heap 
+    #keep bubbling the element at the given index up the heap until it reachs a va...
+    while (index-1) // 2 >= 0:
+      if self.storage[(index-1)//2] < self.storage[index]:
+      #swap the values
+        self.storage[index], self.storage[(index-1) // 2] = self.storage[(index -1) // 2], self.storage[index]
+      #update the index to be the parent's indes so that we can continte moving up the heap
+      index = (index -1) // 2
 
   def _sift_down(self, index):
     pass

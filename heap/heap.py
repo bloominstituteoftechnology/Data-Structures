3a0303816a7195647e3d4293ff782c18899f84b0
Kyle Meltzer <jimmy13@Kyles-MacBook-Pro.local>
2018-10-11 16:07:27 -0500

    more work on heap

--- heap/heap.py
+++ heap/heap.py
@@ -17,7 +17,26 @@ class Heap:
     return len(self.storage)
 
   def _bubble_up(self, index):
-    pass
+    if index == 0:
+    # if self.storage[index] == 0:
+      current_node = self.storage.index(self.storage[-1])
+    else: 
+      current_node = index
+
+    if len(self.storage) == 1:
+      return
+
+    parent_index = (current_node // 2)
+
+    parent_value = self.storage[parent_index]
+
+    if self.storage[current_node] > parent_value:
+      temp = self.storage[parent_index] 
+      self.storage[parent_index] = self.storage[current_node]
+      self.storage[current_node] = temp
+      current_node = parent_index
+      return self._bubble_up(current_node)
+
 
   def _sift_down(self, index):
     pass

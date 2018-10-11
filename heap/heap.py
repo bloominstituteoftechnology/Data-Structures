4f9db46740996e80edb6aa132ff0e32bdf15c3b9
Rachel DiCesare <racheldicesare@missrachelthedeveloper.com>
2018-10-11 15:34:46 -0400

    heap WIP

--- heap/heap.py
+++ heap/heap.py
@@ -3,7 +3,8 @@ class Heap:
     self.storage = []
 
   def insert(self, value):
-    pass
+    index = len(self.storage)
+    self.storage.append(value)
 
   def delete(self):
     pass
@@ -15,7 +16,15 @@ class Heap:
     pass
 
   def _bubble_up(self, index):
-    pass
+    # index is the index of the element that will be moving up the heap
+    # keep bubbling the element at the given index up the heap until it reaches a valid spot
+    while (index - 1) // 2 >= 0:
+      # check to see if the element's parent value is less than the current value
+      if self.storage[(index - 1 // 2)] < self.storage[index]:
+        # swap the values
+        self.storage[index], self.storage[(index - 1) // 2] = self.storage[(index -1) // 2],self.storage[index]
+      # update the index to be the parent's index so that can continue moving up
+      index = (index - 1) // 2
 
   def _sift_down(self, index):
     pass

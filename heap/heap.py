8f6c559b75f3c211067184f5f72074acad8ee2d2
Charlie Winslow <cwinslow22@yahoo.com>
2018-10-11 16:56:26 -0500

    implemented bubble up, but need to implement delete before I can test it
    starting implementation of delete

--- heap/heap.py
+++ heap/heap.py
@@ -13,7 +13,7 @@ class Heap:
     return self.storage[0]
 
   def get_size(self):
-    pass
+    return len(self.storage)
 
   def _bubble_up(self, index):
     # index is the index of the element that will be moving up the heap
@@ -27,4 +27,18 @@ class Heap:
       index = (index - 1) // 2
 
   def _sift_down(self, index):
-    pass
+    # index is the index of the element that will be moving down the heap
+    # keep sifting the element at the given index down the heap until it reaches a valid spot
+    while self.storage[(2 * index + 1)] is not None:
+      # check to see which child is larger
+      if self.storage[(2 * index + 1)] > self.storage[(2 * index + 2)]:
+        # check to see if the child is larger than the parent
+        if self.storage[(2 * index + 1)] > self.storage[index]:
+          #swap values
+          self.storage[index], self.storage[(2 * index + 1)] = self.storage[(2 * index + 1)], self.storage[index]
+        index = 2 * index + 1
+      elif self.storage[(2 * index + 1)] <= self.storage[(2 * index + 2)]:
+        if self.storage[(2 * index + 2)] > self.storage[index]:
+          # swap values
+          self.storage[index], self.storage[(2 * index + 2)] = self.storage[(2 * index + 2)], self.storage[index]
+        index = 2 * index + 2

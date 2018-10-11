a1a03504358069652484ef1c60d9936c761ba7ff
“Kennedykid1995” <34872885+Kennedykid1995@users.noreply.github.com>
2018-10-11 18:25:16 -0500

    Heap finished

--- heap/heap.py
+++ heap/heap.py
@@ -3,16 +3,23 @@ class Heap:
     self.storage = []
 
   def insert(self, value):
-    pass
-
+    self.storage.append(value)
+    self._bubble_up(len(self.storage) - 1)
+    
   def delete(self):
-    pass
+    retval = self.storage[0]
+    #replace the first storage element with the last element in the heap
+    self.storage[0] = self.storage[len(self.storage) - 1]
+    #remove the last element in the heap
+    self.storage.pop()
+    self._sift_down(0)
+    return retval
 
   def get_max(self):
-    pass
+    return self.storage[0]
 
   def get_size(self):
-    pass
+    return len(self.storage)
 
   def _bubble_up(self, index):
     while (index - 1)//2 >= 0:
@@ -21,4 +28,18 @@ class Heap:
       index = (index - 1) // 2
 
   def _sift_down(self, index):
-    pass
+    while index * 2 + 1 <= len(self.storage) - 1:
+      # figure out the larger of the two children
+      mc = self._max_child(index)
+      #check to see if we need to swap
+      if self.storage[index] < self.storage[mc]:
+        self.storage[index], self.storage[mc] = self.storage[mc], self.storage[index]
+      index = mc
+
+  def _max_child(self, index):
+      #check if the right child index is out of bounds of our storage array
+    if index * 2 + 2 > len(self.storage) - 1:
+        return index * 2 + 1
+    else:
+        #return the index corresponding to the child node that has the larger value
+      return index * 2 + 1 if self.storage[index * 2 + 1] > self.storage[index * 2 + 2 ] else index * 2 + 2

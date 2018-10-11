184efe1220019d8889f1398463823746c1611a95
mkerbleski <mkerbles@gmail.com>
2018-10-11 16:24:10 -0600

    complete 'test_get_max_after_delete'

--- heap/heap.py
+++ heap/heap.py
@@ -3,19 +3,16 @@ class Heap:
     self.storage = []
 
   def insert(self, value):
-    # if len(self.storage) <= 1:
-    #   self.storage.append(value)
-    # else: 
-      self.storage.append(value)
-      self._bubble_up(len(self.storage)-1)
-      print(self.storage)
-    
+    self.storage.append(value)
+    self._bubble_up(len(self.storage)-1)
+    print(self.storage)
 
   def delete(self):
-    pass
+    self.storage.remove(self.get_max())
+    self._bubble_up(len(self.storage)-1)
 
   def get_max(self):
-    pass
+    return self.storage[0]
 
   def get_size(self):
     pass

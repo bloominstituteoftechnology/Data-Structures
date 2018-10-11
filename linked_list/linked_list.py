1a270a76459b341ed79e7f39f4453c1b7ea25c29
Naaz Shaikh <naazhome@Naazs-MBP-3.hsd1.or.comcast.net>
2018-10-11 05:03:05 -0700

    working on LinkedList

--- linked_list/linked_list.py
+++ linked_list/linked_list.py
@@ -24,12 +24,27 @@ class LinkedList:
     def __init__(self):
         self.head = None
         self.tail = None
+        self.length = 0
 
     def add_to_tail(self, value):
-        pass
+        # creates a new node and assign it as the head of the list since length is 0
+        # when run again would assign next on previous item and set default next to none
+        newNode = Node(self, value)
+        if self.head == None:
+            self.head = newNode
+            self.tail = self.head
+            self.length += 1
+        else:
+            self.tail.next_node = newNode
+            self.length += 1
+            self.tail = newNode
 
     def remove_head(self):
         pass
+# if self.head = true then
+# removed_head= LinkedList.head
+# LinkedList.head.remove()
+# return removed_head
 
     def contains(self, value):
         pass

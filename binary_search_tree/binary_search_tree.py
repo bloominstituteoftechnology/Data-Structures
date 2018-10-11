3f03874644f210d12b5b7a908cf87d153f0e100e
Kyle Meltzer <jimmy13@Kyles-MacBook-Pro.local>
2018-10-10 22:15:11 -0500

    passed insert for binary tree

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -5,10 +5,43 @@ class BinarySearchTree:
     self.right = None
 
   def insert(self, value):
-    pass
+    if self.value:
+      if value < self.value:
+        if self.left is None:
+          self.left = BinarySearchTree(value)
+        else:
+          self.left.insert(value)
+      elif value > self.value:
+        if self.right is None:
+          self.right = BinarySearchTree(value)
+        else:
+          self.right.insert(value)
+    else:
+        self.value = value
+    # if self.value is None:
+    #   self.value = value
+    # else:
+    #   if self.value < value:
+    #     if self.right is None:
+    #       self.right = value
+    #     else:
+    #       self.right.insert(value)
+    #   else:
+    #     if self.left is None:
+    #       self.left = value
+    #     else:
+    #       self.left.insert(value)
 
-  def contains(self, target):
-    pass
+    
 
-  def get_max(self):
-    pass
+  # def contains(self, target):
+  #   if self.value is None or self.value == target:
+  #     return self.value
+    
+  #   if self.value < target:
+  #     return self.right.contains(target)
+
+  #   return self.left.contains(target)
+
+  # def get_max(self):
+  #   pass 

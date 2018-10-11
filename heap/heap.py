1287b94e8dee2d3364dc59fc90ca06ce3ffb1eb1
Charlie Winslow <cwinslow22@yahoo.com>
2018-10-11 15:24:28 -0500

    implemented bubble up function from lecture

--- heap/heap.py
+++ heap/heap.py
@@ -9,13 +9,18 @@ class Heap:
     pass
 
   def get_max(self):
-    pass
+    return self.storage[0]
 
   def get_size(self):
     pass
 
   def _bubble_up(self, index):
-    pass
+    while (index - 1) // 2 >= 0:
+      if self.storage[(index - 1) // 2] < self.storage[index]:
+        # swap the values
+        self.storage[index], self.storage[(index - 1) // 2] = self.storage[(index - 1) // 2], self.storage[index]
+      # update the index to be the parent's index so that we can continue moving up
+      index = (index - 1) // 2
 
   def _sift_down(self, index):
     pass

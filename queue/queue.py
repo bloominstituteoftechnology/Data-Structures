a8fc9aee20b5ca229a83ff45d20ddf1c026b472f
Kyle Meltzer <jimmy13@Kyles-MacBook-Pro.local>
2018-10-10 21:30:28 -0500

    completed queue

--- queue/queue.py
+++ queue/queue.py
@@ -1,28 +1,28 @@
-# import sys
-# sys.path.append('../linked_list')
-# from linked_list import LinkedList 
+import sys
+sys.path.append('../linked_list')
+from linked_list import LinkedList 
 
 
-# class Queue:
-#   def __init__(self):
-#     self.size = 0
-#     self.storage = LinkedList()
+class Queue:
+  def __init__(self):
+    self.size = 0
+    self.storage = LinkedList()
 
-#   def enqueue(self, item):
-#     self.storage.add_to_tail(item)
-#     self.size = self.size + 1
+  def enqueue(self, item):
+    self.storage.add_to_tail(item)
+    self.size = self.size + 1
   
-#   def dequeue(self):
-#     item = self.storage.remove_head()
-#     if self.size > 0:
-#       self.size = self.size - 1
-#     else: 
-#       self.size = 0
-#     if item.value is not None:
-#       return item
-#     else: 
-#       return None
+  def dequeue(self):
+    item = self.storage.remove_head()
+    if self.size > 0:
+      self.size = self.size - 1
+    else: 
+      self.size = 0
+    if item is not None:
+      return item
+    else: 
+      return None
 
 
-#   def len(self):
-#     return self.size
+  def len(self):
+    return self.size

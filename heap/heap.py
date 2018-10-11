12a6a4554535300e4ec0785385c7b64faa0d3391
AI2EJAY <JHammett1314@gmail.com>
2018-10-11 17:35:35 -0500

    Progress so far

--- heap/heap.py
+++ heap/heap.py
@@ -3,19 +3,33 @@ class Heap:
     self.storage = []
 
   def insert(self, value):
-    pass
+    self.storage.append(value)
+    self._bubble_up(len(self.storage) - 1)
 
   def delete(self):
     pass
 
   def get_max(self):
-    pass
+    print('self.storage:', self.storage)
+    return self.storage[0]
 
   def get_size(self):
-    pass
+    return len(self.storage)
 
   def _bubble_up(self, index):
-    pass
+    current_value = self.storage[index]
+    parent_value = self.storage[(index - 1) // 2]
+    while index >= 0:
+      if current_value > parent_value:
+        self.storage[index] = parent_value 
+        self.storage[(index - 1) // 2] = current_value
+        index = (index - 1) // 2
 
-  def _sift_down(self, index):
-    pass
+  # def _sift_down(self, index):
+  #   first_child = self.storage[2 * index + 1 ]
+  #   second_child = self.storage[2 * index + 2 ]
+  #   while self.storage[index] < first_child or second_child:
+  #     better_child = first_child >
+
+
+      

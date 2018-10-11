03f46e925da9f0892e0aafdd22a697a8959df2ea
Luis Fernando Salazar <lfsalazarcruz@gmail.com>
2018-10-11 13:33:11 -0700

    linked_list working

--- linked_list/linked_list.py
+++ linked_list/linked_list.py
@@ -30,13 +30,38 @@ class LinkedList:
     else:
       self.tail.next_node = Node(value)
       self.tail = self.tail.next_node
-    pass
 
   def remove_head(self):
-    pass
+    if self.head == None:
+      return self.head
+    else:
+      temp = self.head.get_value()
+      self.head = self.head.get_next()
+      if self.head == None:
+        self.tail = None
+      return temp
 
   def contains(self, value):
-    pass
+    cur_node = self.head
+    if self.head is None:
+      return False
+    else:
+      while cur_node is not None:
+        if cur_node.get_value() == value: # RETURNS TRUE IF CURRENT NODE IS EQUAL TO VALUE
+          return True     
+        else:
+          cur_node = cur_node.get_next() # CURRENT NODE CHANGES TO THE NEXT ONE IF NOT EQUAL TO VALUE. 
 
   def get_max(self):
-    pass
+    max_value = 0
+    cur_node = self.head
+    if cur_node == None:
+      self.tail = None
+      return None
+    else:
+      while cur_node is not None:
+        if cur_node.get_value() > max_value:
+          max_value = cur_node.get_value()
+        elif cur_node.get_value() < max_value:
+          cur_node = cur_node.get_next()
+    return max_value

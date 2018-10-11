b894d93c32243a9cb8cc5d83eeaa349778bb2d83
adamcglee <adamcglee@gmail.com>
2018-10-11 13:44:03 -0700

    finshed the binary search tree problems and got all the tests to pass

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -3,56 +3,38 @@ class BinarySearchTree:
     self.value = value
     self.left = None
     self.right = None
-    self.root = None
 
   def insert(self, value):
-    if self.root == None:
-      self.root = value
-    else:
-      self._insert(value, self.root)
-      def _insert(self, value, current_node):
-          if value < current_node.value:
-            if current_node.left == None:
-              current_node.left = value
-            else:
-              self._insert(value, current_node.left)
-          elif value > current_node.value: 
-            if current_node.right == None:
-              current_node.right = value
-            else:
-              self._insert(value, current_node.right)
-          else:
-            print("Value already exists in the tree")
+    if value < self.value:
+      if self.left == None:
+        self.left = BinarySearchTree(value)
+      else:
+        self.left.insert(value)
+    elif value >= self.value: 
+      if self.right == None:
+        self.right = BinarySearchTree(value)
+      else:
+        self.right.insert(value)
 
   def contains(self, target):
-    if self.root == target:
+    if target == self.value:
       return True
     else:
-      self._contains(target, self.root)
-      def _contains(self, target, current_node):
-          if target < current_node.target and current_node.left:
-            if current_node.left == target:
-              return True
-            else:
-              self._contains(target, current_node.left)
-          elif target > current_node.target and current_node.right: 
-            if current_node.right == target:
-              return True
-            else:
-              self._contains(target, current_node.right)
+      if target < self.value and self.left is not None:
+        if self.left.value == target:
+          return True
+        else:
+          return self.left.contains(target)
+      elif target > self.value and self.right is not None: 
+        if self.right.value == target:
+          return True
+        else:
+          return self.right.contains(target)
+      return False
     
-
-
   def get_max(self):
-    maxNode = None
-    maxed = False
-    current_node = self.root
-    if current_node is not None:
-      maxNode = current_node.value
-    while maxed is False:
-      if current_node.right:
-        maxNode = current_node.value
-        current_node = current_node.right
-      else:
-        maxed = True
-        return maxNode
+    maxNode = self.value
+    if self.right is None:
+      return maxNode
+    else:
+      return self.right.get_max()

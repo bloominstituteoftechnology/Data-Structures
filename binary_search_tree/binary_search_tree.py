1b98fab5155c2e40a8f9736c8fbad30b6f983251
adamcglee <adamcglee@gmail.com>
2018-10-11 00:32:13 -0700

    attempted to complete BST problem. I got it down to only 1 fail, but then stuff started to break

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -3,12 +3,56 @@ class BinarySearchTree:
     self.value = value
     self.left = None
     self.right = None
+    self.root = None
 
   def insert(self, value):
-    pass
+    if self.root == None:
+      self.root = value
+    else:
+      self._insert(value, self.root)
+      def _insert(self, value, current_node):
+          if value < current_node.value:
+            if current_node.left == None:
+              current_node.left = value
+            else:
+              self._insert(value, current_node.left)
+          elif value > current_node.value: 
+            if current_node.right == None:
+              current_node.right = value
+            else:
+              self._insert(value, current_node.right)
+          else:
+            print("Value already exists in the tree")
 
   def contains(self, target):
-    pass
+    if self.root == target:
+      return True
+    else:
+      self._contains(target, self.root)
+      def _contains(self, target, current_node):
+          if target < current_node.target and current_node.left:
+            if current_node.left == target:
+              return True
+            else:
+              self._contains(target, current_node.left)
+          elif target > current_node.target and current_node.right: 
+            if current_node.right == target:
+              return True
+            else:
+              self._contains(target, current_node.right)
+    
+
 
   def get_max(self):
-    pass
+    maxNode = None
+    maxed = False
+    current_node = self.root
+    if current_node is not None:
+      maxNode = current_node.value
+    while maxed is False:
+      if current_node.right:
+        maxNode = current_node.value
+        current_node = current_node.right
+      else:
+        maxed = True
+        return maxNode

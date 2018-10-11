bfe3b223f968776a9081be7a1d27c52f6cf2d756
Luis Fernando Salazar <lfsalazarcruz@gmail.com>
2018-10-11 14:14:55 -0700

    Queue.py working properly

--- queue/queue.py
+++ queue/queue.py
@@ -8,10 +8,21 @@ class Queue:
     self.storage = LinkedList()
 
   def enqueue(self, item):
+    self.size += 1
+    self.storage.add_to_tail(item)
     pass
   
   def dequeue(self):
+    if self.size == 0:
+      return None
+    self.size -= 1
+    return self.storage.remove_head()
     pass
 
   def len(self):
+    return self.size
     pass
+
+# LAST IN FIRST OUT
+# FIRST IN FIRST OUT
+# QUEUE IS A LIST OR COLLECTION WITH THE RESTRICTION THAT INSERTION CAN BE PERFORMED AT ONE END (BACK) AND DELETION CAN BE PERFORMED AT OTHER END (FRONT)

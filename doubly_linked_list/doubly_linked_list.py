bc56bae78f7255894471787a08f9cc9d2424f174
codejoncode <jonathanjholloway@gmail.com>
2018-10-11 16:51:35 -0500

    started working on doublely_linked_list

--- doubly_linked_list/doubly_linked_list.py
+++ doubly_linked_list/doubly_linked_list.py
@@ -13,6 +13,13 @@ class ListNode:
   def delete(self):
     pass
 
+  def get_value(self):
+    return self.value
+  def get_next(self):
+    return self.next 
+  def set_next(self,new_next):
+    self.next = new_next
+
 class DoublyLinkedList:
   def __init__(self, node=None):
     self.head = node
@@ -22,7 +29,18 @@ class DoublyLinkedList:
     pass
 
   def remove_from_head(self):
-    pass
+    if self.head == None:
+        return self.head
+    else:
+      returning = self.head.get_value()
+      self.head = self.head.get_next()
+      if self.head is None:
+          self.tail = None
+      return returning 
+      # get previous on head isn't needed because this isn't set 
+      #up in a circular setting it was then if head was removed  previous
+      #would be the tail so a connect with the new head and tail would have to be made.
+      # because it is not a circle connection all other connections should be set.
 
   def add_to_tail(self, value):
     pass
@@ -37,4 +55,9 @@ class DoublyLinkedList:
     pass
 
   def delete(self, node):
+    """ just have to link out the node
+    take the connect of the node and connect
+    so left connection and right connection just needs to be 
+    connected. 
+    """
     pass

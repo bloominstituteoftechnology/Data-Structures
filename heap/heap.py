334704d6bc4de8f219a0ea179eb17732de04f686
Sukhada Gholba <sukhadagholba@gmail.com>
2018-10-11 13:53:33 -0700

    completed bubble_up method to move the element at the specified index up the heap if greater than the parent element

--- heap/heap.py
+++ heap/heap.py
@@ -17,7 +17,13 @@ class Heap:
     return len(self.storage)
 
   def _bubble_up(self, index):
-    pass
+    while (index-1)//2>=0:
+
+        if self.storage[index-1)//2] < self.storage[index]:
+            #swap values
+            self.storage[index], self.storage[index-1)//2]=self.storage[index-1)//2], self.storage[index]
+        
+        index=(index-1)//2 #update the index to parent's index to keep moving up
 
   def _sift_down(self, index):
     pass

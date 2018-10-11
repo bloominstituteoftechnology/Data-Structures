8bdbc567d0b33c38233534f028a81b042a622a4f
francistse23 <wtse.ucsd@gmail.com>
2018-10-11 14:25:45 -0700

    ListNode class in doubly_linked_list completed. test passed.

--- doubly_linked_list/doubly_linked_list.py
+++ doubly_linked_list/doubly_linked_list.py
@@ -5,13 +5,32 @@ class ListNode:
     self.next = next
 
   def insert_after(self, value):
-    pass
+    new_node = ListNode(value)
+    if self.next == None:
+      self.next = new_node
+      new_node.prev = self
+    else:
+      new_node.next = self.next
+      new_node.prev = self
+      self.next.prev = new_node
+      self.next = new_node
 
   def insert_before(self, value):
-    pass
+    new_node = ListNode(value)
+    if self.prev == None:
+      self.prev = new_node
+      new_node.next = self
+    else:
+      new_node.prev = self.prev
+      new_node.next = self
+      self.prev.next = new_node
+      self.prev = new_node
 
   def delete(self):
-    pass
+    self.next.prev = self.prev
+    self.prev.next = self.next
+    self.next = None
+    self.prev = None
 
 class DoublyLinkedList:
   def __init__(self, node=None):

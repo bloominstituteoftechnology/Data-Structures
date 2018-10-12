f87f0b2a369e52b1b753540fc84019e7b28f2d99
Charlie Winslow <cwinslow22@yahoo.com>
2018-10-11 19:22:34 -0500

    replaced my sift down implementation with the solution lecture version.  Now passing all tests, will go back and debug my original version

--- heap/heap.py
+++ heap/heap.py
@@ -4,14 +4,16 @@ class Heap:
 
   def insert(self, value):
     self.storage.append(value)
-    self._bubble_up(len(self.storage)-1)
+    self._bubble_up(len(self.storage) - 1)
 
   def delete(self):
-    deleted = self.storage.pop(0)
-    root_replace = self.storage.pop()
-    self.storage.insert(0, root_replace)
-    self._sift_down(0)
-    return deleted
+    if len(self.storage) > 0:
+      deleted = self.storage.pop(0)
+      if len(self.storage) > 0:
+        root_replace = self.storage.pop()
+        self.storage.insert(0, root_replace)
+        self._sift_down(0)
+      return deleted
 
   def get_max(self):
     return self.storage[0]
@@ -33,16 +35,33 @@ class Heap:
   def _sift_down(self, index):
     # index is the index of the element that will be moving down the heap
     # keep sifting the element at the given index down the heap until it reaches a valid spot
-    while self.storage[(2 * index + 1)] is not None:
-      # check to see which child is larger
-      if self.storage[(2 * index + 1)] > self.storage[(2 * index + 2)]:
-        # check to see if the child is larger than the parent
-        if self.storage[(2 * index + 1)] > self.storage[index]:
-          #swap values
-          self.storage[index], self.storage[(2 * index + 1)] = self.storage[(2 * index + 1)], self.storage[index]
-        index = 2 * index + 1
-      elif self.storage[(2 * index + 1)] <= self.storage[(2 * index + 2)]:
-        if self.storage[(2 * index + 2)] > self.storage[index]:
-          # swap values
-          self.storage[index], self.storage[(2 * index + 2)] = self.storage[(2 * index + 2)], self.storage[index]
-        index = 2 * index + 2
+    # while self.storage[(2 * index + 1)]:
+    # if (2 * index + 2) < len(self.storage):
+    #   # check to see which child is larger
+    #   if self.storage[(2 * index + 1)] > self.storage[(2 * index + 2)]:
+    #     # check to see if the child is larger than the parent
+    #     if self.storage[(2 * index + 1)] > self.storage[index]:
+    #       #swap values
+    #       self.storage[index], self.storage[(2 * index + 1)] = self.storage[(2 * index + 1)], self.storage[index]
+    #     index = 2 * index + 1
+    #   elif self.storage[(2 * index + 1)] <= self.storage[(2 * index + 2)]:
+    #     if self.storage[(2 * index + 2)] > self.storage[index]:
+    #       # swap values
+    #       self.storage[index], self.storage[(2 * index + 2)] = self.storage[(2 * index + 2)], self.storage[index]
+    #     index = 2 * index + 2
+
+
+    while index * 2 + 1 <= len(self.storage) - 1:
+      mc = self._max_child(index)
+      if self.storage[index] <  self.storage[mc]:
+        self.storage[index], self.storage[mc] = self.storage[mc], self.storage[index]
+      index = mc
+
+  def _max_child(self, index):
+    # check if the right child index is out of bouns of our strage array
+    if index * 2 + 2 > len(self.storage) - 1:
+      # return the left child index
+      return index * 2 + 1
+    else:
+      # return the index of child node that has larger value
+      return index * 2 + 1 if self.storage[index * 2 + 1] > self.storage[index * 2 + 2] else index * 2 + 2 

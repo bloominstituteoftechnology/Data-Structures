cebb54cb5078f5166b09ec6704baea24632ec27a
sliw <slioweng@gmail.com>
2018-10-13 00:48:58 +0800

    Improved binary search tree

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -7,32 +7,34 @@ class BinarySearchTree:
   def insert(self, value):
     if value <= self.value:
       if self.left:
-        self.insert(self.left, value) 
+        self.left.insert(value)
       else:
-        self.left = value
+        self.left = BinarySearchTree(value)
     else:
       if self.right:
-        self.insert(self.right, value) 
+        self.right.insert(value)
       else:
-        self.right = value
+        self.right = BinarySearchTree(value)
 
   def contains(self, target):
     if target == self.value:
       return True
-    elif target <= self.value:
-      self.contains(self.left, target)
-    elif target > self.value:
-      self.contains(self.right, target)
-    else:
-      return False
+    elif target < self.value:
+      if self.left:
+        return self.left.contains(target)
+    elif target >= self.value:
+      if self.right:
+        return self.right.contains(target)
+
+    return False
 
   def get_max(self):
-    temp_array = []
-
-    if self.left is None:
-      temp_array.append(self.value)
-
-    if self.right is None:
-      temp_array.append(self.value)
-
-    return self.get_max(self.left) + self.get_max(self.right)
+    if not self:
+      return None
+    max_value = self.value
+    current = self
+    while current:
+      if current.value > max_value:
+        max_value = current.value
+      current = current.right
+    return max_value

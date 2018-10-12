793bdd45a57d60bb5b34cfe25beba0e11d1baa1d
David Pok <david.pok@gmail.com>
2018-10-12 00:09:56 -0700

    i'm so bad at understanding these problems

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -5,10 +5,49 @@ class BinarySearchTree:
     self.right = None
 
   def insert(self, value):
-    pass
+#check to see if we need to go left or right
+    if value < self.value:
+#if no left child just set it here
+      if not self.left:
+#set the left value to BST value
+        self.left = BinarySearchTree(value)
+#if it is, we recurse to the left and call the insert method
+      else:
+        self.left.insert(value)
+#same thing for the right side
+    else:
+      if not self.right:
+        self.right = BinarySearchTree(value)
+      else:
+        self.right.insert(value)
 
   def contains(self, target):
-    pass
+#base case
+#if the value at the current node we are on matches target
+#we return true
+    if self.value == target:
+      return True
+#if target is less than value then we go left
+#WHAT PART OF THE CODE MAKES IT GO LEFT????????????????
+    elif target < self.value:
+#if self.left exists, then we call contains target on it
+      if self.left:
+        return self.left.contains(target)
+    else:
+      if self.right:
+        return self.right.contains(target)
+    return False
 
   def get_max(self):
-    pass
+#if self does not exist return none
+    if not self:
+      return None
+#or we can create a max_value variable and a current variable
+    max_value = self.value
+    current = self
+#if current value is greater than max than current becomes the max
+    while current:
+      if current.value > max_value:
+        max_value = current.value
+      current = current.right
+    return max_value
--- heap/heap.py
+++ heap/heap.py
@@ -3,19 +3,57 @@ class Heap:
     self.storage = []
 
   def insert(self, value):
-    pass
+    self.storage.append(value)
+    self._bubble_up(len(self.storage) - 1)
 
   def delete(self):
-    pass
+    retval = self.storage[0]
+    #repalce the 1st storage elem with the last elem in the heap
+    self.storage[0] = self.storage[-1]
+    #remove the last elem in heap
+    self.storage.pop()
+    self._sift_down(0)
+    return retval
 
   def get_max(self):
-    pass
+    return self.storage[0]
 
   def get_size(self):
-    pass
+    return len(self.storage)
 
   def _bubble_up(self, index):
-    pass
+#index is the index of the element that will be moving up the heap
+#keep bubbling the element at the given index up the heap until it reaches a valid spot
+    while (index - 1) // 2 >= 0:
+#check to see if the element's parent's value is less than the current value
+      if self.storage[(index - 1) // 2] < self.storage[index]:
+        #swap the values
+        self.storage[index], self.storage[(index - 1 ) // 2] = self.storage[(index - 1) // 2], self.storage[index]
+#update the index to be the parent's index so that we can continue moving up the heap
+      index = (index - 1) // 2
 
   def _sift_down(self, index):
-    pass
+#as long as left index is in bound of total indices we keep moving
+    while index * 2 + 1 <= len(self.storage) - 1:
+#figure out the larger of the 2 children, assuming there are 2
+  #create a function and define it below
+  #this function receives the index, look at the children of that index
+  #then return the index of the higher priority
+      maxChild = self._max_child(index)
+#check to see if we need to swap
+      if self.storage[index] < self.storage[maxChild]:
+        self.storage[index], self.storage[maxChild] = self.storage[maxChild], self.storage[index]
+#then update the index
+      index = maxChild
+
+#DEFINE THE MAXCHILD FUNCTION FROM ABOVE
+  def _max_child(self, index):
+#check if the right child index is out of bounds of storage array
+#we check the right side because
+    if index * 2 + 2 > len(self.storage) - 1:
+      #return the left child index
+      return index * 2 + 1
+    else:
+#return the index connected to the child node that has the larger value
+#THIS IS A PYTHON TERNARY
+      return index * 2 + 1 if self.storage[index * 2 + 1] > self.storage[index * 2 + 2] else index * 2 + 2

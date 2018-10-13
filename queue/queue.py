22231bb5e322b22dc29da791ce190ec80fb00268
Naaz Shaikh <naazhome@Naazs-MBP-3.hsd1.or.comcast.net>
2018-10-12 18:27:41 -0700

    init

--- queue/queue.py
+++ queue/queue.py
@@ -2,16 +2,17 @@ import sys
 sys.path.append('../linked_list')
 from linked_list import LinkedList
 
+
 class Queue:
-  def __init__(self):
-    self.size = 0
-    self.storage = LinkedList()
+    def __init__(self):
+        self.size = 0
+        self.storage = LinkedList()
 
-  def enqueue(self, item):
-    pass
-  
-  def dequeue(self):
-    pass
+    def enqueue(self, item):
+        pa
 
-  def len(self):
-    pass
+    def dequeue(self):
+        pass
+
+    def len(self):
+        pass

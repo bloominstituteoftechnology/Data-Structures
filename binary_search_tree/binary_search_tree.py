4bd02247ac8bd688d51a8cd0f8e13076e0c00ab4
Munsu <dark_specineff2002@yahoo.com>
2018-10-15 07:50:11 -0700

    Linked Lists, Queues, Binary Search Trees, Heaps

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -1,14 +1,39 @@
 class BinarySearchTree:
-  def __init__(self, value):
-    self.value = value
-    self.left = None
-    self.right = None
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
 
-  def insert(self, value):
-    pass
+    def insert(self, value):
+        if value < self.value:
+            if not self.left:
+                self.left = BinarySearchTree(value)
+            else:
+                self.left.insert(value)
+        else:
+            if not self.right:
+                self.right = BinarySearchTree(value)
+            else:
+                self.right.insert(value)
 
-  def contains(self, target):
-    pass
+    def contains(self, target):
+        if self.value == target:
+            return True
+        elif target < self.value:
+            if self.left:
+                return self.left.contains(target)
+        else:
+            if self.right:
+                return self.right.contains(target)
+        return False
 
-  def get_max(self):
-    pass
+    def get_max(self):
+        if not self:
+            return None
+        max_value = self.value
+        current = self
+        while current:
+            if current.value > max_value:
+                max_value = current.value
+            current = current.right
+        return max_value
--- linked_list/linked_list.py
+++ linked_list/linked_list.py
@@ -3,33 +3,67 @@ Class that represents a single linked
 list node that holds a single value
 and a reference to the next node in the list
 """
+
+
 class Node:
-  def __init__(self, value=None, next_node=None):
-    self.value = value
-    self.next_node = next_node
+    def __init__(self, value=None, next_node=None):
+        self.value = value
+        self.next_node = next_node
 
-  def get_value(self):
-    return self.value
+    def get_value(self):
+        return self.value
 
-  def get_next(self):
-    return self.next_node
+    def get_next(self):
+        return self.next_node
 
-  def set_next(self, new_next):
-    self.next_node = new_next
+    def set_next(self, new_next):
+        self.next_node = new_next
+
 
 class LinkedList:
-  def __init__(self):
-    self.head = None
-    self.tail = None
+    def __init__(self):
+        self.head = None
+        self.tail = None
 
-  def add_to_tail(self, value):
-    pass
+    def add_to_tail(self, value):
+        new_node = Node(value, None)
+        if not self.head:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.set_next(new_node)
+            self.tail = new_node
 
-  def remove_head(self):
-    pass
+    def remove_head(self):
+        if not self.head:
+            return None
+        if not self.head.get_next():
+            value = self.head.get_value()
+            self.head = None
+            self.tail = None
+            return value
+        else:
+            value = self.head.get_value()
+            self.head = self.head.get_next()
+            return value
 
-  def contains(self, value):
-    pass
+    def contains(self, value):
+        if not self.head:
+            return False
+        current = self.head
+        while current:
+            if current.get_value() == value:
+                return True
+            current = current.get_next()
+        return False
 
-  def get_max(self):
-    pass
+    def get_max(self):
+        if not self.head:
+            return None
+        max_value = self.head.get_value()
+        current = self.head.get_next()
+        while current:
+            if current.get_value() > max_value:
+                max_value = current.get_value()
+            current = current.get_next()
+        return max_value

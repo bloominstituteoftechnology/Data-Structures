c712c528703ab0404bbfe4806b96ef44d89b6333
decagondev <decagondevelopment@gmail.com>
2018-11-14 21:22:13 +0000

    (MVP) : queue : done len method : completed queue with all tests passing : day 1 mvp complete

--- queue/queue.py
+++ queue/queue.py
@@ -24,16 +24,48 @@ class Queue:
     return previous_head
 
   def len(self):
-    # TODO: do some length / size setting in this method with some logic after a coffee break
+    # grab the current head
+    current_head = self.storage.head
+
+    # if there is no current head
+    if current_head == None:
+
+      # set the size to zero and return the size to the caller
+      self.size = 0
+      return self.size
+
+    # if there is no next node from the current head
+    if current_head.get_next() == None:
+
+      # set size to 1 and return the size to the caller
+      self.size = 1
+      return self.size
+    else:
+
+      # otherwise create a count variable initialize it to 1
+      count = 1
+
+      #and while there are still nodes in the linked list
+      while current_head.get_next() != None:
+
+        # increment count and set the current_head to the next node
+        count += 1
+        current_head = current_head.get_next()
+
+      # set the size to the current value of count and return it to the caller
+      self.size = count
+      return self.size
+      
+    # if all conditions fall out then just return the current size to the caller
     return self.size
 
 # some debug tests
-q = Queue()
+# q = Queue()
 
-q.enqueue(2)
-q.enqueue(4)
-q.enqueue(100)
-q.enqueue(1)
+# q.enqueue(2)
+# q.enqueue(4)
+# q.enqueue(100)
+# q.enqueue(1)
 
-print(q.storage.get_max())
-print(q.dequeue())
+# print(q.storage.get_max())
+# print(q.dequeue())

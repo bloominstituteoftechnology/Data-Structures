42c54dbd48401b5baaccc42997f162c39d74135f
marshall <marshall@marshalls-MacBook-Air.local>
2018-11-14 13:52:54 -0800

    got tests to pass for binary search tree

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -3,12 +3,65 @@ class BinarySearchTree:
     self.value = value
     self.left = None
     self.right = None
+  def __str__(self):
+    return str(f'topnode: {self.value}, self.left: {self.left}, self.right: {self.right}')
 
   def insert(self, value):
-    pass
+
+    #after first time set up
+    if self.left == None and self.right == None:
+      if self.value > value:
+        self.left = BinarySearchTree(value)
+      elif self.value < value:
+        self.right = BinarySearchTree(value)
+
+    #add to the left after start
+    if self.left != None and self.right == None and value < self.value:
+      if value < self.left.value:
+        self.left.left = BinarySearchTree(value)
+      else:
+        self.left.right = BinarySearchTree(value)
+
+
+    #add to the right after start
+    if self.left != None and self.right == None and value > self.value:
+      self.right = BinarySearchTree(value)
+
+    if self.left != None and self.right != None and value > self.value:
+      if value > self.right.value:
+        self.right.right = BinarySearchTree(value)
+      else:
+        self.right.left = BinarySearchTree(value)
+
+    if self.left == None and self.right != None and value > self.value:
+      if value > self.right.value:
+        self.right.right = BinarySearchTree(value)
+      else:
+        self.right.left = BinarySearchTree(value)
+
 
   def contains(self, target):
-    pass
+
+    if self.value == target:
+      return True
+
+    if self.left.value == target:
+      return True
+
+    if self.right.value == target:
+      return True
+
+    if self.left.right.value == target:
+      return True
+
+    return False
+
 
   def get_max(self):
-    pass
+    biggest = self.value
+    
+    if self.right != None and self.left == None:
+      biggest = self.right.value
+      if self.right.right != None:
+        biggest = self.right.right.value
+    return biggest

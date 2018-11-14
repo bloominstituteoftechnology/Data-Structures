a11fa8d1af66ab6cfcc574db4482ad071e275029
duckets615 <33557104+duckets615@users.noreply.github.com>
2018-11-14 18:47:16 -0500

    linked list complete, search tree halfway

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -5,8 +5,19 @@ class BinarySearchTree:
     self.right = None
 
   def insert(self, value):
-    pass
-
+    if self.value:
+      if value < self.value:
+        if self.left is None:
+          self.left = BinarySearchTree(value)
+        else:
+          self.left.insert(value)
+      if value >= self.value:
+        if self.right is None:
+          self.right = BinarySearchTree(value)
+        else:
+          self.right.insert(value)
+    else:
+      self.value = value
   def contains(self, target):
     pass
 

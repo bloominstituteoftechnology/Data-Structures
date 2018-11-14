8d793c60394fae29b3fc33bbeff056c0c3f8c836
Timothy Hoang <timh1203@gmail.com>
2018-11-14 17:44:08 -0500

    Complete add_to_tail

--- linked_list/linked_list.py
+++ linked_list/linked_list.py
@@ -3,33 +3,44 @@ Class that represents a single linked
 list node that holds a single value
 and a reference to the next node in the list
 """
+
+
 class Node:
-  def __init__(self, value=None, next_node=None):
-    self.value = value
-    self.next_node = next_node
+    def __init__(self, value=None, next_node=None):
+        self.value = value
+        self.next_node = next_node
 
-  def get_value(self):
-    return self.value
+    def get_value(self):
+        return self.value
 
-  def get_next(self):
-    return self.next_node
+    def get_next(self):
+        return self.next_node
 
-  def set_next(self, new_next):
-    self.next_node = new_next
+    def set_next(self, new_next):
+        self.next_node = new_next
+
 
 class LinkedList:
-  def __init__(self):
-    self.head = None
-    self.tail = None
+    def __init__(self):
+        self.head = None
+        self.tail = None
 
-  def add_to_tail(self, value):
-    pass
+    def add_to_tail(self, value):
+        new_node = Node(value)
+        if self.head == None:
+            self.head = new_node
+            self.tail = new_node
+            return
+        else:
+            self.tail.set_next(new_node)
+            self.tail = new_node
+            return
 
-  def remove_head(self):
-    pass
+    def remove_head(self):
+        pass
 
-  def contains(self, value):
-    pass
+    def contains(self, value):
+        pass
 
-  def get_max(self):
-    pass
+    def get_max(self):
+        pass

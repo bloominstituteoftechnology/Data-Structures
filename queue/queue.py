adc9e0e0296386c5ccbc7dd3f6be710af79d6883
Tsai Huang <tsaihuangsd@gmail.com>
2018-11-14 15:38:11 -0800

    Completed working queue.py

--- queue/queue.py
+++ queue/queue.py
@@ -8,10 +8,23 @@ class Queue:
     self.storage = LinkedList()
 
   def enqueue(self, item):
-    pass
+    self.storage.add_to_tail(item)
   
   def dequeue(self):
-    pass
+    return self.storage.remove_head()
 
   def len(self):
-    pass
+    current_node = self.storage.head
+    if self.storage.head == None:
+      self.size = 0
+      return 0
+    elif self.storage.head == self.storage.tail:
+      self.size = 1
+      return 1
+    else:
+      count = 0
+      while current_node != None:
+        count +=1
+        current_node = current_node.get_next()
+      self.size = count
+      return count

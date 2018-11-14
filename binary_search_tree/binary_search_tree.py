0649af48e3bf308813ceb7fa06c8818a0116a6b0
Csillag61 <csillagok@gmail.com>
2018-11-14 17:10:01 -0500

    BinaryST insert, getMax

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -5,10 +5,25 @@ class BinarySearchTree:
     self.right = None
 
   def insert(self, value):
-    pass
+      if value< self.value:
+        if self.left is None:
+          self.left = BinarySearchTree(value)
+        else:
+         self.left.insert(value)
+      else:
+        if self.right is None:
+           self.right = BinarySearchTree(value)
+        else:
+          self.right.insert(value)
+
+    
 
   def contains(self, target):
     pass
 
   def get_max(self):
-    pass
+    # for a max value keep looking right
+    if self.right:
+      return self.right.get_max()
+    return self.value
+

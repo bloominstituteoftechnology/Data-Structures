73506e31dd9af62f4137958603529fe2ab29fc9d
decagondev <decagondevelopment@gmail.com>
2018-11-14 20:53:52 +0000

    (MVP) : queue : added some todos

--- queue/queue.py
+++ queue/queue.py
@@ -8,12 +8,13 @@ class Queue:
     self.storage = LinkedList()
 
   def enqueue(self, item):
+    # add the item to the tail of the storage of the queue
     self.storage.add_to_tail(item)
-    self.size += 1
   
   def dequeue(self):
+    # TODO: add some logic here
     self.storage.remove_head()
-    self.size -= 1
 
   def len(self):
+    # TODO: do some length / size setting in this method with some logic after a coffee break
     return self.size

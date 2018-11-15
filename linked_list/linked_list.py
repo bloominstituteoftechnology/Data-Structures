9e651daef8751ba624e2aa3f00250f52f959ccba
lyang9 <lyang9@buffalo.edu>
2018-11-14 23:40:46 -0500

    Added comments

--- linked_list/linked_list.py
+++ linked_list/linked_list.py
@@ -23,44 +23,44 @@ class LinkedList:
     self.tail = None
 
   def add_to_tail(self, value):
-    new_node = Node(value)
-    if self.head == None:
-      self.head = new_node
-      self.tail = new_node
+    new_node = Node(value)    # create a new node and put in value
+    if self.head == None:     # if linked list is empty 
+      self.head = new_node    # make new node for head
+      self.tail = new_node    # make new node for tail
     else:
-      self.tail.next_node = new_node
-      self.tail = self.tail.next_node
+      self.tail.next_node = new_node    # create new node after tail
+      self.tail = self.tail.next_node   # set new node as new tail
 
   def remove_head(self):
-    removed = self.head
-    if self.head == None:
-      return None
-    elif self.head.next_node == None:
-      self.head = None
-      self.tail = None
+    removed = self.head                 # assign removed to head
+    if self.head == None:               # if linked list is empty
+      return None                       # return None
+    elif self.head.next_node == None:   # if head next node is empty
+      self.head = None                  # head is empty (when head value is removed)
+      self.tail = None                  # tail is empty
     else:
-      self.head = self.head.next_node
-    return removed.value
+      self.head = self.head.next_node   # set new head and head next node
+    return removed.value                # removed head value
 
   def contains(self, value):
-    if self.head == None:
-      return False
+    if self.head == None:         # if linked list is empty  
+      return False                # return False
     else:
-      node = self.head
-      while node is not None:
-        if node.value == value:
-          return True
-        node = node.next_node
-      return False
+      node = self.head            # assign node to head
+      while node is not None:     # while node is not empty
+        if node.value == value:   # if the node value is equal to value
+          return True             # return True
+        node = node.next_node     # assign node as the next node (move on to next node)
+      return False                # return False
 
   def get_max(self):
-    if self.head == None:
-      return None
+    if self.head == None:             # if linked list is empty
+      return None                     # return None
     else:
-      start = self.head
-      largest = self.head.value
-      while start:
-        if start.value > largest:
-          largest = start.value
-        start = start.next_node
-      return largest
+      start = self.head               # assign start to head
+      largest = self.head.value       # assign largest to the head value
+      while start:                    # while start is not None
+        if start.value > largest:     # if start value is greater than largest (head value)
+          largest = start.value       # assign largest as the start.value
+        start = start.next_node       # assign start to the next node
+      return largest                  # return largest

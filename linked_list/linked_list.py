f84e4a9b77963fd325dc308b0c08d31123fc4ff7
Timothy Hoang <timh1203@gmail.com>
2018-11-14 22:07:40 -0500

    Add remove_head

--- linked_list/linked_list.py
+++ linked_list/linked_list.py
@@ -32,12 +32,25 @@ class LinkedList:
             self.tail = new_node
             return
         else:
-            self.tail.set_next(new_node)
-            self.tail = new_node
+            self.tail.set_next(
+                new_node
+            )  # sets the 1st node to current node, providing connection
+            self.tail = new_node  # instantiates new node, new next_node is now None
             return
 
     def remove_head(self):
-        pass
+        prev_head = self.head
+        if prev_head == None:  # if no nodes exists
+            return None
+        elif (
+            prev_head == self.tail
+        ):  # if only 1 node exists where head and tail are same
+            self.head = None
+            self.tail = None
+            return prev_head.value
+        else:
+            self.head = self.head.get_next()  # set the next node as the current head
+            return prev_head.value
 
     def contains(self, value):
         pass

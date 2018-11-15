22121dd8f2a73d393cca911e36782ce267e0836b
Tsai Huang <tsaihuangsd@gmail.com>
2018-11-15 09:13:03 -0800

    Fixed minor errors in queue

--- queue/queue.py
+++ queue/queue.py
@@ -9,22 +9,28 @@ class Queue:
 
   def enqueue(self, item):
     self.storage.add_to_tail(item)
+    self.size +=1
   
   def dequeue(self):
-    return self.storage.remove_head()
+    if self.size == 0:
+      return None
+    else:
+      self.size -= 1
+      return self.storage.remove_head()
 
   def len(self):
-    current_node = self.storage.head
-    if self.storage.head == None:
-      self.size = 0
-      return 0
-    elif self.storage.head == self.storage.tail:
-      self.size = 1
-      return 1
-    else:
-      count = 0
-      while current_node != None:
-        count +=1
-        current_node = current_node.get_next()
-      self.size = count
-      return count
+    return self.size
+    # current_node = self.storage.head
+    # if self.storage.head == None:
+    #   self.size = 0
+    #   return 0
+    # elif self.storage.head == self.storage.tail:
+    #   self.size = 1
+    #   return 1
+    # else:
+    #   count = 0
+    #   while current_node != None:
+    #     count +=1
+    #     current_node = current_node.get_next()
+    #   self.size = count
+    #   return count

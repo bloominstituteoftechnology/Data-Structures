a76b58a13561b0441f4f62ec29f8f4e8ab772ee7
Jonathan T. Miles <jonathan.t.miles@gmail.com>
2018-11-14 19:20:42 -0500

    partial implementaiton of binary_search_tree

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -5,10 +5,21 @@ class BinarySearchTree:
     self.right = None
 
   def insert(self, value):
-    pass
+    if value > self.value:
+        self.left = BinarySearchTree(self.value)
+        self.value = value
+    else:
+        self.right = BinarySearchTree(self.value)
+        self.value = value
 
   def contains(self, target):
-    pass
+    if self.value == target:
+        return True
+    if self.right != None and self.left != None:
+        if self.right == target:
+            return True
+        elif self.left == target:
+            return True
 
   def get_max(self):
     pass

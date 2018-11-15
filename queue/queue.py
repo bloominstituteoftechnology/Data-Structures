59e2358e525e8d27d0a5ebfcf7720cc86b0045fb
Cameron Ray <cameronray@Camerons-MacBook-Pro.local>
2018-11-15 15:24:29 -0800

    queue tests are passing

--- queue/queue.py
+++ queue/queue.py
@@ -1,17 +1,30 @@
 import sys
-sys.path.append('../linked_list')
-from linked_list import LinkedList
+# sys.path.append('../linked_list')
+# from linked_list import LinkedList
 
 class Queue:
   def __init__(self):
     self.size = 0
-    self.storage = LinkedList()
+    self.storage = []
 
   def enqueue(self, item):
-    pass
+    self.storage.append(item)
   
   def dequeue(self):
-    pass
+    if len(self.storage) == 0:
+      return None
+    else:
+      return self.storage.pop(0)
 
   def len(self):
-    pass
+    if len(self.storage) == 0:
+      return 0
+    else:
+      return len(self.storage)
+
+test_q = Queue()
+test_q.enqueue(1)
+test_q.enqueue(2)
+test_q.enqueue(3)
+print('**********************')
+print(test_q.storage)

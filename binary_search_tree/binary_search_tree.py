714257ad55d34b1c37b4805452a99d822ffd7937
duckets615 <33557104+duckets615@users.noreply.github.com>
2018-11-15 17:51:44 -0500

    binary complete

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -18,8 +18,30 @@ class BinarySearchTree:
           self.right.insert(value)
     else:
       self.value = value
+
   def contains(self, target):
-    pass
+    if target == self.value:
+      return True
+    elif target > self.value:
+      if self.right is None:
+        return False
+      elif target == self.right.value:
+        return True
+      else:
+        self.right.contains(target)
+    elif target < self.value:
+      if self.left is None:
+        return False
+      elif target == self.left.value:
+        return True
+      else:
+        self.left.contains(target)
+    else:
+      return False
+
 
   def get_max(self):
-    pass
+      if self.right is None:
+        return self.value
+      else:
+        return self.right.get_max()

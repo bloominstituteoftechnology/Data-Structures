2b6b940f6ccec76f3e7b7622216c89b70afed595
James Cassidy <cassidyjamesw@gmail.com>
2018-11-15 17:30:45 -0500

    binary search tree working

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -5,10 +5,24 @@ class BinarySearchTree:
     self.right = None
 
   def insert(self, value):
-    pass
+    if self.left is None and value < self.value:
+       self.left = BinarySearchTree(value)
+       return
+    if self.right is None and value >= self.value:
+      self.right = BinarySearchTree(value)
+      return
+    branch = self.left if value < self.value else self.right
+    branch.insert(value)
 
   def contains(self, target):
-    pass
-
+    if self.value == target:
+      return True
+    branch = self.left if target < self.value else self.right
+    if branch is None:
+      return False
+    return branch.contains(target)
+    
+    
   def get_max(self):
-    pass
+    right_branch = self.value if self.right is None else self.right.get_max()
+    return max(self.value, right_branch)

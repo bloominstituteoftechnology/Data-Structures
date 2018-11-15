453f17678f5144f7d9acb7888c0e3daaa1f6fb0e
Csillag61 <csillagok@gmail.com>
2018-11-14 22:20:38 -0500

    bubble up the heap

--- heap/heap.py
+++ heap/heap.py
@@ -8,7 +8,13 @@ class Heap:
     self._bubble_up(len(self.storage)-1)
 
   def delete(self):
-    pass
+    value= self.storage[0]
+    # first element replaced by last element
+    self.storage[0]=self.storage[(len(self.storage)-1)]
+    #delete last -pop off
+    self.storage.pop()
+    self._sift_down(0)
+    return value
 
   def get_max(self):
    return self.storage[0]
@@ -18,7 +24,15 @@ class Heap:
     
 
   def _bubble_up(self, index):
-    pass
+    # bubble up up up the heap
+    # use the parent formula i-1//2 
+    # swap values!!!
+    # update the index
+    while (index-1)//2>=0:
+      if self.storage[(index-1)//2]<self.storage[index]:
+        self.storage[index], self.storage[(index-1)//2] = self.storage[(index-1)//2], self.storage[index]
+      index = ((index-1)//2)
+    
 
   def _sift_down(self, index):
     pass

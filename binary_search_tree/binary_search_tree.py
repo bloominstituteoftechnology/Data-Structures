93cdb6fc33e5c53d3bf925065db1e4e6861e4593
MichelleOk <umbralcompendium@gmail.com>
2018-11-15 16:47:55 -0700

    woring on heap still

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -1,14 +1,68 @@
+#https://www.tutorialspoint.com/python/python_binary_tree.htm
+# Tree represents the nodes connected by edges. It is a non-linear data structure. 
+# It has the following properties.
+# One node is marked as Root node.
+# Every node other than the root is associated with one parent node.
+# Each node can have an arbiatry number of chid node.
+# We designate one node as root node and then add more nodes as child nodes.
+
+
 class BinarySearchTree:
   def __init__(self, value):
+    # We just create a Node class and add assign a value to the node.
+    # This becomes tree with only a root node.
     self.value = value
     self.left = None
     self.right = None
 
   def insert(self, value):
-    pass
+    #compare the new value wit the parent node
+    if self.value:
+      if value < self.value:
+        if self.left is None:
+          self.left = BinarySearchTree(value)
+        else:
+          self.left.insert(value)
+      elif value > self.value:
+        if self.right is None:
+          self.right = BinarySearchTree(value)
+        else:
+          self.right.insert(value)
+    else:
+      self.value = value
 
   def contains(self, target):
-    pass
+    #Searching for a value in a tree involves comparing the incoming value 
+    # with the value exiting nodes. Here also we traverse the nodes from left 
+    # to right and then finally with the parent. If the searched for value does 
+    # not match any of the exitign value, then we return not found message else 
+    # the found message is returned.
+    if target < self.value:
+      if self.left is None:
+        return str(target) + "Sorry Not found"
+      return self.left.contains(target)
+    elif target > self.value:
+      if self.right is None:
+        return str(target) + "Sorry Not found"
+      return self.right.contains(target)
+    else:
+      print(str(self.value) + " is found")
+
 
   def get_max(self):
     pass
+
+
+  # #Print Tree
+  # def PrintTree(self):
+  #   if self.left:
+  #     self.left.PrintTree()
+  #   print(self.value),
+  #   if self.right:
+  #     self.right.PrintTree()
+
+  # #use insert to add nodes
+  # root = Node(12)
+  # root.insert(8)
+  # root.insert(13)
+  # #etc
--- heap/heap.py
+++ heap/heap.py
@@ -1,12 +1,23 @@
+#https://www.tutorialspoint.com/python/python_heaps.htm
+#Heap is a special tree structure in which each parent node is less 
+# than or equal to its child node. Then it is called a Min Heap. If each 
+# parent node is greater than or equal to its child node then it is called 
+# a max heap. It is very useful is implementing priority queues where the queue 
+# item with higher weightage is given more priority in processing.
+
+#Just remeber we are turning the heap into an array
+import heapq
+
 class Heap:
   def __init__(self):
     self.storage = []
 
   def insert(self, value):
-    pass
+    heapq.heappush(self.value)
 
   def delete(self):
-    pass
+    #remove item at first index
+    heapq.heappop(self.storage)
 
   def get_max(self):
     pass
@@ -19,3 +30,16 @@ class Heap:
 
   def _sift_down(self, index):
     pass
+
+
+# Create a Heap
+# A heap is created by using python’s inbuilt library named heapq. This library has the relevant functions to carry out various operations on heap data structure. Below is a list of these functions.
+# heapify - This function converts a regular list to a heap. In the resulting heap the smallest element gets pushed to the index position 0. But rest of the data elements are not necessarily sorted.
+    # H = [21,2,3,7,4,88,23,100,42]
+    # Convert to a Heap
+    # heapq.heapify(H)
+    # print(H)
+    # //op [2,3,4,7,21,23,42,88,100]
+# heappush – This function adds an element to the heap without altering the current heap.
+# heappop - This function returns the smallest data element from the heap.
+# heapreplace – This function replaces the smallest data element with a new value supplied in the function.

ec98114cef43c3b44e72c490b20fbd16331a40d7
Andrew Liao <andrewoliao@gmail.com>
2018-11-14 16:21:31 -0800

    Implement BST insert and contains

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -5,10 +5,42 @@ class BinarySearchTree:
     self.right = None
 
   def insert(self, value):
-    pass
+    if value <= self.value:
+      if self.left:
+        self.left.insert(value)
+      else:
+        self.left = BinarySearchTree(value)
+    else:
+      if self.right:
+        self.right.insert(value)
+      else:
+        self.right = BinarySearchTree(value)
 
   def contains(self, target):
-    pass
+    if self.value == target:
+      return True
+    
+    if target < self.value: 
+      if self.left:
+        return self.left.contains(target)
+      else:
+        return False
+    else:
+      if self.right:
+        return self.right.contains(target)
+      else:
+        return False
+
 
   def get_max(self):
     pass
+
+
+bst = BinarySearchTree(20)
+bst.insert(19)
+bst.insert(22)
+bst.insert(10)
+bst.insert(40)
+
+
+print(bst.contains(10))

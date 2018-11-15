c3480d72ef9f2c435dbcccd3634014d1b02d7d8e
decagondev <decagondevelopment@gmail.com>
2018-11-15 20:54:14 +0000

    (MVP) : heap : delete method filled in

--- heap/heap.py
+++ heap/heap.py
@@ -14,15 +14,24 @@ class Heap:
 
   def delete(self):
     # if empty return none
-
+    if self.size == 0:
+      return None
     # create variables for the storage top and storage end
+    storage_top = self.storage[self.size - 1]
+    storage_end = self.storage.pop()
     # decrement the size
+    self.size -= 1
     # if size zero return storage top
+    if self.size == 0:
+      return storage_top
 
     # set storage of index 1 to storage end
+    self.storage[1] = storage_end
     # shift down
+    self._sift_down(1)
     #return storage top
-    pass
+    return storage_top
+    #pass
 
   def get_max(self):
     # return the storage at index 1

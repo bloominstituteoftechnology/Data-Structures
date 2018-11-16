11e0b21fbdc4c1074c2fcf8b153b4106fa71825f
BrianARuff <thelonewolf198927@gmail.com>
2018-11-16 00:09:15 -0500

    BST

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -1,52 +1,97 @@
 class Node:
-
-    def __init__(self, value=None, next_node=None):
-        self.value = value
-        self.next_node = next_node
-
-    def __str__(self):
-        return f'<Node: {self.value}>'
-
-    def get_value(self):
-        return self.value
-
-    def get_next(self):
-        return self.next_node
-
-    def set_next(self, new_next):
-        self.next_node = new_next
-
-
-class BinarySearchTree:
-
     def __init__(self, value=None):
-        self.value = Node(value)
+        self.value = value
         self.left = None
         self.right = None
 
-    def insert(self, value):
-        if value < self.value.get_value():
-            if self.left is None:
-                self.left = Node(value)
-            else:
-                self.left.insert(value)
 
-        if value > self.value.get_value():
-            if self.right is None:
-                self.right = Node(value)
-            else:
-                self.right.insert(value)
+class BinarySearchTree:
+    def __init__(self, v):
+        self.root = Node(v)
+        self.max = v
 
-    def contains(self, target):
-        pass
+    def get_root(self):
+        return self.root
 
     def get_max(self):
-        pass
+        return self.max
+
+    def insert(self, value):
+        if self.root is None:
+            self.root = Node(value)
+            self.max = value
+        else:
+            self._insert(value, self.root)
+
+    def _insert(self, value, current_node):
+        if value > self.max:
+            self.max = value
+
+        if value < current_node.value:
+            if current_node.left is None:
+                current_node.left = Node(value)
+            else:
+                self._insert(value, current_node.left)
+
+        if value > current_node.value:
+            if current_node.right is None:
+                current_node.right = Node(value)
+            else:
+                self._insert(value, current_node.right)
+        else:
+            print("Entry already in BST")
+
+    def print_tree(self):
+        if self.root is not None:
+            self._print_tree(self.root)
+
+    def _print_tree(self, current_node):
+        if current_node is not None:
+            self._print_tree(current_node.left)
+            print("Node: " + str(current_node.value))
+            self._print_tree(current_node.right)
+
+    def height(self):
+        if self.root is not None:
+            return self._height(self.root, 0)
+        else:
+            return 0
+
+    def _height(self, current_node, current_height):
+        if current_node is None:
+            return current_height
+        left_height = self._height(current_node.left, current_height + 1)
+        right_height = self._height(current_node.right, current_height + 1)
+        return max(left_height, right_height)
+
+    def contains(self, value):
+        if self.root is not None:
+            return self._search(value, self.root)
+        else:
+            return False
+
+    def _search(self, value, current_node):
+        if value is current_node.value:
+            return True
+        elif value < current_node.value and current_node.left is not None:
+            return self._search(value, current_node.left)
+        elif value > current_node.value and current_node.right is not None:
+            return self._search(value, current_node.right)
+        else:
+            return False
 
 
-bst = BinarySearchTree("50")
-bst.insert("25")
-bst.insert("75")
-print("SELF" + str(bst.value))
-print("LEFT: " + str(bst.left))
-print("RIGHT: " + str(bst.right))
+def fill_tree(bst, n=100, max_int=100):
+    from random import randint
+    for _ in range(n):
+        curr = randint(0, max_int)
+        bst.insert(curr)
+    return bst
+
+tree = BinarySearchTree(5)
+tree.insert(0)
+tree.insert(3)
+tree.insert(3)
+tree.print_tree()
+print(tree.get_root().left.right.value)
+print(tree.get_max())

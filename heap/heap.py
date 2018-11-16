aa14cd11fd4ad468e3d7948868d12db16796cd75
travis-russell <russell.travis@yahoo.com>
2018-11-15 18:34:39 -0600

    heap

--- heap/heap.py
+++ heap/heap.py
@@ -1,21 +1,36 @@
 class Heap:
-  def __init__(self):
-    self.storage = []
+    def __init__(self):
+        self.storage = []
 
-  def insert(self, value):
-    pass
+    def insert(self, value):
+        self.storage.append(value)
+        self._bubble_up(len(self.storage) - 1)
 
-  def delete(self):
-    pass
+    def delete(self):
+        pass
 
-  def get_max(self):
-    pass
+    def parent_index(self, index):
+        return (index - 1) // 2
 
-  def get_size(self):
-    pass
+    def parent_val(self, index):
 
-  def _bubble_up(self, index):
-    pass
+        return self.storage[self.parent_index(index)]
 
-  def _sift_down(self, index):
-    pass
+    def get_max(self):
+        if len(self.storage) == 0:
+            return None
+
+        return self.storage[0]
+
+    def get_size(self):
+        return len(self.storage)
+
+    def _bubble_up(self, index):
+        while index > 0 and self.storage[index] > self.parent_val(index):
+            pindex = self.parent_index(index)
+
+            self.storage[index], self.storage[pindex]
+            self.storage[index], self.storage[pindex]
+
+    def _sift_down(self, index):
+        pass

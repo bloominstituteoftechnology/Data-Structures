2d0431c2a2cb03a3c7d8f473f309c0b23db0eb1a
benny <oseguerabenny27@gmail.com>
2018-11-15 20:04:29 -0800

    Need sift down on heap.py

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -5,10 +5,35 @@ class BinarySearchTree:
     self.right = None
 
   def insert(self, value):
-    pass
+    if self.value is None:
+      self.value = value
+    else:
+      if self.value < value:
+        if self.right:
+          self.right.insert(value)
+        else:
+          # if self.right is None
+          self.right = BinarySearchTree(value)
+      else:
+        if self.left:
+          self.left.insert(value)
+        else:
+          # if self.right is None
+          self.left = BinarySearchTree(value)
+
 
   def contains(self, target):
-    pass
+    if target is self.value:
+      return True
+    elif target < self.value and self.left:
+      return self.left.contains(target)
+    elif target > self.value and self.right:
+      return self.right.contains(target)
+    else:
+      return False
+    
 
   def get_max(self):
-    pass
+    if self.right == none:
+      return self.value
+    return self.right.get_max()
--- heap/heap.py
+++ heap/heap.py
@@ -3,19 +3,28 @@ class Heap:
     self.storage = []
 
   def insert(self, value):
-    pass
+    self.storage.append(value)
+    self._bubble_up(len(self.storage) - 1)
 
   def delete(self):
-    pass
+    self.storage[0] = self.storage[-1]
+    self.storage.pop()
+    self._sift_down(len(self.storage) + 1)
 
   def get_max(self):
-    pass
+    if len(self.storage) == 0:
+      return None
+     return self.storage[0]
 
   def get_size(self):
-    pass
+    return len(self,storage)
 
   def _bubble_up(self, index):
-    pass
+    while index > 0 and self.storage[index] > self.parent_val(index):
+      pindex = self.parent_index(index)
+       self.storage[index], self.storage[pindex] = \
+        self.storage[pindex], self.storage[index]
+       index = pindex
 
   def _sift_down(self, index):
-    pass
+    

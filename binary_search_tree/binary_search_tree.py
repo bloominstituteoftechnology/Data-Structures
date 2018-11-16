6573038c613d2d48430d8e96c51474cd28349ef3
Cameron Ray <cameronray@Camerons-MacBook-Pro.local>
2018-11-15 18:31:27 -0800

    binary search tree insert and contains are passing all tests

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -5,13 +5,31 @@ class BinarySearchTree:
     self.right = None
 
   def insert(self, value):
-    if self is None:
-      self = value
-    else: 
-      if self
+    if self.value:
+      
+      if value < self.value:
+        if self.left:
+          self.left.insert(value)
+        else:
+          self.left = BinarySearchTree(value)
+        
+      else:
+        if self.right:
+          self.right.insert(value)
+        else:
+          self.right = BinarySearchTree(value)
+        
 
   def contains(self, target):
-    pass
-
+    if self.value == target:
+      return True
+    elif target < self.value:
+      branch = self.left
+    elif target > self.value:
+      branch = self.right
+      if branch == None:
+        return False
+    return branch.contains(target)
+    
   def get_max(self):
     pass
--- heap/heap.py
+++ heap/heap.py
@@ -3,19 +3,86 @@ class Heap:
     self.storage = []
 
   def insert(self, value):
-    pass
+    self.storage.append(value)
+    self._bubble_up(len(self.storage) - 1)
 
   def delete(self):
-    pass
+    self.storage[0] = self.storage[-1]
+
+
+  def left_index(self, index):
+    return index * 2 + 1
+
+  def left_val(self, index):
+    return self.storage[self.left_index(index)]
+
+  def right_index(self, index):
+    return index * 2 + 2
+  
+  def right_val(self, index):
+    return self.storage[self.right_index(index)]
+
+  def parent_index(self, index):
+    return (index - 1) // 2 
+
+  def parent_val(self, index):
+    return self.storage[self.parent_index(index)]
 
   def get_max(self):
-    pass
+    if len(self.storage) == 0:
+      return None
+    return self.storage[0]
 
   def get_size(self):
-    pass
+    return len(self.storage)
 
   def _bubble_up(self, index):
-    pass
+    while index > 0 and self.storage[index] > self.parent_val(index):
+      pindex = self.parent_index(index)
+
+      self.storage[index], self.storage[pindex] = \
+        self.storage[pindex], self.storage[index] 
+      index = pindex
+      print(self)
+
+  def get_max_child_index(self, index):
+    # if right is out of bounds, left must be bigger
+    if self.right_index(index) >= len(self.storage):
+      return self.left_index(index)
+
+    if self.right_val(index) > self.left_val(index):
+      return self.right_index(index)
+
+    return self.left_index(index)
 
   def _sift_down(self, index):
-    pass
+    while self.left_index(index) < len(self.storage):
+      max_child_index = self.get_max_child_index(index)
+
+      if self.storage[index] < self.storage[max_child_index]:
+        self.storage[index], self.storage[max_child_index] = \
+          self.storage[max_child_index], self.storage[index]
+        index = max_child_index
+        
+      else:
+        break
+
+  def __str__(self):
+      rv = "Heap:\n"
+
+      l = 1
+      c = 0
+
+      for i in range(len(self.storage)):
+        rv += str(self.storage[i]) + "  "
+
+        c += 1
+
+        if c >= l:
+          rv += "\n" + "  " * l
+          c = 0
+          l *= 2
+
+      rv += "\n"
+
+      return rv

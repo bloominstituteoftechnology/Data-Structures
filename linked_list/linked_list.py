a81300a1efd8bf8d4bb94324c42c230b3ae6b1c4
Farhan Farooqui <farhan@farooqui.net>
2019-01-22 18:57:58 -0500

    LinkedList complete

--- linked_list/linked_list.py
+++ linked_list/linked_list.py
@@ -22,14 +22,51 @@ class LinkedList:
     self.head = None
     self.tail = None
 
+  def insert(self, value):
+    new_node = Node(value)
+    new_node.set_next(self.head)
+    self.head = new_node
+
   def add_to_tail(self, value):
-    
+    node = Node(value)
+    if self.tail is not None:
+        self.tail.set_next(node)
+    else:
+        self.tail = node
 
   def remove_head(self):
-    pass
+    if self.head.next_node is not None:
+      next = self.head.next_node
+      self.head.set_next(next)
+      return next
 
   def contains(self, value):
-    pass
+    currentNode = self.head
+    while currentNode is not None:
+      if currentNode.value == value:
+        # print("yes")
+        return True
+      else:
+        currentNode = currentNode.next_node
+    return False
+      
 
   def get_max(self):
-    pass
+    currentNode = self.head
+    largest = self.head.value
+    while currentNode is not None:
+      if currentNode.value > largest:
+        largest = currentNode.value
+        currentNode = currentNode.next_node
+      else:
+        currentNode = currentNode.next_node
+    return largest
+
+# myList = LinkedList()
+# myList.insert(10)
+# myList.insert(2)
+# myList.insert(66)
+# myList.add_to_tail(1997)
+
+# myList.contains(10)
+

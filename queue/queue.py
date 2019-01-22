05d9e0336cdbf211284edc8d4c5d736004fca8f7
Conner Alegre <cga0@pm.me>
2019-01-22 15:15:47 -0700

    Implement Queue data structure with passing tests

--- queue/queue.py
+++ queue/queue.py
@@ -1,5 +1,8 @@
 import sys
-sys.path.append('../linked_list')
+import os
+
+sys.path.append(f'{os.getcwd()}/linked_list')
+
 from linked_list import LinkedList
 
 class Queue:
@@ -8,10 +11,15 @@ class Queue:
     self.storage = LinkedList()
 
   def enqueue(self, item):
-    pass
+    self.storage.add_to_tail(item)
   
   def dequeue(self):
-    pass
+    return self.storage.remove_head()
 
   def len(self):
-    pass
+    amount_of_nodes = 0
+    current_node = self.storage.head
+    while current_node is not None:
+      amount_of_nodes += 1
+      current_node = current_node.next_node
+    return amount_of_nodes

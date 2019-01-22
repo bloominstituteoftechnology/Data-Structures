35e3afdf9898f6663eacfed0c567dd8014e1e8fb
Drew <drewsup123c@gmail.com>
2019-01-22 15:45:14 -0700

    queue now passes all tests

--- queue/queue.py
+++ queue/queue.py
@@ -12,13 +12,23 @@ class Queue:
         self.storage.add_to_tail(item)
 
     def dequeue(self):
-        self.storage.remove_head()
         if self.storage.head is None:
             return None
-        if self.storage.head.value:
-            return self.storage.head.value - 1
-
-
+        else:
+            old_head = self.storage.head.value
+            self.storage.remove_head()
+            return old_head
 
     def len(self):
-        return self.size
+        head = self.storage.head
+        if head is None:
+            return 0
+        elif head.get_next() is None:
+            return 1
+        else:
+            count = 1
+            while head.get_next() is not None:
+                count += 1
+                head = head.get_next()
+            return count
+

06bc51b957eafce4eaddc08f537041dab4f32687
pedro <pedroluismontesinosrivera@gmail.com>
2019-01-22 18:13:16 -0500

    linked_list.py done fr

--- linked_list/linked_list.py
+++ linked_list/linked_list.py
@@ -1,8 +1,10 @@
+
 """
 Class that represents a single linked
 list node that holds a single value
 and a reference to the next node in the list
 """
+
 class Node:
   def __init__(self, value=None, next_node=None):
     self.value = value
@@ -17,24 +19,35 @@ class Node:
   def set_next(self, new_next):
     self.next_node = new_next
 
+
 class LinkedList:
   def __init__(self):
     self.head = None
-    self.tail = None 
+    self.tail = None
+    self.max = None
 
   def add_to_tail(self, value):
     node = Node(value)
-    if self.tail is not Node:
+    if self.tail is not None:
       self.tail.set_next(node)
+      self.tail = node 
+      if self.max.value < node.value:
+        self.max = node
     else:
-      self.head = node
-    self.tail = node
+      self.head = node 
+      self.tail = node
+      self.max = node
 
   def remove_head(self):
     if self.head is not None:
-      new_head = self.head.next_node
+      new_head = self.head
       del(self.head)
-      self.head = new_head
+      self.head = new_head.next_node
+
+      if self.head is None:
+        self.tail = None
+      return new_head.value
+
 
   def contains(self, value):
     curr_node = self.head
@@ -47,4 +60,7 @@ class LinkedList:
         curr_node = curr_node.next_node
 
   def get_max(self):
-    pass
+    if self.max is not None:
+      return self.max.value
+    else:
+      return None

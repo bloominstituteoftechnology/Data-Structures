da2fb01f941b6e114122d3513a0b7ec002ee8816
Hunter315 <hunter.mckay.smith@gmail.com>
2019-01-22 13:07:48 -0700

    linked_list.py completed

--- linked_list/linked_list.py
+++ linked_list/linked_list.py
@@ -23,13 +23,43 @@ class LinkedList:
     self.tail = None
 
   def add_to_tail(self, value):
-    pass
+    new_node = Node(value)
+    if self.tail:
+      self.tail.set_next(new_node)
+      self.tail = new_node
+    else:
+      self.head = new_node
+      self.tail = new_node
 
   def remove_head(self):
-    pass
+    if not self.head:
+      return None
+    else:
+      removed_head = self.head
+      if self.tail:
+        if self.head == self.tail:
+          self.head = None
+          self.tail = None
+        else: self.head = self.head.get_next()
+      else:
+        self.head = None
+    return removed_head.get_value()
 
   def contains(self, value):
-    pass
+    current_node = self.head
+    while current_node:
+      if current_node.get_value() == value:
+        return True
+      current_node = current_node.get_next()
+    return False
 
   def get_max(self):
-    pass
+    if not self.head:
+      return None
+    max_value = self.head.get_value()
+    current_node = self.head.get_next()
+    while current_node:
+      if current_node.get_value() > max_value:
+        max_value = current_node.get_value()
+      current_node = current_node.get_next()
+    return max_value

8a03bd66eae091a39df64b1363fd4adfa14db4b7
michaelagard <mdagard@gmail.com>
2019-01-22 18:22:07 -0500

    there was an attempt

--- linked_list/linked_list.py
+++ linked_list/linked_list.py
@@ -27,24 +27,26 @@ class LinkedList:
 
     def add_to_tail(self, value):
         node = Node(value)
+
         if self.tail is None:
             self.head = node
             self.tail = node
-
         else:
             self.tail.set_next(node)
             self.tail = node
 
     def remove_head(self):
-        # Check if the head is None
+        temp_var = self.head.get_value()
         if self.head is not None:
             new_head = self.head.next_node
-            del(self.head)
             self.head = new_head
+        return temp_var
 
     def contains(self, value):
         curr_node = self.head
+
         while True:
+
             if curr_node is None:
                 return False
             elif curr_node.value == value:
@@ -53,14 +55,17 @@ class LinkedList:
                 curr_node = curr_node.next_node
 
     def get_max(self):
-        pass
 
+        if self.head is None:
+            return None
 
-# ll = LinkedList()
-# ll.add_to_tail(6)
-# print(ll.head.value)
-# ll.add_to_tail(1)
-# print(ll.tail.value)
-# print(ll.contains(2))
-# print(ll.contains)
-# print()
+        node_head = self.head
+        value = node_head.get_value()
+
+        while node_head.get_next() is not None:
+            node_head = node_head.get_next()
+
+            if node_head.get_value() > value:
+                value = node_head.get_value()
+
+        return value

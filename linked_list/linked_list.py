02eb7004753cd5f91b7bc2621b08ba926c2ed792
lajawanti <myemail>
2019-01-22 16:52:28 -0500

    linked_list.py ... test-fails

--- linked_list/linked_list.py
+++ linked_list/linked_list.py
@@ -23,13 +23,49 @@ class LinkedList:
     self.tail = None
 
   def add_to_tail(self, value):
-    
+      #create new node with passed value
+      node_to_add = Node(value)
+      #check if head->next is pointing to Null , means empty linked list if yes head and tail will be same
+      if self.head == None:
+          self.head = node_to_add
+          self.tail = node_to_add
+      else:
+          self.tail = node_to_add    
 
   def remove_head(self):
-    pass
+      #check if the linked-list is empty .. return None
+      if self.head == None:
+          return None
+      else:
+          head = self.head
+          self.head = head.get_next()
+          return head.value
 
   def contains(self, value):
-    pass
+      #check if the linked-list is empty .. return None
+      if self.head == None:
+          return None
+      else:
+          current = self.head
+          while current.get_next() != None:
+            if current.get_value() == value:
+                return True
+            else:
+                current = current.get_next()
+        
+      return False
 
   def get_max(self):
-    pass
+      #check if the linked-list is empty .. return None
+      if self.head == None:
+          return None
+      else:
+          max = 0
+          current = self.head
+          while current != None:
+            if current.get_value() > max:
+                max = current.get_value()
+            else:
+                current = current.get_next()
+        
+      return max

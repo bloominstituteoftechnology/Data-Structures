d632dfa4046f3756cdfe34ba0e573b23c25b4e65
Katie Cleary <katiecleary@airmaster.lan>
2019-01-22 16:52:30 -0500

    tweaks for passing tests

--- linked_list/linked_list.py
+++ linked_list/linked_list.py
@@ -21,6 +21,8 @@ class LinkedList:
   def __init__(self):
     self.head = None
     self.tail = None
+    # And set it to max
+    self.max = None
 
   def add_to_tail(self, value):
     node = Node(value)
@@ -28,12 +30,14 @@ class LinkedList:
     if self.tail is not None:
       # Then set the tail's next to the new node
       self.tail.set_next(node)
+      self.tail = node
       # Set max to node if higher than current value
       if self.max.value < node.value:
         self.max = node
     else:
       # If it is empty, set the new node to the head
       self.head = node
+      self.tail = node
       # And set it to max
       self.max = node
 
@@ -41,11 +45,16 @@ class LinkedList:
     # Check if the head is None
     if self.head is not None:
       # set the head nodes next node value to a temp var
-      new_head = self.head.next_node
+      prev_head = self.head
       # delete the head node (not actually needed because python deallocates on its own if no more refrences)
       del(self.head)
       # Then set head to that temp
-      self.head = new_head
+      self.head = prev_head.next_node
+      
+      if self.head is None:
+        self.tail = None
+
+      return prev_head.value
 
   def contains(self, value):
     curr_node = self.head
@@ -54,7 +63,7 @@ class LinkedList:
       # 1. If the node is null, return False (exit case)
       if curr_node is None:
         return False
-      elif curr_node.value = value:
+      elif curr_node.value == value:
         # 2. Else if the node's value matches the query value, return True
         return True
       else:
@@ -63,4 +72,4 @@ class LinkedList:
 
 
   def get_max(self):
-    return self.max
+    pass

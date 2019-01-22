d1f18754fc9d8a411c648975c6d10d017de3ca72
alejandrok93 <alejandrok93@gmail.com>
2019-01-22 14:56:21 -0800

    Doubly Linked List, WIP

--- doubly_linked_list/doubly_linked_list.py
+++ doubly_linked_list/doubly_linked_list.py
@@ -40,25 +40,66 @@ class DoublyLinkedList:
     self.tail = node
 
   def add_to_head(self, value):
-    pass
+    new_node = ListNode(value)
+    if self.head is None:
+      self.head = new_node
+    elif self.head is not None:
+      self.head.insert_before(value)
+      self.head = new_node
+
 
   def remove_from_head(self):
-    pass
+    if self.head is None:
+      return None
+    elif self.head is not None:
+      tmp = self.head
+      if self.head.next is not None:
+        self.head.delete()
+        self.head = self.head.next
+      return tmp.value
 
   def add_to_tail(self, value):
-    pass
+    new_node = ListNode(value)
+    if self.tail is None:
+      self.tail = new_node
+    elif self.tail is not None:
+      self.tail.insert_after(value)
+      self.tail = new_node
 
   def remove_from_tail(self):
-    pass
+    if self.tail is not None:
+      tmp = self.tail
+      self.tail.delete()
+      return tmp
 
   def move_to_front(self, node):
-    pass
+    current_node = self.head
+    while current_node is not None:
+      if current_node == node:
+        self.add_to_head(node.value)
+        node.delete()
+      current_node = current_node.next
+
+
 
   def move_to_end(self, node):
-    pass
+    current_node = self.head
+    while current_node is not None:
+      if current_node == node:
+        self.add_to_tail(node.value)
+        node.delete()
+      current_node = current_node.next
 
   def delete(self, node):
-    pass
+    tmp = node
+    node.delete()
+    return tmp.value
     
   def get_max(self):
-    pass
+    current_node = self.head
+    max = 0
+    while current_node is not None:
+      if current_node.value > max:
+        max = current_node.value
+      current_node = current_node.next
+    return max

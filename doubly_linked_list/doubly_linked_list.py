3951d7e082b3c784399bc97b672c45218a536cc6
Austinlp4 <apendergrast44@gmail.com>
2019-01-22 17:49:46 -0600

    progress on double linked list

--- doubly_linked_list/doubly_linked_list.py
+++ doubly_linked_list/doubly_linked_list.py
@@ -40,18 +40,62 @@ class DoublyLinkedList:
     self.tail = node
 
   def add_to_head(self, value):
+    node = ListNode(value, self)
+    node.next = self.head
+    self.head = node
+    if self.tail == None:
+      self.tail = node
     pass
 
   def remove_from_head(self):
+    if self.head == None and self.tail == None:
+      return None
+
+    if self.head.next != None:
+      return_val = self.head.value
+      self.head = self.head.next
+      self.tail = self.head.value
+      # print(self)
+      return return_val
+    else:
+      return_val = self.head.value
+      self.head = None
+      self.tail = None
+      # print(self)
+      return return_val
     pass
 
   def add_to_tail(self, value):
+    node = ListNode(value, self)
+    node.next = self.tail
+    self.tail = node
+    if self.head == None:
+      self.head = node
     pass
 
   def remove_from_tail(self):
+    current_node = self.head
+
+    while current_node is not None:
+      current_node.prev.next = current_node.next
+      current_node.next.prev = current_node.prev
+
+      current_node = current_node.next
     pass
 
   def move_to_front(self, node):
+    current = self.head
+    node.next = self.head
+    node.prev = None
+
+    if current == None:
+      return None
+    
+    while current is not None:
+      current.prev.next = current
+      current.next.prev = current
+
+      current = current.next
     pass
 
   def move_to_end(self, node):
@@ -61,4 +105,14 @@ class DoublyLinkedList:
     pass
     
   def get_max(self):
-    pass
+    current = self.tail
+
+    if current == None:
+      return None
+    else:
+      maximum = self.tail.value
+      while current is not None:
+        if current.value > maximum:
+          maximum = current.value
+        current = current.next
+      return maximum

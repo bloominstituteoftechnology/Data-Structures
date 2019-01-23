5eed9105b3ca2457da31d0d2e0c6a765798109f5
Linh Bouniol <paulalinh89@gmail.com>
2019-01-23 10:38:08 -0800

    Completed queue

--- queue/queue.py
+++ queue/queue.py
@@ -8,10 +8,29 @@ class Queue:
     self.storage = LinkedList()
 
   def enqueue(self, item):
-    pass
+    
+    # Use add_to_tail method from LinkedList to add to the queue
+    # Increment size by 1 for every enqueue
+    
+    self.storage.add_to_tail(item)
+    self. size += 1
   
   def dequeue(self):
-    pass
+    
+    # Use remove_head method to dequeue
+    # Want to return the dequeued_item because the test expects a return value
+    # Decrement size by 1 for every dequeue as long as the size is not 0
+    
+    dequeued_item = self.storage.remove_head()
+    
+    if self.size > 0:
+      self.size -= 1
+    
+    return dequeued_item
+    
 
   def len(self):
-    pass
+    
+    # Return the queue's size
+    
+    return self.size

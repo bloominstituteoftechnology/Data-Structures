956e7914da7eb0a985c3f2be805207b118cc0d2f
IAN CAMERON <iancameron@IANs-MacBook-Pro.local>
2019-01-22 21:56:01 -0700

    adds queue

--- queue/queue.py
+++ queue/queue.py
@@ -1,17 +1,26 @@
-import sys
 sys.path.append('../linked_list')
+import sys
 from linked_list import LinkedList
+
 
 class Queue:
   def __init__(self):
     self.size = 0
     self.storage = LinkedList()
 
+  #add elements to queue
   def enqueue(self, item):
-    pass
-  
+    #check to avoid dupe entry
+    if item not in self.storage:
+      self.storage.insert(0, item)
+      return True
+    return False
+
+  #delete last item from queue
   def dequeue(self):
-    pass
+    if len(self.storage) > 0: 
+      return self.storage.pop()
+    return ("queue empty")
 
   def len(self):
-    pass
+    return len(self.storage)

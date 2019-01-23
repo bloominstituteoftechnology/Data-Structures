dfc6cffc532da9b7cfae7bb7447a769621e30d03
lajawanti <myemail>
2019-01-23 16:17:23 -0500

    BST insert method

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -5,7 +5,34 @@ class BinarySearchTree:
     self.right = None
 
   def insert(self, value):
-    pass
+      """check
+            -- if value is less than root.value
+                 go to left
+            -- if value is greater than root.value
+                 go to right
+      """
+      #check is passed BST is empty
+      root = self
+      if root.value is None:
+          return None
+      
+      # else check value_to_be_inserted is less than root value
+      if value < root.value:
+          #if the left child is empty insert the value_to_be_inserted as left child
+          if root.left is None:
+              root.left = BinarySearchTree(value)
+          else:
+              #if left child is present recursive call to check where to insert...
+              root.left.insert(value)
+      else:
+          #if value_to_be_inserted is greater than root value go to right side
+          #if right-child empty insert value_to_be_inserted
+          if root.right is None:
+              root.right = BinarySearchTree(value)
+          else:
+              #if right-child is there .. recursive call to check for value insertion
+              root.right.insert(value)
+
 
   def contains(self, target):
     pass

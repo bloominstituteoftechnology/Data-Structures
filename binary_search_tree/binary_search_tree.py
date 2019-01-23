6bc2b4a8e5214398c33b0e556f038b808b27ede9
Conner Alegre <cga0@pm.me>
2019-01-23 14:56:33 -0700

    Implement BST insert with tests passing

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -5,7 +5,25 @@ class BinarySearchTree:
     self.right = None
 
   def insert(self, value):
-    pass
+    # 1. Create a new tree to add
+    # 2. If the new tree is less than the current trees value, go left
+    # 2-1. If there is no left node, set the left node to the new tree
+    # 2-2. Otherwise call insert on the current left node with the value to add (Recursively)
+    # 3. If the new tree is greater than the current trees value, go right
+    # 3-1. If there is no right node, set the right node to the new tree
+    # 3-2. Otherwise call insert on the current right now with the value to add (Recursively)
+    tree_to_add = BinarySearchTree(value) #1
+
+    if tree_to_add.value < self.value: #2
+      if self.left is None: #2-1
+        self.left = tree_to_add
+      else:
+        self.left.insert(tree_to_add.value) #2-2
+    elif tree_to_add.value > self.value: #3
+      if self.right is None: #3-1
+        self.right = tree_to_add
+      else:
+        self.right.insert(tree_to_add.value) #3-2
 
   def contains(self, target):
     pass

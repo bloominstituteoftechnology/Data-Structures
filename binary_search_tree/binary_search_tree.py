f8f1d3ecaec8df2b0c567534abf6d802cf4ff93c
Troy Amlee <troyamlee19@gmail.com>
2019-01-23 01:45:55 -0600

    [mvp] added comments to binary search tree

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -2,7 +2,11 @@ class BinarySearchTree:
   def __init__(self, value):
     self.value = value
     self.left = None
+    # Everything to the left of the binary tree,
+    #  the child nodes must be smaller than the parent node
     self.right = None
+    # Everything to the right of the binary tree,
+    # the child nodes must be bigger than the parent node
 
   def insert(self, value):
     pass

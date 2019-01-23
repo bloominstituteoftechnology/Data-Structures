42c65ea16610c34f2d9adbe56c5b4e3a8e845da3
Troy Amlee <troyamlee19@gmail.com>
2019-01-23 01:48:36 -0600

    [mvp] more comments [binary_search_tree]

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -1,6 +1,8 @@
 class BinarySearchTree:
   def __init__(self, value):
     self.value = value
+    # The value at the top is the root node
+    # The nodes with no children are leaf nodes
     self.left = None
     # Everything to the left of the binary tree,
     #  the child nodes must be smaller than the parent node

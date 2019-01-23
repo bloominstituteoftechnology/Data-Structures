dcdf72db21b03ea7ba4abbd27e5ccde616e79167
Kevin Brack <brackkevin@gmail.com>
2019-01-23 18:09:48 -0500

    binary_search_tree insert method complete

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -5,7 +5,16 @@ class BinarySearchTree:
     self.right = None
 
   def insert(self, value):
-    pass
+    if value < self.value:
+      if self.left == None:
+        self.left = BinarySearchTree(value)
+      else:
+        self.left.insert(value)
+    else:
+      if self.right == None:
+        self.right = BinarySearchTree(value)
+      else:
+        self.right.insert(value)
 
   def contains(self, target):
     pass
@@ -14,3 +23,40 @@ class BinarySearchTree:
     pass
 
 print('binary search tree ran')
+
+bst = BinarySearchTree(5)
+bst.insert(6)
+
+
+
+# import unittest
+
+# class BinarySearchTreeTests(unittest.TestCase):
+#   def setUp(self):
+#     self.bst = BinarySearchTree(5)
+
+#   def test_insert(self):
+#     self.bst.insert(2)
+#     self.bst.insert(3)
+#     self.bst.insert(7)
+#     self.bst.insert(6)
+#     self.assertEqual(self.bst.left.right.value, 3)
+#     self.assertEqual(self.bst.right.left.value, 6)
+
+#   def test_contains(self):
+#     self.bst.insert(2)
+#     self.bst.insert(3)
+#     self.bst.insert(7)
+#     self.assertTrue(self.bst.contains(7))
+#     self.assertFalse(self.bst.contains(8))
+
+#   def test_get_max(self):
+#     self.assertEqual(self.bst.get_max(), 5)
+#     self.bst.insert(30)
+#     self.assertEqual(self.bst.get_max(), 30)
+#     self.bst.insert(300)
+#     self.bst.insert(3)
+#     self.assertEqual(self.bst.get_max(), 300)
+
+# if __name__ == '__main__':
+#   unittest.main()

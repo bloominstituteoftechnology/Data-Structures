11d955204147256262c9d2b9de0d6701b518a008
Hunter315 <hunter.mckay.smith@gmail.com>
2019-01-23 12:39:56 -0700

    binary_search_tree.py competed

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -5,10 +5,37 @@ class BinarySearchTree:
     self.right = None
 
   def insert(self, value):
-    pass
+    if not self.value:
+      self.value = BinarySearchTree(value)
+      return
+
+    if self.value > value:
+      if not self.left:
+        self.left = BinarySearchTree(value)
+        return
+      self.left.insert(value)
+      return
+
+    if not self.right:
+      self.right = BinarySearchTree(value)
+      return
+    self.right.insert(value)
+    return
 
   def contains(self, target):
-    pass
+    if self.value == target:
+      return True
+
+    if self.value > target:
+      if self.left == None:
+        return False
+      return self.left.contains(target)
+
+    if self.right == None:
+      return False
+    return self.right.contains(target)
 
   def get_max(self):
-    pass
+    if self.right == None:
+      return self.value
+    return self.right.get_max()

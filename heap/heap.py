4d431c9dcd1c39a0215402e02c4a4ce476471918
ckirkwood94 <ckirkwood94@gmail.com>
2019-01-23 18:40:51 -0500

    Heap insert test passes

--- heap/heap.py
+++ heap/heap.py
@@ -1,21 +1,35 @@
 class Heap:
-  def __init__(self):
-    self.storage = []
+    def __init__(self):
+        self.storage = []
 
-  def insert(self, value):
-    pass
+    def insert(self, value):
+        # append value to storage
+        self.storage.append(value)
+        # bubble up pass in last index
+        self._bubble_up(len(self.storage) - 1)
 
-  def delete(self):
-    pass
+    def delete(self):
+        pass
 
-  def get_max(self):
-    pass
+    def get_max(self):
+        # Max at index 0
+        return self.storage[0]
 
-  def get_size(self):
-    pass
+    def get_size(self):
+        # return length of storage
+        return len(self.storage)
 
-  def _bubble_up(self, index):
-    pass
+    def _bubble_up(self, index):
+        parent_index = (index - 1) // 2
+        if parent_index == 0 and self.storage[parent_index] < self.storage[index]:
+            self._swap_indices(parent_index, index)
 
-  def _sift_down(self, index):
-    pass
+        if self.storage[parent_index] < self.storage[index]:
+            self._swap_indices(parent_index, index)
+            self._bubble_up(parent_index)
+
+    def _sift_down(self, index):
+        pass
+
+    def _swap_indices(self, index1, index2):
+        self.storage[index1], self.storage[index2] = self.storage[index2], self.storage[index1]

d9f413c33887d09716fc38744f04549d17dd0be5
Brock Maraee Rohloff <00AgentB@gmail.com>
2019-01-23 16:30:01 -0700

    Finish binary_search_tree passing all tests.

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -6,10 +6,25 @@ class BinarySearchTree:
 
   def insert(self, value):
     current_node = self
-    # while True: ##  First Pass Solution <-- Actual Insanity
-    #   if current_node.right == None and current_node.left == None:
-    #     current_node.right = BinarySearchTree(value)
-    #     break
+
+    while True: # Iterative Solution for Funsies
+      if value < current_node.value:
+        if current_node.left is None:
+          current_node.left = BinarySearchTree(value)
+          break
+        else:
+          current_node = current_node.left
+      else:
+        if current_node.right is None:
+          current_node.right = BinarySearchTree(value)
+          break
+        else: 
+          current_node = current_node.right
+
+    # while True: ##  First Pass Solution <-- WARNING - DOES NOT WORK - ACTUAL MADNESS
+      # if current_node.right == None and current_node.left == None:
+      #   current_node.right = BinarySearchTree(value)
+      #   break
     #   elif current_node.right == None:
     #     if value >= current_node.left.value:
     #       current_node.right = BinarySearchTree(value)
@@ -28,20 +43,22 @@ class BinarySearchTree:
     #     current_node = current_node.left
 
 
+        
+
+
   def contains(self, target):
-    pass
+    if self.value is target:
+      return True
+    elif target < self.value and self.left is not None:
+      return self.left.contains(target)
+    elif self.right is not None:
+      return self.right.contains(target)
+    else:
+      return False
 
   def get_max(self):
-    pass
-
-bst = BinarySearchTree(5)
-bst.insert(2)
-bst.insert(3)
-bst.insert(7)
-bst.insert(6)
-
-print(bst.value)
-print(bst.right.value)
-print(bst.left.value)
-print(bst.right.right.value)
+    if self.right is None:
+      return self.value
+    else:
+      return self.right.get_max()
 

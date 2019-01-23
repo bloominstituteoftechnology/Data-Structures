b2610930fa05390c6fce75bb84d05d5d2d3ab381
michaelagard <mdagard@gmail.com>
2019-01-23 16:34:28 -0500

    attempt #1

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -1,14 +1,26 @@
 class BinarySearchTree:
-  def __init__(self, value):
-    self.value = value
-    self.left = None
-    self.right = None
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
 
-  def insert(self, value):
-    pass
+    def insert(self, value):
+        # # check if self.value exists
+        # if self.value:
+        #     # check if value is less than self.value
+        #     if value < self.value:
+        #         # check if self.left exists
+        #         if self.left:
+        #             self.right = BinarySearchTree(value)
+        #         else:
+        #             self.left = BinarySearchTree(value)
+        # else:
+        #     self.value = value
+        # return
+        pass
 
-  def contains(self, target):
-    pass
+    def contains(self, target):
+        pass
 
-  def get_max(self):
-    pass
+    def get_max(self):
+        pass

44b252da25ec9ff4923791eac48649dce5178be7
Austinlp4 <apendergrast44@gmail.com>
2019-01-23 14:54:44 -0600

    finished binary search tree

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -5,10 +5,40 @@ class BinarySearchTree:
     self.right = None
 
   def insert(self, value):
+    if(self.value == None):
+      self.value = BinarySearchTree(value)
+    else:
+      self._insert(value, self)
+  def _insert(self, value, node):
+    if(value < node.value):
+      if(node.left != None):
+        self._insert(value, node.left)
+      else:
+        node.left = BinarySearchTree(value)
+    else:
+      if(node.right != None):
+        self._insert(value, node.right)
+      else:
+        node.right = BinarySearchTree(value)
+        
     pass
 
   def contains(self, target):
+    if target < self.value:
+      if self.left is None:
+        return False
+      return self.left.contains(target)
+    elif target > self.value:
+      if self.right is None:
+        return False
+      return self.right.contains(target)
+    else:
+      return self.value
     pass
 
   def get_max(self):
-    pass
+    current = self
+    while current.right is not None:
+      current = current.right
+    return current.value
+

ad3550da716112bbdb310653c71d59a6250002bc
Edwin Torres <edwin.torres.jr2@gmail.com>
2019-01-23 16:39:31 -0500

    linked list updated to meet requirements

--- linked_list/linked_list.py
+++ linked_list/linked_list.py
@@ -37,8 +37,10 @@ class LinkedList:
         if self.head == None:
             return ("empty list")
         new_head = self.head.next_node
+        value = self.head
         del(self.head)
         self.head = new_head
+        return value
 
     def contains(self, value):
         curr_node = self.head
@@ -60,3 +62,27 @@ class LinkedList:
                 max_number = curr_node.value
             else:
                 curr_node = curr_node.next_node
+
+
+class Queue:
+    def __init__(self):
+        self.size = 0
+        self.storage = LinkedList()
+
+    def enqueue(self, item):
+        self.storage.add_to_tail(item)
+
+    def dequeue(self):
+        value = self.storage.head.value
+        self.storage.remove_head()
+        return value
+
+    def len(self):
+        curr_node = self.storage.head
+        iterations = 0
+        while True:
+            if curr_node is None:
+                return iterations
+            else:
+                curr_node = curr_node.next_node
+                iterations += 1

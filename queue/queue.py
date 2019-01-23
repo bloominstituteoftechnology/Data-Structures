2957f85285f01a122e940f8b2ca85cb4c08b41db
sean-one <basement365@gmail.com>
2019-01-22 23:30:46 -0800

    added some psuedo code for queue.py

--- queue/queue.py
+++ queue/queue.py
@@ -8,10 +8,27 @@ class Queue:
     self.storage = LinkedList()
 
   def enqueue(self, item):
+    # create the new node
+    # add the ned node to the linkedlist add_to_tail
+    # increment size
     pass
   
   def dequeue(self):
+    # step to the last element
+    # remove last element
+    # decrement size
     pass
 
   def len(self):
+    # create a counter
+    # step through elements incrementing counter
+    # return counter
+    # -------------------------------
+    # or just return self.size
     pass
+
+ll = LinkedList()
+ll.add_to_tail(4)
+ll.add_to_tail(8)
+print(ll.head.value)
+print(ll.tail.value)

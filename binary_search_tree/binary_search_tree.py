6337308291f4ba9f9ce6ad76715932a41a66c10c
Patrick Thompson <patrick.jthompson93@gmail.com>
2019-01-23 13:51:16 -0800

    makes some changes to the methods, still doesnt solve default param issue

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -5,33 +5,39 @@ class BinarySearchTree:
         self.right = None
 
     def insert(self, value, leaf=self.value):
-        if value > leaf:
-            # go right
-            if self.right is None:
-                self.right = value
+        if leaf is not None:
+            if value > leaf:
+                # go right
+                if self.right is None:
+                    self.right = BinarySearchTree(value)
+                    return value
+                else:
+                  # make self.right new value, and recall insert
+                    new_value = self.right
+                    BinarySearchTree.insert(value, new_value)
             else:
-                # make self.right new value, and recall insert
-                new_value = self.right
-                insert(value, new_value)
+                # go left
+                if self.left is None:
+                    self.left = BinarySearchTree(value)
+                    return value
+                else:
+                    # make self.left new value, and recall insert
+                    new_value = self.left
+                    BinarySearchTree.insert(value, new_value)
         else:
-            # go left
-            if self.left is None:
-                self.left = value
-            else:
-                # make self.left new value, and recall insert
-                new_value = self.left
-                insert(value, new_value)
+            self.value = BinarySearchTree(value)
+            return value
 
     def contains(self, target, leaf=self.value):
         if leaf is not None:
             if target > leaf:
-                # if target is greater than leaf, go right
+                  # if target is greater than leaf, go right
                 leaf = self.right
-                contains(target, leaf)
+                BinarySearchTree.contains(target, leaf)
             elif target < leaf:
                 # target is less than leaf
                 leaf = self.left
-                contains(target, leaf)
+                BinarySearchTree.contains(target, leaf)
             else:
                 # target is == leaf
                 if target == leaf:
@@ -41,9 +47,9 @@ class BinarySearchTree:
 
     def get_max(self, leaf=self.right):
         if leaf is not None:
-          # if the leaf (self.right) is not none, make it new leaf and recheck
+            # if the leaf (self.right) is not none, make it new leaf and recheck
             leaf = self.right
-            get_max(leaf)
+            BinarySearchTree.get_max(leaf)
         else:
-          # there is no more to the right, return current leaf
+            # there is no more to the right, return current leaf
             return leaf

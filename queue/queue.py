b79de7c719be200e50dc693cb8085e7e24d9da90
Farhan Farooqui <farhan@farooqui.net>
2019-01-23 15:40:19 -0500

    Queue implemented

--- queue/queue.py
+++ queue/queue.py
@@ -1,6 +1,7 @@
 import sys
 sys.path.append('../linked_list')
 from linked_list import LinkedList
+# noqa
 
 class Queue:
   def __init__(self):
@@ -8,10 +9,26 @@ class Queue:
     self.storage = LinkedList()
 
   def enqueue(self, item):
-    pass
+    self.storage.add_to_tail(item)
+    self.size += 1
   
   def dequeue(self):
-    pass
+    if not self.storage.head:
+      return None
+    dq_val = self.storage.head.get_value()
+    self.storage.remove_head()
+    self.size -= 1
+    return dq_val
+    
 
   def len(self):
-    pass
+    return self.size
+
+# que = Queue()
+# que.enqueue(10)
+# que.enqueue(12)
+# que.enqueue(22)
+# que.enqueue(32)
+# que.enqueue(42)
+# print(que.storage.tail.get_value())
+# print(que.storage.head.get_next().get_value())
---
+++ queue/tempCodeRunnerFile.py
@@ -0,0 +1,7 @@
+)
+# que.enqueue(10)
+# que.enqueue(12)
+# que.enqueue(22)
+# que.enqueue(32)
+# que.enqueue(42)
+# print(que.st

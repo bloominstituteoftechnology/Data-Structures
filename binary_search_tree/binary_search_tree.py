ced755a3a1cc3c711a6a40c6404340f3ba074456
Vuk Radosavljevic <vukrado1@gmail.com>
2019-01-23 16:14:12 -0600

    Completed bst

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -5,10 +5,40 @@ class BinarySearchTree:
     self.right = None
 
   def insert(self, value):
-    pass
+    if value <= self.value:
+      if self.left is None:
+        tree = BinarySearchTree(value)
+        self.left = tree
+      else:
+        self.left.insert(value)
+    else:
+      if self.right is None:
+        tree = BinarySearchTree(value)
+        self.right = tree
+      else:
+        self.right.insert(value)
+
+      
 
   def contains(self, target):
-    pass
+    if target == self.value:
+      return True
+
+    if target <= self.value:
+      if self.left is None:
+        return False
+      return self.left.contains(target)
+    else:
+      if self.right is None:
+        return False
+      return self.right.contains(target)
+    
 
   def get_max(self):
-    pass
+    if self.value is None:
+      return None
+    
+    if self.right is None:
+      return self.value
+    else:
+      return self.right.get_max()

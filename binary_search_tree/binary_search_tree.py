1188234f2f9c10b7b56747567ffcc380f7119347
Drew <drewsup123c@gmail.com>
2019-01-24 09:44:03 -0700

    peer code review edits

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -4,29 +4,25 @@ class BinarySearchTree:
         self.left = None
         self.right = None
 
-    # Why can I not reference self in start?
-    def insert(self, value, start=self.value):
-        if start is not None:
-            # Moves it to the right if value is larger
-            if value > self.value:
-                if self.right is None:
-                    self.right = BinarySearchTree(value)
-                    return value
-                else:
-                    BinarySearchTree.insert(value, self.right)
-            # Moves to the left
-            else:
-                if self.left is None:
-                    self.left = BinarySearchTree(value)
-                    return value
-                else:
-                    BinarySearchTree.insert(value, self.left)
+    def insert(self, value):
+        direction = 'right' if value > self.value else 'left'
+        if getattr(self, direction) is not None:
+            self = getattr(self, direction)
         else:
-            self.value = BinarySearchTree(value)
+            setattr(self, direction, BinarySearchTree(value))
             return value
+        return self.insert(value)
 
-    def contains(self, target):
-        pass
+    def contains(self, value):
+        direction = 'right' if value > self.value else 'left'
+        if self.value is not None and self.value != value and getattr(self, direction) is not None:
+            self = getattr(self, direction)
+        elif self.value is not None and self.value == value:
+            return True
+        else:
+            return False
+        return self.contains(value)
+
 
     def get_max(self):
         temp = self.right
@@ -34,3 +30,25 @@ class BinarySearchTree:
             BinarySearchTree.get_max(self.right)
         else:
             return temp
+
+
+
+
+        # if start is not None:
+        #     # Moves it to the right if value is larger
+        #     if value > self.value:
+        #         if self.right is None:
+        #             self.right = BinarySearchTree(value)
+        #             return value
+        #         else:
+        #             BinarySearchTree.insert(value, self.right)
+        #     # Moves to the left
+        #     else:
+        #         if self.left is None:
+        #             self.left = BinarySearchTree(value)
+        #             return value
+        #         else:
+        #             BinarySearchTree.insert(value, self.left)
+        # else:
+        #     self.value = BinarySearchTree(value)
+        #     return value

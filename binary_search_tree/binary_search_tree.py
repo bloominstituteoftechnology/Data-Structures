dbed61a4ab0ae09025c89d2148b62def7019bbbb
Kevin Brack <brackkevin@gmail.com>
2019-01-24 10:48:09 -0500

    Adds print function and random generator

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -37,13 +37,29 @@ class BinarySearchTree:
       return self.value
 
   def print_tree(self):
+    # import time
     if self.left is not None:
       self.left.print_tree()
+    # time.sleep(.1)
     print(self.value)
     if self.right is not None:
       self.right.print_tree()
 
 print('binary search tree ran')
+
+def fill_tree(root, num_elems = 100, max_int = 1000):
+  from random import randint
+  tree = BinarySearchTree(root)
+  for i in range(num_elems):
+    cur_elem = randint(0, max_int)
+    tree.insert(cur_elem)
+  return tree
+
+bst = fill_tree(500)
+print(f'Highest: {bst.get_max()}')
+print(f'Contains 500: {bst.contains(500)}\n')
+print(f'Sorted:')
+bst.print_tree()
 
 # bst = BinarySearchTree(5)
 # bst.insert(6)

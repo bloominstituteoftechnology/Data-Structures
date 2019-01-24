d4d9c2e0d914c8a1b216be19633e77dfb4bd11a7
lndubose <lndubose13@gmail.com>
2019-01-24 15:13:06 -0600

    Finish insert, _bubble_up, get_max, and get_size
    Had to take a peak at some code to get the buble up. I had the right idea, but I forgot
    to subtract 1 from the index.

--- heap/heap.py
+++ heap/heap.py
@@ -1,21 +1,30 @@
 class Heap:
-  def __init__(self):
-    self.storage = []
+    def __init__(self):
+        self.storage = []
 
-  def insert(self, value):
-    pass
+    def insert(self, value):
+        self.storage.append(value)
+        return self._bubble_up(self.get_size() - 1)
 
-  def delete(self):
-    pass
+    def delete(self):
+        pass
 
-  def get_max(self):
-    pass
+    def get_max(self):
+        return self.storage[0]
 
-  def get_size(self):
-    pass
+    def get_size(self):
+        return len(self.storage)
 
-  def _bubble_up(self, index):
-    pass
+    def _bubble_up(self, index):
+        storage = self.storage
+        if index == 0:
+            return
+        if storage[(index - 1)//2] > storage[index]:
+            return
+        elif storage[(index-1)//2] < storage[index]:
+            storage[(index-1) //
+                    2], storage[index] = storage[index], storage[(index - 1)//2]
+            return self._bubble_up((index - 1)//2)
 
-  def _sift_down(self, index):
-    pass
+    def _sift_down(self, index):
+        pass

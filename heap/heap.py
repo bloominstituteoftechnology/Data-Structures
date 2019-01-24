502dcde42f6a5e7f46e33db027fddc4549aa7c04
sean-one <basement365@gmail.com>
2019-01-24 15:50:11 -0800

    update before stand up

--- heap/heap.py
+++ heap/heap.py
@@ -1,14 +1,20 @@
 class Heap:
   def __init__(self):
     self.storage = []
+    self.count = 0
 
   def insert(self, value):
+    self.storage.append(value)
+    self.count += 1
+    # need to write a bubble up function to put element in place
     pass
 
   def delete(self):
+
     pass
 
   def get_max(self):
+    return self.storage[0]
     pass
 
   def get_size(self):

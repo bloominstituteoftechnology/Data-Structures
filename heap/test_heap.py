0a6130832da8c9e3bd0b8800df90b269b0dec106
Chiboba <briangfelix@gmail.com>
2019-01-24 15:37:39 -0800

    Heap Sort

--- heap/test_heap.py
+++ heap/test_heap.py
@@ -65,18 +65,18 @@ class HeapTests(unittest.TestCase):
       descending_order.append(self.heap.delete())
 
     self.assertEqual(descending_order, [10, 8, 7, 6, 5, 5, 2, 1])
-
+  
   def test_bubble_up_was_called(self):
     self.heap._bubble_up = MagicMock()
     self.heap.insert(5)
     self.assertTrue(self.heap._bubble_up.called)
-
+  
   def test_sift_down_was_called(self):
     self.heap._sift_down = MagicMock()
     self.heap.insert(10)
     self.heap.insert(11)
     self.heap.delete()
     self.assertTrue(self.heap._sift_down.called)
-
+ 
 if __name__ == '__main__':
   unittest.main()

b70c3736b0ed094e4f2fdb351c1942915a1d71b4
Linh Bouniol <paulalinh89@gmail.com>
2019-01-23 20:43:12 -0800

    Completed binary search tree

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -5,10 +5,46 @@ class BinarySearchTree:
     self.right = None
 
   def insert(self, value):
-    pass
-
+    
+    # If the number already exists, skip it
+    if value == self.value:
+      return
+      
+    if value < self.value:
+      if self.left is None:
+        # making a new tree with the value with left and right being none
+        self.left = BinarySearchTree(value)
+      else: # left is not none
+        self.left.insert(value)
+    elif self.value < value:
+      if self.right is None:
+        self.right = BinarySearchTree(value)
+      else:
+        self.right.insert(value)
+        
   def contains(self, target):
-    pass
+    
+    if target == self.value:
+      return True
+      
+    if target < self.value:
+      if self.left is None:   # our target is not in there
+        return False
+      
+      # If there is a left, check its children. Does the left contain the target?
+      return self.left.contains(target)
+      
+    elif self.value < target:
+      if self.right is None:
+        return False
+        
+      return self.right.contains(target)
 
   def get_max(self):
-    pass
+    
+    # The max is always on the right side. We can check is the current node has a right child, if it doesn't then it is the max value.
+    
+    if self.right is None:
+      return self.value
+    
+    return self.right.get_max()

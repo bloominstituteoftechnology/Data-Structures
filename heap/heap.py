b2ea7513448e6f499596192a25fe94456dff2574
Katie Cleary <katiecleary@airmaster.lan>
2019-01-24 17:43:49 -0500

    completed heap class, all test pass

--- heap/heap.py
+++ heap/heap.py
@@ -3,19 +3,42 @@ class Heap:
     self.storage = []
 
   def insert(self, value):
-    pass
+    self.storage.append(value)
+    self._bubble_up(len(self.storage) - 1)
+     #if right or left child is greater than root, swap/reconstruct
 
   def delete(self):
-    pass
+    #start at root, delete 
+    retval = self.storage[0]
+    self.storage[0] = self.storage[len(self.storage) - 1]
+    self.storage.pop()
+    self._sift_down(0)
+    return retval 
 
   def get_max(self):
-    pass
+    return self.storage[0]
 
   def get_size(self):
-    pass
+    return len(self.storage)
 
   def _bubble_up(self, index):
-    pass
+    #if right or left child is greater than root, swap/reconstruct
+    while (index - 1) // 2 >= 0:
+      if self.storage[(index - 1) // 2] < self.storage[index]:
+        self.storage[index], self.storage[(index - 1) // 2] = self.storage[(index - 1) // 2], self.storage[index]
+      index = (index - 1) // 2
 
   def _sift_down(self, index):
-    pass
+     #moving down through tree
+    while index * 2 + 1 <= len(self.storage) - 1:
+      mc = self._max_child(index)
+      if self.storage[index] < self.storage[mc]:
+        self.storage[index], self.storage[mc] = self.storage[mc], self.storage[index]
+      index = mc
+      
+  #added function _max_child - I like how it reads better in _sift_down
+  def _max_child(self, index):
+    if index * 2 + 2 > len(self.storage) - 1:
+      return index * 2 + 1
+    else:
+      return index * 2 + 1 if self.storage[index * 2 + 1] > self.storage[index * 2 + 2] else index * 2 + 2

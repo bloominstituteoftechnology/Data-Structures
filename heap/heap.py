9b8b28993259467ba5a6e2a1ef94f6484f05d617
Kevin Brack <brackkevin@gmail.com>
2019-01-24 18:45:00 -0500

    heap delete and _sift_down methods implemented

--- heap/heap.py
+++ heap/heap.py
@@ -6,27 +6,35 @@ class Heap:
   def insert(self, value):
     index = self.count
     self.storage.append(value)
-    print(self.storage)
     self._bubble_up(index)
     self.count += 1
 
   def delete(self):
-    pass
+    if self.count > 0:
+      retval = self.get_max()
+      print(f'before delete: {self.storage}')# <-- debugging
+      last = self.storage.pop()
+      self.storage[0] = last
+      self.count -= 1
+      print(f'after delete: {self.storage}')# <-- debugging
+      self._sift_down(0)
+      print(f'after sift down: {self.storage}')# <-- debugging
+      return retval
 
   def get_max(self):
-    pass
+    return self.storage[0]
 
   def get_size(self):
-    pass
+    return self.count
 
   def _find_parent(self, index):
     return (index - 1) // 2
 
   def _find_child(self, index, lr = 0):
     if lr is 0:
-      return (index * 2) + 1
+      return self._index_exists((index * 2) + 1)
     if lr is 1:
-      return (index * 2) + 2
+      return self._index_exists((index * 2) + 2)
 
   def _bubble_up(self, index):
     if index is 0:
@@ -39,8 +47,47 @@ class Heap:
       else:
         return
 
+  def _index_exists(self, index):
+    if index > self.count -1:
+      return None
+    else:
+      return index
+
   def _sift_down(self, index):
-    pass
+    if index >= len(self.storage) - 1:
+      return
+    else:
+      l = self._find_child(index, 0)
+      r = self._find_child(index, 1)
+      s = None
+
+      # finds out if left and right children exist
+      # if one or the other exists swap var becomes childs index
+      # if both exist and one is bigger, uses that index for swap
+      # if neither exist we are done sifting
+      if l is None and r is None:
+        return
+      elif r is not None and l is None:
+        s = r
+      elif r is None and l is not None:
+        s = l
+      elif self.storage[l] > self.storage[r]:
+        s = l
+      else:
+        s = r
+
+      # if storage[swap] is greater than storage[index]
+      # flip them and recurse at storage index
+      if self.storage[s] > self.storage[index]:
+        self.storage[s], self.storage[index] = self.storage[index], self.storage[s]
+        print(f'post swap: {self.storage}')# <-- Debugging
+        return self._sift_down(s)
+
+      else:
+        return
+
+        
+
 
 
 #######################
@@ -68,3 +115,15 @@ class Heap:
 # test_insertion.insert(1010)
 # print(test_insertion.storage)
 ###############################
+
+####################
+# Testing Deletion #
+####################
+# test_delete = Heap()
+# arr = [12,17,36,25,5,19,100]
+# for i in arr:
+#   test_delete.insert(i)
+# print(f'before delete: {test_delete.storage}')
+# print(f'delete result: {test_delete.delete()}')
+# print(f'after delete: {test_delete.storage}'
+#################################################

35ab5166b17959d5273a77e4cd819613ca555ddc
Max <maxkajiwara@gmail.com>
2019-01-24 00:03:56 -0800

    implement binary_search_tree

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -1,14 +1,45 @@
 class BinarySearchTree:
-  def __init__(self, value):
-    self.value = value
-    self.left = None
-    self.right = None
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
 
-  def insert(self, value):
-    pass
+    # this implementation doesn't balance itself or allow duplicates
+    def insert(self, value):
+        if value < self.value:
+            if self.left is None:
+                # new leaf
+                self.left = BinarySearchTree(value)
+            else:
+                # keep going
+                self.left.insert(value)
 
-  def contains(self, target):
-    pass
+        elif value > self.value:
+            if self.right is None:
+                # new leaf
+                self.right = BinarySearchTree(value)
+            else:
+                # keep going
+                self.right.insert(value)
 
-  def get_max(self):
-    pass
+    def contains(self, target):
+        if self.value is target:
+            return True
+
+        elif target < self.value:
+            if self.left is None:
+                return False
+            else:
+                return self.left.contains(target)
+
+        elif target > self.value:
+            if self.right is None:
+                return False
+            else:
+                return self.right.contains(target)
+
+    def get_max(self):
+        if self.right is None:
+            return self.value
+        else:
+            return self.right.get_max()

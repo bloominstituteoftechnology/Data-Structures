c854eb1ab127d9645220c9f2e60b21ffd85ccf74
Andrew <ghobrial.andrew@gmail.com>
2019-01-24 18:24:53 -0500

    get max and get size  methods of heap.py

--- heap/heap.py
+++ heap/heap.py
@@ -10,10 +10,10 @@ class Heap:
     pass
 
   def get_max(self):
-    pass
+    return self.storage[0]
 
   def get_size(self):
-    pass
+    return len(self.storage)
 
   def _bubble_up(self, index):
     storage = self.storage

1d07beb012f7004cdfbc24ff3d1ab4bbf261851c
jsmnglng <jsmn.glng@gmail.com>
2019-01-24 14:14:13 -0800

    Finished insert, get_max and get_size

--- heap/heap.py
+++ heap/heap.py
@@ -1,21 +1,55 @@
 class Heap:
-  def __init__(self):
-    self.storage = []
+    def __init__(self):
+        self.storage = []
 
-  def insert(self, value):
-    pass
+    def insert(self, value):
+        # add new element at the end of the array
+        self.storage.append(value)
+        # bubble up until the heap has been reestablished
+        self._bubble_up(self.get_size() - 1)
 
-  def delete(self):
-    pass
+    def delete(self):
+        # remove root node
+        self.storage.pop(0)
+        # move last element to root
+        last_element = self.storage.pop()
+        self.storage.insert(0, last_element)
+        # sift down until the heap has been reestablished
+        self._sift_down(0)
 
-  def get_max(self):
-    pass
+    def get_max(self):
+        return self.storage[0]
 
-  def get_size(self):
-    pass
+    def get_size(self):
+        return len(self.storage)
 
-  def _bubble_up(self, index):
-    pass
+    def _bubble_up(self, index):
+        p_index = (index - 1) // 2
 
-  def _sift_down(self, index):
-    pass
+        if index <= 0:
+            return
+
+        # if parent node < child node
+        if self.storage[p_index] < self.storage[index]:
+            # swap positions
+            self.storage[index], self.storage[p_index] = (
+                self.storage[p_index],
+                self.storage[index],
+            )
+
+        return self._bubble_up(p_index)
+
+    def _sift_down(self, index):
+        pass
+
+
+heap = Heap()
+heap.insert(25)
+heap.insert(1)
+heap.insert(3)
+heap.insert(36)
+heap.insert(100)
+heap.insert(19)
+heap.insert(17)
+# print(heap.delete())
+print(heap.storage)

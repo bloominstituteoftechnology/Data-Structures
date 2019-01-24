231f936bbdd2f93805c9f3d0db026aa229b1d65b
Brandon Strellis <bstrellis@gmail.com>
2019-01-24 18:48:23 -0500

    heap.py done except for delete and sift down.

--- heap/heap.py
+++ heap/heap.py
@@ -3,19 +3,32 @@ class Heap:
     self.storage = []
 
   def insert(self, value):
-    pass
+    # append at first convenient place, aka end of storage
+    self.storage.append(value)
+    # shuffle value to appropriate place
+    self._bubble_up(len(self.storage) - 1)
+    
 
   def delete(self):
-    pass
+    # only deletes first item in self.storage
+    temp = self.storage[0]
+    self.storage.pop()
+    
 
   def get_max(self):
-    pass
+    return self.storage[0]
 
   def get_size(self):
-    pass
+    return len(self.storage)
 
   def _bubble_up(self, index):
-    pass
+    parent_index = (index - 1) // 2
+    
+    if parent_index >= 0 and self.storage[parent_index] < self.storage[index]:
+      temp = self.storage[parent_index]
+      self.storage[parent_index] = self.storage[index]
+      self.storage[index] = temp
+      self._bubble_up(parent_index)
 
   def _sift_down(self, index):
     pass

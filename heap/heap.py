c2705bf6915f1fd06df0c97d0e41589911ecc0f0
Hunter315 <hunter.mckay.smith@gmail.com>
2019-01-24 11:51:48 -0700

    fixed bubble up and get_max, finished heap

--- heap/heap.py
+++ heap/heap.py
@@ -13,25 +13,49 @@ class Heap:
         self._bubble_up(current_index)
 
     def delete(self):
-        pass
+        deleted_value = self.storage.pop(0)
+        self._sift_down(0)
+        return deleted_value
 
     def get_max(self):
-        pass
+      if self.get_size() is 0:
+        return None
+      return self.storage[0]
 
     def get_size(self):
-        pass
+        return len(self.storage)
 
     def _bubble_up(self, index):
-        if index is not 0:
-            current_index = index
-            parent_index = self.get_parent_index(current_index)
-            current_value = self.storage[current_index]
-            parent_value = self.storage[parent_index]
-            if parent_value < current_value:
-                self.storage[current_index] = parent_value
-                self.storage[parent_index] = current_value
-                if parent_index != 0:
-                    self._bubble_up(parent_index)
-
+        parent_index = self.get_parent_index(index)
+        parent_value = self.storage[parent_index]
+        current_value = self.storage[index]
+        if index is not 0 and parent_value < current_value:
+          self.storage[index], self.storage[parent_index] = \
+          self.storage[parent_index], self.storage[index]
+          if parent_index is not 0:
+            self._bubble_up(parent_index)
     def _sift_down(self, index):
-        pass
+        size = self.get_size()
+        first_child_index = (2 * index) + 1
+        second_child_index = (2 * index) + 2
+        # if parent has child
+        if first_child_index < size:
+          parent_value = self.storage[index]
+          first_child_value = self.storage[first_child_index]
+          # if parent has 2 children
+          if second_child_index < size:
+            second_child_value = self.storage[second_child_index]
+            if first_child_value >= second_child_value:
+              if parent_value < first_child_value:
+                self.storage[index], self.storage[first_child_index] = \
+                first_child_value, parent_value
+            elif parent_value < second_child_value:
+              self.storage[index], self.storage[second_child_index] = \
+              second_child_value, parent_value
+            self._sift_down(index + 1)
+          # else parent has only 1 child
+          else:
+            if parent_value < first_child_value:
+              self.storage[index], self.storage[first_child_index] = \
+              first_child_value, parent_value
+            self._sift_down(index + 1)

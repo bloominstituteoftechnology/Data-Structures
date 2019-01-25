559240d72569f27e29adc88ccd25f1de932a604a
IAN CAMERON <iancameron@IANs-MacBook-Pro.local>
2019-01-24 22:32:14 -0700

    adds heap

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -7,31 +7,34 @@ class BinarySearchTree:
   def insert(self, value): #insert includes getmax
     #if left branch is empty and value is less than root node (self.value)
     #set new instance with that value to the left.
-    if self.left is None and value < self.value:
-      self.left = BinarySearchTree(value)
-      return 
+    if value <= self.value:
+      if self.left is None:
+        self.left = BinarySearchTree(value)
+      else:
+        return self.left.insert(value)
     # if right empty and value bigger: vice versa
-    if self.right is None and value > self.value:
-      self.right = BinarySearchTree(value)
-      return 
-    #set new node to left if lesser else to right
-    if value < self.value:
-      new_node = self.left
-    else: 
-      self.right
-    new_node.insert(value)
-#what about = to??
+    if value > self.value:
+      if self.right is None:
+        self.right = BinarySearchTree(value)
+      else:
+        return self.right.insert(value)
 
   def contains(self, target):
     #first compare target to root
-    if self.value is None or self.value == target:
-      return self.value 
+    if target == self.value:
+      return True
     #if root is less than target search right
-    elif self.value < target: 
-      return search(self.right, target)
+    if self.value < target:
+      if self.right is None:
+        return False
+      else: 
+        return self.right.contains(target)
     #if root is greater than target
-    else:
-      return search(self.left, target)
+    if self.value > target:
+      if self.left is None:
+        return False
+      else:
+        return self.left.contains(target)
 
   def get_max(self): 
     #loop through right side to find leaf with largest value
--- heap/heap.py
+++ heap/heap.py
@@ -4,43 +4,63 @@ class Heap:
     self.count = 0
 
   def insert(self, value):
+    self.storage.append(value)
+    self.size += 1
+    index = self.size - 1
+    self._bubble_up(index)
     #if empty just insert item
-    if len(self.storage) == 0:
-      self.storage.append(value)
-      self.count += 1
-      parent = _get_parent(self.count -1) #off by one??? maybe?
-      while parent < value: 
-        self._bubble_up(self.count -1)
-        parent = _get_parent(self.count -1) #off by one??? maybe not -1?
+    # if len(self.storage) == 0:
+    #   self.storage.append(value)
+    #   self.count += 1
+    #   parent = _get_parent(self.count -1) #off by one??? maybe?
+    #   while parent < value: 
+    #     self._bubble_up(self.count -1)
+    #     parent = _get_parent(self.count -1) #off by one??? maybe not -1?
         
     #otherwise insert item to end and sift it up
 
 
   def delete(self):
-    pass
+    val = self.storage[0]
+    self.size += 1
+    index = self.size - 1
+    self.storage[0] = self.storage[length]
+    self.storage.pop()
+    self._sift_down(0)
+    return val
 
   def get_max(self):
-    pass
+    return self.storage[0]
 
   def get_size(self):
-    return self.count 
+    return self.size
 
 #underscore means private function for internal use only
-  def _get_parent(self, index):
-    return storage[(index-1)//2]
+#   def _get_parent(self, index):
+#     return storage[(index-1)//2]
 
-  def _get_left(self, index):
-    left_index = index*2+1
-    if left_index > len(self.storage) - 1:
-      
+#   def _get_left(self, index):
+#     left_index = index*2+1
+#     if left_index > len(self.storage) - 1:
+#       return None
+#     else: 
+#       return left_index
 
-  # def _get_right(self, index):
-  #   return 
+#  def _get_right(self, index):
+#     right_index = index*2+2
+#     if left_index > len(self.storage) - 1:
+#       return None
+#     else: 
+#       return right_index
 
-  def _get_left()
+  
 
   def _bubble_up(self, index):
-    pass
+    while (index - 1) // 2 >= 0:
+      if self.storage[(index - 1) // 2] < self.storage[index]:
+        self.storage[(index - 1) // 2], self.storage[index] = self.storage[index], self.storage[(index - 1) // 2]
+      index = (index - 1) // 2
+
 
   def _sift_down(self, index):
     pass

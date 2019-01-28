7990e812eed5ffa7dc9374e870f6b6f302f7ea2b
luiscmartinez <mrluismartinezzz@gmail.com>
2019-01-28 13:02:52 -0800

    implemented  add_to_tail method

--- linked_list/linked_list.py
+++ linked_list/linked_list.py
@@ -9,7 +9,6 @@ class Node:
     self.next_node = next_node
 
   def get_value(self):
-    return self.value
 
   def get_next(self):
     return self.next_node
@@ -23,7 +22,18 @@ class LinkedList:
     self.tail = None
 
   def add_to_tail(self, value):
-    
+    # create a new node with the given value
+    new_node = Node(value)
+    # only time that the LL's tail will not have a value is when it is first initialized 
+    # check that the LinkedList's tail has a value meaning LL is not empty
+    if self.tail is not None:
+      # set the LinkedList's tail's pointer to the new add value
+      self.tail.set_next(new_node)
+    # if it is empty set the new node to the head
+    else:
+      self.head = new_node
+    # end the method with always setting the tail attribute value no matter what because thats what the method is for  
+    self.tail = new_node
 
   def remove_head(self):
     pass
@@ -33,3 +43,4 @@ class LinkedList:
 
   def get_max(self):
     pass
+

d35ba90204bf4b4003923ef62c88539fe013f529
luiscmartinez <mrluismartinezzz@gmail.com>
2019-01-28 15:03:11 -0800

    completed and passed all test for queue

--- queue/queue.py
+++ queue/queue.py
@@ -8,10 +8,24 @@ class Queue:
     self.storage = LinkedList()
 
   def enqueue(self, item):
-    pass
+    self.storage.add_to_tail(item)
   
   def dequeue(self):
-    pass
+    return self.storage.remove_head()
 
   def len(self):
-    pass
+    # to get the length check first check if there is a length
+    # get the value of the current head
+    current_head = self.storage.head
+    # check if current head exist
+    if current_head == None:
+      return 0
+    if current_head.get_next() == None:
+      return 1
+    else:
+      count = 1
+      while current_head.get_next() is not None:
+        count += 1
+        current_head = current_head.get_next()
+      return count
+

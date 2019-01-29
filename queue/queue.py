093b20911606844eac9de9dae043b1f8aab6882a
Riley-Brown <riley@riley.gg>
2019-01-29 15:31:22 -0500

    fixed queue file importing linked list incorrectly, added enqueue method

--- queue/queue.py
+++ queue/queue.py
@@ -3,15 +3,16 @@ sys.path.append('../linked_list')
 from linked_list import LinkedList
 
 class Queue:
-  def __init__(self):
-    self.size = 0
-    self.storage = LinkedList()
+    def __init__(self):
+        self.size = 0
+        self.storage = LinkedList()
 
-  def enqueue(self, item):
-    pass
-  
-  def dequeue(self):
-    pass
+    def enqueue(self, item):
+        self.size += 1
+        self.storage.add_to_tail(item)
 
-  def len(self):
-    pass
+    def dequeue(self):
+        pass
+
+    def len(self):
+        pass

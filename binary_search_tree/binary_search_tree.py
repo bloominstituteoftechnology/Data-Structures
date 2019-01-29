38590e50b28f194813e87bc5037b5ad7553871a5
Riley-Brown <riley@riley.gg>
2019-01-29 18:23:28 -0500

    updated binary search tree

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -4,34 +4,27 @@ class BinarySearchTree:
     self.left = None
     self.right = None
 
-  def insert(self, value):
-    if self.value == value:
-      return False
-    elif self.value > value:
-      if self.left:
-        return self.left.insert(value)
-      else:
-        self.left = BinarySearchTree(value)
-    else:
-      if self.right:
-        return self.right.insert(value)
-      else:
-        self.right = BinarySearchTree(value)
-        return True
-      
+  # def insert(self, value):
+  #   if self.value == value:
+  #     return False
+  #   elif self.value > value:
+  #     if self.left:
+  #       return self.left.insert(value)
+  #     else:
+  #       self.left = BinarySearchTree(value)
+  #   else:
+  #     if self.right:
+  #       return self.right.insert(value)
+  #     else:
+  #       self.right = BinarySearchTree(value)
+  #       return True
+      if
 
   def contains(self, target):
-    mid = target[len(target) // 2]
-    
-    if len(target) == 0 or (len(target) == 1 and target[0] != self.value):
-      return False
-
-    if self.value == mid:
-      return True
-    elif self.value < mid:
-      return BinarySearchTree.contains(target[:len(target) // 2], self.value)
-    elif self.value > mid:
-      return BinarySearchTree.contains(target[len(target) // 2 + 1], self.value)
+    if target > self.value:
+      # search left side
+    elif target < self.value:
+      # search right side
    
 
   def get_max(self):
@@ -40,5 +33,5 @@ class BinarySearchTree:
 
 tree_test = BinarySearchTree(5)
 tree_test.insert(10)
-print(tree_test.contains([1,2]))
+print(tree_test.contains([1,2,5.9]))
 print(tree_test.insert(15))

f3af35dc49c0ec761ffa205beeee1f056375b422
Riley-Brown <riley@riley.gg>
2019-01-30 13:49:23 -0500

    updated binary search contains method to pass tests

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -1,59 +1,75 @@
 class BinarySearchTree:
-  def __init__(self, value):
-    self.value = value
-    self.left = None
-    self.right = None
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
 
-  def insert(self, value):
-    current = self
-    while True:
-      if value < current.value:
-        if current.left == None:
-          current.left = BinarySearchTree(value)
-          print(current.left.value)
-          break
-        else:
-          current = current.left
-          
-      else:
-        if current.right == None:
-          current.right = BinarySearchTree(value)
-          print(current.right.value)
-          break
-        else:
-          current = current.right
-          
-          
+    def insert(self, value):
+        current = self
+        while True:
+            if value < current.value:
+                if current.left == None:
+                    current.left = BinarySearchTree(value)
+                    print(current.left.value)
+                    break
+                else:
+                    current = current.left
+
+            else:
+                if current.right == None:
+                    current.right = BinarySearchTree(value)
+                    print(current.right.value)
+                    break
+                else:
+                    current = current.right
+
+    def contains(self, target):
+        # if target > self.value:
+        #   # search left side
+        # elif target < self.value:
+            # search right side
+
+        current = self
+        while True:
+            if target == current.value:
+                return True
+            elif target < current.value: 
+                if current.left is not None:
+                    current = current.left
+                    
+                else:
+                    return False
+            elif target > current.value: 
+                if current.right is not None:
+                    current = current.right
+                    
+                else:
+                    return False
+            
         
+        
+        
+        # while True:
+        #   if current.value == target:
+        #     return True
+        #   elif target < current.value:
+        #     current = current.left
+        #   elif target < current.value:
+        #     current = current.right
+        #   else:
+        #     if current.value == None:
+        #       return False
 
-  def contains(self, target):
-    # if target > self.value:
-    #   # search left side
-    # elif target < self.value:
-      # search right side
 
-    current = self
-    while True:
-      if target == current.value:
-        return True
-      elif current.left is not None: 
-        if target < current.value:
-          current.left = BinarySearchTree(target)
-          print(f'Looking for number {target}, current value: {current.left.value}')
-          break
-        else:
-          current = current.left
-      else:
-        if current.right is not None:
+    def get_max(self):
+        # current = self
+        # highestValue = 0
+        # while True:
+        #   if highestValue <
+        pass
 
-          current.right = BinarySearchTree(target)
-          print(f'Looking for number {target}, current value: {current.right.value}')
-          break
-        else:
-          current = current.right
-    return False
-  def get_max(self):
-    pass
+
+
 
 
 tree_test = BinarySearchTree(5)
@@ -61,5 +77,5 @@ tree_test.insert(2)
 tree_test.insert(3)
 tree_test.insert(7)
 tree_test.insert(6)
-print(tree_test.contains(3))
+print(tree_test.contains(8))
 print(tree_test.right.value)

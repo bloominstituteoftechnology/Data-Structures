243ae69bfc4d9e3dd491be5223397876952dabac
Riley-Brown <riley@riley.gg>
2019-01-31 17:01:39 -0500

    Added delete method for heap

--- heap/heap.py
+++ heap/heap.py
@@ -9,6 +9,9 @@ class Heap:
 
   def delete(self):
     self.size -= 1
+    deleted_node = self.storage[0]
+    del(self.storage[0])
+    return deleted_node
 
   def get_max(self):
     return self.storage[0]
@@ -27,4 +30,9 @@ new_heap = Heap()
 new_heap.insert(5)
 new_heap.insert(10)
 new_heap.insert(2)
+print(new_heap.storage)
+print(new_heap.delete())
+print(new_heap.storage)
+
+print(new_heap.get_max())
 print(new_heap.get_size())

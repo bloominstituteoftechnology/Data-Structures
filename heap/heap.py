9547993e03dc6e7b9119a4d15b5c13d8af1ad63a
luiscmartinez <mrluismartinezzz@gmail.com>
2019-01-31 16:01:31 -0800

    implemented some methods on heap.py

--- heap/heap.py
+++ heap/heap.py
@@ -1,21 +1,49 @@
 class Heap:
   def __init__(self):
-    self.storage = []
+    self.storage = [0]
+    self.size = 0
 
   def insert(self, value):
-    pass
+    # append value to storage
+    self.storage.append(value)
+    #increment size
+    self.size += 1
+    # bubble up
+    self._bubble_up(self.size)
+    #pass
 
   def delete(self):
-    pass
+    # if empty return none
+    if self.size == 0:
+      return None
+    # create variables for the storage top and storage end
+    storage_top = self.storage[1]
+    storage_end = self.storage.pop()
+    # decrement the size
+    self.size -= 1
+    # if size zero return storage top
+    if self.size == 0:
+      return storage_top
+
+    # set storage of index 1 to storage end
+    self.storage[1] = storage_end
+    # shift down
+    self._sift_down(1)
+    #return storage top
+    return storage_top
+    #pass
 
   def get_max(self):
-    pass
+    # return the storage at index 1
+    return self.storage[1]
+    #pass
 
   def get_size(self):
-    pass
+    # return size
+    return self.size
+    #pass
 
   def _bubble_up(self, index):
-    pass
+
 
   def _sift_down(self, index):
-    pass

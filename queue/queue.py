edf9c88991828bd6f91e2096a493635f11233b42
Sean Chen <skypemaster007@gmail.com>
2019-02-05 10:35:47 -0800

    Remove linked list import from queue

--- queue/queue.py
+++ queue/queue.py
@@ -1,11 +1,9 @@
-import sys
-sys.path.append('../linked_list')
-from linked_list import LinkedList
-
 class Queue:
   def __init__(self):
     self.size = 0
-    self.storage = LinkedList()
+    # what data structure should we
+    # use to store queue elements?
+    self.storage = 
 
   def enqueue(self, item):
     pass

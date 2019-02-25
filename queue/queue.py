67a91e16dff76338ec7b7b7c16ecd55e07971dbb
MEBonham <programmer@mckaybonham.com>
2019-02-25 13:27:03 -0700

    Passes queue tests

--- queue/queue.py
+++ queue/queue.py
@@ -1,15 +1,20 @@
 class Queue:
-  def __init__(self):
-    self.size = 0
-    # what data structure should we
-    # use to store queue elements?
-    self.storage = 
+    def __init__(self):
+        self.size = 0
+        # what data structure should we
+        # use to store queue elements?
+        self.storage = []
 
-  def enqueue(self, item):
-    pass
-  
-  def dequeue(self):
-    pass
+    def enqueue(self, item):
+        self.storage.append(item)
+        self.size += 1
+    
+    def dequeue(self):
+        if self.size == 0:
+            return None
+        else:
+            self.size -= 1
+            return self.storage.pop(0)        
 
-  def len(self):
-    pass
+    def len(self):
+        return self.size

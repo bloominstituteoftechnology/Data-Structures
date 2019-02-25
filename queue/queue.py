9710e281a17b6c27d55753a6d100bddf565e1df2
Axolotl <samuelmachat@gmail.com>
2019-02-25 17:28:10 -0500

    get import working to queue file

--- queue/queue.py
+++ queue/queue.py
@@ -1,15 +1,34 @@
+import sys
+from pathlib import Path
+file = Path(__file__).resolve()
+parent, root = file.parent, file.parents[1]
+sys.path.append(str(root))
+
+# Additionally remove the current file's directory from sys.path
+try:
+    sys.path.remove(str(parent))
+except ValueError:  # Already removed
+    pass
+
+from doubly_linked_list.doubly_linked_list import ListNode  # nopep8
+from doubly_linked_list.doubly_linked_list import DoublyLinkedList  # nopep8
+
+
 class Queue:
-  def __init__(self):
-    self.size = 0
-    # what data structure should we
-    # use to store queue elements?
-    self.storage = 
+    def __init__(self):
+        self.size = 0
+        # what data structure should we
+        # use to store queue elements?
+        self.storage = []
 
-  def enqueue(self, item):
-    pass
-  
-  def dequeue(self):
-    pass
+    def enqueue(self, item):
+        self.storage.append(item)
 
-  def len(self):
-    pass
+    def dequeue(self):
+        try:
+            return self.storage.pop(0)
+        except IndexError:
+            pass
+
+    def len(self):
+        return len(self.storage)

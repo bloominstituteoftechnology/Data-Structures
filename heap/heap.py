d469251e0a7d11987cb49353d791d1086d1c3130
Axolotl <samuelmachat@gmail.com>
2019-02-25 23:09:19 -0500

    heap working butsolution is hacky

--- heap/heap.py
+++ heap/heap.py
@@ -7,7 +7,6 @@ class Heap:
         self._bubble_up(len(self.storage) - 1)
 
     def delete(self):
-        print(self.storage)
         top_value = self.storage[0]
         self.storage[0] = self.storage[len(self.storage) - 1]
         del self.storage[len(self.storage) - 1]
@@ -22,29 +21,43 @@ class Heap:
         return len(self.storage)
 
     def _bubble_up(self, index):
-        print(index)
         parent_index = max(0, ((index + 1)//2) - 1)  # round down
-        print(self.storage)
-        print(f'parent index {parent_index}')
         if self.storage[parent_index] < self.storage[index]:
             self.storage[parent_index], self.storage[index] = self.storage[index], self.storage[parent_index]
             self._bubble_up(parent_index)
 
     def _sift_down(self, index):
-        path = []
-        location = len(self.storage) - 1
-        while location > 0:
-            path.append(location)
-            location = max(0, ((location + 1)//2) - 1)
-        print(path)
+        pointer = index
+        left = pointer * 2
+        right = pointer * 2 + 1
 
-        while len(path):
-            sift_loc = 0
-            swap_loc = path.pop(len(path) - 1)
-            while self.storage[sift_loc] < self.storage[swap_loc]:
-                self.storage[sift_loc], self.storage[swap_loc] = self.storage[swap_loc], self.storage[sift_loc]
-                sift_loc = swap_loc
+        finished = False
+
+        while not finished:
+            try:
+                if self.storage[pointer] < self.storage[left] or self.storage[pointer] < self.storage[right]:
+                    if self.storage[left] > self.storage[right]:
+                        self.storage[pointer], self.storage[left] = self.storage[left], self.storage[pointer]
+                        pointer = left
+                        left = pointer * 2
+                        right = pointer * 2 + 1
+                    else:
+                        self.storage[pointer], self.storage[right] = self.storage[right], self.storage[pointer]
+                        pointer = right
+                        left = pointer * 2
+                        right = pointer * 2 + 1
+                else:
+                    finished = True
+            except IndexError:
                 try:
-                    swap_loc = path.pop(len(path) - 1)
+                    if self.storage[pointer] < self.storage[left]:
+                        self.storage[pointer], self.storage[left] = self.storage[left], self.storage[pointer]
+                        pointer = left
+                        finished = True
+                    else:
+                        finished = True
                 except IndexError:
-                    swap_loc = sift_loc
+                    finished = True
+
+        for index, val in enumerate(self.storage):
+            self._bubble_up(index)

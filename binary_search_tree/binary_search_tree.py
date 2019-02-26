eed1bf8868ad693ed3643a365fe4f622fae3076d
Carlos Lantigua <clantigua954@gmail.com>
2019-02-26 17:58:42 -0500

    first instert test passes

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -1,14 +1,31 @@
 class BinarySearchTree:
-  def __init__(self, value):
-    self.value = value
-    self.left = None
-    self.right = None
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
 
-  def insert(self, value):
-    pass
+    def insert(self, value):
+        current = self
+        given = value
+        complete = False
+        while not complete:
+            if given < current.value:
+                # add value to the left node
+                if current.left:
+                    current = current.left
+                else:
+                    current.left = BinarySearchTree(given)
+                    complete = True
+            if given > current.value:
+                # add value to the left node
+                if current.right:
+                    current = current.right
+                else:
+                    current.right = BinarySearchTree(given)
+                    complete = True
 
-  def contains(self, target):
-    pass
+    def contains(self, target):
+        pass
 
-  def get_max(self):
-    pass
+    def get_max(self):
+        pass

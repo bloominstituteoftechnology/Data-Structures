7aff00b05ee31eefedf558a113e8b9a6b7d7db24
qtn2932 <quan.nguyen199712@gmail.com>
2019-02-26 12:17:04 -0500

    initial commit

--- queue/queue.py
+++ queue/queue.py
@@ -1,11 +1,44 @@
+class ListNode:
+  def __init__(self, value, prev=None, next=None):
+    self.value = value
+    self.prev = prev
+    self.next = next
+
+
+  def insert_after(self, value):
+    current_next = self.next
+    self.next = ListNode(value, self, current_next)
+    if current_next:
+      current_next.prev = self.next
+
+
+  def insert_before(self, value):
+    current_prev = self.prev
+    self.prev = ListNode(value, current_prev, self)
+    if current_prev:
+      current_prev.next = self.prev
+
+
+  def delete(self):
+    if self.prev:
+      self.prev.next = self.next
+    if self.next:
+      self.next.prev = self.prev
 class Queue:
   def __init__(self):
     self.size = 0
     # what data structure should we
     # use to store queue elements?
-    self.storage = 
+    self.storage=None
+    self.currentNode=None
 
   def enqueue(self, item):
+    if self.storage==None:
+      self.storage= ListNode(item)
+      self.size+=1
+    else:
+      self.storage=self.currentNode.next
+      self.size+=1
     pass
   
   def dequeue(self):

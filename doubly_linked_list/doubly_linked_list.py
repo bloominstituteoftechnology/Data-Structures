148a9bbac5f301d1a3883ee822c8cbeab63bf893
Carlos Lantigua <clantigua954@gmail.com>
2019-02-25 19:10:12 -0500

    got most of doubly link tests to pass

--- doubly_linked_list/doubly_linked_list.py
+++ doubly_linked_list/doubly_linked_list.py
@@ -1,64 +1,120 @@
 """Each ListNode holds a reference to its previous node
 as well as its next node in the List."""
-class ListNode:
-  def __init__(self, value, prev=None, next=None):
-    self.value = value
-    self.prev = prev
-    self.next = next
 
-  """Wrap the given value in a ListNode and insert it
+
+class ListNode:
+    def __init__(self, value, prev=None, next=None):
+        self.value = value
+        self.prev = prev
+        self.next = next
+
+    """Wrap the given value in a ListNode and insert it
   after this node. Note that this node could already
   have a next node it is point to."""
-  def insert_after(self, value):
-    current_next = self.next
-    self.next = ListNode(value, self, current_next)
-    if current_next:
-      current_next.prev = self.next
 
-  """Wrap the given value in a ListNode and insert it
+    def insert_after(self, value):
+        current_next = self.next
+        self.next = ListNode(value, self, current_next)
+        if current_next:
+            current_next.prev = self.next
+
+    """Wrap the given value in a ListNode and insert it
   before this node. Note that this node could already
   have a previous node it is point to."""
-  def insert_before(self, value):
-    current_prev = self.prev
-    self.prev = ListNode(value, current_prev, self)
-    if current_prev:
-      current_prev.next = self.prev
 
-  """Rearranges this ListNode's previous and next pointers
+    def insert_before(self, value):
+        current_prev = self.prev
+        self.prev = ListNode(value, current_prev, self)
+        if current_prev:
+            current_prev.next = self.prev
+
+    """Rearranges this ListNode's previous and next pointers
   accordingly, effectively deleting this ListNode."""
-  def delete(self):
-    if self.prev:
-      self.prev.next = self.next
-    if self.next:
-      self.next.prev = self.prev
+
+    def delete(self):
+        if self.prev:
+            self.prev.next = self.next
+        if self.next:
+            self.next.prev = self.prev
+
 
 """Our doubly-linked list class. It holds references to
 the list's head and tail nodes."""
+
+
 class DoublyLinkedList:
-  def __init__(self, node=None):
-    self.head = node
-    self.tail = node
+    def __init__(self, node=None):
+        self.head = node
+        self.tail = node
 
-  def add_to_head(self, value):
-    pass
+    def add_to_head(self, value):
+          # get a ref to the current head [0]
+        current_head = self.head
+        # if it is none, make a new head from passed value
+        if not current_head:
+            self.head = ListNode(value)
+        else:
+              # if it is not the head, add it before and turn the previous head into the previous value
+            current_head.insert_before(value)
+            self.head = current_head.prev
 
-  def remove_from_head(self):
-    pass
+    def remove_from_head(self):
+        if not self.head:
+            return None
+        else:
+            head = self.head
+            self.head.delete()
+            return head.value
 
-  def add_to_tail(self, value):
-    pass
+    def add_to_tail(self, value):
+        new_node = ListNode(value)
+        if not self.head:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.insert_after(new_node)
+            self.tail = new_node
 
-  def remove_from_tail(self):
-    pass
+    def remove_from_tail(self):
+        # if no tail return none
+        if not self.tail:
+            return None
+            # if tail is also the head, make both == None
+        if not self.tail.prev:
+            tail = self.tail
+            self.head = None
+            self.tail = None
+            return tail.value
+            # if tail is not also the head, remove it
+        else:
+            tail = self.tail
+            self.tail.delete()
+            self.tail = self.tail.prev
+            return tail.value
 
-  def move_to_front(self, node):
-    pass
+    def move_to_front(self, node):
+        # check if already is in front
+        if self.head is not node:
+            if node.next and node.prev:  # if in a middle spot
+                node.prev.next = node.next
+                node.next.prev = node.prev
+            current_head = self.head
+            self.head = node
+            node.next = current_head
+            current_head.prev = node
 
-  def move_to_end(self, node):
-    pass
+    def move_to_end(self, node):
+            # check if the nodelist is empty
+        if not self.head:
+            return None
+        current_node = self.tail
+        while current_node:
+            if current_node.value == node:
+                return self.add_to_tail(node)
+            current_node = current_node.prev
 
-  def delete(self, node):
-    pass
-    
-  def get_max(self):
-    pass
+    def delete(self, node):
+        pass
+
+    def get_max(self):
+        pass
--- doubly_linked_list/test_doubly_linked_list.py
+++ doubly_linked_list/test_doubly_linked_list.py
@@ -2,87 +2,89 @@ import unittest
 from doubly_linked_list import ListNode
 from doubly_linked_list import DoublyLinkedList
 
+
 class DoublyLinkedListTests(unittest.TestCase):
-  def setUp(self):
-    self.node = ListNode(1)
-    self.dll = DoublyLinkedList(self.node)
+    def setUp(self):
+        self.node = ListNode(1)
+        self.dll = DoublyLinkedList(self.node)
 
-  def test_list_remove_from_tail(self):
-    self.dll.add_to_tail(33)
-    self.assertEqual(self.dll.remove_from_tail(), 33)
+    def test_list_remove_from_tail(self):
+        self.dll.add_to_tail(33)
+        self.assertEqual(self.dll.remove_from_tail(), 33)
 
-    self.dll.add_to_tail(68)
-    self.assertEqual(self.dll.remove_from_tail(), 68)
+        self.dll.add_to_tail(68)
+        self.assertEqual(self.dll.remove_from_tail(), 68)
 
-  def test_list_remove_from_head(self):
-    self.dll.add_to_head(2)
-    self.assertEqual(self.dll.remove_from_head(), 2)
-    
-    self.dll.add_to_head(55)
-    self.assertEqual(self.dll.remove_from_head(), 55)
+    def test_list_remove_from_head(self):
+        self.dll.add_to_head(2)
+        self.assertEqual(self.dll.remove_from_head(), 2)
 
-  def test_list_add_to_tail(self):
-    self.dll.add_to_tail(30)
-    self.assertEqual(self.dll.tail.value, 30)
+        self.dll.add_to_head(55)
+        self.assertEqual(self.dll.remove_from_head(), 55)
 
-    self.dll.add_to_tail(20)
-    self.assertEqual(self.dll.tail.value, 20)
+    def test_list_add_to_tail(self):
+        self.dll.add_to_tail(30)
+        self.assertEqual(self.dll.tail.value, 30)
 
-  def test_node_delete(self):
-    node_1 = ListNode(3)
-    node_2 = ListNode(4)
-    node_3 = ListNode(5)
+        self.dll.add_to_tail(20)
+        self.assertEqual(self.dll.tail.value, 20)
 
-    node_1.next = node_2
-    node_2.next = node_3
-    node_2.prev = node_1
-    node_3.prev = node_2
+    def test_node_delete(self):
+        node_1 = ListNode(3)
+        node_2 = ListNode(4)
+        node_3 = ListNode(5)
 
-    node_2.delete()
+        node_1.next = node_2
+        node_2.next = node_3
+        node_2.prev = node_1
+        node_3.prev = node_2
 
-    self.assertEqual(node_1.next, node_3)
-    self.assertEqual(node_3.prev, node_1)
+        node_2.delete()
 
-  def test_node_insert_before(self):
-    self.node.insert_before(0)
-    self.assertEqual(self.node.prev.value, 0)
+        self.assertEqual(node_1.next, node_3)
+        self.assertEqual(node_3.prev, node_1)
 
-  def test_list_add_to_head(self):
-    self.assertEqual(self.dll.head.value, 1)
-    self.dll.add_to_head(10)
-    self.assertEqual(self.dll.head.value, 10)
-    self.assertEqual(self.dll.head.next.value, 1)
+    def test_node_insert_before(self):
+        self.node.insert_before(0)
+        self.assertEqual(self.node.prev.value, 0)
 
-  def test_node_insert_after(self):
-    self.node.insert_after(2)
-    self.assertEqual(self.node.next.value, 2)
+    def test_list_add_to_head(self):
+        self.assertEqual(self.dll.head.value, 1)
+        self.dll.add_to_head(10)
+        self.assertEqual(self.dll.head.value, 10)
+        self.assertEqual(self.dll.head.next.value, 1)
 
-  def test_list_move_to_end(self):
-    self.dll.add_to_head(40)
-    self.assertEqual(self.dll.tail.value, 1)
-    self.assertEqual(self.dll.head.value, 40)
+    def test_node_insert_after(self):
+        self.node.insert_after(2)
+        self.assertEqual(self.node.next.value, 2)
 
-    self.dll.move_to_end(self.dll.head)
-    self.assertEqual(self.dll.tail.value, 40)
-    self.assertEqual(self.dll.tail.prev.value, 1)
+    def test_list_move_to_end(self):
+        self.dll.add_to_head(40)
+        self.assertEqual(self.dll.tail.value, 1)
+        self.assertEqual(self.dll.head.value, 40)
 
-  def test_list_move_to_front(self):
-    self.dll.add_to_tail(3)
-    self.assertEqual(self.dll.head.value, 1)
-    self.assertEqual(self.dll.tail.value, 3)
+        self.dll.move_to_end(self.dll.head)
+        self.assertEqual(self.dll.tail.value, 40)
+        self.assertEqual(self.dll.tail.prev.value, 1)
 
-    self.dll.move_to_front(self.dll.tail)
-    self.assertEqual(self.dll.head.value, 3)
-    self.assertEqual(self.dll.head.next.value, 1)
+    def test_list_move_to_front(self):
+        self.dll.add_to_tail(3)
+        self.assertEqual(self.dll.head.value, 1)
+        self.assertEqual(self.dll.tail.value, 3)
 
-  def test_get_max(self):
-    self.assertEqual(self.dll.get_max(), 1)
-    self.dll.add_to_tail(100)
-    self.assertEqual(self.dll.get_max(), 100)
-    self.dll.add_to_tail(55)
-    self.assertEqual(self.dll.get_max(), 100)
-    self.dll.add_to_tail(101)
-    self.assertEqual(self.dll.get_max(), 101)
+        self.dll.move_to_front(self.dll.tail)
+        self.assertEqual(self.dll.head.value, 3)
+        self.assertEqual(self.dll.head.next.value, 1)
+
+    def test_get_max(self):
+        self.assertEqual(self.dll.get_max(), 1)
+        self.dll.add_to_tail(100)
+        self.assertEqual(self.dll.get_max(), 100)
+        self.dll.add_to_tail(55)
+        self.assertEqual(self.dll.get_max(), 100)
+        self.dll.add_to_tail(101)
+        self.assertEqual(self.dll.get_max(), 101)
+
 
 if __name__ == '__main__':
-  unittest.main()
+    unittest.main()

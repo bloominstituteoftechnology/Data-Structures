75a03a0dd7ad636cbc1705f250a524aef9c79f31
qtn2932 <quan.nguyen199712@gmail.com>
2019-02-27 15:45:03 -0500

    finished heap

--- heap/heap.py
+++ heap/heap.py
@@ -1,21 +1,55 @@
 class Heap:
   def __init__(self):
     self.storage = []
+    self.size=0
 
   def insert(self, value):
-    pass
+    self.storage.append(value)
+    self._bubble_up(len(self.storage)-1)
+    self.size+=1
 
   def delete(self):
-    pass
+    #print(f"Storage 0  {self.storage[0]}")
+    #print(f"Storage last  {self.storage[-1]}")
+    if len(self.storage)==1:
+      return self.storage.pop()
+    else:
+      if len(self.storage)==0:
+        return None
+      else:
+        self.storage[0]=self.storage.pop()
+        self._sift_down(0)
+        return self.storage[0]
 
   def get_max(self):
-    pass
+    return self.storage[0]
 
   def get_size(self):
-    pass
+    return self.size
 
   def _bubble_up(self, index):
-    pass
+    parent_index= (index-1) //2
+    while parent_index >= 0:
+      if self.storage[parent_index]< self.storage[index]:
+        self.storage[parent_index], self.storage[index]= self.storage[index], self.storage[parent_index]
+      else:
+        break
+      parent_index= (parent_index-1) //2
 
   def _sift_down(self, index):
-    pass
+    current_index=index
+    checker=False
+    while not checker:
+      if ((current_index*2)+1)> len(self.storage)-1 or ((current_index*2)+2)> len(self.storage)-1:
+        break
+      elif self.storage[(current_index*2)+1]> self.storage[(current_index*2)+2]:
+        if self.storage[(current_index*2)+1]> self.storage[current_index]:
+          self.storage[(current_index*2)+1], self.storage[index]=self.storage[index],self.storage[(current_index*2)+1]
+        else:
+          checker=True
+      elif self.storage[(current_index*2)+2]>= self.storage[(current_index*2)+1]:
+        if self.storage[(current_index*2)+2]> self.storage[current_index]:
+          self.storage[(current_index*2)+2], self.storage[index]=self.storage[index],self.storage[(current_index*2)+2]
+        else:
+          checker=True
+

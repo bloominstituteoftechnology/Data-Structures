19a5e943e9b1879e5901ee3c57359c7f02054011
qtn2932 <quan.nguyen199712@gmail.com>
2019-02-27 16:31:28 -0500

    made neccessary changes

--- heap/heap.py
+++ heap/heap.py
@@ -19,10 +19,11 @@ class Heap:
       if len(self.storage)==0:
         return None
       else:
+        temp=self.storage[0]
         self.storage[0]=self.storage.pop()
         self._sift_down(0)
         self.size-=1
-        return self.storage[0]
+        return temp
     
 
   def get_max(self):
@@ -33,11 +34,13 @@ class Heap:
 
   def _bubble_up(self, index):
     parent_index= (index-1) //2
+    current_index= index
     while parent_index >= 0:
-      if self.storage[parent_index]< self.storage[index]:
-        self.storage[parent_index], self.storage[index]= self.storage[index], self.storage[parent_index]
+      if self.storage[parent_index]< self.storage[current_index]:
+        self.storage[parent_index], self.storage[current_index]= self.storage[current_index], self.storage[parent_index]
       else:
         break
+      current_index=parent_index
       parent_index= (parent_index-1) //2
 
   def _sift_down(self, index):
@@ -45,15 +48,21 @@ class Heap:
     checker=False
     while not checker:
       if ((current_index*2)+1)> len(self.storage)-1 or ((current_index*2)+2)> len(self.storage)-1:
-        break
+        if ((current_index*2)+1)> len(self.storage)-1:
+          break
+        else:
+          if self.storage[(current_index*2)+1]> self.storage[current_index]:
+            self.storage[(current_index*2)+1], self.storage[current_index]=self.storage[current_index],self.storage[(current_index*2)+1]
+          else:
+            checker=True
       elif self.storage[(current_index*2)+1]> self.storage[(current_index*2)+2]:
         if self.storage[(current_index*2)+1]> self.storage[current_index]:
-          self.storage[(current_index*2)+1], self.storage[index]=self.storage[index],self.storage[(current_index*2)+1]
+          self.storage[(current_index*2)+1], self.storage[current_index]=self.storage[current_index],self.storage[(current_index*2)+1]
         else:
           checker=True
       elif self.storage[(current_index*2)+2]>= self.storage[(current_index*2)+1]:
         if self.storage[(current_index*2)+2]> self.storage[current_index]:
-          self.storage[(current_index*2)+2], self.storage[index]=self.storage[index],self.storage[(current_index*2)+2]
+          self.storage[(current_index*2)+2], self.storage[current_index]=self.storage[current_index],self.storage[(current_index*2)+2]
         else:
           checker=True
 

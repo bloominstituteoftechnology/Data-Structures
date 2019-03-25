8a44525580ca9a67c14fe5629ee5f87e29f01e0b
Sean Chen <skypemaster007@gmail.com>
2019-03-25 14:18:12 -0700

    Add __len__ method to dll implementation and update tests accordingly

--- doubly_linked_list/doubly_linked_list.py
+++ doubly_linked_list/doubly_linked_list.py
@@ -38,6 +38,10 @@ class DoublyLinkedList:
   def __init__(self, node=None):
     self.head = node
     self.tail = node
+    self.length = 1 if node is not None else 0
+
+  def __len__(self):
+    return self.length
 
   def add_to_head(self, value):
     pass
--- doubly_linked_list/test_doubly_linked_list.py
+++ doubly_linked_list/test_doubly_linked_list.py
@@ -11,38 +11,51 @@ class DoublyLinkedListTests(unittest.TestCase):
     self.dll.remove_from_tail()
     self.assertIsNone(self.dll.head)
     self.assertIsNone(self.dll.tail)
+    self.assertEqual(len(self.dll), 0)
 
     self.dll.add_to_tail(33)
     self.assertEqual(self.dll.head.value, 33)
     self.assertEqual(self.dll.tail.value, 33)
+    self.assertEqual(len(self.dll), 1)
     self.assertEqual(self.dll.remove_from_tail(), 33)
+    self.assertEqual(len(self.dll), 0)
 
     self.dll.add_to_tail(68)
+    self.assertEqual(len(self.dll), 1)
     self.assertEqual(self.dll.remove_from_tail(), 68)
+    self.assertEqual(len(self.dll), 0)
 
   def test_list_remove_from_head(self):
     self.dll.remove_from_head()
     self.assertIsNone(self.dll.head)
     self.assertIsNone(self.dll.tail)
+    self.assertEqual(len(self.dll), 0)
 
     self.dll.add_to_head(2)
     self.assertEqual(self.dll.head.value, 2)
     self.assertEqual(self.dll.tail.value, 2)
+    self.assertEqual(len(self.dll), 1)
     self.assertEqual(self.dll.remove_from_head(), 2)
+    self.assertEqual(len(self.dll), 0)
     
     self.dll.add_to_head(55)
+    self.assertEqual(len(self.dll), 1)
     self.assertEqual(self.dll.remove_from_head(), 55)
+    self.assertEqual(len(self.dll), 0)
 
   def test_list_add_to_tail(self):
     self.assertEqual(self.dll.tail.value, 1)
+    self.assertEqual(len(self.dll), 1)
 
     self.dll.add_to_tail(30)
     self.assertEqual(self.dll.tail.prev.value, 1)
     self.assertEqual(self.dll.tail.value, 30)
+    self.assertEqual(len(self.dll), 2)
 
     self.dll.add_to_tail(20)
     self.assertEqual(self.dll.tail.prev.value, 30)
     self.assertEqual(self.dll.tail.value, 20)
+    self.assertEqual(len(self.dll), 3)
 
   def test_node_delete(self):
     node_1 = ListNode(3)
@@ -65,9 +78,11 @@ class DoublyLinkedListTests(unittest.TestCase):
 
   def test_list_add_to_head(self):
     self.assertEqual(self.dll.head.value, 1)
+
     self.dll.add_to_head(10)
     self.assertEqual(self.dll.head.value, 10)
     self.assertEqual(self.dll.head.next.value, 1)
+    self.assertEqual(len(self.dll), 2)
 
   def test_node_insert_after(self):
     self.node.insert_after(2)
@@ -81,6 +96,13 @@ class DoublyLinkedListTests(unittest.TestCase):
     self.dll.move_to_end(self.dll.head)
     self.assertEqual(self.dll.tail.value, 40)
     self.assertEqual(self.dll.tail.prev.value, 1)
+    self.assertEqual(len(self.dll), 2)
+
+    self.dll.add_to_tail(4)
+    self.dll.move_to_end(self.dll.head.next)
+    self.assertEqual(self.dll.tail.value, 40)
+    self.assertEqual(self.dll.tail.prev.value, 4)
+    self.assertEqual(len(self.dll), 3)
 
   def test_list_move_to_front(self):
     self.dll.add_to_tail(3)
@@ -90,11 +112,19 @@ class DoublyLinkedListTests(unittest.TestCase):
     self.dll.move_to_front(self.dll.tail)
     self.assertEqual(self.dll.head.value, 3)
     self.assertEqual(self.dll.head.next.value, 1)
+    self.assertEqual(len(self.dll), 2)
+
+    self.dll.add_to_head(29)
+    self.dll.move_to_front(self.dll.head.next)
+    self.assertEqual(self.dll.head.value, 3)
+    self.assertEqual(self.dll.head.next.value, 29)
+    self.assertEqual(len(self.dll), 3)
 
   def test_list_delete(self):
     self.dll.delete(self.node)
     self.assertIsNone(self.dll.head)
     self.assertIsNone(self.dll.tail)
+    self.assertEqual(len(self.dll), 0)
 
     self.dll.add_to_tail(1)
     self.dll.add_to_head(9)
@@ -103,10 +133,12 @@ class DoublyLinkedListTests(unittest.TestCase):
     self.dll.delete(self.dll.head)
     self.assertEqual(self.dll.head.value, 1)
     self.assertEqual(self.dll.tail.value, 6)
+    self.assertEqual(len(self.dll), 2)
 
     self.dll.delete(self.dll.head)
     self.assertEqual(self.dll.head.value, 6)
     self.assertEqual(self.dll.tail.value, 6)
+    self.assertEqual(len(self.dll), 1)
 
   def test_get_max(self):
     self.assertEqual(self.dll.get_max(), 1)

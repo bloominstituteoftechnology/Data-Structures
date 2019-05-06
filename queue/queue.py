f73e999a58db0322118ce299e5b1772a50b94a3d
Moonkaman <nickbatmanglidj@gmail.com>
2019-05-06 13:51:17 -0600

    Complete queue.py
    Might do it with a linked list later for practice and to see if it's any faster

--- queue/queue.py
+++ queue/queue.py
@@ -1,15 +1,20 @@
 class Queue:
-  def __init__(self):
-    self.size = 0
-    # what data structure should we
-    # use to store queue elements?
-    self.storage = 
+    def __init__(self):
+        self.size = 0
+        # what data structure should we
+        # use to store queue elements?
+        self.storage = []
 
-  def enqueue(self, item):
-    pass
-  
-  def dequeue(self):
-    pass
+    def enqueue(self, item):
+        self.storage.insert(0, item)
+        self.size = len(self.storage)
 
-  def len(self):
-    pass
+    def dequeue(self):
+        if self.size == 0:
+            return None
+        popped_item = self.storage.pop()
+        self.size = len(self.storage)
+        return popped_item
+
+    def len(self):
+        return self.size

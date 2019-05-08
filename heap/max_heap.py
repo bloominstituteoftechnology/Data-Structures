6a5118b91ee0f22ddc945ad40cbba6176f7ef192
Thomas Claydon <tomclaydon102@gmail.com>
2019-05-08 18:53:23 -0400

    Completed MVP Day 4 - max_heap

--- heap/max_heap.py
+++ heap/max_heap.py
@@ -3,19 +3,64 @@ class Heap:
     self.storage = []
 
   def insert(self, value):
-    pass
+    self.storage.append(value)
+    self._bubble_up(len(self.storage)-1)
 
   def delete(self):
-    pass
+    print(f'arr:{self.storage}')
+    self.storage[0], self.storage[len(self.storage)-1] = self.storage[len(self.storage)-1], self.storage[0]
+    x = self.storage.pop()
+    self._sift_down(0)
+    # print(f'x:{x}')
+    return x
 
   def get_max(self):
-    pass
+    return self.storage[0]
 
   def get_size(self):
-    pass
+    return len(self.storage)
 
   def _bubble_up(self, index):
-    pass
+    while index > 0:
+      parent = (index-1)//2
+      if self.storage[parent] < self.storage[index]:
+        self.storage[parent], self.storage[index] = self.storage[index], self.storage[parent]
+        index = parent
+      else:
+        break
 
   def _sift_down(self, index):
-    pass
+    loop = True
+    while loop == True:
+      left = 2*(index) + 1
+      right = 2*(index) + 2
+      print(f'index:{index}')
+      print(f'left:{left}')
+      print(f'right:{right}')
+      arr = self.storage
+      length = len(arr)-1
+      print(f'length:{length}')
+
+      if left <= length and right <= length:
+        if arr[left] >= arr[index] and arr[left] >= arr[right]:
+          arr[left], arr[index] = arr[index], arr[left]
+          index = left
+        elif arr[right] >= arr[index] and arr[left] < arr[right]:
+          arr[right], arr[index] = arr[index], arr[right]
+          index = right
+        else:
+          loop = False
+      elif left <= length and right > length:
+        if arr[left] >= arr[index]:
+          arr[left], arr[index] = arr[index], arr[left]
+          index = left
+        else:
+          loop = False
+      elif right <= length and left > length:
+        if arr[right] >= arr[index]:
+          arr[right], arr[index] = arr[index], arr[right]
+          index = right
+        else:
+          loop = False
+      else:
+        loop = False

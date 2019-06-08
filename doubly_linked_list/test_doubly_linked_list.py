51a9cf33af6a7fe52bcfb29fb0ebaab408daeb16
Abigayle Franklin <abifanklin@gmail.com>
2019-06-07 21:46:29 -0500

    All tests passing. DLL complete.

--- doubly_linked_list/test_doubly_linked_list.py
+++ doubly_linked_list/test_doubly_linked_list.py
@@ -57,24 +57,24 @@ class DoublyLinkedListTests(unittest.TestCase):
     self.assertEqual(self.dll.tail.value, 20)
     self.assertEqual(len(self.dll), 3)
 
-  # def test_node_delete(self):
-  #   node_1 = ListNode(3)
-  #   node_2 = ListNode(4)
-  #   node_3 = ListNode(5)
+  def test_node_delete(self):
+    node_1 = ListNode(3)
+    node_2 = ListNode(4)
+    node_3 = ListNode(5)
 
-  #   node_1.next = node_2
-  #   node_2.next = node_3
-  #   node_2.prev = node_1
-  #   node_3.prev = node_2
+    node_1.next = node_2
+    node_2.next = node_3
+    node_2.prev = node_1
+    node_3.prev = node_2
 
-  #   node_2.delete()
+    node_2.delete()
 
-  #   self.assertEqual(node_1.next, node_3)
-  #   self.assertEqual(node_3.prev, node_1)
+    self.assertEqual(node_1.next, node_3)
+    self.assertEqual(node_3.prev, node_1)
 
-  # def test_node_insert_before(self):
-  #   self.node.insert_before(0)
-  #   self.assertEqual(self.node.prev.value, 0)
+  def test_node_insert_before(self):
+    self.node.insert_before(0)
+    self.assertEqual(self.node.prev.value, 0)
 
   def test_list_add_to_head(self):
     self.assertEqual(self.dll.head.value, 1)

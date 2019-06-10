39e50443ede88cdec7ca52aaccce3b5562e0c89d
Ivana Huckova <ivana.huckova@gmail.com>
2019-06-10 15:15:41 +0200

    Add linked list solution

--- queue/queue.py
+++ queue/queue.py
@@ -1,4 +1,9 @@
-class Queue:
+from linked_list import LinkedList
+
+# Array solution
+
+
+class Queue2:
     def __init__(self):
         self.size = 0
         # what data structure should we
@@ -15,3 +20,26 @@ class Queue:
 
     def len(self):
         return len(self.storage)
+
+# Linked List solution
+
+
+class Queue:
+    def __init__(self):
+        self.size = 0
+        # what data structure should we
+        # use to store queue elements?
+        self.storage = LinkedList()
+
+    def enqueue(self, item):
+        self.storage.add_to_tail(item)
+        self.size = self.size + 1
+
+    def dequeue(self):
+        if self.size is not 0:
+            deleted_head = self.storage.remove_head()
+            self.size = self.size - 1
+            return deleted_head
+
+    def len(self):
+        return self.size

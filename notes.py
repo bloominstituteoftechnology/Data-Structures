570fc70013519f97f35f1079b7bf2503dada2601
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-06-10 13:28:41 -0400

    Updates notes for edge cases

--- notes.py
+++ notes.py
@@ -23,8 +23,17 @@ class LinkedList:
     def add_to_tail(self, value):
         #  wrap the value in a node
         new_node = Node(value)
-        #  update the old tail's next reference to refer to the new code
-        self.tail.set_next(new_node)
-        #  update the LinkedList's 'tail' reference
-        self.tail = new_node
+        #  check if we're in an empty list state
+        if not self.head and not self.tail:
+            #  set the list's head reference to point to new_node
+            self.head = new_node
+            #  set the list's tail reference to point to new_node
+            self.tail = new_node
+        else:
+            #  update the old tail's next reference to refer to the new code
+            self.tail.set_next(new_node)  
+            #  update the LinkedList's 'tail' reference
+            self.tail = new_node
+        
+        
         

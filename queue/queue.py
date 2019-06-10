af6eae326b0f610bbedf352d87bf793c0077eaba
Ivana Huckova <ivana.huckova@gmail.com>
2019-06-10 14:40:53 +0200

    Create methods in Queue

--- queue/queue.py
+++ queue/queue.py
@@ -1,15 +1,17 @@
 class Queue:
-  def __init__(self):
-    self.size = 0
-    # what data structure should we
-    # use to store queue elements?
-    self.storage = 
+    def __init__(self):
+        self.size = 0
+        # what data structure should we
+        # use to store queue elements?
+        self.storage = []
 
-  def enqueue(self, item):
-    pass
-  
-  def dequeue(self):
-    pass
+    def enqueue(self, item):
+        self.storage.append(item)
 
-  def len(self):
-    pass
+    def dequeue(self):
+        if len(self.storage) < 1:
+            return None
+        return self.storage.pop(0)
+
+    def len(self):
+        return len(self.storage)

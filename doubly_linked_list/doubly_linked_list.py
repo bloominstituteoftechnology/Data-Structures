2130c05d50c873c3eac789faf5a39eda4907593c
AustinBCole <austin.cole.chileno@gmail.com>
2019-06-10 14:53:30 -0600

    Refactor code to use helper methods in ListNode class.

--- doubly_linked_list/doubly_linked_list.py
+++ doubly_linked_list/doubly_linked_list.py
@@ -44,98 +44,40 @@ class DoublyLinkedList:
     return self.length
 
   def add_to_head(self, value):
-    new_node = ListNode(value)
-    old_head = self.head
-    self.head = new_node
-    self.head.next = old_head
-    self.head.next.prev = self
-    
-    pass
+    self.head.insert_before(value)
 
   def remove_from_head(self):
-    if not self.head or not self.tail:
-        return
-    if not self.head.next:
-        self.head = None
-    else:
-        self.head.next.prev = None
+    old_head = self.head
+    if self.head.next:
         self.head = self.head.next
-    pass
+        self.head.prev = None
+    self.head.delete()
+    return old_head
 
   def add_to_tail(self, value):
-    old_tail = self.tail
-    old_tail.next = ListNode(value)
-    self.tail = old_tail.next
-    pass
-
+      self.tail.insert_after(value)
+  
   def remove_from_tail(self):
     old_tail = self.tail
-    if not old_tail.prev:
-        self.tail = None
-    else:
-        old_tail.prev.next = None
-        self.tail = old_tail.prev
-    pass
+    if self.tail.prev:
+        self.tail = self.tail.prev
+        self.tail.next = None
+    self.tail.delete()
+    return old_tail
 
   def move_to_front(self, node):
-    if node is not self.tail and node is not self.head:
-        node.next.prev = node.prev
-        node.prev.next = node.next
-    
-    elif node is self.tail:
-        print(f"Here it is!!!!\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nValue: {node.value}\n{self.tail.value}, {self.head.value}")
-        self.tail = node.prev
-        if not self.tail:
-            self.tail = self.head
-    
-    elif node is self.head:
-        return
-    
-    node.next = self.head
-    node.prev = None
-    self.head.prev = node
-    self.head = node
+    node.delete()
+    self.head.insert_before(node.value)
 
   def move_to_end(self, node):
-    if node is not self.tail and node is not self.head:
-        node.next.prev = node.prev
-        node.prev.next = node.next
-    
-    elif node is self.head:
-        node.next.prev = None
-        self.head = node.next
-    
-    elif node is self.tail:
-        return
-    
-    node.prev = self.tail
-    node.next = None
-    self.tail.next = node
-    self.tail = node
+    node.delete()
+    self.tail.insert_after(node.value)
+    pass
 
   def delete(self, node):
-    if not self.head and not self.tail: return
-    if not self.head or not self.tail:
-        self.head = None
-        self.tail = None
-        return
-    if node is self.tail and node is self.head:
-        self.tail = None
-        self.head = None
-    elif node is self.tail:
-        print(f"Here it is!!!!\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nValue: {node.value}\n{self.tail.value}, {self.head.value}")
-        node.prev.next = None
-        self.tail = node.prev
-    elif node is self.head:
-        node.next.prev = None
-        self.head = node.next
-    else:
-        node.prev = node.next.prev
-        node.next = node.prev.next
+    node.delete()
     
   def get_max(self):
-    if not self.head and not self.tail: return None
-    
     current = self.head
     max_value = 0
     while current:
@@ -143,3 +85,4 @@ class DoublyLinkedList:
             max_value = current.value
         current = current.next
     return max_value
+

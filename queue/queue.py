28748a4f2174123239d7d71fbdd3d9a6ca8712be
Jacob Angulo <jacobangulo@hotmail.com>
2019-06-10 13:33:15 -0700

    initial commit - queue completed

--- queue/queue.py
+++ queue/queue.py
@@ -1,15 +1,19 @@
 class Queue:
-  def __init__(self):
-    self.size = 0
-    # what data structure should we
-    # use to store queue elements?
-    self.storage = 
+    def __init__(self):
+        self.size = 0
+        # what data structure should we
+        # use to store queue elements?
+        self.storage = []
 
-  def enqueue(self, item):
-    pass
-  
-  def dequeue(self):
-    pass
+    def enqueue(self, item):
+        self.storage.insert(0, item)
 
-  def len(self):
-    pass
+    def dequeue(self):
+        if len(self.storage) == 0:
+            pass
+        else:
+            removed_item = self.storage.pop()
+            return removed_item
+
+    def len(self):
+        return len(self.storage)

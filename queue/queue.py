4cbfa976c05ba8ed0eb9b1433fd61028a1a1010a
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-06-10 15:37:55 -0400

    Adds notes to queue.py

--- queue/queue.py
+++ queue/queue.py
@@ -1,3 +1,32 @@
+  #  Linear data structures
+  #----------------------------------------------------------------------
+    #  Queues => FIFO => First one In, First one Out
+        #  => FIFO 
+          #  => First one In, First one Out 
+          #  => i.e. - line to ride a roller coaster, printer jobs
+      #  lookup 
+          #  => O(n)
+      #  enqueue (add an item to the BACK of the queue) 
+          #  => O(1)
+      #  dequeue (remove an item from the FRONT of the queue) 
+          #  => O(1)
+      #  peek (view the FRONT-most item in the queue)
+          #  => O(1)
+#----------------------------------------------------------------------
+  #  Stacks 
+      #  => LIFO 
+          #  => Last one In, First one Out 
+          #  => i.e. - stack of plates
+      #  lookup 
+          #  => O(n)
+      #  pop (remove last item) 
+          #  => O(1)
+      #  push (add an item to end of the stack) 
+          #  => O(1)
+      #  peek (view the top-most item in the stack)
+          #  => O(1)
+#----------------------------------------------------------------------
+
 class Queue:
   def __init__(self):
     self.size = 0

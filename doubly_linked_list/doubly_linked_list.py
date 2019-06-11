a006751dcb731f705ff236c37f750f7487cc9d3d
AustinBCole <austin.cole.chileno@gmail.com>
2019-06-11 13:39:23 -0600

    Finish all methods but delete.

--- doubly_linked_list/doubly_linked_list.py
+++ doubly_linked_list/doubly_linked_list.py
@@ -58,14 +58,18 @@ class DoublyLinkedList:
         self.head = self.head.prev
     else:
         self.head = ListNode(value)
+        self.tail = self.head
 
   def remove_from_head(self):
     old_head = self.head
+    self.head.delete()
     if self.head.next:
         self.head = self.head.next
         self.head.prev = None
-    self.head.delete()
-    return old_head
+    else:
+        self.head = None
+        self.tail = None
+    return old_head.value
 
   def add_to_tail(self, value):
     if self.tail:
@@ -73,14 +77,19 @@ class DoublyLinkedList:
         self.tail = self.tail.next
     else:
         self.tail = ListNode(value)
+        self.head = self.tail
   
   def remove_from_tail(self):
     old_tail = self.tail
-    if self.tail.prev:
+    if self.tail == self.head:
+        self.tail = None
+        self.head = None
+        return old_tail.value
+    elif self.tail.prev:
         self.tail = self.tail.prev
         self.tail.next = None
-    self.tail.delete()
-    return old_tail
+        self.tail.delete()
+        return old_tail.value
 
   def move_to_front(self, node):
     if self.head == node:
@@ -92,17 +101,21 @@ class DoublyLinkedList:
     self.head = self.head.prev
 
   def move_to_end(self, node):
+    if self.head == node:
+        self.head = self.head.next
     node.delete()
-    self.tail.insert_after(node.value)
-    pass
+    self.add_to_tail(node.value)
+    self.tail = self.tail.next
+
 
   def delete(self, node):
     node.delete()
     if node == self.head:
-        self.head = None
         if node.next:
             self.head = node.next
-            node.next.prev = None
+            self.head.prev = None
+        else:
+            self.head = None
     if node == self.tail:
         self.tail = None
     if self.tail is None and self.head is None:

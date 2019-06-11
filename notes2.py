a133a0beaf9c6c951972c1da1202365fdc4661b3
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-06-11 12:33:13 -0400

    Creates a notes2.py file to stor lecture notes for day 2 of Sean's lecture

---
+++ notes2.py
@@ -0,0 +1,5 @@
+#----------------Text Buffer------------------
+#  1) Add characters from front and back
+#  2) Remove characters from front and back
+#  3) Render the contents of our buffer
+#  4) Concatenate two buffers together (copying/pasting)

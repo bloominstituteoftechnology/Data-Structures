b20c568d5d088b2e05052f156c0d399ffcd58372
bcabanayan <bcabanayan@gmail.com>
2019-06-11 20:02:50 -0700

    Completed get_max and contains for BST

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -19,22 +19,29 @@ class BinarySearchTree:
   def contains(self, target):
     # test if the value is the target
     # if not the target, test if value is less than
-    # run co left value if it exists
+    # run left value if it exists
     # if not less than, value is greater than
     # check right value if it exists
-    if self.value == target:
+    if target == self.value:
+      print('True')
       return True
-    elif self.value < target:
-      if self.right:
+    elif target > self.value and self.right != None:
         print(self.right.value)
-        self.right.contains(target)
-    elif self.left:
-      print(self.left.value)
-      self.left.contains(target)
+        return self.right.contains(target)
+    elif target < self.value and self.left != None:
+        print(self.left.value)
+        return self.left.contains(target)
     return False
 
   def get_max(self):
-    pass
+    # get current value
+    # see if there is a right value
+    # if there is a right value, store as max
+    if self.value == None or self.right == None:
+      return self.value
+    return self.right.get_max()
+
+    
 
   def for_each(self, cb):
     pass

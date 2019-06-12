b87e84c479e5338b7fcb7d9fbc03dee509eddf56
AustinBCole <austin.cole.chileno@gmail.com>
2019-06-12 15:15:05 -0600

    Edit lru test file to correct error. Finishe lru cache.

--- lru_cache/doubly_linked_list.py
+++ lru_cache/doubly_linked_list.py
@@ -38,31 +38,97 @@ class DoublyLinkedList:
   def __init__(self, node=None):
     self.head = node
     self.tail = node
+    self.node = node
     self.length = 1 if node is not None else 0
 
   def __len__(self):
-    return self.length
+    if self.node == None:
+        return 0
+    else:
+        count = 0
+        current = self.head
+        while current:
+            count += 1
+            current = current.next
+        return count
 
   def add_to_head(self, value):
-    pass
+    if self.head:
+        self.head.insert_before(value)
+        self.head = self.head.prev
+    else:
+        self.head = ListNode(value)
+        self.tail = self.head
 
   def remove_from_head(self):
-    pass
+    old_head = self.head
+    self.head.delete()
+    if self.head.next:
+        self.head = self.head.next
+        self.head.prev = None
+    else:
+        self.head = None
+        self.tail = None
+    return old_head.value
 
   def add_to_tail(self, value):
-    pass
-
+    if self.tail:
+        self.tail.insert_after(value)
+        self.tail = self.tail.next
+    else:
+        self.tail = ListNode(value)
+        self.head = self.tail
+  
   def remove_from_tail(self):
-    pass
+    old_tail = self.tail
+    if self.tail == self.head:
+        self.tail = None
+        self.head = None
+        return old_tail.value
+    elif self.tail.prev:
+        self.tail = self.tail.prev
+        self.tail.next = None
+        self.tail.delete()
+        return old_tail.value
 
   def move_to_front(self, node):
-    pass
+    if self.head == node:
+        return
+    elif self.tail == node:
+        self.tail = node.prev
+    node.delete()
+    self.head.insert_before(node.value)
+    self.head = self.head.prev
 
   def move_to_end(self, node):
-    pass
+    if self.head == node:
+        if self.head.next:
+            self.head = self.head.next
+        else:
+            self.head = None
+            self.tail = None
+            self.node = None
+    node.delete()
+    self.add_to_tail(node.value)
 
   def delete(self, node):
-    pass
+    node.delete()
+    if node == self.head:
+        if node.next:
+            self.head = node.next
+            self.head.prev = None
+        else:
+            self.head = None
+    if node == self.tail:
+        self.tail = None
+
     
   def get_max(self):
-    pass
+    current = self.head
+    max_value = 0
+    while current:
+        if current.value > max_value:
+            max_value = current.value
+        current = current.next
+    return max_value
+
--- lru_cache/lru_cache.py
+++ lru_cache/lru_cache.py
@@ -1,6 +1,10 @@
+from doubly_linked_list import ListNode
+from doubly_linked_list import DoublyLinkedList
+
 class LRUCache:
   def __init__(self, limit=10):
-    pass
+    self.storage = {}
+    self.order_of_use = []
 
   """
   Retrieves the value associated with the given key. Also
@@ -10,8 +14,15 @@ class LRUCache:
   key-value pair doesn't exist in the cache. 
   """
   def get(self, key):
-    pass
-
+    if key in self.storage:
+        for i in range(0, len(self.order_of_use)):
+            if self.order_of_use[i] == key:
+                swapped_element = self.order_of_use[0]
+                self.order_of_use[0] = self.order_of_use[i]
+                self.order_of_use[i] = swapped_element
+                break
+        return self.storage[key]
+    return None
   """
   Adds the given key-value pair to the cache. The newly-
   added pair should be considered the most-recently used
@@ -23,4 +34,16 @@ class LRUCache:
   the newly-specified value. 
   """
   def set(self, key, value):
-    pass
+    if len(self.storage) == 10:
+        self.storage[self.order_of_use[-1]] = None
+        self.order_of_use.remove(-1)
+    if key in self.storage:
+        for i in range(0, len(self.order_of_use)):
+            if self.order_of_use[i] == key:
+                swapped_element = self.order_of_use[0]
+                self.order_of_use[0] = self.order_of_use[i]
+                self.order_of_use[i] = swapped_element
+                break
+    else:
+        self.order_of_use.insert(0, key)
+    self.storage[key] = value

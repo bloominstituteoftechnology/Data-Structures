ab43b82dc23e1a73c29b6d5edf2bf4f53b1d796f
Jor_Poon <jormpoon@gmail.com>
2019-06-12 17:26:55 -0400

    completed insert for BST

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -5,7 +5,24 @@ class BinarySearchTree:
     self.right = None
 
   def insert(self, value):
-    pass
+    #check if the value being inserted is less than current value (left)
+    if value < self.value:
+      #check if the left child node has spot, if it does not place a new tree
+      if not self.left:
+        self.left = BinarySearchTree(value)
+      #if it does, rerun the function for the next spot
+      else:
+        self.left.insert(value)
+    #check if value being inserted is greater than current value (right)
+    elif value >= self.value:
+      #check if the right child node has spot open
+      if not self.right:
+        self.right = BinarySearchTree(value)
+        #repeat process if spot is not empty
+      else:
+        self.right.insert(value)
+
+    
 
   def contains(self, target):
     pass

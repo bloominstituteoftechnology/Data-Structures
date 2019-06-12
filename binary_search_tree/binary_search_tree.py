4c5c207cfa45545f7bd109c857daee16a5bda6cc
Abigayle Franklin <abifanklin@gmail.com>
2019-06-11 22:21:30 -0500

    BST complete and passing all tests

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -36,4 +36,8 @@ class BinarySearchTree:
       return self.right.get_max()
 
   def for_each(self, cb):
-    pass
+      cb(self.value)
+      if self.left:
+        self.left.for_each(cb)
+      if self.right:
+        self.right.for_each(cb)

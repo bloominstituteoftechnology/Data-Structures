386008480e376ffcc4e6a878ffe3878640ba48ea
Ivana Huckova <ivana.huckova@gmail.com>
2019-06-13 15:28:02 +0200

    Bubble up and insert

--- heap/max_heap.py
+++ heap/max_heap.py
@@ -3,37 +3,24 @@ class Heap:
         self.storage = []
 
     def insert(self, value):
-        k = 0
-        while (2*k + 2) <= len(self.storage):
-            if value > self.storage[k]:
-                self.storage.insert(k, value)
-                break
-
-            elif self.storage[2*k + 1] > self.storage[2*k + 2]:
-                if value > self.storage[2*k + 1]:
-                    self.storage.insert(2*k + 1, value)
-
-            elif self.storage[2*k + 1] <= self.storage[2*k + 2]:
-                if value > self.storage[2*k + 2]:
-                    self.storage.insert(2*k + 2, value)
-
-        k += 1
+        self.storage.append(value)
+        self._bubble_up(len(self.storage) - 1)
 
     def delete(self):
         pass
 
     def get_max(self):
-        pass
+        return self.storage[0]
 
     def get_size(self):
         pass
 
     def _bubble_up(self, index):
         while (index - 1) // 2 >= 0:
-            if self.storage[(index - 1) // 2] < self.storage[index]:
-                self.storage[index], self.storage[(
-                    index - 1) // 2] = self.storage[(index - 1) // 2], self.storage[index]
-            index = (index - 1) // 2
+            parent_idx = (index - 1) // 2
+            if self.storage[parent_idx] < self.storage[index]:
+                self.storage[index], self.storage[parent_idx] = self.storage[parent_idx], self.storage[index]
+            index = parent_idx
 
     def _sift_down(self, index):
         pass

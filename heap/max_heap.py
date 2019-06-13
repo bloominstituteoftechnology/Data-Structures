8b5f5e3905e558fcb8fac687b2e2291322bbdf8f
Ivana Huckova <ivana.huckova@gmail.com>
2019-06-13 14:46:33 +0200

    Complete bubble up

--- heap/max_heap.py
+++ heap/max_heap.py
@@ -1,21 +1,39 @@
 class Heap:
-  def __init__(self):
-    self.storage = []
+    def __init__(self):
+        self.storage = []
 
-  def insert(self, value):
-    pass
+    def insert(self, value):
+        k = 0
+        while (2*k + 2) <= len(self.storage):
+            if value > self.storage[k]:
+                self.storage.insert(k, value)
+                break
 
-  def delete(self):
-    pass
+            elif self.storage[2*k + 1] > self.storage[2*k + 2]:
+                if value > self.storage[2*k + 1]:
+                    self.storage.insert(2*k + 1, value)
 
-  def get_max(self):
-    pass
+            elif self.storage[2*k + 1] <= self.storage[2*k + 2]:
+                if value > self.storage[2*k + 2]:
+                    self.storage.insert(2*k + 2, value)
 
-  def get_size(self):
-    pass
+        k += 1
 
-  def _bubble_up(self, index):
-    pass
+    def delete(self):
+        pass
 
-  def _sift_down(self, index):
-    pass
+    def get_max(self):
+        pass
+
+    def get_size(self):
+        pass
+
+    def _bubble_up(self, index):
+        while (index - 1) // 2 >= 0:
+            if self.storage[(index - 1) // 2] < self.storage[index]:
+                self.storage[index], self.storage[(
+                    index - 1) // 2] = self.storage[(index - 1) // 2], self.storage[index]
+            index = (index - 1) // 2
+
+    def _sift_down(self, index):
+        pass

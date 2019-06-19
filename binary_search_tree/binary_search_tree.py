d44f8859f3b9d3202e6ab23fdbf2fa1279b0144e
ills5771 <ilyayelizarov1@gmail.com>
2019-06-19 15:39:13 -0700

    adds for each function to bst

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -32,4 +32,8 @@ class BinarySearchTree:
             return self.right.get_max()
 
     def for_each(self, cb):
-        pass
+        cb(self.value)
+        if self.right:
+            self.right.for_each(cb)
+        if self.left:
+            self.left.for_each(cb)
--- binary_search_tree/test_binary_search_tree.py
+++ binary_search_tree/test_binary_search_tree.py
@@ -30,30 +30,30 @@ class BinarySearchTreeTests(unittest.TestCase):
         self.bst.insert(3)
         self.assertEqual(self.bst.get_max(), 300)
 
-    # def test_for_each(self):
-    #     arr = []
-    #     def cb(x): return arr.append(x)
+    def test_for_each(self):
+        arr = []
+        def cb(x): return arr.append(x)
 
-    #     v1 = random.randint(1, 101)
-    #     v2 = random.randint(1, 101)
-    #     v3 = random.randint(1, 101)
-    #     v4 = random.randint(1, 101)
-    #     v5 = random.randint(1, 101)
+        v1 = random.randint(1, 101)
+        v2 = random.randint(1, 101)
+        v3 = random.randint(1, 101)
+        v4 = random.randint(1, 101)
+        v5 = random.randint(1, 101)
 
-    #     self.bst.insert(v1)
-    #     self.bst.insert(v2)
-    #     self.bst.insert(v3)
-    #     self.bst.insert(v4)
-    #     self.bst.insert(v5)
+        self.bst.insert(v1)
+        self.bst.insert(v2)
+        self.bst.insert(v3)
+        self.bst.insert(v4)
+        self.bst.insert(v5)
 
-    #     self.bst.for_each(cb)
+        self.bst.for_each(cb)
 
-    #     self.assertTrue(5 in arr)
-    #     self.assertTrue(v1 in arr)
-    #     self.assertTrue(v2 in arr)
-    #     self.assertTrue(v3 in arr)
-    #     self.assertTrue(v4 in arr)
-    #     self.assertTrue(v5 in arr)
+        self.assertTrue(5 in arr)
+        self.assertTrue(v1 in arr)
+        self.assertTrue(v2 in arr)
+        self.assertTrue(v3 in arr)
+        self.assertTrue(v4 in arr)
+        self.assertTrue(v5 in arr)
 
 
 if __name__ == '__main__':

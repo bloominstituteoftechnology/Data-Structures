849eb8f42bc4a23840dae4ad23f630bc6281f88e
Denise Escobar <descobar1490@gmail.com>
2019-06-18 22:39:55 -0500

    Started Working on queue.py

--- queue/queue.py
+++ queue/queue.py
@@ -3,11 +3,18 @@ class Queue:
     self.size = 0
     # what data structure should we
     # use to store queue elements?
-    self.storage = 
+    self.storage = def __init__(self, head=None, tail=None ):
+      self.head = head
+      self.tail = tail
 
   def enqueue(self, item):
     pass
-  
+    if head == None: 
+      self.head = item
+      self.tail = item
+    else: 
+      self.head = item
+      
   def dequeue(self):
     pass
 

f076b1fee30d9f28aa478eebbc9945a765cbaa42
Joseph Mercado <josephmercado@Joes-MacBook-Pro.local>
2019-06-19 20:40:34 -0400

    initial commit

--- queue/queue.py
+++ queue/queue.py
@@ -3,10 +3,16 @@ class Queue:
     self.size = 0
     # what data structure should we
     # use to store queue elements?
-    self.storage = 
+    self.storage = Linked_List()
 
   def enqueue(self, item):
-    pass
+    if self.storage.head == None:
+    			value = Node(item)
+			self.storage.change_head(value)
+			self.size =+ 1
+		else:
+			self.storage.change_tail(item)
+			self.size += 1
   
   def dequeue(self):
     pass

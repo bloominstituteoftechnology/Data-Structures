33cd5f3c38c5e497fd9c78845b0fbf6951e9450c
Emmanium <pradoe.ep@gmail.com>
2019-06-19 20:53:13 -0600

    initial commit filled out queue class

--- queue/queue.py
+++ queue/queue.py
@@ -1,15 +1,21 @@
 class Queue:
-  def __init__(self):
-    self.size = 0
-    # what data structure should we
-    # use to store queue elements?
-    self.storage = 
+    def __init__(self):
+        self.size = 0
+        # what data structure should we
+        # use to store queue elements?
+        # Answer: Linked List
+        self.storage = []
 
-  def enqueue(self, item):
-    pass
-  
-  def dequeue(self):
-    pass
+    def enqueue(self, item):
+        self.storage.append(item)
 
-  def len(self):
-    pass
+    def dequeue(self):
+        return self.storage.pop()
+
+    def len(self):
+        return len(self.size)
+
+
+class LinkedList:
+    def __init__(self, head=None):
+        self.head = head

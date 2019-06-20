dfab793c458b0983654783ec6dc77ef8ab350362
LorenzoEvans <lorenzo.evans94@gmail.com>
2019-06-19 21:51:47 -0400

    Queue passing all 4 tests.

--- queue/queue.py
+++ queue/queue.py
@@ -11,9 +11,9 @@ class Queue:
   
   def dequeue(self):
     if len(self.storage) > 1 or len(self.storage) == 1:
+     temp = self.storage[:1].pop()
      self.storage.pop(0)
-    elif len(self.storage) < 1:
-     return None 
+     return temp
 
   def len(self):
    return len(self.storage)
@@ -21,14 +21,18 @@ class Queue:
 
 new_Queue = Queue()
 
-new_Queue.enqueue(1)
+new_Queue.enqueue(100)
 
-new_Queue.enqueue(2)
+new_Queue.enqueue(101)
 
-new_Queue.enqueue(3)
+new_Queue.enqueue(105)
+
+new_Queue.dequeue()
 
 def printer(struct):
  for x in struct.storage:
   print(x)
+
+# print('dequeue test', new_Queue.dequeue())
 
 print(printer(new_Queue))

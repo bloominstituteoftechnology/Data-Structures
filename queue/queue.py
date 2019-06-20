bad62ba51b5534f44cc34d019c928faeeffba4fc
Emmanium <pradoe.ep@gmail.com>
2019-06-19 22:10:48 -0600

    filled out more of queue.py

--- queue/queue.py
+++ queue/queue.py
@@ -1,16 +1,52 @@
+class Node(object):
+    def __init__(self, data=None, next_node="None"):
+        self.data = data
+        self.next_node = next_node
+
+    def get_data(self):
+        return self.data
+
+    def get_next(self):
+        return self.next_node
+
+    def set_next(self, new_next):
+        self.next_node = new_next
+
+
+class LinkedList(object):
+    def __init__(self, head=None):
+        self.head = head
+        self.tail = None
+
+    def add_to_tail(self, data):
+        new_node = Node(data)
+        new_node.set_next(self.tail)
+        self.tail = new_node
+
+    def size(self):
+        current = self.head
+        count = 0
+        while current:
+            count += 1
+            current = current.get_next()
+        return count
+
+    def remove_from_head():
+
+
 class Queue:
     def __init__(self):
         self.size = 0
         # what data structure should we
         # use to store queue elements?
         # Answer: Linked List
-        self.storage = []
+        self.storage = LinkedList()
 
     def enqueue(self, item):
-        self.storage.append(item)
+        pass
 
     def dequeue(self):
-        return self.storage.pop()
+        pass
 
     def len(self):
-        return len(self.storage)
+        return self.size

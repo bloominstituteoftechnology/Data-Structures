d24e6cfa7251f8df776bc4c3c411198543ae41fa
ills5771 <ilyayelizarov1@gmail.com>
2019-06-20 13:57:54 -0700

    adds insert and get max methods to max heap

--- heap/max_heap.py
+++ heap/max_heap.py
@@ -1,21 +1,31 @@
 class Heap:
-  def __init__(self):
-    self.storage = []
+    def __init__(self):
+        self.storage = []
 
-  def insert(self, value):
-    pass
+    def insert(self, value):
+        self.storage.append(value)  # add new value to the end of the heap
+        self._bubble_up(len(self.storage) - 1)
 
-  def delete(self):
-    pass
+    def delete(self):
+        pass
 
-  def get_max(self):
-    pass
+    def get_max(self):
+        if len(self.storage) > 0:
+            return self.storage[0]
+        else:
+            return None
 
-  def get_size(self):
-    pass
+    def get_size(self):
+        pass
 
-  def _bubble_up(self, index):
-    pass
+    def _bubble_up(self, index):
+        while index > 0:
+            parent = (index - 1) // 2
+            if self.storage[index] > self.storage[parent]:
+                self.storage[index], self.storage[parent] = self.storage[parent], self.storage[index]
+                index = parent
+            else:
+                break
 
-  def _sift_down(self, index):
-    pass
+    def _sift_down(self, index):
+        pass

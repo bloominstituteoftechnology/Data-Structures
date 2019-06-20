4fd5038aa1f02edd4a7a79b13f8e75d105788aa6
LorenzoEvans <lorenzo.evans94@gmail.com>
2019-06-19 21:42:01 -0400

    Wrote some code to test my struct, w00t.

--- queue/queue.py
+++ queue/queue.py
@@ -11,9 +11,24 @@ class Queue:
   
   def dequeue(self):
     if len(self.storage) > 1 or len(self.storage) == 1:
-     self.storage.remove(0)
+     self.storage.pop(0)
     elif len(self.storage) < 1:
      return None 
 
   def len(self):
    return len(self.storage)
+
+
+new_Queue = Queue()
+
+new_Queue.enqueue(1)
+
+new_Queue.enqueue(2)
+
+new_Queue.enqueue(3)
+
+def printer(struct):
+ for x in struct.storage:
+  print(x)
+
+print(printer(new_Queue))

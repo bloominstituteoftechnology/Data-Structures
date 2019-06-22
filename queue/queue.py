2769d227048f198c36ce0c7e55bc9b39edf4b139
Denise Escobar <descobar1490@gmail.com>
2019-06-22 13:10:57 -0500

    added a node class

--- queue/queue.py
+++ queue/queue.py
@@ -14,9 +14,26 @@ class Queue:
       self.tail = item
     else: 
       self.head = item
-      
+
   def dequeue(self):
     pass
 
   def len(self):
     pass
+
+class Node: 
+  def __init__(self, value=None, next_node=None):
+    self.value = value
+    self.next_node = next_node
+
+  def get_value(self):
+    return self.value
+  
+  def get_next(self):
+    return self.next_node
+
+  def set_next(self, new_next):
+    self.next_node = new_next
+
+  def set_value(self, value):
+    self.value = value

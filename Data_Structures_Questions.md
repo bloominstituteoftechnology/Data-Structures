75cf132a6e15a77dddc1f002c97503e99246de1c
Frulwinn <frulwinn@gmail.com>
2019-06-24 14:50:32 -0500

    queues and questions for queues

--- Data_Structures_Questions.md
+++ Data_Structures_Questions.md
@@ -2,11 +2,11 @@ Answer the following questions for each of the data structures you implemented a
 
 ## Queue
 
-1. What is the runtime complexity of `enqueue`?
+1. What is the runtime complexity of `enqueue`? O(1)
 
-2. What is the runtime complexity of `dequeue`?
+2. What is the runtime complexity of `dequeue`? O(1)
 
-3. What is the runtime complexity of `len`?
+3. What is the runtime complexity of `len`? O(n)
 
 ## Binary Search Tree
 
--- queue/queue.py
+++ queue/queue.py
@@ -3,13 +3,28 @@ class Queue:
     self.size = 0
     # what data structure should we
     # use to store queue elements?
-    self.storage = 
+    self.storage = []
 
+  #enqueue should add an item to the back of the queue.
   def enqueue(self, item):
-    pass
+    self.storage.append(item)
   
+  #should remove and return an item from the front of the queue
   def dequeue(self):
-    pass
+    #check if there are items
+    if not self.storage:
+      return None
+    #remove item at first position
+    remove_item = self.storage[0]
 
+    self.storage.remove(self.storage[0])
+    return remove_item
+
+  #len returns the number of items in the queue
   def len(self):
-    pass
+    count = 0
+
+    for items in self.storage:
+      count += 1
+    
+    return count

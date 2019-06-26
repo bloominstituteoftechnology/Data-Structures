0ec9dc7e386f95ff50c85aa8ba2e826c35732e83
cocoitali <cmburatto@gmail.com>
2019-06-25 21:35:27 -0600

    max_heap : bubble_up

--- heap/max_heap.py
+++ heap/max_heap.py
@@ -15,7 +15,21 @@ class Heap:
     pass
 
   def _bubble_up(self, index):
-    pass
+    # 1. Check to see if the index is greater than zero
+        # 2. Grab parent index
+        # 3. Check if current value is greater than or less than parent value
+        #     a. If current is greater than
+        #     b. Swap
+        # 4. If current is lesser than parent
+        #     a. Leave it alone - break
+        while index > 0:
+            parent = (index - 1) // 2
+
+            if self.storage[index] > self.storage[parent]:
+                self.storage[index], self.storage[parent] = self.storage[parent], self.storage[index]
+                index = parent #Updating your new index
+            else:
+                break
 
   def _sift_down(self, index):
     pass

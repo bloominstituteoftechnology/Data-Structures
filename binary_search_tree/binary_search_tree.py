67be255fb66fc99d70cd22bb36206a1582a26078
FreeKeyBoy <jjhutton81@gmail.com>
2019-06-25 18:23:36 -0700

    Created function for insert in BST.

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -1,17 +1,34 @@
 class BinarySearchTree:
-  def __init__(self, value):
-    self.value = value
-    self.left = None
-    self.right = None
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
 
-  def insert(self, value):
+
+def insert(self, value):
+    # need a recursive function that movs along the tree and inserts a node where it belongs.
+    # if value less than parent go down left branch
+    if value < self.value:
+        if self.left != None:
+            self.left.insert(value)
+        else:
+            self.left = BinarySearchTree(value)
+
+    elif value > self.value:
+        # Same as before, but down the right branch for greater values
+        if self.right != None:
+            self.right.insert(value)
+        else:
+            self.right = BinarySearchTree(value)
+
+
+def contains(self, target):
     pass
 
-  def contains(self, target):
+
+def get_max(self):
     pass
 
-  def get_max(self):
-    pass
 
-  def for_each(self, cb):
+def for_each(self, cb):
     pass

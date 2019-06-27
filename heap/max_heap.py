0aa8b2a68e65844b8eeb95cdf1d54bb475dc9f99
FreeKeyBoy <jjhutton81@gmail.com>
2019-06-26 20:38:38 -0700

    Wrote insert for max heap.

--- heap/max_heap.py
+++ heap/max_heap.py
@@ -1,21 +1,24 @@
 class Heap:
-  def __init__(self):
-    self.storage = []
+    def __init__(self):
+        self.storage = []
 
-  def insert(self, value):
-    pass
+    def insert(self, value):
+        #store value at end of list
+        #bubble up value if needed
+        self.storage.append(value)
+        self._bubble_up(len(self.storage)-1)
 
-  def delete(self):
-    pass
+    def delete(self):
+        pass
 
-  def get_max(self):
-    pass
+    def get_max(self):
+        pass
 
-  def get_size(self):
-    pass
+    def get_size(self):
+        pass
 
-  def _bubble_up(self, index):
-    pass
+    def _bubble_up(self, index):
+        pass
 
-  def _sift_down(self, index):
-    pass
+    def _sift_down(self, index):
+        pass

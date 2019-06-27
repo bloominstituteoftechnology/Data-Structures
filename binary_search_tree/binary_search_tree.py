1109bf4e96cde9e60a4d0b270163f5eef3261405
LorenzoEvans <lorenzo.evans94@gmail.com>
2019-06-26 22:36:45 -0400

    Working on BST.

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -5,7 +5,20 @@ class BinarySearchTree:
     self.right = None
 
   def insert(self, value):
-    pass
+    # Wrap value in node.
+    # 1. Compare the value against the current node value.
+    # 2. Based on the result of the comparison, go left or right.
+    # 3. If we find an empty spot, park the value there.
+    # 4. Otherwise, repeat.
+    # Base case: we've found an empty spot to park a value.
+
+    if value > self.value:
+      # If value is less, we go left.
+      # If there is no left child, we park the node here
+      if self.left is None:
+        self.left = BinarySearchTree(value)
+      else:
+        self.left.insert(value)
 
   def contains(self, target):
     pass

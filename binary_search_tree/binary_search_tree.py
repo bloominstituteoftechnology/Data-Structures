e6a52a9881c7621babbeb33e0083a9fd51d346a7
Kendra Williams <kmonique.williams@gmail.com>
2019-06-28 01:31:52 -0400

    one failure on contains

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -5,13 +5,38 @@ class BinarySearchTree:
     self.right = None
 
   def insert(self, value):
-    pass
+    if value < self.value:
+      if self.left != None:
+        self.left.insert(value)
+      else:
+        self.left = BinarySearchTree(value)
+    elif value > self.value:
+      if self.right != None:
+        self.right.insert(value)
+      else:
+        self.right = BinarySearchTree(value)
 
   def contains(self, target):
-    pass
+    if target == self.value:
+      return True
+    
+    if target < self.value:
+      if self.left == None:
+        return False
+      return self.left.contains(target)
 
   def get_max(self):
-    pass
+    if self.right == None:
+      return self.value
+    return self.right.get_max()
 
   def for_each(self, cb):
-    pass
+    cb(self.value)
+    if self.right is None and self.left is None:
+      return
+    else:
+      if self.left is not None:
+        self.left.for_each(cb)
+      if self.right is not None:
+        self.right.for_each(cb)
+    return

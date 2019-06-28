6998eb85a76b4c79e84150ef0ef9379783411aa2
Jennifer King <jenbellpgh@gmail.com>
2019-06-28 09:19:08 -0400

    Finished max_heap

--- heap/max_heap.py
+++ heap/max_heap.py
@@ -7,20 +7,33 @@ class Heap:
     self._bubble_up(len(self.storage)-1)
 
   def delete(self):
-    pass
+    
+    if self.storage[0]:
+      val = self.storage[0]
+
+      if self.get_size() <= 1:
+        self.storage = []
+      else:
+        # Set index 0 to the last value in list, and remove last
+        self.storage[0] = self.storage.pop(self.get_size()-1)
+        self._sift_down(0)
+      return val
 
   def get_max(self):
-    pass
+    if self.get_size() >= 1:
+      return self.storage[0]
+    else:
+      return None
 
   def get_size(self):
-    pass
+    return len(self.storage)
 
   def _bubble_up(self, index):
     # 1. Check if index is greater than zero
     # 2. Get parent index
     # 3. If current value > parent: Swap
     # 4. Otherwise leave it & break
-    print(self.storage)
+    
     while index > 0:
       parent = (index - 1) // 2
 
@@ -33,15 +46,19 @@ class Heap:
 
 
   def _sift_down(self, index):
-    pass
+    # Check if index is greater than 0
+    # Calc left & right
+    # Determine > of left/right
+    # If > left/right > index value - swap
 
+    while index < self.get_size():
+      left = (2 * index) + 1 if (2 * index) + 1 < self.get_size() else index
+      right = (2 * index) + 2 if (2 * index) + 2 < self.get_size() else index
+      maxIndex = left if self.storage[left] > self.storage[right] else right
 
-heap = Heap()
-heap.insert(6)
-heap.insert(8)
-heap.insert(10)
-heap.insert(9)
-heap.insert(1)
-heap.insert(9)
-heap.insert(9)
-heap.insert(5)
+      if self.storage[index] < self.storage[maxIndex]:
+        # Swap them!
+        self.storage[index], self.storage[maxIndex] = self.storage[maxIndex], self.storage[index]
+        index = maxIndex
+      else:
+        break

47c9c6d26a645252532798060cab193f597377b7
unknown <kyleteeter12@gmail.com>
2019-06-27 21:49:28 -0500

    Getting max of heap

--- heap/max_heap.py
+++ heap/max_heap.py
@@ -3,13 +3,24 @@ class Heap:
     self.storage = []
 
   def insert(self, value):
-    pass
+    # 1. Check last 
+    self[len(self)] = v
+    # 2. Find Index of last value
+    index = self.length-1
+    # 3.
+    while (i > 1 and self[parent(i)] < self(i))
+        (self[i], self[parent(i)])
+
 
   def delete(self):
-    pass
+    # 1. Find 
+    parent = (len(self) -1) // 2
+    # 2. Remove parent
+    # 3. Replace parent position.
 
   def get_max(self):
-    pass
+    max = (len(self) -1) // 2
+    return max
 
   def get_size(self):
     pass

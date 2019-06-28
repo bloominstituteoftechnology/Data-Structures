9d9619c3f5d82ada09ffe7addf57be1a14936ebb
FreeKeyBoy <jjhutton81@gmail.com>
2019-06-27 18:38:36 -0700

    Wrote delete, get_max, and get_size in max heap.

--- heap/max_heap.py
+++ heap/max_heap.py
@@ -3,19 +3,29 @@ class Heap:
         self.storage = []
 
     def insert(self, value):
-        #store value at end of list
-        #bubble up value if needed
+        # store value at end of list
+        # bubble up value if needed
         self.storage.append(value)
         self._bubble_up(len(self.storage)-1)
 
     def delete(self):
-        pass
+        # swap index 0 with last index if needed
+        # sift down and pop the new largest node to top
+        if len(self.storage) > 1:
+            self.swap(0,len(self.storage)-1)
+            max = self.storage.pop()
+            self._sift_down(0)
+        elif len(self.storage) == 1:
+            max = self.storage.pop()
+        else:
+            max = False
+        return max
 
     def get_max(self):
-        pass
+        return self.storage[0]
 
     def get_size(self):
-        pass
+        return len(self.storage)
 
     def _bubble_up(self, index):
         pass

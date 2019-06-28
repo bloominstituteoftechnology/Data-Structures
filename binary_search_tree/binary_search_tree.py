2baaa7b55e3aa7585cdaa5b94471c34ade3e6425
LorenzoEvans <lorenzo.evans94@gmail.com>
2019-06-27 23:22:28 -0400

    Saving changes so far.

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -53,7 +53,8 @@ class BinarySearchTree:
   def for_each(self, cb):
     if self.left is None and self.right is None:
       cb(self.value)
-    while self.left is not None or self.right is not None:
+      exit()
+    elif self.left is not None or self.right is not None:
       if self.left:
         self.for_each(self.left)
       if self.right:

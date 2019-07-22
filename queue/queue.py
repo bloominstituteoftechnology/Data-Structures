c34de28ecd3e3c43066e922ed01065d1c3276e04
Phil Hart <phil@Phils-MacBook-Pro.local>
2019-07-22 12:14:24 -0600

    Starting on queue

--- queue/queue.py
+++ queue/queue.py
@@ -3,10 +3,10 @@ class Queue:
     self.size = 0
     # what data structure should we
     # use to store queue elements?
-    self.storage = 
+    self.storage = []
 
   def enqueue(self, item):
-    pass
+    
   
   def dequeue(self):
     pass

7fb7b2805f1a251a8faa269cb0a88752f04684b6
Julie Jonak <juliejonak10@gmail.com>
2019-07-22 17:56:04 -0500

    Adds repr() statements and add_to_head

--- doubly_linked_list/doubly_linked_list.py
+++ doubly_linked_list/doubly_linked_list.py
@@ -6,9 +6,12 @@ class ListNode:
     self.prev = prev
     self.next = next
 
+  def __repr__(self):
+    return "Value: {}, ".format(self.value if self.value else None) + "Prev: {}, ".format(self.prev.value if self.prev else None) + "Next: {} \n".format(self.next.value if self.next else None)
+
   """Wrap the given value in a ListNode and insert it
   after this node. Note that this node could already
-  have a next node it is point to."""
+  have a next node it is pointing to."""
   def insert_after(self, value):
     current_next = self.next
     self.next = ListNode(value, self, current_next)
@@ -17,7 +20,7 @@ class ListNode:
 
   """Wrap the given value in a ListNode and insert it
   before this node. Note that this node could already
-  have a previous node it is point to."""
+  have a previous node it is pointing to."""
   def insert_before(self, value):
     current_prev = self.prev
     self.prev = ListNode(value, current_prev, self)
@@ -39,30 +42,60 @@ class DoublyLinkedList:
     self.head = node
     self.tail = node
     self.length = 1 if node is not None else 0
+  
+  def __repr__(self):
+    return f"Head: {self.head} \n Tail: {self.tail} \n Length: {self.length}"
 
   def __len__(self):
     return self.length
 
+  """Replaces the head of the list with a new value that is passed in."""
   def add_to_head(self, value):
-    pass
+    if self.length == 0:
+      self.head = ListNode(value, None, None)
+      self.tail = self.head
+      self.length = 1
+    else:
+      self.head.prev = ListNode(value, None, self.head)
+      self.head = self.head.prev
+      self.length += 1
 
+  """Replaces the tail of the list with a new value that is passed in."""
   def remove_from_head(self):
     pass
 
+  """Removes the head node and returns the value stored in it."""
   def add_to_tail(self, value):
-    pass
+    # If len is 0:
+      # Add new ListNode and += length
+    # If len > 0: 
+      # Create new ListNode(value, None, list[0].head.next)
+      # Set list[0].head.prev = ListNode
+      # += length
+      pass
 
+  """Removes the tail node and returns the value stored in it"""
   def remove_from_tail(self):
     pass
 
+  """Takes a reference to a node in the list and moves it to the front of the list, shifting all other list nodes down."""
   def move_to_front(self, node):
     pass
 
+  """Takes a reference to a node in the list and moves it to the end of the list, shifting all other list nodes up."""
   def move_to_end(self, node):
     pass
 
+  """Takes a reference to a node in the list and removes it from the list. The deleted node's `previous` and `next` pointers should point to each afterwards."""
   def delete(self, node):
     pass
     
+  """Returns the maximum value in the list."""
   def get_max(self):
     pass
+
+ll = DoublyLinkedList()
+print(f"ll: {ll}")
+ll.add_to_head(2)
+ll.add_to_head(5)
+ll.add_to_head(7)

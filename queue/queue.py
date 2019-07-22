0d0abe2054ec72a718bb273d305c1a7582ebfb28
nomadic4life <orbison.dennis@gmail.com>
2019-07-22 13:31:05 -0700

    queue

--- queue/queue.py
+++ queue/queue.py
@@ -1,15 +1,25 @@
+from collections import deque
+
+
 class Queue:
-  def __init__(self):
-    self.size = 0
-    # what data structure should we
-    # use to store queue elements?
-    self.storage = 
+    def __init__(self):
+        self.size = 0
+        # what data structure should we
+        # use to store queue elements?
+        self.storage = deque()
 
-  def enqueue(self, item):
-    pass
-  
-  def dequeue(self):
-    pass
+    def enqueue(self, item):
+        self.storage.append(item)
+        self.size += 1
 
-  def len(self):
-    pass
+    def dequeue(self):
+        if not self.is_empty():
+            self.size -= 1
+            return self.storage.popleft()
+        return None
+
+    def len(self):
+        return self.size
+
+    def is_empty(self):
+        return self.len() == 0

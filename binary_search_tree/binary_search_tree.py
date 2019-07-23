4dab1a64ebc461c5499a8f57d72540e4343dc385
Cole Hudson <cole@colejhudson.com>
2019-07-23 13:48:07 -0700

    add binary search tree exercise

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -5,13 +5,38 @@ class BinarySearchTree:
     self.right = None
 
   def insert(self, value):
-    pass
+      if self.value < value:
+          if self.right is None:
+              self.right = BinarySearchTree(value)
+          else:
+              self.right.insert(value)
+      elif self.value >= value:
+          if self.left is None:
+              self.left = BinarySearchTree(value)
+          else:
+            self.left.insert(value)
 
   def contains(self, target):
-    pass
+      if target == self.value:
+          return True
+      elif self.right and self.value < target:
+          return self.right.contains(target)
+      elif self.left and self.value > target:
+          return self.left.contains(target)
+      else:
+          return False
 
   def get_max(self):
-    pass
+      if self.right != None:
+          return self.right.get_max()
+      else:
+          return self.value
 
   def for_each(self, cb):
-    pass
+      cb(self.value)
+
+      if self.left != None:
+          self.left.for_each(cb)
+    
+      if self.right != None:
+          self.right.for_each(cb)

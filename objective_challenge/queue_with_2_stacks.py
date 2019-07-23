27ee27342bae74f71ad42a4040e1ad7803f437b0
Winston <wxl51@yahoo.com>
2019-07-23 11:07:45 -0500

    implement queue_with_2_stacks.py in objective_challenge

---
+++ objective_challenge/queue_with_2_stacks.py
@@ -0,0 +1,40 @@
+
+class Queue:
+    def __init__(self):
+        self.in_stack = []
+        self.out_stack = []
+
+    def enqueue(self, item):
+        self.in_stack.append(item)
+
+    def dequeue(self):
+        if len(self.out_stack) == 0:
+            while len(self.in_stack) > 0:
+                self.out_stack.append(self.in_stack.pop())
+        return self.out_stack.pop() if self.out_stack else False
+
+    def len(self):
+        return len(self.out_stack)
+
+queue = Queue()
+
+print(queue.dequeue())
+
+queue.enqueue(5)
+queue.enqueue(8)
+queue.enqueue(13)
+
+print(queue.len()) # will print 0 'cause out_stack not yet populated
+
+print(queue.dequeue())
+
+print(queue.len())
+
+queue.enqueue(5)
+queue.enqueue(8)
+
+print(queue.len())
+
+print(queue.dequeue())
+
+print(queue.len())

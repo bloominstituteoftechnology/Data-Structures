0e0a61fbddd82709dab0526794a3d4a41afd51ab
Andrew Richardson <andrewlowellr@gmail.com>
2019-07-23 10:49:38 -0600

    Finish queue.

--- queue/queue.py
+++ queue/queue.py
@@ -3,16 +3,44 @@ class Queue:
     self.size = 0
     # what data structure should we
     # use to store queue elements?
-    self.storage = []
+    self.storage = LinkedList()
 
   def enqueue(self, item):
-    self.storage.append(item)
-    self.size += 1
+    self.storage.push(item)
   
   def dequeue(self):
-    head = self.storage[0]
-    self.storage = self.storage[0:]
-    self.size -= 1
+    return self.storage.pop()
 
   def len(self):
-    return self.size
+    return self.storage.length
+
+class LinkedList:
+  def __init__(self):
+    self.head = None
+    self.tail = None
+    self.length = 0
+
+  def push(self, value):
+    new_node = Node(value)
+    if not self.head:
+      self.head = new_node
+    elif not self.tail:
+      self.head.next = new_node
+      self.tail = new_node
+    else:
+      self.tail.next = new_node
+      self.tail = new_node
+    self.length += 1
+
+  def pop(self):
+    if self.length == 0:
+      return None
+    popped = self.head.value
+    self.head = self.head.next
+    self.length -= 1
+    return popped
+
+class Node:
+  def __init__(self, value):
+    self.value = value
+    self.next = None

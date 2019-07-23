8fcb6fbf5f2c10fd340e73a628bcde2868dc3cb5
derek-shing <hpshing@gmail.com>
2019-07-22 23:13:12 -0700

    finish queue

--- queue/queue.py
+++ queue/queue.py
@@ -1,18 +1,20 @@
+from link_list import Node, Linklist
+
 class Queue:
   def __init__(self):
     self.size = 0
     # what data structure should we
     # use to store queue elements?
-    self.storage = []
+    self.storage = Linklist()
 
   def enqueue(self, item):
-    self.storage.append((item))
+    self.storage.add_to_tail(item)
     self.size +=1
     pass
   
   def dequeue(self):
     if self.size > 0:
-      element = self.storage.pop(0)
+      element = self.storage.remove_head()
       self.size -=1
       return element
     pass

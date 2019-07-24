8f34f27baf8bd9e1f10671e0828086e79963af0e
xyz <dkav@live.com>
2019-07-24 16:27:42 -0400

    g heap bubble up

--- heap/generic_heap.py
+++ heap/generic_heap.py
@@ -16,7 +16,16 @@ class Heap:
     pass
 
   def _bubble_up(self, index):
-    pass
+      while index > 0:
+          # compare to parent
+          parent = (index - 1) // 2
+
+          if self.storage[index] > self.storage[parent]:
+              self.storage[index], self.storage[parent] = self.storage[parent], self.storage[index]
+
+              index = parent
+        else:
+            break
 
   def _sift_down(self, index):
     pass

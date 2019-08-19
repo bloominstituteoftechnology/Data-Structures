8c486396d2382bdeffdfec7106aabb9a4b497d6f
Brian Doyle <briandoyle81@gmail.com>
2019-08-19 12:21:46 -0600

    Added specs for unwritten methods

--- doubly_linked_list/doubly_linked_list.py
+++ doubly_linked_list/doubly_linked_list.py
@@ -42,27 +42,46 @@ class DoublyLinkedList:
 
   def __len__(self):
     return self.length
-
+  
+  """Wraps the given value in a ListNode and inserts it 
+  as the new head of the list. Don't forget to handle 
+  the old head node's previous pointer accordingly."""
   def add_to_head(self, value):
     pass
-
+  
+  """Removes the List's current head node, making the
+  current head's next node the new head of the List.
+  Returns the removed Node."""
   def remove_from_head(self):
     pass
 
+   """Wraps the given value in a ListNode and inserts it 
+  as the new tail of the list. Don't forget to handle 
+  the old tail node's next pointer accordingly."""
   def add_to_tail(self, value):
     pass
 
+  """Removes the List's current tail node, making the 
+  current tail's previous node the new tail of the List.
+  Returns the removed Node."""
   def remove_from_tail(self):
     pass
 
+  """Removes the input node from its current spot in the 
+  List and inserts it as the new head node of the List."""
   def move_to_front(self, node):
     pass
 
+  """Removes the input node from its current spot in the 
+  List and inserts it as the new tail node of the List."""
   def move_to_end(self, node):
     pass
 
+  """Removes a node from the list and handles cases where
+  the node was the head or the tail"""
   def delete(self, node):
     pass
     
+  """Returns the highest value currently in the list"""
   def get_max(self):
     pass

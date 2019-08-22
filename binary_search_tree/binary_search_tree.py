fb5865f0a70a7352df50930cc607652ac1f1e97a
Angel <angelbuenrostro@outlook.com>
2019-08-22 12:44:52 -0400

    BST 2 Tests Passed

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -9,14 +9,46 @@ class BinarySearchTree:
     # We start searching a key from root 
     # till we hit a leaf node. Once a leaf node is found,
     # the new node is added as a child of the leaf node.
-    if self.value:
-      if self.value > value:
-        
+    if value < self.value:
+      #Go Left
+      if not self.left:
+        #add as leaf
+        self.left = BinarySearchTree(value)
+      else:
+        #check again
+        self.left.insert(value)
+    else:
+      #Go Right
+      if not self.right:
+        self.right = BinarySearchTree(value)
+      else:
+        self.right.insert(value)
+
   def contains(self, target):
+    # Searching a key
+    # To search a given key in Binary Search Tree, we first compare it with root, 
+    # if the key is present at root, we return root. If key is greater than root's 
+    # key, we recur for right subtree of root node. Otherwise we recur for left 
+    # subtree.
+    if self.value == target:
+      return True
+    if target < self.value:
+      if not self.left:
+        return False
+      else:
+        return self.left.contains(target)
+    else:
+      if not self.right:
+        return False
+      else:
+        return self.right.contains(target)
     pass
 
   def get_max(self):
+    # Keep going right until you find a node without a child to the right
     pass
 
   def for_each(self, callback):
+    # We need to traverse the tree similar to how the print works in the demo
+    # For each value append it to the array
     pass

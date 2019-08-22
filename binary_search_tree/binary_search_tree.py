61cdc4472d09bb8e4ca68f2a760a33bbfeb2ce7c
Phil Hart <phil@Phils-MacBook-Pro.local>
2019-08-21 19:49:12 -0600

    binary search complete

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -18,10 +18,31 @@ class BinarySearchTree:
 
 
   def contains(self, target):
-    pass
+    if target == self.value:
+      return True
+
+    if target > self.value:
+      if self.right:
+        return self.right.contains(target)
+      else:
+        return False
+    if target < self.value:
+      if self.left:
+        return self.left.contains(target)
+      else:
+        return False
+    
 
   def get_max(self):
-    pass
+    if self.right:
+      return self.right.get_max()
+    else:
+      return self.value
+
 
   def for_each(self, cb):
-    pass
+    cb(self.value)
+    if self.left:
+      self.left.for_each(cb)
+    if self.right:
+      self.right.for_each(cb)

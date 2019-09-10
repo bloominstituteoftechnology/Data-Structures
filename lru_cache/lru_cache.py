425b183db85e97c6d410b2bfc22f78ace48646e2
cocoitali <cmburatto@gmail.com>
2019-09-09 22:04:57 -0600

    lru_cache.py : complete

--- lru_cache/lru_cache.py
+++ lru_cache/lru_cache.py
@@ -1,6 +1,14 @@
+import sys
+sys.path.append('../doubly_linked_list')
+
+from doubly_linked_list import DoublyLinkedList
+
 class LRUCache:
   def __init__(self, limit=10):
-    pass
+    self.max_num_nodes = limit
+    self.current_nodes = 0
+    self.cache = {}
+    self.order = DoublyLinkedList()
 
   """
   Retrieves the value associated with the given key. Also
@@ -10,7 +18,14 @@ class LRUCache:
   key-value pair doesn't exist in the cache. 
   """
   def get(self, key):
-    pass
+    value = None
+    if key in self.cache:
+        
+        node = self.cache[key]
+        value = node[1]
+        self.order.move_to_front(node)
+
+    return value
 
   """
   Adds the given key-value pair to the cache. The newly-
@@ -23,4 +38,23 @@ class LRUCache:
   the newly-specified value. 
   """
   def set(self, key, value):
-    pass
+    # is the key in our cache?
+    # Case 3
+    if key in self.cache:
+        # overwrite the value in our DLL, self.order
+        # move to the front, it's the most recently used
+        node = self.cache[key]
+        self.order.move_to_front(node)
+        node.value = (key, value)
+
+    else:
+        # Case 2
+        if self.current_nodes == self.max_num_nodes:
+            del self.cache[self.order.tail.value[0]]# deletes key from cache
+            self.order.remove_from_tail()
+            self.current_nodes -= 1
+
+        # Case 1
+        self.order.add_to_head((key, value)) #most recently used value, add key/value pair (tuple)
+        self.cache[key] = self.order.head
+        self.current_nodes += 1

d8c73fb9373fa75c40e7a56da82c63ebd0df8eb0
ckopecky <kopecky12112@gmail.com>
2019-09-16 15:24:02 -0700

    Implement Stack and testcases

--- queue_and_stack/dll_stack.py
+++ queue_and_stack/dll_stack.py
@@ -1,18 +1,24 @@
 import sys
 sys.path.append('../doubly_linked_list')
-from doubly_linked_list import DoublyLinkedList
+from doubly_linked_list import DoublyLinkedList, ListNode
 
 class Stack:
   def __init__(self):
     self.size = 0
     # Why is our DLL a good choice to store our elements?
-    self.storage = 
+    self.storage = DoublyLinkedList()
 
   def push(self, value):
-    pass
+    self.storage.add_to_tail(value)
+    self.size += 1
   
   def pop(self):
-    pass
+    if self.size > 0:
+      self.size -= 1
+      return self.storage.remove_from_head()
+    else:
+      return None
+
 
   def len(self):
-    pass
+    return self.size
---
+++ queue_and_stack/test_stack.py
@@ -0,0 +1,44 @@
+import unittest
+from dll_stack import Stack
+
+class StackTests(unittest.TestCase):
+  def setUp(self):
+    self.s = Stack()
+
+  def test_len_returns_0_for_empty_queue(self):
+    self.assertEqual(self.s.size, 0)
+
+  def test_len_returns_correct_length_after_push(self):
+    self.assertEqual(self.s.size, 0)
+    self.s.push(2)
+    self.assertEqual(self.s.size, 1)
+    self.s.push(4)
+    self.assertEqual(self.s.size, 2)
+    self.s.push(6)
+    self.s.push(8)
+    self.s.push(10)
+    self.s.push(12)
+    self.s.push(14)
+    self.s.push(16)
+    self.s.push(18)
+    self.assertEqual(self.s.size, 9)
+  
+  def test_empty_pop(self):
+    self.assertIsNone(self.s.pop())
+    self.assertEqual(self.s.size, 0)
+
+  def test_pop_respects_order(self):
+    self.s.push(100)
+    self.s.push(101)
+    self.s.push(105)
+    self.assertEqual(self.s.pop(), 100)
+    self.assertEqual(self.s.size, 2)
+    self.assertEqual(self.s.pop(), 101)
+    self.assertEqual(self.s.size, 1)
+    self.assertEqual(self.s.pop(), 105)
+    self.assertEqual(self.s.size, 0)
+    self.assertIsNone(self.s.pop())
+    self.assertEqual(self.s.size, 0)
+
+if __name__ == '__main__':
+  unittest.main()

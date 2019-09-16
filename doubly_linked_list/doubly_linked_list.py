864a28a2cadf54b5f56359463c6be4e7232fc046
ckopecky <kopecky12112@gmail.com>
2019-09-16 11:03:49 -0700

    Add add_to_tail to dll

--- doubly_linked_list/doubly_linked_list.py
+++ doubly_linked_list/doubly_linked_list.py
@@ -49,7 +49,16 @@ class DoublyLinkedList:
     return deleted_head_value
 
   def add_to_tail(self, value):
-    pass
+    new_node = ListNode(value)
+    if not self.head and not self.tail:
+      self.head = new_node
+      self.tail = new_node
+    else:
+      new_node.prev = self.tail
+      new_node.next = None
+      self.tail = new_node
+
+    self.length += 1
 
   def remove_from_tail(self):
     pass

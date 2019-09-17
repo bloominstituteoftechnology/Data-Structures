33dee6878d39314e85f3e7d954e626a12b63a398
ThaDocIzN <rodan72@msn.com>
2019-09-17 06:30:51 -0500

    add to tail is done

--- doubly_linked_list/doubly_linked_list.py
+++ doubly_linked_list/doubly_linked_list.py
@@ -73,7 +73,15 @@ class DoublyLinkedList:
   as the new tail of the list. Don't forget to handle 
   the old tail node's next pointer accordingly."""
   def add_to_tail(self, value):
-    pass
+    self.length += 1
+    new_node = ListNode(value, None, None)
+    if not self.head and not self.tail:
+      self.head = new_node
+      self.tail = new_node
+    else:
+      new_node.prev = self.tail
+      self.tail.next = new_node
+      self.tail = new_node
 
   """Removes the List's current tail node, making the 
   current tail's previous node the new tail of the List.

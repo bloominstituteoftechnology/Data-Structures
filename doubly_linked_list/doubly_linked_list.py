09d2c7579c85d3869b0c6e5e31ffbe7a12d554b2
ThaDocIzN <rodan72@msn.com>
2019-09-17 07:03:18 -0500

    reformat code to try to get rid of errors

--- doubly_linked_list/doubly_linked_list.py
+++ doubly_linked_list/doubly_linked_list.py
@@ -1,5 +1,7 @@
 """Each ListNode holds a reference to its previous node
 as well as its next node in the List."""
+
+
 class ListNode:
   def __init__(self, value, prev=None, next=None):
     self.value = value
@@ -9,6 +11,7 @@ class ListNode:
   """Wrap the given value in a ListNode and insert it
   after this node. Note that this node could already
   have a next node it is point to."""
+
   def insert_after(self, value):
     current_next = self.next
     self.next = ListNode(value, self, current_next)
@@ -18,6 +21,7 @@ class ListNode:
   """Wrap the given value in a ListNode and insert it
   before this node. Note that this node could already
   have a previous node it is point to."""
+
   def insert_before(self, value):
     current_prev = self.prev
     self.prev = ListNode(value, current_prev, self)
@@ -26,14 +30,18 @@ class ListNode:
 
   """Rearranges this ListNode's previous and next pointers
   accordingly, effectively deleting this ListNode."""
+
   def delete(self):
     if self.prev:
       self.prev.next = self.next
     if self.next:
       self.next.prev = self.prev
 
+
 """Our doubly-linked list class. It holds references to
 the list's head and tail nodes."""
+
+
 class DoublyLinkedList:
   def __init__(self, node=None):
     self.head = node
@@ -42,25 +50,26 @@ class DoublyLinkedList:
 
   def __len__(self):
     return self.length
-  
+
   """Wraps the given value in a ListNode and inserts it 
   as the new head of the list. Don't forget to handle 
   the old head node's previous pointer accordingly."""
-  def add_to_head(self, value):
-     self.length += 1
-     new_node = ListNode(value, None, None)
-     if not self.head and not self.tail:
-        self.head = new_node
-        self.tail = new_node
-     else:
-         new_node.next = self.head
-         self.head.prev = new_node
-         self.head = new_node
 
-  
+  def add_to_head(self, value):
+    self.length += 1
+    new_node = ListNode(value, None, None)
+    if not self.head and not self.tail:
+      self.head = new_node
+      self.tail = new_node
+    else:
+      new_node.next = self.head
+      self.head.prev = new_node
+      self.head = new_node
+
   """Removes the List's current head node, making the
   current head's next node the new head of the List.
   Returns the value of the removed Node."""
+
   def remove_from_head(self):
     if self.head is not None:
       value = self.head.value
@@ -69,9 +78,10 @@ class DoublyLinkedList:
     else:
       return None
 
-   """Wraps the given value in a ListNode and inserts it 
-  as the new tail of the list. Don't forget to handle 
-  the old tail node's next pointer accordingly."""
+  """Wraps the given value in a ListNode and inserts it 
+ as the new tail of the list. Don't forget to handle 
+ the old tail node's next pointer accordingly."""
+
   def add_to_tail(self, value):
     self.length += 1
     new_node = ListNode(value, None, None)
@@ -86,6 +96,7 @@ class DoublyLinkedList:
   """Removes the List's current tail node, making the 
   current tail's previous node the new tail of the List.
   Returns the value of the removed Node."""
+
   def remove_from_tail(self):
     if self.tail is not None:
       value = self.tail.value
@@ -96,6 +107,7 @@ class DoublyLinkedList:
 
   """Removes the input node from its current spot in the 
   List and inserts it as the new head node of the List."""
+
   def move_to_front(self, node):
     if node is self.head:
       return
@@ -106,14 +118,17 @@ class DoublyLinkedList:
 
   """Removes the input node from its current spot in the 
   List and inserts it as the new tail node of the List."""
+
   def move_to_end(self, node):
     pass
 
   """Removes a node from the list and handles cases where
   the node was the head or the tail"""
+
   def delete(self, node):
     pass
-    
+
   """Returns the highest value currently in the list"""
+
   def get_max(self):
     pass
--- queue_and_stack/dll_queue.py
+++ queue_and_stack/dll_queue.py
@@ -4,23 +4,25 @@ from doubly_linked_list.doubly_linked_list import DoublyLinkedList
 
 
 class Queue:
-   def __init__(self):
-    self.size = 0
-    # Why is our DLL a good choice to store our elements?
-    self.storage = DoublyLinkedList()
+    def __init__(self):
+        self.size = 0
+        # Why is our DLL a good choice to store our elements?
+        self.storage = DoublyLinkedList()
 
-  def enqueue(self, value):
+
+def enqueue(self, value):
     self.size += 1
     self.storage.add_to_head(value)
 
 
-  def dequeue(self):
+def dequeue(self):
     if self.len() > 0:
-      self.size -= 1
-      val = self.storage.tail.value
-      self.storage.remove_from_tail()
-      return val
+        self.size -= 1
+        val = self.storage.tail.value
+        self.storage.remove_from_tail()
+        return val
     return None
 
-  def len(self):
+
+def len(self):
     return self.size

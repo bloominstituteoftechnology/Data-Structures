8c801c8d6b30a51d40f8cc20cf24260289c9e4ae
briandoyle81 <briandoyle81@gmail.com>
2019-09-19 12:09:31 -0600

    fix parameters

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -32,7 +32,7 @@ class BinarySearchTree:
 
     # Print all the values in order from low to high
     # Hint:  Use a recursive, depth first traversal
-    def in_order_print(self):
+    def in_order_print(self, node):
         pass
 
     # Print the value of every node, starting with the given node,

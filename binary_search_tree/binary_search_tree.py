15ae9600705b53474a7c64cae583ea2f3ee34a96
briandoyle81 <briandoyle81@gmail.com>
2019-09-19 12:03:13 -0600

    fix parementers

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -32,17 +32,17 @@ class BinarySearchTree:
 
     # Print all the values in order from low to high
     # Hint:  Use a recursive, depth first traversal
-    def in_order_print():
+    def in_order_print(self):
         pass
 
     # Print the value of every node, starting with the given node,
     # in an iterative breadth first traversal
-    def bft_print(node):
+    def bft_print(self, node):
         pass
 
     # Print the value of every node, starting with the given node,
     # in an iterative depth first traversal
-    def dft_print(node):
+    def dft_print(self, node):
         pass
 
     # STRETCH Goals -------------------------

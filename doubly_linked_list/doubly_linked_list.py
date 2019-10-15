7be8b2fe9eeb4ec40e6a8082cbcef0802295dbd8
Mitchell Robles <mitchellr92@live.com>
2019-10-15 13:31:55 -0700

    filling out doubly linked list class methods

--- doubly_linked_list/doubly_linked_list.py
+++ doubly_linked_list/doubly_linked_list.py
@@ -52,7 +52,17 @@ class DoublyLinkedList:
     as the new head of the list. Don't forget to handle 
     the old head node's previous pointer accordingly."""
     def add_to_head(self, value):
-        pass
+        new_node = ListNode(value, None, None)
+
+        if not self.head and not self.tail:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.next = self.head
+            self.head.prev = new_node
+            self.head = new_node
+
+        self.length += 1
 
     """Removes the List's current head node, making the
     current head's next node the new head of the List.

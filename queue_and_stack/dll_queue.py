e6fa55e7e8d08bdbd945c40027bdc4b03d987276
pedrolmr <pedroluismontesinosrivera@gmail.com>
2019-10-15 16:21:50 -0400

    test_dequeue_respects_order not passing

--- queue_and_stack/dll_queue.py
+++ queue_and_stack/dll_queue.py
@@ -3,35 +3,23 @@ sys.path.append('../doubly_linked_list')
 from doubly_linked_list import DoublyLinkedList
 
 #FIFO
-'''
-1. Should have the methods: enqueue, dequeue, and len.
-2. enqueue should add an item to the back of the queue.
-3. dequeue should remove and return an item from the front of the queue.
-4. len returns the number of items in the queue.
-'''
+
 class Queue:
     def __init__(self):
         self.size = 0
         # Why is our DLL a good choice to store our elements?
-        # self.storage = ?
         self.storage = DoublyLinkedList()
 
     def enqueue(self, value):
-        self.storage.add_to_tail(value)
+        self.storage.add_to_head(value)
         self.size += 1
 
     def dequeue(self):
-        self.storage.remove_from_tail()
-        self.size -= 1
-        return self
+        if self.size > 0:
+            self.size -= 1
+            return self.storage.remove_from_tail()
+        else:
+            return None
 
     def len(self):
         return self.size
-
-
-queue = Queue()
-queue.enqueue("hi")
-queue.enqueue("hello")
-queue.enqueue("yo")
-queue.dequeue()
-print(f'length: {queue.len()}')

bcef4c2a73e1486c0073f015be0258427ab8b509
pedrolmr <pedroluismontesinosrivera@gmail.com>
2019-10-15 03:59:23 -0400

    delete, remove_from_head, and remove_from_tail are working

--- doubly_linked_list/doubly_linked_list.py
+++ doubly_linked_list/doubly_linked_list.py
@@ -74,7 +74,9 @@ class DoublyLinkedList:
     Returns the value of the removed Node."""
 
     def remove_from_head(self):
-        pass
+      value = self.head.value
+      self.delete(self.head)
+      return value
 
     """Wraps the given value in a ListNode and inserts it 
     as the new tail of the list. Don't forget to handle 
@@ -95,7 +97,9 @@ class DoublyLinkedList:
     current tail's previous node the new tail of the List.
     Returns the value of the removed Node."""
     def remove_from_tail(self):
-        pass
+      value = self.head.value
+      self.delete(self.head)
+      return value
 
     """Removes the input node from its current spot in the 
     List and inserts it as the new head node of the List."""
@@ -110,7 +114,19 @@ class DoublyLinkedList:
     """Removes a node from the list and handles cases where
     the node was the head or the tail"""
     def delete(self, node):
-        pass
+      self.length -= 1
+
+      if self.head is self.tail:
+        self.head = None
+        self.tail = None
+      elif node is self.head:
+        self.head = node.next
+        node.delete()
+      elif node is self.tail:
+        self.tail = node.prev
+        node.delete()
+      else:
+        node.delete()
         
     """Returns the highest value currently in the list"""
     def get_max(self):

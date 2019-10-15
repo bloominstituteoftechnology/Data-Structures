4b2b6aeaa5e2426b94f1ecbf64a5e553f6748ada
pedrolmr <pedroluismontesinosrivera@gmail.com>
2019-10-15 16:37:17 -0400

    queue_and_stack tests are passing

--- queue_and_stack/dll_stack.py
+++ queue_and_stack/dll_stack.py
@@ -11,19 +11,15 @@ class Stack:
         self.storage = DoublyLinkedList()
 
     def push(self, value):
-        self.storage.add_to_head(value)
+        self.storage.add_to_tail(value)
         self.size += 1
 
     def pop(self):
-        self.storage.remove_from_head()
-        self.size -= 1
+        if self.size > 0:
+            self.size -= 1
+            return self.storage.remove_from_tail()
+        else:
+            return None
 
     def len(self):
         return self.size
-
-stack = Stack()
-stack.push(34)
-stack.push(45)
-stack.push(14)
-stack.pop()
-print(f'length: {stack.len()}')

9940c5635a64021484730758710e17cf8bf8bfbe
Tom Higgins <higgins2718@outlook.com>
2019-10-18 13:48:26 -0500

    completed binary search assignment

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -10,40 +10,67 @@ class BinarySearchTree:
         self.left = None
         self.right = None
 
-    # Insert the given value into the tree
+    # Insert value into tree
     def insert(self, value):
-        pass
+        if value < self.value:
+            # go left
+            if not self.left:
+                self.left = BinarySearchTree(value)
+            else:
+                self.left.insert(value)
+        else:
+            # go right
+            if not self.right:
+                self.right = BinarySearchTree(value)
+            else:
+                self.right.insert(value)
 
-    # Return True if the tree contains the value
-    # False if it does not
+    # Return True if tree contains value, else False
     def contains(self, target):
-        pass
+        if self.value == target:
+            return True
+        if target < self.value:
+            # go left
+            if not self.left:
+                return False
+            else:
+                return self.left.contains(target)
+        else: # Target is >= self.value
+            # go right
+            if not self.right:
+                return False
+            else:
+                return self.right.contains(target)
 
-    # Return the maximum value found in the tree
+
     def get_max(self):
-        pass
+        if not self.right:
+            return self.value
+        return self.right.get_max()
 
-    # Call the function `cb` on the value of each node
-    # You may use a recursive or iterative approach
+    # call this function on the value of each node
+    # recursive or iterative approach okay
     def for_each(self, cb):
-        pass
+        cb(self.value)
 
-    # DAY 2 Project -----------------------
-
-    # Print all the values in order from low to high
-    # Hint:  Use a recursive, depth first traversal
-    def in_order_print(self, node):
-        pass
-
-    # Print the value of every node, starting with the given node,
-    # in an iterative breadth first traversal
-    def bft_print(self, node):
-        pass
+        if self.left:
+            self.left.for_each(cb)
+        if self.right:
+            self.right.for_each(cb)
 
     # Print the value of every node, starting with the given node,
     # in an iterative depth first traversal
     def dft_print(self, node):
-        pass
+        storage = Stack()
+        current = self
+        storage.push(current)
+        while storage.len() > 0:
+            current = storage.pop()  # call the pop fn
+            print(current.value)
+            if current.left:
+                storage.push(current.left)
+            if current.right:
+                storage.push(current.right)
 
     # STRETCH Goals -------------------------
     # Note: Research may be required
--- lru_cache/lru_cache.py
+++ lru_cache/lru_cache.py
@@ -1,3 +1,7 @@
+import sys
+sys.path.append('../doubly_linked_list')
+from doubly_linked_list import DoublyLinkedList
+
 class LRUCache:
     """
     Our LRUCache class keeps track of the max number of nodes it
@@ -7,7 +11,10 @@ class LRUCache:
     to every node stored in the cache.
     """
     def __init__(self, limit=10):
-        pass
+        self.order = DoublyLinkedList()
+        self.storage = dict()
+        self.size = 0
+        self.limit = limit
 
     """
     Retrieves the value associated with the given key. Also
@@ -17,7 +24,12 @@ class LRUCache:
     key-value pair doesn't exist in the cache.
     """
     def get(self, key):
-        pass
+        if key in self.storage:
+            node = self.storage[key]
+            self.order.move_to_front(node)
+            return node.value[1]
+        else:
+            return None
 
     """
     Adds the given key-value pair to the cache. The newly-
@@ -30,4 +42,16 @@ class LRUCache:
     the newly-specified value.
     """
     def set(self, key, value):
-        pass
+        if key in self.storage:
+            node = self.storage[key]
+            node.value = (key, value)
+            self.order.move_to_front(node)
+            return
+        if self.size == self.limit:
+            del self.storage[self.order.tail.value[0]]
+            self.order.remove_from_tail()
+            self.size += 1
+
+        self.order.add_to_head((key, value))
+        self.storage[key] = self.order.head
+        self.size += 1

0480e5d67bda7a5b276da80c80b81c274a4b264d
mmastin <matthew.mastin@gmail.com>
2019-11-11 17:39:39 -0500

    day 1 work, queue and stack

--- queue_and_stack/dll_queue.py
+++ queue_and_stack/dll_queue.py
@@ -7,13 +7,24 @@ class Queue:
     def __init__(self):
         self.size = 0
         # Why is our DLL a good choice to store our elements?
-        # self.storage = ?
+        # It's set up for ease of use to push or pop in FIFO and LIFO
+
+        self.storage = DoublyLinkedList()
+        # self.value = value
 
     def enqueue(self, value):
-        pass
+        self.value = value
+        self.storage.add_to_head(value)
+        self.size += 1
 
     def dequeue(self):
-        pass
+        # self.value = value
+        if self.size > 0:
+            self.size -= 1
+            return self.storage.remove_from_tail()
+        else:
+            return None
 
     def len(self):
-        pass
+        # self.storage.length
+        return self.size

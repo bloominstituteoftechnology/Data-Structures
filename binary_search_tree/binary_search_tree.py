1f7f88f6781460f99884882736ec52257d7a0b79
Rice_from_data <nedderlander@gmail.com>
2019-11-13 11:34:57 -0800

    added bst insert and contains

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -12,20 +12,49 @@ class BinarySearchTree:
 
     # Insert the given value into the tree
     def insert(self, value):
-        pass
+        
+        # base case, tree of 1 value. If value is > insert right, else left
+        if value > self.value:
+
+            if not self.right:
+                self.right = BinarySearchTree(value)
+            else:
+                self.right.insert(value)
+
+        else:
+
+            if not self.left:
+                self.left = BinarySearchTree(value)
+            else:
+                self.left.insert(value)
+
 
     # Return True if the tree contains the value
     # False if it does not
     def contains(self, target):
-        pass
+        # compare target to root, if it's in the root return, else < go left > go right
+        # print(self.value, target)
+        if self.value == target:
+            # print('returning true')
+            return True
+        elif self.value < target and self.right:
+            return self.right.contains(target)
+        elif self.value > target and self.left:
+            return self.left.contains(target)
+        else:
+            return False
+            
 
     # Return the maximum value found in the tree
     def get_max(self):
+        #go right all the way
         pass
 
     # Call the function `cb` on the value of each node
     # You may use a recursive or iterative approach
     def for_each(self, cb):
+        #visit each node one time
+        # so go all the way left (or right) and store the rights then apply all
         pass
 
     # DAY 2 Project -----------------------

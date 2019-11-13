59ebc7b9374fe983c151d7e6d93044f0339de32e
Rice_from_data <nedderlander@gmail.com>
2019-11-13 13:46:59 -0800

    new testing file

---
+++ lru_cache/test_clean_lru_cache.py
@@ -0,0 +1,34 @@
+import unittest
+from lru_cache_clean import LRUCache
+
+
+class CacheTests(unittest.TestCase):
+    def setUp(self):
+        self.cache = LRUCache(3)
+
+    def test_cache_overwrite_appropriately(self):
+        self.cache.set('item1', 'a')
+        self.cache.set('item2', 'b')
+        self.cache.set('item3', 'c')
+
+        self.cache.set('item2', 'z')
+
+        self.assertEqual(self.cache.get('item1'), 'a')
+        self.assertEqual(self.cache.get('item2'), 'z')
+
+    def test_cache_insertion_and_retrieval(self):
+        self.cache.set('item1', 'a')
+        self.cache.set('item2', 'b')
+        self.cache.set('item3', 'c')
+
+        self.assertEqual(self.cache.get('item1'), 'a')
+        self.cache.set('item4', 'd')
+
+        self.assertEqual(self.cache.get('item1'), 'a')
+        self.assertEqual(self.cache.get('item3'), 'c')
+        self.assertEqual(self.cache.get('item4'), 'd')
+        self.assertIsNone(self.cache.get('item2'))
+
+
+if __name__ == '__main__':
+    unittest.main()

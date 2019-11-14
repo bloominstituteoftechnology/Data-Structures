0429a9be9de93113e97cf10d0f09c0b3ffef2f2c
Brian Doyle <briandoyle81@gmail.com>
2019-11-14 17:59:50 -0600

    Update test for traversals to handle right before left

--- binary_search_tree/test_binary_search_tree.py
+++ binary_search_tree/test_binary_search_tree.py
@@ -81,12 +81,14 @@ class BinarySearchTreeTests(unittest.TestCase):
         sys.stdout = io.StringIO()
         self.bst.bft_print(self.bst)
         output = sys.stdout.getvalue()
-        self.assertEqual(output, "1\n8\n5\n3\n7\n2\n4\n6\n")
+        self.assertTrue(output == "1\n8\n5\n3\n7\n2\n4\n6\n" or
+                        output == "1\n8\n5\n7\n3\n6\n4\n2\n")
 
         sys.stdout = io.StringIO()
         self.bst.dft_print(self.bst)
         output = sys.stdout.getvalue()
-        self.assertEqual(output, "1\n8\n5\n7\n6\n3\n4\n2\n")
+        self.assertTrue(output == "1\n8\n5\n7\n6\n3\n4\n2\n" or
+                        output == "1\n8\n5\n3\n2\n4\n7\n6\n")
 
         sys.stdout = io.StringIO()
         self.bst.pre_order_dft(self.bst)

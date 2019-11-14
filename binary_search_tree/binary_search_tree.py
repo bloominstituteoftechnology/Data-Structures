2a36f2acbf2002e645b848b94247ee9094df1514
Rice_from_data <nedderlander@gmail.com>
2019-11-14 13:03:49 -0800

    bft, dft done

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -45,33 +45,38 @@ class BinarySearchTree:
 
     # Return the maximum value found in the tree
     def get_max(self):
-        #go right all the way
-        current = self
-        while current.right:
-            current = current.right
-        return current.value
+        #go right all the way - iterative
+        # current = self
+        # while current.right:
+        #     current = current.right
+        # return current.value
+
+        # recursive
+        if not self.right:
+            return self.value
+        return self.right.get_max()
 
     # Call the function `cb` on the value of each node
     # You may use a recursive or iterative approach
     def for_each(self, cb):
         #visit each node one time
         # iterative solution
-        # stack = Stack()
-        # stack.push(self)
-        # while stack.len():
-        #     current_node = stack.pop()
-        #     if current_node.right:
-        #         stack.push(current_node.right)
-        #     if current_node.left:
-        #         stack.push(current_node.left)
-        #     cb(current_node.value)
+        stack = Stack()
+        stack.push(self)
+        while stack.len():
+            current_node = stack.pop()
+            if current_node.right:
+                stack.push(current_node.right)
+            if current_node.left:
+                stack.push(current_node.left)
+            cb(current_node.value)
 
         # recursive solution
-        if self.right:
-            self.right.for_each(cb)
-        if self.left:
-            self.left.for_each(cb)
-        return cb(self.value)
+        # if self.right:
+        #     self.right.for_each(cb)
+        # if self.left:
+        #     self.left.for_each(cb)
+        # return cb(self.value)
 
 
 
@@ -82,17 +87,87 @@ class BinarySearchTree:
     # Print all the values in order from low to high
     # Hint:  Use a recursive, depth first traversal
     def in_order_print(self, node):
-        pass
+               
+        # # iterative solution
+        # stack = Stack()
+        # current = self
+        
+        # # traverse all the way to the left
+        # while current.left:
+        #     if current.left:
+        #         stack.push(current)
+        #     current = current.left
+        
+        # while stack.len() > 0:
+        #     smallest = stack.pop()
+        #     if smallest.right:
+        #         stack.push(smallest.right)
+        #     print(smallest.value)
+            
+        # recursive solution
+        # call fnc on root
+        # if left call left
+        # if right call right
+        # # return print
+        # if self.left:
+        #     self.in_order_print(self.left)
+        # if self.right:
+        #     self.in_order_print(self.right)
+        # return print(self.value)
+        current = self
+        if current.left:
+            current.left.in_order_print(current.left)
+        if current.right:
+            current.right.in_order_print(current.right)
+        print(current.value)        
+
+
+        # pass
 
     # Print the value of every node, starting with the given node,
     # in an iterative breadth first traversal
     def bft_print(self, node):
-        pass
+
+        queue = Queue()
+        queue.enqueue(self)        
+        
+        while queue.len() > 0:
+            current = queue.dequeue()
+            print(current.value)
+            if current.left:
+                queue.enqueue(current.left)
+            if current.right:
+                queue.enqueue(current.right)
+            
+            
+
+
+
 
     # Print the value of every node, starting with the given node,
     # in an iterative depth first traversal
     def dft_print(self, node):
-        pass
+        # make stack
+        # call func on root
+        # put the root in the stack
+        # pop the top item in the stack
+        # look right
+        # push the right to the stack
+        # look left
+        # if there is a left push to stack
+        # pop the top of the stack
+
+        stack = Stack()
+        stack.push(self)
+
+        while stack.len() > 0:
+            current = stack.pop()
+            print(current.value)
+            if current.left:
+                stack.push(current.left)
+            if current.right:
+                stack.push(current.right)
+            
 
     # STRETCH Goals -------------------------
     # Note: Research may be required
--- lru_cache/lru_cache.py
+++ lru_cache/lru_cache.py
@@ -1,3 +1,5 @@
+import sys
+sys.path.append('../doubly_linked_list')
 from doubly_linked_list import DoublyLinkedList
 
 class LRUCache:

83430ee35bcffa9f7c50ff4663753c408a712c49
ChanceDurr <chancedurr@gmail.com>
2019-12-19 12:03:01 -0800

    Finished BST day 2

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -1,7 +1,7 @@
-# import sys
-# sys.path.append('./queue_and_stack')
-# from dll_queue import Queue
-# from dll_stack import Stack
+import sys
+sys.path.append('../queue_and_stack')
+from dll_queue import Queue
+from dll_stack import Stack
 
 
 class BinarySearchTree:
@@ -42,19 +42,20 @@ class BinarySearchTree:
 
     # Return the maximum value found in the tree
     def get_max(self):
-        if self.right is None:
-            return self.value
-        else:
+        if self.right:
             return self.right.get_max()
+        else:
+            return self.value
+
 
     # Call the function `cb` on the value of each node
     # You may use a recursive or iterative approach
     def for_each(self, cb):
         cb(self.value)
 
-        if self.left is not None:
+        if self.left:
             self.left.for_each(cb)
-        if self.right is not None:
+        if self.right:
             self.right.for_each(cb)
 
 
@@ -63,25 +64,62 @@ class BinarySearchTree:
     # Print all the values in order from low to high
     # Hint:  Use a recursive, depth first traversal
     def in_order_print(self, node):
-        pass
+        if node.left:
+            self.in_order_print(node.left)
+
+        if node.right:
+            print(node.value)
+            self.in_order_print(node.right)
+        else:
+            print(node.value)
 
     # Print the value of every node, starting with the given node,
     # in an iterative breadth first traversal
     def bft_print(self, node):
-        pass
+        q = Queue()
+        q.enqueue(node)
+
+        while q.len() > 0:
+            current_node = q.dequeue().value
+            print(current_node.value)
+
+            if current_node.left:
+                q.enqueue(current_node.left)
+            if current_node.right:
+                q.enqueue(current_node.right)
 
     # Print the value of every node, starting with the given node,
     # in an iterative depth first traversal
     def dft_print(self, node):
-        pass
+        stack = Stack()
+        stack.push(node)
+        while stack.len() > 0:
+            current_node = stack.pop().value
+            print(current_node.value)
+
+            if current_node.left:
+                stack.push(current_node.left)
+            if current_node.right:
+                stack.push(current_node.right)
+        
 
     # STRETCH Goals -------------------------
     # Note: Research may be required
 
     # Print In-order recursive DFT
     def pre_order_dft(self, node):
-        pass
+        print(node.value)
+        if node.left:
+            self.pre_order_dft(node.left)
+        if node.right:
+            self.pre_order_dft(node.right)
 
     # Print Post-order recursive DFT
     def post_order_dft(self, node):
-        pass
+        if node.left:
+            self.post_order_dft(node.left)
+        
+        if node.right:
+            self.post_order_dft(node.right)
+        
+        print(node.value)
--- doubly_linked_list/doubly_linked_list.py
+++ doubly_linked_list/doubly_linked_list.py
@@ -1,13 +1,13 @@
 """Each ListNode holds a reference to its previous node
 as well as its next node in the List."""
-​
-​
+
+
 class ListNode:
     def __init__(self, value, prev=None, next=None):
         self.value = value
         self.prev = prev
         self.next = next
-​
+
     """Wrap the given value in a ListNode and insert it
     after this node. Note that this node could already
     have a next node it is point to."""
@@ -16,7 +16,7 @@ class ListNode:
         self.next = ListNode(value, self, current_next)
         if current_next:
             current_next.prev = self.next
-​
+
     """Wrap the given value in a ListNode and insert it
     before this node. Note that this node could already
     have a previous node it is point to."""
@@ -25,7 +25,7 @@ class ListNode:
         self.prev = ListNode(value, current_prev, self)
         if current_prev:
             current_prev.next = self.prev
-​
+
     """Rearranges this ListNode's previous and next pointers
     accordingly, effectively deleting this ListNode."""
     def delete(self):
@@ -33,21 +33,21 @@ class ListNode:
             self.prev.next = self.next
         if self.next:
             self.next.prev = self.prev
-​
-​
+
+
 """Our doubly-linked list class. It holds references to
 the list's head and tail nodes."""
-​
-​
+
+
 class DoublyLinkedList:
     def __init__(self, node=None):
         self.head = node
         self.tail = node
         self.length = 1 if node is not None else 0
-​
+
     def __len__(self):
         return self.length
-​
+
     """Wraps the given value in a ListNode and inserts it 
     as the new head of the list. Don't forget to handle 
     the old head node's previous pointer accordingly."""
@@ -62,7 +62,7 @@ class DoublyLinkedList:
             new_node.next = self.head
             self.head.prev = new_node
             self.head = new_node
-​
+
     """Removes the List's current head node, making the
     current head's next node the new head of the List.
     Returns the value of the removed Node."""
@@ -70,7 +70,7 @@ class DoublyLinkedList:
         value = self.head.value
         self.delete(self.head)
         return value
-​
+
     """Wraps the given value in a ListNode and inserts it 
     as the new tail of the list. Don't forget to handle 
     the old tail node's next pointer accordingly."""
@@ -85,7 +85,7 @@ class DoublyLinkedList:
             new_node.prev = self.tail
             self.tail.next = new_node
             self.tail = new_node
-​
+
     """Removes the List's current tail node, making the 
     current tail's previous node the new tail of the List.
     Returns the value of the removed Node."""
@@ -93,7 +93,7 @@ class DoublyLinkedList:
         value = self.tail.value
         self.delete(self.tail)
         return value
-​
+
     """Removes the input node from its current spot in the 
     List and inserts it as the new head node of the List."""
     def move_to_front(self, node):
@@ -102,8 +102,8 @@ class DoublyLinkedList:
         value = node.value
         self.delete(node)
         self.add_to_head(value)
-​
-​
+
+
     """Removes the input node from its current spot in the 
     List and inserts it as the new tail node of the List."""
     def move_to_end(self, node):
@@ -112,8 +112,8 @@ class DoublyLinkedList:
         value = node.value
         self.delete(node)
         self.add_to_tail(value)
-​
-​
+
+
     """Removes a node from the list and handles cases where
     the node was the head or the tail"""
     def delete(self, node):
@@ -132,8 +132,8 @@ class DoublyLinkedList:
             node.delete()
         else:
             node.delete()
-​
-​
+
+
     """Returns the highest value currently in the list"""
     def get_max(self):
         current = self.head
@@ -142,5 +142,5 @@ class DoublyLinkedList:
             if current.value > max_value:
                 max_value = current.value
             current = current.next
-​
+
         return max_value

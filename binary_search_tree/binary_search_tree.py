dab28541bf98bda6abe57f9633a218f67e1a299e
Adetunji <Ashennaike@gmail.com>
2019-12-18 20:55:07 -0500

    almost mvp

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -14,12 +14,12 @@ class BinarySearchTree:
     def insert(self, value):
       # Create a new root using the given value and check against the current tree
       new_leaf = BinarySearchTree(value)
-      # if the value is >= the current spot move to the right and add it to the right if the field is empty
+      # If the value is >= the current spot move to the right and add it to the right if the field is empty
       if value >= self.value and self.right is None:
         self.right = new_leaf
       elif value >= self.value:
         self.right.insert(value)
-      # if the value is < the current spot move to the left and add it to the left if the field is empty
+      # If the value is < the current spot move to the left and add it to the left if the field is empty
       elif value < self.value and self.left is None:
         self.left = new_leaf
       elif value < self.value:
@@ -28,16 +28,30 @@ class BinarySearchTree:
     # Return True if the tree contains the value
     # False if it does not
     def contains(self, target):
-        pass
+      # Check if the current value is equal to the target
+      if self.value == target:
+        return True
+      # Check if the left is equal to the target
+      if self.left != target:
+        return 
+      # Check if the right is equal to target
 
     # Return the maximum value found in the tree
     def get_max(self):
-        pass
+      if self.right is not None:
+        self.right.get_max()
+      else:
+        return self.right 
 
     # Call the function `cb` on the value of each node
     # You may use a recursive or iterative approach
     def for_each(self, cb):
-        pass
+      # Go to each value and use the callback function 
+      cb(self.value)
+      if self.right is not None:
+        self.right.for_each(cb)
+      if self.left is not None:
+        self.left.for_each(cb)
 
     # DAY 2 Project -----------------------
 

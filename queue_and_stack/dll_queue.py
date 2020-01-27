a3ec97c869d99ae315fbef4aa0499bb2b550e629
Jeffrey Whitaker <jeffrey.david.whitaker@zoho.com>
2020-01-27 12:19:12 -0800

    move dll to folder so import works

--- queue_and_stack/dll_queue.py
+++ queue_and_stack/dll_queue.py
@@ -1,5 +1,3 @@
-import sys
-sys.path.append('../doubly_linked_list')
 from doubly_linked_list import DoublyLinkedList
 
 
@@ -8,6 +6,7 @@ class Queue:
         self.size = 0
         # Why is our DLL a good choice to store our elements?
         # self.storage = ?
+        self.storage = None
 
     def enqueue(self, value):
         pass
@@ -16,4 +15,4 @@ class Queue:
         pass
 
     def len(self):
-        pass
+        return
--- queue_and_stack/dll_stack.py
+++ queue_and_stack/dll_stack.py
@@ -1,6 +1,5 @@
-import sys
-sys.path.append('../doubly_linked_list')
 from doubly_linked_list import DoublyLinkedList
+
 
 class Stack:
     def __init__(self):

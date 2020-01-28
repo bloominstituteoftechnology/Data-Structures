fc962c067d4a1f266f2c486aaf067d4f97f3d240
Sean Doyle <sdoylelambda@gmail.com>
2020-01-28 15:33:10 -0500

    stack and queue all tests pass.

--- queue_and_stack/dll_queue.py
+++ queue_and_stack/dll_queue.py
@@ -2,21 +2,25 @@ import sys
 sys.path.append('../doubly_linked_list')
 from doubly_linked_list import DoublyLinkedList
 
-
+# FIFO -- FIRST IN FIRST OUT -- WAITING IN LINE AT 711, DMV, ETC
 class Queue:
     def __init__(self):
         self.size = 0
         # Why is our DLL a good choice to store our elements?
-        self.storage = {}
+        self.storage = DoublyLinkedList()
 
+    # Add item to back of queue
     def enqueue(self, value):
-        DoublyLinkedList.add_to_tail(value)
+        self.size += 1
+        return self.storage.add_to_tail(value)
 
+    # Remove and return item from front of queue
     def dequeue(self):
-        if self.head:
-            self.head = self.head.next()
-            DoublyLinkedList.delete(self)
+        if self.size is not 0:
+            self.size -= 1
+            return self.storage.remove_from_head()
         else:
-            DoublyLinkedList.delete(self)
+            return None
+    # Number of items in queue
     def len(self):
-        return self.length
+        return self.size

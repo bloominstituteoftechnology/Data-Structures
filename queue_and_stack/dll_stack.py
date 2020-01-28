e763736b916ae53d010c222dc45bc07dbb0da75e
Christopher Pupo <ChrisPupo@Christophers-MacBook-Pro.local>
2020-01-27 19:27:08 -0500

    its working

--- queue_and_stack/dll_stack.py
+++ queue_and_stack/dll_stack.py
@@ -6,10 +6,10 @@ class Stack:
     def __init__(self):
         self.size = 0
         # Why is our DLL a good choice to store our elements?
-        # self.storage = ?
+        self.storage = DoublyLinkedList
 
     def push(self, value):
-        pass
+        self.storage.add_to_head(value)
 
     def pop(self):
         pass

5c8653e8b05fe6d0b9c1d3a7bf1bf4eceaa74aa2
Mildred Awuor <otieno.awuor.m@gmail.com>
2020-02-24 20:17:07 +0300

    attempted remove head and add to tail

--- doubly_linked_list/doubly_linked_list.py
+++ doubly_linked_list/doubly_linked_list.py
@@ -51,30 +51,40 @@ class DoublyLinkedList:
     """Wraps the given value in a ListNode and inserts it 
     as the new head of the list. Don't forget to handle 
     the old head node's previous pointer accordingly."""
-    def add_to_head(self, value):
+    def add_to_head(self, value):  #  5 -> 7 -> 13 -> N  This is equivalent to push method ?
         #pass in the value to be added
         new_node = ListNode(value,None,None)
         self.length += 1
         # if the list is empty
         if not self.head and self.tail:
-            self.head = new_node
+            self.head = new_node # The new_node becomes head, also tail because there's only one element in the list
             self.tail = new_node
         else:
-            new_node.next = self.head
-            self.head.prev = new_node
-            self.head = new_node
+            new_node.next = self.head  #otherwise,point the next ref to the current head
+            self.head.prev = new_node  #point the prev ref to the new_node
+            self.head = new_node       # assign the value of the head to the value of new_node
 
     """Removes the List's current head node, making the
     current head's next node the new head of the List.
     Returns the value of the removed Node."""
-    def remove_from_head(self):
-        pass
+    def remove_from_head(self): # This is equivalent to pop method ?
+        value = self.head.value # get the current value of head
+        self.delete(self.head) # delete the current head
+        return value            # return value of head
 
     """Wraps the given value in a ListNode and inserts it 
     as the new tail of the list. Don't forget to handle 
     the old tail node's next pointer accordingly."""
-    def add_to_tail(self, value):
-        pass
+    def add_to_tail(self, value): # This is equivalent to append method ? opposite of add_to_head()
+        new_node = ListNode(value,None,None)
+        self.length += 1
+        if not self.head and not self.tail:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.prev = self.tail 
+            self.tail.next = new_node
+            self.tail = new_node
 
     """Removes the List's current tail node, making the 
     current tail's previous node the new tail of the List.

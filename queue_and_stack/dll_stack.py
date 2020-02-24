25967da37f1b8c11ea7b66634ad5ae1a8b438731
tjclawson <tjamesclawson@gmail.com>
2020-02-24 16:28:04 -0500

    Revert "mend"
    This reverts commit 4bf987a2cda8a64a7c8488c6d3a2fb7302c6edfb.

--- queue_and_stack/dll_stack.py
+++ queue_and_stack/dll_stack.py
@@ -1,23 +1,18 @@
 import sys
-
 sys.path.append('../doubly_linked_list')
 from doubly_linked_list import DoublyLinkedList
-
 
 class Stack:
     def __init__(self):
         self.size = 0
         # Why is our DLL a good choice to store our elements?
-        self.storage = DoublyLinkedList()
+        # self.storage = ?
 
     def push(self, value):
-        self.storage.add_to_head(value)
-        self.size += 1
+        pass
 
     def pop(self):
-        if self.size != 0:
-            self.size -= 1
-            return self.storage.remove_from_head()
+        pass
 
     def len(self):
-        return self.size
+        pass

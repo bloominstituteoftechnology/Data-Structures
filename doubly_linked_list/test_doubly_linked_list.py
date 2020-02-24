2f4c4c8a03092e0529cef412475648a57c94b40b
Akak Almazbekova <akak.almazbekova@gmail.com>
2020-02-24 14:46:18 -0800

    add doubly_linked

--- doubly_linked_list/test_doubly_linked_list.py
+++ doubly_linked_list/test_doubly_linked_list.py
@@ -1,6 +1,6 @@
 import unittest
-from doubly_linked_list import ListNode
-from doubly_linked_list import DoublyLinkedList
+ from doubly_linked_list import ListNode
+ from doubly_linked_list import DoublyLinkedList
 
 
 class DoublyLinkedListTests(unittest.TestCase):

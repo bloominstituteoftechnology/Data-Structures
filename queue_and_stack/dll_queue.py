a00a995355428fea91e4386d86f284df263275c5
LilySu <LilySu@users.noreply.github.com>
2020-02-24 18:39:03 -0500

    Update Monday's assignment stack and queue

--- queue_and_stack/dll_queue.py
+++ queue_and_stack/dll_queue.py
@@ -4,17 +4,25 @@ from doubly_linked_list import DoublyLinkedList
 
 # Not allowed to use lists
 
-class Queue:
+class Queue: # first in first out
     def __init__(self):
         self.size = 0
         # Why is our DLL a good choice to store our elements?
-        # self.storage = ?
+        self.storage = DoublyLinkedList()
 
-    def enqueue(self, value):
-        pass
+    def is_empty(self):
+        return self.size == 0
+
+    def enqueue(self, value): #
+        self.size += 1
+        return self.storage.add_to_head(value)
 
     def dequeue(self):
-        pass
+        if self.is_empty():
+            return
+        else:
+            self.size -= 1
+            return self.storage.remove_from_tail()
 
     def len(self):
-        pass
+        return self.size
--- queue_and_stack/dll_stack.py
+++ queue_and_stack/dll_stack.py
@@ -5,17 +5,22 @@ from doubly_linked_list import DoublyLinkedList
 
 # Not allowed to use lists
 
-class Stack:
+class Stack:# In a stack, a new element is added at one end and an element is removed from that end only. 
     def __init__(self):
         self.size = 0
         # Why is our DLL a good choice to store our elements?
-        # self.storage = ?
+        self.storage = DoublyLinkedList()
 
     def push(self, value):
-        pass
+        self.size += 1
+        self.storage.add_to_tail(value)
 
     def pop(self):
-        pass
+        if self.size > 0:
+            self.size -= 1
+            return self.storage.remove_from_tail()
+        else:
+            return None
 
     def len(self):
-        pass
+        return self.size

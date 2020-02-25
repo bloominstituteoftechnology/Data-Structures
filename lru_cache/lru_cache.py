931ed100e82dd322cb1e44eda5edd145eacf7053
LilySu <LilySu@users.noreply.github.com>
2020-02-24 19:50:44 -0500

    Update assignment other weekdays

--- lru_cache/lru_cache.py
+++ lru_cache/lru_cache.py
@@ -1,4 +1,6 @@
-
+import sys
+sys.path.append('../doubly_linked_list')
+from doubly_linked_list import DoublyLinkedList
 # benefits of linked list and hash table to create an index, if you want to grab somethign from the middle you can. 
 
 # since python works as pass by reference
@@ -13,7 +15,10 @@ class LRUCache:
     to every node stored in the cache.
     """
     def __init__(self, limit=10):
-        pass
+        self.limit = limit
+        self.size = 0 
+        self.storage = dict()
+        self.order = DoublyLinkedList()
 
     """
     Retrieves the value associated with the given key. Also
@@ -23,7 +28,12 @@ class LRUCache:
     key-value pair doesn't exist in the cache.
     """
     def get(self, key):
-        pass
+        if key in self.storage:
+            node = self.storage[key]
+            self.order.move_to_end(node)
+            return node.value[1]
+        else:
+            return
 
     """
     Adds the given key-value pair to the cache. The newly-
@@ -36,4 +46,17 @@ class LRUCache:
     the newly-specified value.
     """
     def set(self, key, value):
-        pass
+        if key in self.storage:
+            node = self.storage[key]
+            node.value = (key,value)
+            self.order.move_to_end(node)
+            return
+        else:
+            if self.size == self.limit:
+                del self.storage[self.order.head.value[0]]
+                self.order.remove_from_head()
+                self.size -= 1
+        self.order.add_to_tail((key, value))
+        self.storage[key] = self.order.tail
+        self.size += 1
+

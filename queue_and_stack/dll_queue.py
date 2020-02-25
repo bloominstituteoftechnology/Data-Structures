ded59f1fedbc7b25e4d5f5e88011f5055f43f8b2
johanaluna <johanaluna@users.noreply.github.com>
2020-02-25 12:31:03 -0500

    more links and comments added

--- queue_and_stack/dll_queue.py
+++ queue_and_stack/dll_queue.py
@@ -5,21 +5,24 @@ from doubly_linked_list import DoublyLinkedList
 
 """
 https://he-s3.s3.amazonaws.com/media/uploads/cf1e1c1.png
-Enqueue: Insert the element from the rear
-Dequeue: Delete the element from the front 
+Enqueue: Insert the element from the tail
+Dequeue: Delete the element from the head 
 """
 
 class Queue:
     def __init__(self):
         self.size = 0
         # Why is our DLL a good choice to store our elements?
+        # because of easy traversal
+        # Because DLL allows to track the pointers(front and tail)
+        # Dynamic memory allocation. *Ease of insertion and deletion
         self.storage = DoublyLinkedList()
 
-    def enqueue(self, value):
+    def enqueue(self, value):#PUSH
         self.storage.add_to_tail(value)
         self.size += 1
 
-    def dequeue(self):
+    def dequeue(self): #POP
         if self.size > 0:
             self.size -= 1
             return self.storage.remove_from_head()

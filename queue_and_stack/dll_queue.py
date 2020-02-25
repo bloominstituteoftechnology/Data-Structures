dd8c6859e01c2deb8721d29dfe373fbdebf0d097
Hira Khan <sunshine9.r8@gmail.com>
2020-02-25 11:06:33 -0500

    updated queue

--- queue_and_stack/dll_queue.py
+++ queue_and_stack/dll_queue.py
@@ -3,25 +3,6 @@ sys.path.append('../doubly_linked_list')
 from doubly_linked_list import DoublyLinkedList
 
 
-#class Queue:
-#    def __init__(self):
-#        self.size = 0
-        # we start from zero
-        # self.storage = ?
-#        self.storage = DoublyLinkedList()
-
-#    def enqueue(self, value):
-#        self.size += 1
-#        return self.storage.add_to_head(value)
-#
-#    def dequeue(self):
-#        if self.size > 0:
-#            return self.storage.remove_from_tail()
-#        else:
-#            return None
-#    def len(self):
-#        return self.size
-
 class Queue: # first in first out
     def __init__(self):
         self.size = 0

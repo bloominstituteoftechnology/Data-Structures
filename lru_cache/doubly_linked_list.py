382470aa9ae65121ed8878a9a884a1115a32f698
Hira Khan <sunshine9.r8@gmail.com>
2020-02-26 11:25:43 -0500

    lru practice

--- lru_cache/doubly_linked_list.py
+++ lru_cache/doubly_linked_list.py
@@ -49,25 +49,86 @@ class DoublyLinkedList:
         return self.length
 
     def add_to_head(self, value):
+        """ add_to_head, define a new node. check"""
+        new_node = ListNode(value, None, None)
+        self.length += 1
+        if self.head is not None and self.tail is not None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.next = self.head
+            self.head.prev = new_node
+            self.head = new_node
         pass
 
     def remove_from_head(self):
-        pass
+        value = self.head.value # grab the value at the head
+        self.delete(self.head)
+        return value
 
     def add_to_tail(self, value):
-        pass
+        new_node = ListNode(value, None, None)
+        self.length += 1
+        if not self.head and not self.tail:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.prev = self.tail
+            self.tail.next = new_node
+            self.tail = new_node
 
     def remove_from_tail(self):
-        pass
+        value = self.tail.value
+        self.delete(self.tail)
+        return value
 
     def move_to_front(self, node):
-        pass
+
+        value = node.value
+        self.delete(node)
+        self.add_to_head(value)
 
     def move_to_end(self, node):
+        value = node.value
+        self.delete(node)
+        self.add_to_tail(value)
         pass
 
     def delete(self, node):
-        pass
+        # let's define the base cases
+        # obviously, delete is going to lower the length of the list
+        self.length -=1
+
+        # think of special cases
+        if not self.head and not self.tail:
+            return
+
+        # if head and tail
+        if self.head == self.tail:
+            self.head = None
+            self.tail = None
+
+        # if head
+        if self.head == node:
+            self.head = self.head.next      # change the pointer of the head
+            node.delete()
+
+        if self.tail == node:
+            self.tail = self.tail.next
+            node.delete()
+
+        else:
+            node.delete()
+
 
     def get_max(self):
-        pass
+        if not self.head:
+            return None
+
+        max_value = self.head.value
+        current = self.head.next
+        while current:
+            if current.value > max_value:
+                max_value = current.value
+            current = current.next
+        return max_value
--- lru_cache/lru_cache.py
+++ lru_cache/lru_cache.py
@@ -1,3 +1,7 @@
+import sys
+sys.path.append('../doubly_linked_list')
+from doubly_linked_list import DoublyLinkedList
+
 class LRUCache:
     """
     Our LRUCache class keeps track of the max number of nodes it
@@ -7,7 +11,10 @@ class LRUCache:
     to every node stored in the cache.
     """
     def __init__(self, limit=10):
-        pass
+        self.limit = limit
+        self.list = DoublyLinkedList()
+        self.storage = {}
+        self.length = 0
 
     """
     Retrieves the value associated with the given key. Also
@@ -16,8 +23,14 @@ class LRUCache:
     Returns the value associated with the key or None if the
     key-value pair doesn't exist in the cache.
     """
-    def get(self, key):
-        pass
+    def get(self, key):                         # passing in the key
+        if key in self.storage:                 # if it is in the storage, we move forward
+            node = self.storage[key]            # set the node to the self.storage[key]
+            self.list.move_to_end(node)         # move to the end using the DoublyLinkedList
+            return node.value[1]                # return the value of the node object
+
+        else:
+            return None
 
     """
     Adds the given key-value pair to the cache. The newly-
@@ -30,4 +43,17 @@ class LRUCache:
     the newly-specified value.
     """
     def set(self, key, value):
-        pass
+        if key in self.storage:                   # if key in storage
+            node = self.storage[key]              # set the node at the key
+            node.value = (key, value)             # the value of the node is equal to key and the value tuple
+            self.list.move_to_end(node)           # we move it to the end of the list bc it's most recently used
+            return
+
+        if self.length == self.limit:                   # however, if the list is full
+            del self.storage[self.list.head.value[0]]   # we delete the head because that is the least used item
+            self.list.remove_from_head()                # we perform the remove from head function
+            self.length -= 1
+
+        self.list.add_to_tail((key, value))             # this the main funciton of adding the value to tail
+        self.storage[key] = self.list.tail              # store the key from tail since we already added it
+        self.length += 1                                # increase the length by 1

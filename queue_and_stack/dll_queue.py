02b3fed1739d61dcaf55b1dd747013923871a5e4
MattGThomas <mgthomas0616@gmail.com>
2020-03-10 23:53:50 -0500

    finished queue and stack

--- queue_and_stack/dll_queue.py
+++ queue_and_stack/dll_queue.py
@@ -1,19 +1,32 @@
-import sys
-sys.path.append('../doubly_linked_list')
+
 from doubly_linked_list import DoublyLinkedList
+from doubly_linked_list import ListNode
 
 
 class Queue:
     def __init__(self):
         self.size = 0
         # Why is our DLL a good choice to store our elements?
-        # self.storage = ?
+        # self.storage = ?c
+        self.storage = DoublyLinkedList()
 
     def enqueue(self, value):
-        pass
+        self.size += 1
+        self.storage.add_to_tail(ListNode(value))
 
     def dequeue(self):
-        pass
+        if self.size > 0:
+            self.size -= 1
+            self.storage.remove_from_head()
 
     def len(self):
-        pass
+        return self.storage.length
+
+
+my_queue = Queue()
+print(my_queue)
+my_queue.enqueue(1)
+my_queue.enqueue(3)
+my_queue.enqueue(6)
+my_queue.enqueue(9)
+print(my_queue)
--- queue_and_stack/dll_stack.py
+++ queue_and_stack/dll_stack.py
@@ -1,18 +1,24 @@
-import sys
-sys.path.append('../doubly_linked_list')
 from doubly_linked_list import DoublyLinkedList
+from doubly_linked_list import ListNode
+# import sys
+# sys.path.append('../doubly_linked_list')
+
 
 class Stack:
     def __init__(self):
         self.size = 0
         # Why is our DLL a good choice to store our elements?
         # self.storage = ?
+        self.storage = DoublyLinkedList()
 
     def push(self, value):
-        pass
+        self.size += 1
+        self.storage.add_to_head(ListNode(value))
 
     def pop(self):
-        pass
+        if self.size > 0:
+            self.size -= 1
+            self.storage.remove_from_head()
 
     def len(self):
-        pass
+        return self.storage.length

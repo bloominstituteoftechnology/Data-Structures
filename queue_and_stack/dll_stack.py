b91a5cb07724b826ca7033f7df1910ee9acbbc0b
umekow <umekolashun@gmail.com>
2020-03-23 17:50:19 -0500

    import was returning an error

--- queue_and_stack/dll_stack.py
+++ queue_and_stack/dll_stack.py
@@ -6,6 +6,7 @@ class Stack:
     def __init__(self):
         self.size = 0
         # Why is our DLL a good choice to store our elements?
+        #
         # self.storage = ?
 
     def push(self, value):

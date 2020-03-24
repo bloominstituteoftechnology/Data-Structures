69c86658acc8baccfd481cc3115114bca7d1bcf8
chip <chiposaur@gmail.com>
2020-03-23 19:02:15 -0500

    dequeue_respects_order test makes everything else fail

---
+++ queue_and_stack/cc.py
@@ -0,0 +1,12 @@
+arr = [85, 46, 27, 81, 94, 9, 27, 38, 43, 99, 37, 63, 31, 42, 14]
+
+# Start loop
+#	if <item> / 3 = <item> // 3
+#	return <item>
+
+for item in arr:
+	if item / 3 == item // 3:
+	# if item % 3 == 0:
+		print(f'{item} is div by 3')
+
+	
--- queue_and_stack/dll_queue.py
+++ queue_and_stack/dll_queue.py
@@ -1,19 +1,29 @@
 import sys
-sys.path.append('../doubly_linked_list')
+# sys.path.append('../doubly_linked_list')
 from doubly_linked_list import DoublyLinkedList
 
+dll = DoublyLinkedList()
 
 class Queue:
-    def __init__(self):
-        self.size = 0
-        # Why is our DLL a good choice to store our elements?
-        # self.storage = ?
+	def __init__(self):
+		self.size = 0
+		# Why is our DLL a good choice to store our elements?
+		# self.storage = ?
 
-    def enqueue(self, value):
-        pass
+	def enqueue(self, value):
+		dll.add_to_tail(value)
+		print(15, value, self.len())
+		
+	def dequeue(self):
+		j = dll.remove_from_head
+		print(19, j)
+		return
 
-    def dequeue(self):
-        pass
-
-    def len(self):
-        pass
+	def len(self):
+		return dll.__len__()
+		
+# f = Queue()
+# f.enqueue(4)
+# print(f.len())
+# f.enqueue(6)
+# print(f.len()) 

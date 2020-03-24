fe041627b28d7ae49f04e19fc9eadf3aa7b3944e
Sameera Leola <sameeraleola@gmail.com>
2020-03-24 07:52:56 -0700

    Reviewing double link lists

--- doubly_linked_list/doubly_linked_list.py
+++ doubly_linked_list/doubly_linked_list.py
@@ -1,12 +1,14 @@
 """Each ListNode holds a reference to its previous node
 as well as its next node in the List."""
 
-
+# These are methods for the node
 class ListNode:
     def __init__(self, value, prev=None, next=None):
-        self.value = value
-        self.prev = prev
-        self.next = next
+        # These three functions are all that is needed to implement a linked list.
+        # https://youtu.be/0y8RGU1-KqI?t=3398
+        self.value = value   # Retreve the value in the current node
+        self.prev = prev    # Retrieve the pointer of the previous node
+        self.next = next   # Retrieve the pointer to the next node in the list.
 
     """Wrap the given value in a ListNode and insert it
     after this node. Note that this node could already
@@ -38,21 +40,23 @@ class ListNode:
 """Our doubly-linked list class. It holds references to
 the list's head and tail nodes."""
 
-
+# These are methods for the linked list
+# https://youtu.be/0y8RGU1-KqI?t=3524
 class DoublyLinkedList:
-    def __init__(self, node=None):
+    def __init__(self, node=None):  # This is useful but not required to create a linked list.
         self.head = node
         self.tail = node
-        self.length = 1 if node is not None else 0
+        self.length = 1 if node is not None else 0 # Initialize this list to have the one
 
     def __len__(self):
-        return self.length
+        return self.length # This returns the length of the list.  It allows calling the built in len() function on the list.
 
     """Wraps the given value in a ListNode and inserts it 
     as the new head of the list. Don't forget to handle 
     the old head node's previous pointer accordingly."""
     def add_to_head(self, value):
-        pass
+    # https://youtu.be/0y8RGU1-KqI?t=3694
+    # Create a double linked list
 
     """Removes the List's current head node, making the
     current head's next node the new head of the List.
@@ -92,10 +96,11 @@ class DoublyLinkedList:
     """Removes a node from the list and handles cases where
     the node was the head or the tail"""
     def delete(self, node):
+        # This delete removes a node from the list.
+        # https://youtu.be/0y8RGU1-KqI?t=3607 
         # TODO: Catch errors if list is empty or node is not in list
         # First pass we are assuming node is in the list
         self.length -= 1 #Adjust the length of the list
-
         # if node is head and tail
         if self.head is self.tail:
             self.head = None
@@ -104,7 +109,7 @@ class DoublyLinkedList:
         
     """Returns the highest value currently in the list"""
     def get_max(self):
-        # We must loop through the entire list to find the biggest value
+        # We must loop through the entire list to find the biggest value: O(n)
         # TODO: Error checking
         # If there is no list return
         # Get the value of the head = max value to start

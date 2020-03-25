5e46f4da84f8ea4b2cd5948c06749ae04f0416e6
umekow <umekolashun@gmail.com>
2020-03-25 14:24:52 -0500

    lru cache

--- lru_cache/test_lru_cache.py
+++ lru_cache/test_lru_cache.py
@@ -10,7 +10,7 @@ class CacheTests(unittest.TestCase):
         self.cache.set('item1', 'a')
         self.cache.set('item2', 'b')
         self.cache.set('item3', 'c')
-
+        
         self.cache.set('item2', 'z')
 
         self.assertEqual(self.cache.get('item1'), 'a')
@@ -35,3 +35,4 @@ class CacheTests(unittest.TestCase):
 
 if __name__ == '__main__':
     unittest.main()
+    

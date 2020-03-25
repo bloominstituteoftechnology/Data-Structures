3aea2c6aef3a510e2d8b990e4391425312998f53
Kerri-Ann <kaybee1jam@yahoo.com>
2020-03-24 21:02:20 -0400

    completed lru

--- lru_cache/doubly_linked_list.py
+++ lru_cache/doubly_linked_list.py
@@ -9,8 +9,9 @@ class ListNode:
         self.next = next
 
     """Wrap the given value in a ListNode and insert it
-    after this node. Note that this node could already
-    have a next node it is point to."""
+	after this node. Note that this node could already
+	have a next node it is point to."""
+
     def insert_after(self, value):
         current_next = self.next
         self.next = ListNode(value, self, current_next)
@@ -18,8 +19,9 @@ class ListNode:
             current_next.prev = self.next
 
     """Wrap the given value in a ListNode and insert it
-    before this node. Note that this node could already
-    have a previous node it is point to."""
+	before this node. Note that this node could already
+	have a previous node it is point to."""
+
     def insert_before(self, value):
         current_prev = self.prev
         self.prev = ListNode(value, current_prev, self)
@@ -27,7 +29,8 @@ class ListNode:
             current_prev.next = self.prev
 
     """Rearranges this ListNode's previous and next pointers
-    accordingly, effectively deleting this ListNode."""
+	accordingly, effectively deleting this ListNode."""
+
     def delete(self):
         if self.prev:
             self.prev.next = self.next
@@ -48,26 +51,110 @@ class DoublyLinkedList:
     def __len__(self):
         return self.length
 
+    """Wraps the given value in a ListNode and inserts it 
+	as the new head of the list. Don't forget to handle 
+	the old head node's previous pointer accordingly."""
+
     def add_to_head(self, value):
-        pass
+        new_node = ListNode(value)
+        self.length += 1
+        if not self.head and not self.tail:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.next = self.head
+            self.head.prev = new_node
+            self.head = new_node
+
+    """Removes the List's current head node, making the
+	current head's next node the new head of the List.
+	Returns the value of the removed Node."""
 
     def remove_from_head(self):
-        pass
+        value = self.head.value
+        self.delete(self.head)
+        return value
+
+    """Wraps the given value in a ListNode and inserts it 
+	as the new tail of the list. Don't forget to handle 
+	the old tail node's next pointer accordingly."""
 
     def add_to_tail(self, value):
-        pass
+        new_node = ListNode(value)
+        self.length += 1
+        if not self.head and not self.tail:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.prev = self.tail
+            self.tail.next = new_node
+            self.tail = new_node
+
+    """Removes the List's current tail node, making the 
+	current tail's previous node the new tail of the List.
+	Returns the value of the removed Node."""
 
     def remove_from_tail(self):
-        pass
+        value = self.tail.value
+        self.delete(self.tail)
+        return value
+
+    """Removes the input node from its current spot in the 
+	List and inserts it as the new head node of the List."""
 
     def move_to_front(self, node):
-        pass
+        if node is self.head:
+            return
+        value = node.value
+        self.delete(node)
+        self.add_to_head(value)
+
+    """Removes the input node from its current spot in the 
+	List and inserts it as the new tail node of the List."""
 
     def move_to_end(self, node):
-        pass
+        if node is self.tail:
+            return
+        value = node.value
+        self.delete(node)
+        self.add_to_tail(value)
+
+    """Removes a node from the list and handles cases where
+	the node was the head or the tail"""
 
     def delete(self, node):
-        pass
+        # TODO: Catch errors if list is empty or node is not in list
+        # For now assumine node is in list
+        self.length -= 1
+        # if head and tail
+        if self.head is self.tail:
+            self.head = None
+            self.tail = None
+        # if head
+        elif node is self.head:
+            self.head = self.head.next
+            node.delete()
+
+        # if tail
+        elif node is self.tail:
+            self.tail = self.tail.prev
+            node.delete()
+        else:
+            # if regular node
+            node.delete()
+
+    """Returns the highest value currently in the list"""
 
     def get_max(self):
-        pass
+        # Loop through all nodes, looking for biggest value
+        # TODO: Error checking
+        if not self.head:
+            return None
+        max_value = self.head.value
+        current = self.head
+        while current:
+            if current.value > max_value:
+                max_value = current.value
+            current = current.next
+
+        return max_value
--- lru_cache/lru_cache.py
+++ lru_cache/lru_cache.py
@@ -1,3 +1,6 @@
+from doubly_linked_list import DoublyLinkedList
+
+
 class LRUCache:
     """
     Our LRUCache class keeps track of the max number of nodes it
@@ -6,28 +9,58 @@ class LRUCache:
     order, as well as a storage dict that provides fast access
     to every node stored in the cache.
     """
+
     def __init__(self, limit=10):
-        pass
+        self.storage = DoublyLinkedList()
+        self.limit = limit  # max number of nodes
+        self.cache = {}
+
+        # Tail - Head
+        # [4,2,3,1]
+        # [ {"item1": "a"}, {"item2": "b"}, {"item3": "c"} ]
+        # most recently used - least recently used
+        # FIFO - Queue
 
     """
-    Retrieves the value associated with the given key. Also
-    needs to move the key-value pair to the end of the order
-    such that the pair is considered most-recently used.
-    Returns the value associated with the key or None if the
-    key-value pair doesn't exist in the cache.
-    """
+	Retrieves the value associated with the given key. Also
+	needs to move the key-value pair to the end of the order
+	such that the pair is considered most-recently used.
+	Returns the value associated with the key or None if the
+	key-value pair doesn't exist in the cache.
+	"""
+
     def get(self, key):
-        pass
+        # Retrieves the value associated with the given key
+
+        # Also needs to move the key-value pair to the end of the order such that the pair is considered most-recently used.
+        # Returns the value associated with the key or None if the key-value pair doesn't exist in the cache.
+        if key in self.cache:
+            node = self.cache[key]
+            self.storage.move_to_front(node)
+            return node.value[1]
+        else:
+            return None
 
     """
-    Adds the given key-value pair to the cache. The newly-
-    added pair should be considered the most-recently used
-    entry in the cache. If the cache is already at max capacity
-    before this entry is added, then the oldest entry in the
-    cache needs to be removed to make room. Additionally, in the
-    case that the key already exists in the cache, we simply
-    want to overwrite the old value associated with the key with
-    the newly-specified value.
-    """
+	Adds the given key-value pair to the cache. The newly-
+	added pair should be considered the most-recently used
+	entry in the cache. If the cache is already at max capacity
+	before this entry is added, then the oldest entry in the
+	cache needs to be removed to make room. Additionally, in the
+	case that the key already exists in the cache, we simply
+	want to overwrite the old value associated with the key with
+	the newly-specified value.
+	"""
+
     def set(self, key, value):
-        pass
+        if key in self.cache:
+            node = self.cache[key]
+            node.value = (key, value)
+            return self.storage.move_to_front(node)
+
+        if len(self.storage) == self.limit:
+            del self.cache[self.storage.tail.value[0]]
+            self.storage.remove_from_tail()
+
+        self.storage.add_to_head((key, value))
+        self.cache[key] = self.storage.head

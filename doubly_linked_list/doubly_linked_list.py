92e4e8f35b058bb39b7a76a144d0e15a676bb59c
Heather Nuffer <zzzuzz@gmail.com>
2020-04-17 18:52:43 -0600

    Adds more code for some methods of DoublyLinkedList

--- doubly_linked_list/doubly_linked_list.py
+++ doubly_linked_list/doubly_linked_list.py
@@ -51,44 +51,76 @@ class DoublyLinkedList:
     def __len__(self):
         return self.length
 
+    def report(self):
+        values = []
+        current_node = self.head
+        while current_node.value:
+            values.append(current_node.value)
+            if current_node.next:
+                current_node = current_node.next
+            else:
+                break
+        print(str(values))
+        print("My DLL's head: " + str(self.head.value))
+        if self.head.next:
+            print("My DLL's head.next: " + str(self.head.next.value))
+        else:
+            print("No head.next")
+        print("My DLL's tail: " + str(self.tail.value))
+        print("My DLL's length: " + str(self.length) + "\n")
+
     """Wraps the given value in a ListNode and inserts it 
     as the new head of the list. Don't forget to handle 
     the old head node's previous pointer accordingly."""
 
     def add_to_head(self, value):
-        newNode = ListNode(value, None, self.head)
-        self.head.prev = newNode
-        self.tail = self.head
-        self.head = newNode
-
-        pass
+        new_node = ListNode(value, None, self.head)
+        self.head.prev = new_node
+        self.head = new_node
+        self.length += 1
 
     """Removes the List's current head node, making the
     current head's next node the new head of the List.
     Returns the value of the removed Node."""
 
     def remove_from_head(self):
-        pass
+        removed_node = self.head
+        next_to_transfer = self.head.next.next
+        self.head = self.head.next
+        self.head.prev = None
+        self.head.next = next_to_transfer
+        self.length -= 1
+        return removed_node.value
 
     """Wraps the given value in a ListNode and inserts it 
     as the new tail of the list. Don't forget to handle 
     the old tail node's next pointer accordingly."""
 
     def add_to_tail(self, value):
-        pass
+        new_node = ListNode(value, self.tail, None)
+        self.tail.next = new_node
+        self.tail = new_node
+        self.length += 1
 
     """Removes the List's current tail node, making the 
     current tail's previous node the new tail of the List.
     Returns the value of the removed Node."""
 
     def remove_from_tail(self):
-        pass
+        remove_node = self.tail
+        self.tail = self.tail.prev
+        self.tail.next = None
+        self.length -= 1
+        return remove_node
 
     """Removes the input node from its current spot in the 
     List and inserts it as the new head node of the List."""
 
     def move_to_front(self, node):
-        pass
+        # Delete the node from the list
+        node.delete()
+        # Add it as the new head
+        self.add_to_head(node.value)
 
     """Removes the input node from its current spot in the 
     List and inserts it as the new tail node of the List."""
@@ -108,11 +140,40 @@ class DoublyLinkedList:
         pass
 
 
-my_first_node = ListNode(5)
+my_first_node = ListNode(1)
+print("Add 1 as the head.")
 my_dll = DoublyLinkedList(my_first_node)
-print("My DLL's head: " + str(my_dll.head.value))
-print("My DLL's tail: " + str(my_dll.tail.value))
-print("Add 6 as the new head.")
-my_dll.add_to_head(6)
-print("My DLL's head: " + str(my_dll.head.value))
-print("My DLL's tail: " + str(my_dll.tail.value))
+my_dll.report()
+
+print("Add 2 as the new head.")
+my_dll.add_to_head(2)
+my_dll.report()
+
+print("Add 3 as the new head.")
+my_dll.add_to_head(3)
+my_dll.report()
+
+print("Remove the head (3).")
+my_dll.remove_from_head()
+my_dll.report()
+
+print("Add 3 as the new head.")
+my_dll.add_to_head(3)
+my_dll.report()
+
+print("Add 10 as the new tail.")
+my_dll.add_to_tail(10)
+my_dll.report()
+
+print("Add 20 as the new tail.")
+my_dll.add_to_tail(20)
+my_dll.report()
+
+print("Remove from tail (20).")
+my_dll.remove_from_tail()
+my_dll.report()
+
+print("Remove the tail (10) and add it as the new head")
+tail = my_dll.remove_from_tail()
+my_dll.move_to_front(tail)
+my_dll.report()

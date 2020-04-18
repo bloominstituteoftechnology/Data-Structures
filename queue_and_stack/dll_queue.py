6605381d523c2878b495d2e40a11281728f96061
Heather Nuffer <zzzuzz@gmail.com>
2020-04-17 21:30:30 -0600

    Adds a copy of doubly_linked_list into queue_and_stack directory so it can be imported and adds some code to dll_queue

--- queue_and_stack/dll_queue.py
+++ queue_and_stack/dll_queue.py
@@ -1,5 +1,7 @@
-import sys
-sys.path.append('../doubly_linked_list')
+# from doubly_linked_list import DoublyLinkedList
+# import sys
+# sys.path.append('../doubly_linked_list')
+from doubly_linked_list import ListNode
 from doubly_linked_list import DoublyLinkedList
 
 
@@ -7,13 +9,19 @@ class Queue:
     def __init__(self):
         self.size = 0
         # Why is our DLL a good choice to store our elements?
+        # Pretty straight-forward to add to head & tail, and remove from head & tail.
         # self.storage = ?
+        self.storage = DoublyLinkedList()
 
     def enqueue(self, value):
-        pass
+        # Add to tail
+        self.storage.add_to_tail(value)
+        self.size += 1
 
     def dequeue(self):
-        pass
+        # Remove head
+        self.storage.remove_from_head()
+        self.size -= 1
 
     def len(self):
-        pass
+        return self.size

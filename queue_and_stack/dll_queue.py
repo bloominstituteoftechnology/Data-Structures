b17997524abaa22c77c01eb7ccad8ebc27ca3ed4
Aasa-Christian <aasachristian@yahoo.com>
2020-04-20 17:19:17 -0400

    first day mvp

--- queue_and_stack/dll_queue.py
+++ queue_and_stack/dll_queue.py
@@ -1,5 +1,5 @@
 import sys
-sys.path.append('../doubly_linked_list')
+# sys.path.append('../doubly_linked_list')
 from doubly_linked_list import DoublyLinkedList
 
 
@@ -7,13 +7,19 @@ class Queue:
     def __init__(self):
         self.size = 0
         # Why is our DLL a good choice to store our elements?
-        # self.storage = ?
+        self.storage = DoublyLinkedList()
 
     def enqueue(self, value):
-        pass
+        self.size += 1
+        return self.storage.add_to_tail(value)
+         
 
     def dequeue(self):
-        pass
+        if self.storage.head == None:
+            return None
+        else:
+            self.size -= 1
+            return self.storage.remove_from_head()
 
     def len(self):
-        pass
+        return self.size
--- queue_and_stack/doubly_linked_list.py
+++ queue_and_stack/doubly_linked_list.py
@@ -95,18 +95,51 @@ class DoublyLinkedList:
     """Removes the input node from its current spot in the 
     List and inserts it as the new head node of the List."""
     def move_to_front(self, node):
-        pass
+        if node is self.head:
+            return
+        self.add_to_head(node.value)
+        self.delete(node)
 
     """Removes the input node from its current spot in the 
     List and inserts it as the new tail node of the List."""
     def move_to_end(self, node):
-        pass
+        if node is self.tail:
+            return
+        self.add_to_tail(node.value)
+        self.delete(node)
 
     """Removes a node from the list and handles cases where
     the node was the head or the tail"""
     def delete(self, node):
-        pass
+        # Planning
+        #  TODO: Do we need error checking if node not in list?
+        self.length -= 1
+        # This is the only node
+        if self.head is self.tail:
+            self.head = None
+            self.tail = None
+        # It's the head
+        elif node is self.head:
+            self.head = node.next
+            node.delete()
+        # it's the tail
+        elif node is self.tail:
+            self.tail = node.prev
+            node.delete()
+        # it's in the middle
+        else:
+            node.delete()
         
     """Returns the highest value currently in the list"""
     def get_max(self):
-        pass
+        # How to get max
+        # create max var
+        current = self.head
+        max = self.head.value
+        # Loop  through nodes
+        while(current is not None):
+            # compare value in node to max found
+            if current.value > max:
+                max = current.value
+            current = current.next
+        # return max found

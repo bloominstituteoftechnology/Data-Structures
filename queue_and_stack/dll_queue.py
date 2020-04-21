9e37514e1ed12bae0a7129206992f7811bbf3863
PercivalN <percival.ngan@gmail.com>
2020-04-20 21:11:52 -0500

    Added dll_queue but it is still failing one test

--- queue_and_stack/dll_queue.py
+++ queue_and_stack/dll_queue.py
@@ -3,17 +3,26 @@ sys.path.append('../doubly_linked_list')
 from doubly_linked_list import DoublyLinkedList
 
 
+
 class Queue:
+
     def __init__(self):
         self.size = 0
         # Why is our DLL a good choice to store our elements?
-        # self.storage = ?
+        self.storage = DoublyLinkedList()
+        
 
     def enqueue(self, value):
-        pass
+        self.storage.add_to_tail(value)
+        self.size += 1
 
-    def dequeue(self):
-        pass
+    def dequeue(self): 
+        if self.size == 0:
+            return
+        else:
+            self.storage.remove_from_head()
+            self.size -= 1
+        
 
     def len(self):
-        pass
+        return self.size
---
+++ queue_and_stack/wip.py
@@ -0,0 +1,8 @@
+from doubly_linked_list import DoublyLinkedList
+
+linked-list = (1, 2, 3, 4, 5)
+
+dll1 = DoublyLinkedList()
+
+
+dll1.add_

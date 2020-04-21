d7c15919aeb36d22f665b8db15ff338d1199958a
patrick-gordon <patrickgordon42@yahoo.com>
2020-04-21 00:44:27 -0500

    MVP-DAY-1

--- doubly_linked_list/doubly_linked_list.py
+++ doubly_linked_list/doubly_linked_list.py
@@ -66,7 +66,9 @@ class DoublyLinkedList:
     current head's next node the new head of the List.
     Returns the value of the removed Node."""
     def remove_from_head(self):
-        pass
+        value = self.head.value
+        self.delete(self.head)
+        return value
 
     """Wraps the given value in a ListNode and inserts it 
     as the new tail of the list. Don't forget to handle 
@@ -86,7 +88,9 @@ class DoublyLinkedList:
     current tail's previous node the new tail of the List.
     Returns the value of the removed Node."""
     def remove_from_tail(self):
-        pass
+        value = self.tail.value
+        self.delete(self.tail)
+        return value
 
     """Removes the input node from its current spot in the 
     List and inserts it as the new head node of the List."""
@@ -101,7 +105,25 @@ class DoublyLinkedList:
     """Removes a node from the list and handles cases where
     the node was the head or the tail"""
     def delete(self, node):
-        pass
+        # PLANNING
+        self.length -= 1
+        # THIS IS ONLY NODE
+        if self.head is self.tail:
+            self.head = None
+            self.tail = None
+        # IT IS THE HEAD
+        elif node is self.head:
+            self.head = node.next
+            node.delete()
+        # IT IS THE TAIL
+        elif node is self.tail:
+            self.tail = node.prev
+            node.delete()
+        # IT IS IN THE MIDDLE
+        else:
+            node.delete()
+
+
         
     """Returns the highest value currently in the list"""
     def get_max(self):

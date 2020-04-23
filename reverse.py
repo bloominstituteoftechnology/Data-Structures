c0af73c2e267167f24b4c674dc5f4ab070b241a4
Dan Morse <djmthedev@gmail.com>
2020-04-23 11:14:50 -0700

    added alternate for_each methods

--- reverse.py
+++ reverse.py
@@ -14,6 +14,36 @@ def reverse(node):
         new_head = future_head
     return head
 
+
+
+
+
+
+
+
+def recursive_reverse(node):
+    if node.next is None:
+        return node
+    node.next = None
+    new_head = node.next
+    future_head = recursive_reverse(new_head)
+    new_head.next = node
+    return future_head
+
+# 123
+#     23
+#         3
+#     3
+
+
+# 1 234
+#     2 34
+#         3 4
+#             4-back
+#         43
+#     432
+# 4321
+
 a = Node(1)
 b = Node(2)
 a.next = b
@@ -31,7 +61,16 @@ while forward is not None:
     forward = forward.next
 
 print("Reversed")
-reversed = reverse(a)
+reversed = recursive_reverse(a)
 while reversed is not None:
     print(reversed.value)
     reversed = reversed.next
+
+word = "five"
+def rev(word):
+    if len(word) == 1:
+        return word
+    word = rev(word[1:]) + (word[0])
+    return word
+
+print(rev(word))

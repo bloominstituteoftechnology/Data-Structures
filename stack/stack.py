595e52e26df8560e27fd076ccb2dda458429c714
Adam <adammonast@Adams-MBP.attlocal.net>
2020-05-11 18:54:57 -0400

    half of MVP: stack array implementation, linked list implementation, passing tests

--- stack/stack.py
+++ stack/stack.py
@@ -1,25 +1,37 @@
 """
 A stack is a data structure whose primary purpose is to store and
-return elements in Last In First Out order. 
+return elements in Last In First Out order.
 
 1. Implement the Stack class using an array as the underlying storage structure.
    Make sure the Stack tests pass.
 2. Re-implement the Stack class, this time using the linked list implementation
    as the underlying storage structure.
    Make sure the Stack tests pass.
-3. What is the difference between using an array vs. a linked list when 
+3. What is the difference between using an array vs. a linked list when
    implementing a Stack?
 """
+
+from linked_list import Node, LinkedList
+
+
 class Stack:
     def __init__(self):
         self.size = 0
-        # self.storage = ?
+        self.storage = LinkedList()
 
     def __len__(self):
         pass
+        return self.size
 
     def push(self, value):
         pass
+        self.storage.add_to_end(value)
+        self.size += 1
 
     def pop(self):
         pass
+        if not self.storage.head:
+            return None
+        else:
+            self.size -= 1
+            return self.storage.remove_at_end()

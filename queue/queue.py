99b9caf063b1447ffe744640c3624b39e76aa819
Sean Chen <skypemaster007@gmail.com>
2020-05-11 07:49:03 -0700

    Add stretch problem to queue.py

--- queue/queue.py
+++ queue/queue.py
@@ -9,6 +9,9 @@ return elements in First In First Out order.
    Make sure the Queue tests pass.
 3. What is the difference between using an array vs. a linked list when 
    implementing a Queue?
+   
+Stretch: What if you could only use instances of your Stack class to implement the Queue?
+         What would that look like? Try it!
 """
 class Queue:
     def __init__(self):

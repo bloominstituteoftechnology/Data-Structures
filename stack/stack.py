8a13ffa373d0308805e9a977299f23980f4847ab
Tyler Christian <tylerc71197@gmail.com>
2020-05-11 17:00:58 -0400

    finished stack with linkList implementation

--- stack/stack.py
+++ stack/stack.py
@@ -11,19 +11,25 @@ return elements in Last In First Out order.
    implementing a Stack?
 """
 
+from linked_list import Node, LinkedList
+
+
 class Stack:
     def __init__(self):
         self.size = 0
-        self.storage = []
+        self.storage = LinkedList()
 
     def __len__(self):
-        return len(self.storage)
+        return self.size
 
     def push(self, value):
-        self.storage.append(value)
+        # self.storage.append(value)
+        self.storage.add_to_end(value)
+        self.size += 1
 
     def pop(self):
-        if not self.storage:
+        if not self.storage.head:
             return None
         else:
-            return self.storage.pop()
+            self.size -= 1
+            return self.storage.remove_at_end()

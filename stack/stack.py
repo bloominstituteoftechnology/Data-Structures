82a41060be020bef18af44e4006968d1aa634896
JFerrer95 <jonathanferrer95@gmail.com>
2020-05-11 16:14:09 -0400

    Stack part 1 passing test

--- stack/stack.py
+++ stack/stack.py
@@ -13,13 +13,14 @@ return elements in Last In First Out order.
 class Stack:
     def __init__(self):
         self.size = 0
-        # self.storage = ?
-
+        self.storage = []
     def __len__(self):
-        pass
-
+        return self.size
     def push(self, value):
-        pass
-
+        self.storage.append(value)
+        self.size += 1
     def pop(self):
-        pass
+        if self.size <= 0:
+            return None
+        self.size += -1
+        self.storage.pop()

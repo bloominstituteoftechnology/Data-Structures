4092c01183b76da123a5a918040f6adbfe114fe0
Mike-Xie <Mike-Xie@users.noreply.github.com>
2020-05-13 11:25:26 -0700

    fix name

--- binary_search_tree/test_binary_search_tree.py
+++ binary_search_tree/test_binary_search_tree.py
@@ -2,7 +2,7 @@ import unittest
 import random
 import sys
 import io
-from binary_search_tree import BinarySearchTree
+from binary_search_tree import BSTNode
 
 
 class BinarySearchTreeTests(unittest.TestCase):

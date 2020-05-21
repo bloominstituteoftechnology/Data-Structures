b56c320b5c2edc1e484c83fd8fc6baecc044872a
Michael Stoffer <mstoffer@michaelstoffer.com>
2020-05-20 23:54:27 -0400

    Finished Day 1 on Data Structures

--- doubly_linked_list/doubly_linked_list.py
+++ doubly_linked_list/doubly_linked_list.py
@@ -88,18 +88,18 @@ class DoublyLinkedList:
 	"""Removes the input node from its current spot in the
 	List and inserts it as the new head node of the List."""
 	def move_to_front(self, node):
-		node.next = self.head
-		self.head.prev = node
-		self.head = node
+		if node is self.head:
+			return
 		self.delete(node)
+		self.add_to_head(node.value)
 
 	"""Removes the input node from its current spot in the
 	List and inserts it as the new tail node of the List."""
 	def move_to_end(self, node):
-		node.prev = self.tail
-		self.tail.next = node
-		self.tail = node
+		if node is self.tail:
+			return
 		self.delete(node)
+		self.add_to_tail(node.value)
 
 	"""Removes a node from the list and handles cases where
 	the node was the head or the tail"""
@@ -131,4 +131,11 @@ class DoublyLinkedList:
 
 	"""Returns the highest value currently in the list"""
 	def get_max(self):
-		pass
+		val = self.head.value
+		this = self.head
+
+		while this is not None:
+			if this.value > val:
+				val = this.value
+			this = this.next
+		return val
--- queue/queue.py
+++ queue/queue.py
@@ -1,28 +1,35 @@
 """
 A queue is a data structure whose primary purpose is to store and
-return elements in First In First Out order. 
+return elements in First In First Out order.
 
 1. Implement the Queue class using an array as the underlying storage structure.
    Make sure the Queue tests pass.
 2. Re-implement the Queue class, this time using the linked list implementation
    as the underlying storage structure.
    Make sure the Queue tests pass.
-3. What is the difference between using an array vs. a linked list when 
+3. What is the difference between using an array vs. a linked list when
    implementing a Queue?
-   
+
 Stretch: What if you could only use instances of your Stack class to implement the Queue?
-         What would that look like? How many Stacks would you need? Try it!
+		 What would that look like? How many Stacks would you need? Try it!
 """
+import sys
+sys.path.append('../doubly_linked_list')
+from doubly_linked_list import DoublyLinkedList
+
 class Queue:
-    def __init__(self):
-        self.size = 0
-        # self.storage = ?
-    
-    def __len__(self):
-        pass
+	def __init__(self):
+		self.size = 0
+		self.storage = DoublyLinkedList()
 
-    def enqueue(self, value):
-        pass
+	def __len__(self):
+		return self.size
 
-    def dequeue(self):
-        pass
+	def enqueue(self, value):
+		self.size += 1
+		self.storage.add_to_tail(value)
+
+	def dequeue(self):
+		if self.size:
+			self.size -= 1
+			return self.storage.remove_from_head()

93b2a78f568b9c752e7ab01d5894c4d05fcb6a76
Tevvin <Tevvin7395@icloud.com>
2020-05-26 23:49:28 -0400

    Queue complete but trying to degbug a syntax error

--- queue/queue.py
+++ queue/queue.py
@@ -1,3 +1,7 @@
+import sys
+import sys.path.append('./doubly_linked_list')
+from doubly_linked_list import DoublyLinkedList  # This is our storage
+
 """
 A queue is a data structure whose primary purpose is to store and
 return elements in First In First Out order. 
@@ -13,16 +17,18 @@ return elements in First In First Out order.
 Stretch: What if you could only use instances of your Stack class to implement the Queue?
          What would that look like? How many Stacks would you need? Try it!
 """
+
+
 class Queue:
     def __init__(self):
         self.size = 0
-        # self.storage = ?
-    
+        self.storage = DoublyLinkedList()
+
     def __len__(self):
-        pass
+        return self.storage.length
 
     def enqueue(self, value):
-        pass
+        self.storage.add_to_tail(value)
 
     def dequeue(self):
-        pass
+        return self.storage.remove_from_head()

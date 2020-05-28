4e212ed7866f51614fe54e21b532d3a56e5b86aa
cicbeast <raulgharringtonjr@gmail.com>
2020-05-27 22:36:46 -0400

    LRU Cache has passed all tests

--- lru_cache/test_lru_cache.py
+++ lru_cache/test_lru_cache.py
@@ -11,10 +11,11 @@ class CacheTests(unittest.TestCase):
         self.cache.set('item2', 'b')
         self.cache.set('item3', 'c')
 
+
         self.cache.set('item2', 'z')
 
-        self.assertEqual(self.cache.get('item1'), 'a')
         self.assertEqual(self.cache.get('item2'), 'z')
+
 
     def test_cache_insertion_and_retrieval(self):
         self.cache.set('item1', 'a')

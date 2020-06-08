10aa351abf130eb4aa7e644b58dc3f29759e1b86
Brianna Keune <briannakeune@gmail.com>
2020-06-08 14:43:52 -0500

    array queue

--- queue/queue.py
+++ queue/queue.py
@@ -1,3 +1,4 @@
+from singly_linked_list import LinkedList
 """
 A queue is a data structure whose primary purpose is to store and
 return elements in First In First Out order. 
@@ -13,16 +14,40 @@ return elements in First In First Out order.
 Stretch: What if you could only use instances of your Stack class to implement the Queue?
          What would that look like? How many Stacks would you need? Try it!
 """
+
+# array
 class Queue:
     def __init__(self):
         self.size = 0
-        # self.storage = ?
+        self.storage = []
+        self.length = 0
     
     def __len__(self):
-        pass
+        return self.length
 
     def enqueue(self, value):
-        pass
+        self.length += 1
+        self.storage.append(value)
 
     def dequeue(self):
-        pass
+        try:
+            removed = self.storage.pop(0)
+            self.length -= 1
+            return removed
+        except:
+            return None
+
+# linked list
+# class Queue:
+#     def __init__(self):
+#         self.size = 0
+#         self.storage = []
+    
+#     def __len__(self):
+#         pass
+
+#     def enqueue(self, value):
+#         pass
+
+#     def dequeue(self):
+#         pass
---
+++ queue/singly_linked_list.py
@@ -0,0 +1,64 @@
+
+class Node:
+    def __init__(self, value=None, next_node=None):
+        self.value = value
+        self.next_node = next_node
+
+    def get_value(self):
+        return self.get_value
+
+    def get_next_node(self):
+        return self.get_next_node
+
+    def set_next(self, new_next):
+        self.next_node = new_next
+
+
+class LinkedList:
+    def __init__(self, head=None, tail=None):
+        self.head = head
+        self.tail = tail
+        self.length = 0
+
+    def add_to_tail(self, value):
+        self.length += 1
+        new_node = Node(value)
+        if not self.head:
+            self.head, self.tail = new_node, new_node
+        else:
+            self.tail.set_next(new_node)
+            self.tail = new_node            
+
+    def remove_head(self):
+        self.length -= 1
+        if not self.head:
+            return None
+        head_value = self.head.value
+        if self.head == self.tail:
+            self.head, self.tail = None, None
+        else:
+            self.head = self.head.next_node
+        return head_value
+
+    def contains(self, target):
+        for i in self:
+            if i.value == target:
+                return True
+        return False
+
+    def __len__(self):
+        return self.length
+
+    def get_max(self):
+        if not self.head:
+            return None
+        if len(self) is 0:
+            return None
+        sorted_ll = sorted([i.value for i in self])
+        return sorted_ll[-1]
+
+    def __iter__(self):
+        current = self.head
+        while current is not None:
+            yield current
+            current = current.next_node

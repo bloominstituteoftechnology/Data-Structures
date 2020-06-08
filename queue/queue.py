02a53a64bffa76460e47c2de7a3f598053687871
Juan Gerardo Madero Flores <juan@Juans-MacBook.attlocal.net>
2020-06-08 16:46:09 -0700

    mvp

--- queue/queue.py
+++ queue/queue.py
@@ -9,20 +9,60 @@ return elements in First In First Out order.
    Make sure the Queue tests pass.
 3. What is the difference between using an array vs. a linked list when 
    implementing a Queue?
+
+Answer #3: Elements of an array don't have a built in reference to the next element in the list, while Linked List nodes do have a next property. Also, Linked Lists don't have the same helper methods as arrays.
    
 Stretch: What if you could only use instances of your Stack class to implement the Queue?
          What would that look like? How many Stacks would you need? Try it!
 """
 class Queue:
-    def __init__(self):
-        self.size = 0
-        # self.storage = ?
-    
-    def __len__(self):
-        pass
+    # Array version
+    class Queue:
+        def __init__(self):
+            self.content = []
+        
+        def __len__(self):
+            return len(self.content)
 
-    def enqueue(self, value):
-        pass
+        def enqueue(self, value):
+            self.content.append(value)
 
-    def dequeue(self):
-        pass
+        def dequeue(self):
+            if len(self.content) > 0:
+                return self.content.pop(0)
+
+    # Linked list version
+    class Node:
+        def __init__(self, value, next_node):
+            self.value = value
+            self.next = next_node
+
+    class LinkedList:
+        def __init__(self, head = None, tail = None):
+            self.head = head
+            self.tail = tail
+
+    class Queue:
+        def __init__(self):
+            self.length = 0
+            self.content = LinkedList()
+        
+        def __len__(self):
+            return self.length
+
+        def enqueue(self, value):
+            new_node = Node(value, None)
+            if self.length == 0:
+                self.content.head = new_node
+                self.content.tail = new_node
+            else:
+                self.content.tail.next = new_node
+                self.content.tail = new_node
+            self.length += 1
+
+        def dequeue(self):
+            if self.length > 0:
+                popped_node = self.content.head
+                self.content.head = self.content.head.next
+                self.length -= 1
+                return popped_node.value
--- stack/stack.py
+++ stack/stack.py
@@ -9,17 +9,60 @@ return elements in Last In First Out order.
    Make sure the Stack tests pass.
 3. What is the difference between using an array vs. a linked list when 
    implementing a Stack?
+
+Answer #3: Elements of an array don't have a built in reference to the next element in the list, while Linked List nodes do have a next property. Also, Linked Lists don't have the same helper methods as arrays.
 """
 class Stack:
-    def __init__(self):
-        self.size = 0
-        # self.storage = ?
+    # Array version
+    class Stack:
+        def __init__(self):
+            self.content = []
 
-    def __len__(self):
-        pass
+        def __len__(self):
+            return len(self.content)
 
-    def push(self, value):
-        pass
+        def push(self, value):
+            self.content.insert(0,value)
 
-    def pop(self):
-        pass
+        def pop(self):
+            if len(self.content) > 0:
+                return self.content.pop(0)
+            else:
+                return None
+
+    # Linked List data structure version
+    class Node:
+        def __init__(self, value, next_node):
+            self.value = value
+            self.next = next_node
+
+    class LinkedList:
+        def __init__(self, head = None, tail = None):
+            self.head = head
+            self.tail = tail
+
+    class Stack:
+        def __init__(self):
+            self.length = 0
+            self.content = LinkedList()
+
+        def __len__(self):
+            return self.length
+
+        def push(self, data):
+            new_node = Node(data, self.content.head)
+            self.content.head = new_node
+            self.length += 1
+            if self.length == 1:
+                self.content.tail == self.content.head
+
+        def pop(self):
+            if self.length > 0:
+                popped = self.content.head
+                self.content.head = self.content.head.next
+                self.length -= 1
+
+                if self.length == 0:
+                    self.content.tail == None
+                    
+                return popped.value

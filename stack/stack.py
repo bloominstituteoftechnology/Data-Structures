5672a58f5a609cbb4fc891df339a92eb72c3b2b7
Wyatt Harrell <wyattaharrell@gmail.com>
2020-06-08 17:12:09 -0400

    Stck implemented with list

--- stack/stack.py
+++ stack/stack.py
@@ -13,13 +13,38 @@ return elements in Last In First Out order.
 class Stack:
     def __init__(self):
         self.size = 0
-        # self.storage = ?
+        self.storage = []
 
     def __len__(self):
-        pass
+        return self.size
 
     def push(self, value):
-        pass
+        self.storage.append(value)
+        self.size += 1
 
     def pop(self):
-        pass
+        if self.size >= 1:
+            val = self.storage.pop()
+            self.size -= 1
+            return val
+
+
+# from singly_linked_list import LinkedList
+#
+# class Stack:
+#     def __init__(self):
+#         self.size = 0
+#         self.storage = LinkedList()
+#
+#     def __len__(self):
+#         return self.size
+#
+#     def push(self, value):
+#         self.storage.add_to_tail(value)
+#         self.size += 1
+#
+#     def pop(self):
+#         if self.size >= 1:
+#             val = self.storage.remove_tail()
+#             self.size -= 1
+#             return val

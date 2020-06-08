3c80589eea5da558d802442cb2227dfe16f92868
Doina Langille <doina.jitoreanu@gmail.com>
2020-06-08 16:19:12 -0400

    day 1 stack and queue

--- queue/queue.py
+++ queue/queue.py
@@ -13,16 +13,57 @@ return elements in First In First Out order.
 Stretch: What if you could only use instances of your Stack class to implement the Queue?
          What would that look like? How many Stacks would you need? Try it!
 """
+
+# 1. Implement the Queue class using an array as the underlying storage structure.
+#    Make sure the Queue tests pass.
+
+# class Queue:
+#     def __init__(self):
+#         self.size = 0
+#         self.storage = []
+    
+#     def __len__(self):
+#         return self.size
+
+#     def enqueue(self, value):
+#         self.size += 1
+#         self.storage.append(value)
+
+#     def dequeue(self):
+#         if self.size !=0:
+#             self.size -= 1
+#             return self.storage.pop(0)
+
+# 2. Re-implement the Queue class, this time using the linked list implementation
+#    as the underlying storage structure.
+
+from singly_linked_list import LinkedList
 class Queue:
     def __init__(self):
         self.size = 0
-        # self.storage = ?
+        self.storage = LinkedList()
     
     def __len__(self):
-        pass
+        return self.size
 
     def enqueue(self, value):
-        pass
+        self.size += 1
+        self.storage.add_to_tail(value)
 
     def dequeue(self):
-        pass
+        if self.size !=0:
+            self.size -= 1
+            return self.storage.remove_head()
+
+
+"""
+3. What is the difference between using an array vs. a linked list when 
+   implementing a Queue?
+
+   When implementing a queue, we have to remove the first element and/or add
+   a new element as the last element if we use an array
+   When using a linked list, we have to add an element to the tail,
+   therefore changing the address of the last element and of the tail.
+   If we have to dequeue, we have to remove the head, and then to reallocate
+   an address to every other elements.
+"""

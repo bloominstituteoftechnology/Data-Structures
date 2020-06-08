cf2c7836ebdc778a2e21897e902f7f87fa3326ef
Alex Pakalniskis <alexpakalniskis3@gmail.com>
2020-06-08 13:30:26 -0700

    feat: complete stack data structure assignment

--- stack/stack.py
+++ stack/stack.py
@@ -10,16 +10,63 @@ return elements in Last In First Out order.
 3. What is the difference between using an array vs. a linked list when 
    implementing a Stack?
 """
-class Stack:
+#class Stack:
+#    """Array (a.k.a. list) implementation of Stack data structure"""
+#
+#    def __init__(self):
+#        """Initialize array-based Stack class"""
+#
+#        self.size = 0
+#        self.storage = []
+#
+#    def __len__(self):
+#        """Return length of Stack class"""
+#
+#        self.size = len(self.storage)
+#        return self.size
+#
+#    def push(self, value):
+#        """Add an element to initialized Stack class"""
+#
+#        self.storage.append(value)
+#
+#    def pop(self):
+#        """Remove most recently added element from Stack class"""
+#
+#        if self.storage.__len__() == 0:
+#            return None
+#        else:
+#            return self.storage.pop(-1)
+
+from singly_linked_list import LinkedList 
+
+class Stack():
+    """Singly-linked list implementation of Stack data structure"""
+
     def __init__(self):
+        """Initialize array-based Stack class"""
+
         self.size = 0
-        # self.storage = ?
+        self.storage = LinkedList()
 
     def __len__(self):
-        pass
+        """Return length of Stack class"""
+
+        return self.size
 
     def push(self, value):
-        pass
+        """Add an element to initialized Stack class"""
+
+        self.storage.add_to_tail(value)
+        self.size += 1
 
     def pop(self):
-        pass
+        """Remove most recently added element from Stack class"""
+        
+        if self.__len__() == 0:
+            return None
+        else:
+            self.size -= 1
+            return self.storage.remove_tail()
+
+

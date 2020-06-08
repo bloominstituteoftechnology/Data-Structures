ba3fecc86b520bfdd8991d710d28f796f844fac8
Brianna Keune <briannakeune@gmail.com>
2020-06-08 15:13:47 -0500

    array stack

--- stack/stack.py
+++ stack/stack.py
@@ -1,3 +1,4 @@
+from singly_linked_list import LinkedList
 """
 A stack is a data structure whose primary purpose is to store and
 return elements in Last In First Out order. 
@@ -13,13 +14,17 @@ return elements in Last In First Out order.
 class Stack:
     def __init__(self):
         self.size = 0
-        # self.storage = ?
+        self.storage = []
 
     def __len__(self):
-        pass
+        return len(self.storage)
 
     def push(self, value):
-        pass
+        self.storage.append(value)
 
     def pop(self):
-        pass
+        try:
+            removed = self.storage.pop()
+            return removed
+        except:
+            return None

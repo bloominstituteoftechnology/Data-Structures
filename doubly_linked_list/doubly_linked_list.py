3a3680694284c2fe80b962a89d8511179658f3f6
Henry Nelson <Henry.Nelson89@gmail.com>
2020-06-09 15:58:54 -0700

    I met MVP! for today!

--- doubly_linked_list/doubly_linked_list.py
+++ doubly_linked_list/doubly_linked_list.py
@@ -63,7 +63,9 @@ class DoublyLinkedList:
     current head's next node the new head of the List.
     Returns the value of the removed Node."""
     def remove_from_head(self):
-        pass
+        value = self.head.value
+        self.delete(self.head)
+        return value
 
     """Wraps the given value in a ListNode and inserts it 
     as the new tail of the list. Don't forget to handle 
@@ -84,23 +86,55 @@ class DoublyLinkedList:
     current tail's previous node the new tail of the List.
     Returns the value of the removed Node."""
     def remove_from_tail(self):
-        pass
+        value = self.tail.value
+        self.delete(self.tail)
+        return value
 
     """Removes the input node from its current spot in the 
     List and inserts it as the new head node of the List."""
     def move_to_front(self, node):
-        pass
+        if node is self.head:
+            return
+        self.add_to_head(node.value)
+        self.delete(node)
 
     """Removes the input node from its current spot in the 
     List and inserts it as the new tail node of the List."""
     def move_to_end(self, node):
-        pass
+        if node is self.tail:
+            return
+        self.add_to_tail(node.value)
+        self.delete(node)
 
     """Removes a node from the list and handles cases where
     the node was the head or the tail"""
     def delete(self, node):
-        pass
+        #Planning
+        #Change Length
+        self.length -= 1
+        #Only Node
+        if self.head is self.tail:
+            self.head = None
+            self.tail = None
+        #the head
+        elif node is self.head:
+            self.head = node.next
+            node.delete()
+        #the tail
+        elif node is self.tail:
+            self.tail = node.prev
+            node.delete()
+        #one in the middle
+        else:
+            node.delete()
+        
         
     """Returns the highest value currently in the list"""
     def get_max(self):
-        pass
+        current = self.head
+        max = self.head.value
+        while(current is not None):
+            if current.value > max:
+                max = current.value
+            current = current.next
+        return max
--- queue/queue.py
+++ queue/queue.py
@@ -143,7 +143,7 @@ Stretch: What if you could only use instances of your Stack class to implement t
 class Queue:
     def __init__(self):
         self.size = 0
-        self.storage = LinkedList
+        self.storage = LinkedList()
     
     def __len__(self):
         return self.size
@@ -154,7 +154,22 @@ class Queue:
 
     def dequeue(self):
         if self.size > 0:
-            self.storage.remove_tail()
+            value = self.storage.remove_head()
             self.size -= 1
-            return self.storage.remove_head
+            return value
         return None
+
+# class Queue:
+#     def __init__(self):
+#         self.size = 0
+#         self.storage = LinkedList()
+#     def __len__(self):
+#         return self.size
+#     def enqueue(self, value):
+#         self.storage.add_to_tail(value)
+#         self.size += 1
+#     def dequeue(self):
+#         if self.size >= 1:
+#             val = self.storage.remove_head()
+#             self.size -= 1
+#             return val

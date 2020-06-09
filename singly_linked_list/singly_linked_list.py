b5705e46b56701ca789dfa92db8bdd16a4e42238
Dimos-Christidis <christidis.dimosthenis@gmail.com>
2020-06-09 15:11:47 +0300

    singly list corrected from the lecture

--- singly_linked_list/singly_linked_list.py
+++ singly_linked_list/singly_linked_list.py
@@ -1,10 +1,8 @@
-# create a Node Class
 class Node:
     def __init__(self, value=None, next_node=None):
-        # set the initial value of our node
+        # the value at this linked list node
         self.value = value
-
-        # set a ref to the next node
+        # reference to the next node in the list
         self.next_node = next_node
 
     def get_value(self):
@@ -13,77 +11,109 @@ class Node:
     def get_next(self):
         return self.next_node
 
-    def set_next(self, new_node):
-        self.next_node = new_node
-
-
+    def set_next(self, new_next):
+        # set this node's next_node reference to the passed in node
+        self.next_node = new_next
 
 class LinkedList:
     def __init__(self):
-        # ref to head of list
+        # reference to the head of the list
         self.head = None
-        #ref to tail of list
+        # reference to the tail of the list
         self.tail = None
 
     def add_to_tail(self, value):
-        # wrap the input value in a new node
+        # wrap the input value in a node
         new_node = Node(value, None)
-
-        # check if there is no head (is the list empty)
+        # check if there is no head (i.e., the list is empty)
         if not self.head:
-            # if it is empty then we will set both the head and the tail to the new node
+            # if the list is initially empty, set both head and tail to the new node
             self.head = new_node
             self.tail = new_node
-        # otherwise (we have a non empty list)
+        # we have a non-empty list, add the new node to the tail
         else:
-            # set the current tails next ref to our new node
-            self.tail.next = new_node
-            # set lists tail ref to our new node
+            # set the current tail's next reference to our new node
+            self.tail.set_next(new_node)
+            # set the list's tail reference to the new node
             self.tail = new_node
 
     def remove_head(self):
-        # if the list is empty then return None
+        # return None if there is no head (i.e. the list is empty)
         if not self.head:
             return None
-
-        # if the head has no next (single element)
+        # if head has no next, then we have a single element in our list
         if not self.head.get_next():
-            # get a ref to the current head
+            # get a reference to the head
             head = self.head
-            # del the lists head ref
+            # delete the list's head reference
             self.head = None
-            # make sure we set tail ref to None
+            # also make sure the tail reference doesn't refer to anything
             self.tail = None
-
-            # return the value of the current heads next
+            # return the value
             return head.get_value()
-        
         # otherwise we have more than one element in our list
-        # store the value
         value = self.head.get_value()
-        # set the head ref to the current heads next node in the list
+        # set the head reference to the current head's next node in the list
         self.head = self.head.get_next()
-        # return the stored value
         return value
 
+    def remove_tail(self):
+        if not self.head:
+            return None
+        
+        if self.head is self.tail:
+            value = self.head.get_value()
+            self.head = None
+            self.tail = None
+            return value
+        
+        current = self.head
+
+        while current.get_next() is not self.tail:
+            current = current.get_next()
+
+        value = self.tail.get_value()
+        self.tail = current
+        return value
 
     def contains(self, value):
-        # if there is no head we have an empty list
         if not self.head:
-            # return None
-            return None
+            return False
 
-        # get ref to the current node and this will be update when we traverse
-        current_node = self.head
-
-        # iterate over the list while current is not None
-        while current_node:
-            # if current value matches target value
-            if current_node.get_value() == value:
-                # return True
+        # Recursive solution
+        # def search(node):
+        #   if node.get_value() == value:
+        #     return True
+        #   if not node.get_next():
+        #     return False
+        #   return search(node.get_next())
+        # return search(self.head)
+    
+        # get a reference to the node we're currently at; update this as we traverse the list
+        current = self.head
+        # check to see if we're at a valid node 
+        while current:
+            # return True if the current value we're looking at matches our target value
+            if current.get_value() == value:
                 return True
-            # update our current node to the current nodes next (increment node)
-            current_node = current_node.get_next()
-
-        # return False
+            # update our current node to the current node's next node
+            current = current.get_next()
+        # if we've gotten here, then the target node isn't in our list
         return False
+
+    def get_max(self):
+        if not self.head:
+            return None
+        # reference to the largest value we've seen so far
+        max_value = self.head.get_value()
+        # reference to our current node as we traverse the list
+        current = self.head.get_next()
+        # check to see if we're still at a valid list node
+        while current:
+            # check to see if the current value is greater than the max_value
+            if current.get_value() > max_value:
+                # if so, update our max_value variable
+                max_value = current.get_value()
+            # update the current node to the next node in the list
+            current = current.get_next()
+        return max_value

459ccf089a131ade3493dcf40f4b290f92752745
emma-rose22 <57078967+emma-rose22@users.noreply.github.com>
2020-06-08 20:10:06 -0400

    integrated new material

--- stack/stack.py
+++ stack/stack.py
@@ -77,18 +77,13 @@ class LinkedList:
 
         new_node = Node(value) # we should do this regardless if empty
 
-        if not self.head and not self.tail:
+        if not self.head:
             self.head = new_node
             self.tail = new_node
-        
-        # and not empty?
-        else:
-            #now we don't need to traverse the whole list
 
-            #set the current tail's next to the new node
-            self.tail.set_next(new_node)
-            #set self.tail to the new node
-            self.tail = new_node
+        else: #if list is not empty
+            self.tail.set_next(new_node) #changes the pointer on old tail to new value
+            self.tail = new_node #sets tail value to new value
 
     #we can directly remove this, no traversing
     #O(1)
@@ -98,54 +93,134 @@ class LinkedList:
 
         if not self.head:
             return None
-        #what if the list isn't empty?
-        else:
-            #we want to return value at current head
-            #also want to remove the value
-            #and update self.head
+        #check if head has no next value, a single-item list
+        if not self.head.get_next():
+            head = self.head
+            self.head = None
+            self.tail = None
+            return head.get_value()
 
+        #if we have a list with more than one item:
+        value = self.head.get_value()
+        #reset head with next value, return removed value
+        self.head = self.head.get_next()
+        return value
+
+    def remove_tail(self):
+        if not self.head:
+            return None
+        #if there is only one value, remove it and return it
+        if self.head is self.tail:
             value = self.head.get_value()
-
-            self.head = self.head.get_next()
-
+            self.head = None
+            self.tail = None
             return value
 
+        #given a list longer than one:
+        current = self.head
+
+        #traverse through the list
+        while current.get_next() is not self.tail:
+            current = current.get_next()
+        
+        #once we get to the tail:
+        value = self.tail.get_value()
+        #set the tail to the value right before it
+        self.tail = current
+        return value
+
+    def contains(self, value):
+        if not self.head:
+            return False
+
+        ## Recursive solution
+        # def search(node):
+        #   if node.get_value() == value:
+        #     return True
+        #   if not node.get_next():
+        #     return False
+        #   return search(node.get_next())
+        # return search(self.head)  
+
+        #traverse the list
+        current = self.head
+        while current:
+            #check to see if value matches target value
+            if current.get_value() == value:
+                return True
+            #regardless, update the current to the next node
+
+        #if we are here, the target node wasn''t in the list
+        return False
+        
+    def get_max(self):
+        if not self.head:
+            return None
+
+        #to start with, max is just the first value
+        max_value = self.head.get_value()
+        #use current to traverse
+        current = self.head.get_next()
+        while current:
+            if current > max_value:
+                max_value = current.get_value()
+            current = current.get_next()
+
+        return max_value
+        
     def add_to_head(self, value):
-      new_node = Node(value)
+        #THIS IS NOT IN THE NEW STARTER CODE
+        new_node = Node(value)
 
-      if not self.head and not self.tail:
-        self.head = new_node
-        self.tail = new_node
+        if not self.head and not self.tail:
+            self.head = new_node
+            self.tail = new_node
 
-      else:
-        new_node.set_next(self.head)
-        self.head = new_node
+        else:
+            new_node.set_next(self.head)
+            self.head = new_node
 
     def print_ll_elements(self):
-      current = self.head
+        #THIS IS NOT IN THE NEW STARTER CODE
+        current = self.head
 
-      while current is not None:
-        print(current.value)
-        current = current.get_next
+        while current is not None:
+            print(current.value)
+            current = current.get_next
 
-class Stack(LinkedList):
+
+
+#Commented out so I can redo from scratch
+
+# class Stack(LinkedList):
+#     def __init__(self):
+#         self.size = 0
+#         self.storage = LinkedList()
+#         self.head = None
+
+#     def __len__(self):
+#         return self.size
+
+#     def push(self, value):
+#         #add items
+#         self.storage.add_to_end(value)
+#         self.size += 1
+
+#     def pop(self):
+#         #remove items
+#         self.size -= 1
+#         return self.storage.remove_from_head()
+
+class Stack:
     def __init__(self):
         self.size = 0
-        self.storage = LinkedList()
-        self.head = None
 
     def __len__(self):
-        return self.size
-
+        pass
     def push(self, value):
-        #add items
-        self.storage.add_to_end(value)
-        self.size += 1
-
+        pass
     def pop(self):
-        #remove items
-        self.size -= 1
-        return self.storage.remove_from_head()
+        pass
 
 s = Stack()
 

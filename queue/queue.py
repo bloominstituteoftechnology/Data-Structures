a4c31b95922d18b7d8d64d673a386b0a6e81e1bf
Jon Haase <haase1020@gmail.com>
2020-06-09 06:20:55 -0400

    complete stack file and test passes

--- queue/queue.py
+++ queue/queue.py
@@ -1,6 +1,13 @@
 """
 A queue is a data structure whose primary purpose is to store and
 return elements in First In First Out order. 
+***Monday assignment***(Mandi inserted)
+FIFO (first in first out): lookup O(n), enqueue O(1), dequeue O(1), peek O(1)
+                1       2     3        4 (for arrays)
+                head                   tails (for linked lists)
+ex: wait list: Matt-- Joy -- Samir --Pavel
+can use arrays or linked lists to create --> SHOULD implelement them with linked lists though
+
 
 1. Implement the Queue class using an array as the underlying storage structure.
    Make sure the Queue tests pass.
@@ -13,11 +20,13 @@ return elements in First In First Out order.
 Stretch: What if you could only use instances of your Stack class to implement the Queue?
          What would that look like? How many Stacks would you need? Try it!
 """
+
+
 class Queue:
     def __init__(self):
         self.size = 0
-        # self.storage = ?
-    
+        self.storage = LinkedList()
+
     def __len__(self):
         pass
 

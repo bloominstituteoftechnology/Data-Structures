f9b6411fca253c298afc98a187de1b14a6ffe71d
Lord-Kanzler <climberalex@gmail.com>
2020-06-08 20:18:29 -0400

    finished module1

--- doubly_linked_list/doubly_linked_list.py
+++ doubly_linked_list/doubly_linked_list.py
@@ -48,25 +48,43 @@ class DoublyLinkedList:
     as the new head of the list. Don't forget to handle 
     the old head node's previous pointer accordingly."""
     def add_to_head(self, value):
-        pass
+        new_node = ListNode(value)
+        self.length += 1
+        if self.head:
+            new_node.next = self.head
+            self.head.prev = new_node
+        else:
+            self.tail = new_node
+            self.head = new_node
 
     """Removes the List's current head node, making the
     current head's next node the new head of the List.
     Returns the value of the removed Node."""
     def remove_from_head(self):
-        pass
+        val = self.head.value
+        self.delete(self.head)
+        return val
 
     """Wraps the given value in a ListNode and inserts it 
     as the new tail of the list. Don't forget to handle 
     the old tail node's next pointer accordingly."""
     def add_to_tail(self, value):
-        pass
+        new_node = ListNode(value)
+        self.length += 1
+        if self.tail:
+	        self.tail.next = new_node
+	        new_node.prev = self.tail
+        else:
+	        self.head = new_node
+	        self.tail = new_node
 
     """Removes the List's current tail node, making the 
     current tail's previous node the new tail of the List.
     Returns the value of the removed Node."""
     def remove_from_tail(self):
-        pass
+        val = self.tail.value
+        self.delete(self.tail)
+        return val
 
     """Removes the input node from its current spot in the 
     List and inserts it as the new head node of the List."""
--- queue/queue.py
+++ queue/queue.py
@@ -13,16 +13,26 @@ return elements in First In First Out order.
 Stretch: What if you could only use instances of your Stack class to implement the Queue?
          What would that look like? How many Stacks would you need? Try it!
 """
+
+import sys
+sys.path.append('../singly_linked_list')
+from singly_linked_list import LinkedList
+
 class Queue:
     def __init__(self):
         self.size = 0
-        # self.storage = ?
-    
+        self.storage = LinkedList()
+
     def __len__(self):
-        pass
+        return self.size
 
     def enqueue(self, value):
-        pass
+        self.storage.add_to_tail(value)
+        self.size += 1
 
     def dequeue(self):
-        pass
+        if self.size:
+            self.size -= 1
+            return self.storage.remove_head()
+        else:
+            return None

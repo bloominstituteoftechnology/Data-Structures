021a33a27a63eb25d6017afe069f37381abf92c0
Dimos-Christidis <christidis.dimosthenis@gmail.com>
2020-06-09 14:57:14 +0300

    answering queue question

--- queue/queue.py
+++ queue/queue.py
@@ -9,7 +9,10 @@ return elements in First In First Out order.
    Make sure the Queue tests pass.
 3. What is the difference between using an array vs. a linked list when 
    implementing a Queue?
-   
+
+   The first difference is in the array all elements are stored in a continuous memory location
+   when in the linked list new elements can be store anywhere in the memory. 
+
 Stretch: What if you could only use instances of your Stack class to implement the Queue?
          What would that look like? How many Stacks would you need? Try it!
 """

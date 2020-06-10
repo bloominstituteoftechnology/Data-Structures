ead6b595dca02d10bbafb1cb4b740b88b3faa5da
Hector <hectoremil21@hotmail.com>
2020-06-10 15:29:10 -0400

    Implement remove_from_head

--- doubly_linked_list/doubly_linked_list.py
+++ doubly_linked_list/doubly_linked_list.py
@@ -48,19 +48,38 @@ class DoublyLinkedList:
     as the new head of the list. Don't forget to handle 
     the old head node's previous pointer accordingly."""
     def add_to_head(self, value):
-        pass
+        if self.head == None:
+            self.head = ListNode(value)
+            self.tail = self.head
+            return
+        prev = self.head
+        self.head = ListNode(value, None, prev)
+        prev.prev = self.head
+        self.length += 1
 
     """Removes the List's current head node, making the
     current head's next node the new head of the List.
     Returns the value of the removed Node."""
     def remove_from_head(self):
-        pass
+        if self.head is None:
+            return None
+
+        data = self.head
+        if self.head is self.tail:
+            self.head = None
+            self.tail = None
+            return data.value
+
+        self.head = self.head.next
+        self.head.prev = None
+        return data.value
 
     """Wraps the given value in a ListNode and inserts it 
     as the new tail of the list. Don't forget to handle 
     the old tail node's next pointer accordingly."""
     def add_to_tail(self, value):
-        pass
+        if self.tail == None:
+            self.add_to_head(value)
 
     """Removes the List's current tail node, making the 
     current tail's previous node the new tail of the List.

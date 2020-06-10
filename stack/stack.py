ba5648a7a15c753fcbb6dd49309804dc428b6a9a
Mario Rivera <mario@Marios-MacBook-Pro.local>
2020-06-09 20:39:18 -0700

    finished stack module

--- stack/stack.py
+++ stack/stack.py
@@ -10,16 +10,48 @@ return elements in Last In First Out order.
 3. What is the difference between using an array vs. a linked list when 
    implementing a Stack?
 """
-class Stack:
+
+from singly_linked_list import LinkedList
+
+class StackFromArray:
     def __init__(self):
         self.size = 0
-        # self.storage = ?
+        self.storage = []
 
     def __len__(self):
-        pass
+        # return the length
+        return self.size
 
     def push(self, value):
-        pass
+        self.storage.append(value)
+        self.size += 1
 
     def pop(self):
-        pass
+        if self.size == 0:
+            return None
+        if self.size > 0:
+            self.size -= 1
+            return self.storage.pop()
+
+class LinkedListStack:
+    def __init__(self):
+        self.size = 0
+        self.storage = LinkedList()
+
+    def __len__(self):
+        return self.size
+
+    def push(self, value):
+        self.storage.add_to_tail(value)
+        self.size += 1
+
+    def pop(self):
+        if self.size > 0:
+            self.size -= 1
+            return self.storage.remove_tail()
+
+
+class Stack(StackFromArray):
+    def __init__(self):
+        super().__init__()
+        

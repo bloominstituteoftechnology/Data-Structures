a216fcb13001ebf626e0e0dc62b4c2a24614bf03
Dimos-Christidis <christidis.dimosthenis@gmail.com>
2020-06-10 16:27:11 +0300

    insert method and contains

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -17,24 +17,64 @@ class BSTNode:
 
     # Insert the given value into the tree
     def insert(self, value):
-             # insert
+        # insert
         # left case
         # check if our value is less than root value
+        if value < self.value:
             # move to the left and check if it is None
+            if self.left is None:
                 # insert node here set the self.left to the new node
+               self.left = BSTNode(value)
             # otherwise
+            else:
                 # do an insert on the root's left node recursive call to the left node using self.left
+                return self.left.insert(value)
         # right case
         # otherwise
+        else:
             # move to the right and check if it is None
+            if self.right is None:
                 # insert node here set the self.right to the new node
+               self.right = BSTNode(value)
             # otherwise
+            else:
                 # do an insert on the root's right node recursive call to insert using self.right
-        pass
+                return self.right.insert(value)
+      
 
     # Return True if the tree contains the value
     # False if it does not
     def contains(self, target):
+         # contains
+        # check the value root node (self.value) against the target
+        # if the root node value and target are the same
+        if self.value == target:
+            # return True
+            return True
+        
+        # left case
+        # check if our target is less than the root val (self.value)
+        elif target < self.value:
+            # check if there is no child to the left (self.left) is None
+            if self.left is None:
+                # return False
+                return False
+            # otherwise
+            else:
+                # call contains on the left child (self.left)
+                self.left.contains(target)
+        
+        # right case
+        # otherwise
+        else:
+            # check if there is no child to the right (self.right) is None
+            if self.right is None:
+                # return False
+                return False
+            # otherwise
+            else:
+                # call contains on the right child (self.right)
+                self.right.contains(target)
         pass
 
     # Return the maximum value found in the tree

06773f7a03bba999e8b7c9370852745d2cc54ffe
Hector <hectoremil21@hotmail.com>
2020-06-10 17:11:38 -0400

    Fix all bugs. test_doubly_linked_list.py passes all tests

--- doubly_linked_list/doubly_linked_list.py
+++ doubly_linked_list/doubly_linked_list.py
@@ -54,8 +54,8 @@ class DoublyLinkedList:
             self.tail = self.head
             return
         prev = self.head
-        self.head = ListNode(value, None, prev)
         prev.prev = self.head
+        self.head = ListNode(value, None, prev)
 
     """Removes the List's current head node, making the
     current head's next node the new head of the List.
@@ -79,16 +79,21 @@ class DoublyLinkedList:
     as the new tail of the list. Don't forget to handle 
     the old tail node's next pointer accordingly."""
     def add_to_tail(self, value):
-        self.length += 1
         if self.tail is None:
             self.add_to_head(value)
             return
 
+        self.length += 1
+        node = ListNode(value)
         if self.tail is self.head:
-            self.head.insert_after(value)
+            self.tail = node
+            self.tail.prev = self.head
+            self.head.next = self.tail
             return
 
-        self.tail.insert_after(value)
+        self.tail.next = node
+        node.prev = self.tail
+        self.tail = node
 
     """Removes the List's current tail node, making the 
     current tail's previous node the new tail of the List.
@@ -135,11 +140,11 @@ class DoublyLinkedList:
     """Removes the input node from its current spot in the 
     List and inserts it as the new tail node of the List."""
     def move_to_end(self, node):
-        # Assuming the node has a prev and a next, link them together skipping this node
-        if node.prev is not None:
-            node.prev.next = node.next
-        if node.next is not None:
-            node.next.prev = node.prev
+        # # Assuming the node has a prev and a next, link them together skipping this node
+        # if node.prev is not None:
+        #     node.prev.next = node.next
+        # if node.next is not None:
+        #     node.next.prev = node.prev
 
         if self.tail is None:
             self.head = node
@@ -151,6 +156,9 @@ class DoublyLinkedList:
             self.head.next = self.tail
             self.tail.prev = self.head
             return
+
+        if node is self.head:
+            self.head = node.next
 
         node.prev = self.tail
         self.tail.next = node

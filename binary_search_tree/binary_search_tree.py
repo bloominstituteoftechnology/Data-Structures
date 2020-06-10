eca98984f4c5b5daa6eec112dc22172049da9566
Alex Pakalniskis <alexpakalniskis3@gmail.com>
2020-06-10 14:58:52 -0700

    feat: complete part 1 methods

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -17,20 +17,85 @@ class BSTNode:
 
     # Insert the given value into the tree
     def insert(self, value):
-        pass
+        # compare the value to the root's value to determine which direction
+        # we're gonna go in 
+        # if the value < root's value 
+        if value < self.value:
+            # go left 
+            # how do we go left?
+            # we have to check if there is another node on the left side
+            if self.left: 
+                # then self.left is a Node 
+                # now what?
+                self.left.insert(value)
+            else:
+                # then we can park the value here
+                self.left = BSTNode(value)
+        # else the value >= root's value 
+        else:
+            # go right
+            # how do we go right? 
+            # we have to check if there is another node on the right side 
+            if self.right:
+                # then self.right is a Node 
+                self.right.insert(value)
+            else:
+                self.right = BSTNode(value)
 
     # Return True if the tree contains the value
     # False if it does not
     def contains(self, target):
-        pass
+        # Base case: Node is equal to target
+        if target == self.value:
+            return True
+        
+        # Check left-hand side
+        elif target < self.value:
+            # Subcase: Nothing to the left of self.value
+            # Therefore, target does not exist in tree
+            if self.left == None:
+                return False
+            # Subcase: There are nodes to the left of self.value
+            # Recursively check left nodes
+            else:
+                return self.left.contains(target)
+
+        # Check right-hand side
+        else:
+            # Subcase: Nothing to the right of self.value
+            # Therefore, target does not exist in tree
+            if self.right == None:
+                return False
+            # Subcase: There are nodes to the right of self.value
+            # Recursively check right nodes
+            else:
+                return self.right.contains(target)
+
 
     # Return the maximum value found in the tree
     def get_max(self):
-        pass
+        # If node is the maximum value in the tree
+        if self.right == None:
+            return self.value
+        # If the node is not the maximum value in the tree
+        else:
+            return self.right.get_max()
+        
+
 
     # Call the function `fn` on the value of each node
     def for_each(self, fn):
-        pass
+        # Apply fn to root node
+        fn(self.value)
+        
+        # Recursively apply fn to left-side nodes, if they exist
+        if self.left:
+            self.left.for_each(fn)
+        
+        # Recursively apply fn to right-side nodes, if they exist
+        if self.right:
+            self.right.for_each(fn)
+
 
     # Part 2 -----------------------
 

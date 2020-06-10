282b3da4b93dfcb9e69da1bca600cd62712e17da
Hector <hectoremil21@hotmail.com>
2020-06-10 10:53:18 -0400

    Create copy of linked list into queue folder. Implement queue using SinglyLinkedList

--- queue/queue.py
+++ queue/queue.py
@@ -13,32 +13,40 @@ return elements in First In First Out order.
 Stretch: What if you could only use instances of your Stack class to implement the Queue?
          What would that look like? How many Stacks would you need? Try it!
 """
+# class Queue:
+#     def __init__(self):
+#         self.size = 0
+#         self.storage = []
+#
+#     def __len__(self):
+#         return self.size
+#
+#     def enqueue(self, value):
+#         self.size += 1
+#         self.storage.append(value)
+#
+#     def dequeue(self):
+#         if self.size <= 0:
+#             return None
+#         self.size -= 1
+#         return self.storage.pop(0)
+
+from singly_linked_list import *
+
 class Queue:
     def __init__(self):
         self.size = 0
-        self.storage = []
+        self.storage = LinkedList()
 
     def __len__(self):
         return self.size
 
-    def enqueue(self, value):
+    def enqueue(self,value):
         self.size += 1
-        self.storage.append(value)
+        self.storage.add_to_tail(value)
 
     def dequeue(self):
         if self.size <= 0:
             return None
         self.size -= 1
-        return self.storage.pop(0)
-
-# from singly_linked_list import *
-#
-# class Queue:
-#     def __init__(self):
-#         self.size = 0
-#         self.storage = LinkedList()
-#     def __len__(self):
-#         return self.size
-#     def enqueue(self,value):
-#
-#     def dequeue(self):
+        return self.storage.remove_head()

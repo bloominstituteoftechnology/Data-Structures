0c0c5c68f4b32693431ffcd6e5759fd8d7b5f971
Mark Gerrior <github@gerrior.com>
2020-06-10 12:25:00 -0700

    2a. Re-implemented the Stack class, this time using the linked list implementation without last_node

--- stack/stack.py
+++ stack/stack.py
@@ -36,11 +36,58 @@ class Node:
         self.value = value
         self.next_node = next_node 
 
+# class Stack:
+#     def __init__(self):
+#         self.size = 0
+#         self.first_node = None
+#         self.last_node = None
+
+#     def __len__(self):
+#         return self.size
+
+#     def push(self, value):
+#         self.size += 1
+#         new_node = Node(value)
+
+#         if self.first_node == None:
+#             # First element 
+#             self.first_node = new_node
+#             self.last_node = new_node
+#             return 
+
+#         self.last_node.next_node = new_node
+#         self.last_node = new_node
+
+#     def pop(self):
+#         if self.size == 0:
+#             return None
+
+#         node_to_pop = self.last_node
+#         current_node = self.first_node
+
+#         if node_to_pop != current_node:
+#             while current_node.next_node != node_to_pop:
+#                 current_node = current_node.next_node
+
+#         current_node.next_node = None
+#         self.last_node = current_node
+#         self.size -= 1
+
+#         if self.size == 0:
+#             self.first_node = None
+
+#         return node_to_pop.value
+
+# 2a. Re-implement the Stack class, this time using the linked list implementation without last_node
+class Node: 
+    def __init__(self, value = None, next_node = None ):
+        self.value = value
+        self.next_node = next_node 
+
 class Stack:
     def __init__(self):
         self.size = 0
         self.first_node = None
-        self.last_node = None
 
     def __len__(self):
         return self.size
@@ -52,28 +99,38 @@ class Stack:
         if self.first_node == None:
             # First element 
             self.first_node = new_node
-            self.last_node = new_node
             return 
 
-        self.last_node.next_node = new_node
-        self.last_node = new_node
+        current_node = self.first_node
+
+        while current_node.next_node != None:
+            current_node = current_node.next_node
+
+        current_node.next_node = new_node
 
     def pop(self):
         if self.size == 0:
             return None
 
-        node_to_pop = self.last_node
         current_node = self.first_node
 
-        if node_to_pop != current_node:
-            while current_node.next_node != node_to_pop:
-                current_node = current_node.next_node
+        if self.first_node.next_node == None:
+            self.size = 0
+            self.first_node = None
+            return current_node.value
 
-        current_node.next_node = None
-        self.last_node = current_node
+        prior_node   = self.first_node
+        current_node = self.first_node.next_node
+
+        while current_node.next_node != None:
+            new_prior_node = current_node
+            current_node = current_node.next_node
+            prior_node = new_prior_node
+
+        prior_node.next_node = None
         self.size -= 1
 
         if self.size == 0:
             self.first_node = None
 
-        return node_to_pop.value
+        return current_node.value

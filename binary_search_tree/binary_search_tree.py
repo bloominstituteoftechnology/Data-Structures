90aaea50f913223747615c8ef8eda1a34aafc7f5
Doina Langille <doina.jitoreanu@gmail.com>
2020-06-10 15:43:27 -0400

    day 3 mvp

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -17,20 +17,65 @@ class BSTNode:
 
     # Insert the given value into the tree
     def insert(self, value):
-        pass
+        if value < self.value:
+            if not self.left:
+                self.left = BSTNode(value)
+            else:
+                self.left.insert(value)
+        else:
+            if not self.right:
+                self.right = BSTNode(value)
+            else:
+                self.right.insert(value)
 
     # Return True if the tree contains the value
     # False if it does not
     def contains(self, target):
-        pass
+        # value == target
+        if self.value == target:
+            return True
+        # value != target
+        else:
+            # is the target lower than the value?
+            if target < self.value:
+                # is there any child node?
+                if not self.left:
+                    return False
+                # is the left child node our target?
+                if self.left.value == target:
+                    return True
+                else:
+                    self.left.contains(target)
+            else: # target >= self.value
+                # is there any child node?
+                if not self.right:
+                    return False
+                # is the right child node our target?
+                if self.right.value == target:
+                    return True
+                else:
+                    self.right.contains(target)
 
     # Return the maximum value found in the tree
     def get_max(self):
-        pass
+        # self.left.value will always ne smaller than the root
+        # so we have to check only the self.right path
+        if not self.right:
+            return self.value
+        else:
+            return self.right.get_max()
+
 
     # Call the function `fn` on the value of each node
     def for_each(self, fn):
-        pass
+        # call the function `fn`
+        fn(self.value)
+        # go to the left node if any
+        if self.left:
+            self.left.for_each(fn)
+        # go to the right node if any
+        if self.right:
+            self.right.for_each(fn)
 
     # Part 2 -----------------------
 
--- doubly_linked_list/doubly_linked_list.py
+++ doubly_linked_list/doubly_linked_list.py
@@ -51,34 +51,27 @@ class DoublyLinkedList:
     def add_to_head(self, value):
         # create a new node
         new_node = ListNode(value, None, None)
-        # check if the DLL is empty
+        # increment the length
+        self.length += 1
+        # if the DLL is empty
         if not self.head and not self.tail:
             self.head = new_node
             self.tail = new_node
+        # if the DLL is not empty
         else:
             new_node.next = self.head
             self.head.prev = new_node
             self.head = new_node
-        self.length += 1
-
+        
     """Removes the List's current head node, making the
     current head's next node the new head of the List.
     Returns the value of the removed Node."""
     def remove_from_head(self):
-        # If there is no node
-        if not self.head:
-            return None
         # Get the value
         value = self.head.value
-        # If only 1 node
-        if self.head == self.tail:
-            self.head = None
-            self.tail = None
-        # if more than 1 node
-        else:
-            self.delete(self.head)
-        self.length -= 1
-        return value      
+        # delete the node
+        self.delete(self.head)
+        return value
         
     """Wraps the given value in a ListNode and inserts it 
     as the new tail of the list. Don't forget to handle 
@@ -86,32 +79,26 @@ class DoublyLinkedList:
     def add_to_tail(self, value):
         # create a new node
         new_node = ListNode(value, None, None)
-        # Check if DLL is empty
-        if not self.tail:
-            self.head = new_node
-        else:
-            self.tail.next = new_node
-            new_node.prev = self.tail
-        self.tail = new_node
+        # increment the length of DLL
         self.length += 1
+        # if DLL is empty
+        if not self.head and not self.tail:
+            self.head = new_node
+            self.tail = new_node
+        # if DLL is not empty
+        else:
+            new_node.prev = self.tail
+            self.tail.next = new_node
+            self.tail = new_node
 
     """Removes the List's current tail node, making the 
     current tail's previous node the new tail of the List.
     Returns the value of the removed Node."""
     def remove_from_tail(self):
-        # if there is no node
-        if not self.tail:
-            return None
         # Get the value
         value = self.tail.value
-        # If only 1 node
-        if self.head == self.tail:
-            self.head = None
-            self.tail = None
-        # if more than 1 node
-        else:
-            self.delete(self.tail)
-        self.length -= 1
+        # delete the node
+        self.delete(self.tail)
         return value
 
     """Removes the input node from its current spot in the 
@@ -133,7 +120,7 @@ class DoublyLinkedList:
     the node was the head or the tail"""
     def delete(self, node):
         # if list is empty
-        if not self.head:
+        if not self.head and not self.tail:
             return None
         # if 1 node
         if self.head==self.tail:

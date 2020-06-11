0af289c0973db4767d252e3fef89988d7870fdd4
Christopher Hernandez <cerberean661@gmail.com>
2020-06-11 06:24:37 -0700

    working on binary, nearly complete, exploring errors

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -9,6 +9,12 @@ This part of the project comprises two days:
 2. Implement the `in_order_print`, `bft_print`, and `dft_print` methods
    on the BSTNode class.
 """
+import sys
+sys.path.append('../queue')
+from queue import Queue
+sys.path.append('../stack')
+from stack import Stack
+
 class BSTNode:
     def __init__(self, value):
         self.value = value
@@ -17,37 +23,96 @@ class BSTNode:
 
     # Insert the given value into the tree
     def insert(self, value):
-        pass
+        current = self
+        while True:
+            if value < current.value:
+                if current.left:
+                    current = current.left
+                else:
+                    current.left = BSTNode(value)
+                    break
+            else:
+                if current.right:
+                    current = current.right
+                else:
+                    current.right = BSTNode(value)
+                    break
 
     # Return True if the tree contains the value
     # False if it does not
     def contains(self, target):
-        pass
+        current = self
+        while True:
+            if target == current.value:
+                return True
+            if target < current.value:
+                if current.left:
+                    current = current.left
+                else:
+                    return False  
+            else:
+                if current.right:
+                    current = current.right
+                else:
+                    return False
 
     # Return the maximum value found in the tree
     def get_max(self):
-        pass
+        current = self
+        max_value = self.value
+        while True:
+            if current.right:
+                current = current.right
+                max_value = current.value
+            else:
+                return max_value
 
     # Call the function `fn` on the value of each node
     def for_each(self, fn):
-        pass
+        fn(self.value)
+        if self.left:
+            self.left.for_each(fn)
+        if self.right:
+            self.right.for_each(fn)
 
     # Part 2 -----------------------
 
     # Print all the values in order from low to high
     # Hint:  Use a recursive, depth first traversal
     def in_order_print(self, node):
-        pass
+        current = node
+        if current.left:
+            self.in_order_print(current.left)
+        print(current.value)
+        if current.right:
+            self.in_order_print(current.right)
 
     # Print the value of every node, starting with the given node,
     # in an iterative breadth first traversal
     def bft_print(self, node):
-        pass
+        queue = Queue()
+        queue.enqueue(node)
+        while queue.len() > 0:
+            popped = queue.dequeue()
+            print(popped.value)
+            if popped.left:
+                queue.enqueue(popped.left)
+            if popped.right:
+                queue.enqueue(popped.right)
 
     # Print the value of every node, starting with the given node,
     # in an iterative depth first traversal
     def dft_print(self, node):
-        pass
+        current = node
+        stack = Stack()
+        stack.push(current)
+        while stack.len() > 0:
+            popped = stack.pop()
+            print(popped.value)
+            if popped.right:
+                stack.push(popped.right)
+            if popped.left:
+                stack.push(popped.left)
 
     # Stretch Goals -------------------------
     # Note: Research may be required

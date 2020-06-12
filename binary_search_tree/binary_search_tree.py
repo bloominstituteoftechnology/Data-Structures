e904cf0830228cd4fd1dbc385b68d8cae77d3b86
Wyatt Harrell <wyattaharrell@gmail.com>
2020-06-11 21:32:58 -0400

    Day 4 MVP

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -9,6 +9,8 @@ This part of the project comprises two days:
 2. Implement the `in_order_print`, `bft_print`, and `dft_print` methods
    on the BSTNode class.
 """
+
+from collections import deque
 class BSTNode:
     def __init__(self, value):
         self.value = value
@@ -97,18 +99,40 @@ class BSTNode:
 
     # Print all the values in order from low to high
     # Hint:  Use a recursive, depth first traversal
-    def in_order_print(self, node):
-        pass
+    def in_order_print(self, node=None):
+        if node:
+            self.in_order_print(node.left)
+            print(node.value)
+            self.in_order_print(node.right)
 
-    # Print the value of every node, starting with the given node,
+            # Print the value of every node, starting with the given node,
     # in an iterative breadth first traversal
     def bft_print(self, node):
-        pass
+        q = deque()
 
-    # Print the value of every node, starting with the given node,
+        q.append(self)
+        while len(q) > 0:
+            current = q.popleft()
+            print(current.value)
+            if current.left:
+                q.append(current.left)
+            if current.right:
+                q.append(current.right)
+
+
+            # Print the value of every node, starting with the given node,
     # in an iterative depth first traversal
     def dft_print(self, node):
-        pass
+        stack = []
+
+        stack.append(self)
+        while len(stack) > 0:
+            current = stack.pop()
+            print(current.value)
+            if current.right:
+                stack.append(current.right)
+            if current.left:
+                stack.append(current.left)
 
     # Stretch Goals -------------------------
     # Note: Research may be required

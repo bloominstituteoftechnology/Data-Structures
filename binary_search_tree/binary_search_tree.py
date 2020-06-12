1a8395f96a7701376f14eba3909146497aa906c0
Mario Rivera <mario@Marios-MacBook-Pro.local>
2020-06-12 08:00:25 -0700

    working on DLL #7

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -9,6 +9,7 @@ This part of the project comprises two days:
 2. Implement the `in_order_print`, `bft_print`, and `dft_print` methods
    on the BSTNode class.
 """
+
 class BSTNode:
     def __init__(self, value):
         self.value = value
@@ -17,20 +18,58 @@ class BSTNode:
 
     # Insert the given value into the tree
     def insert(self, value):
-        pass
+        if value < self.value:
+            # checks for value on left
+            if not self.left:
+                self.left = BSTNode(value)
+            # Inserts on left
+            else:
+                self.left.insert(value)
+        else:
+            # checks for right node and sets it
+            if not self.right:
+                self.right = BSTNode(value)
+            else:
+                # inserts on the right
+                self.right.insert(value)
 
     # Return True if the tree contains the value
     # False if it does not
     def contains(self, target):
-        pass
+        if self.value === target:
+            return True
+        elif target < self.value:
+            if self.left:
+                # checks the contents
+                return self.left.contains(target)
+        else:
+            if self.right:
+                # checks the contents
+                return self.right.contains(target)
+        #is not contained
+        return False
+
 
     # Return the maximum value found in the tree
     def get_max(self):
-        pass
+        current_node = self
+        # will rewrite current node right
+        while current_node.right:
+            current_node = current_node.right
+        return current_node.value
+
+        # if self.right is None:
+        #     return self.value
+        # else:
+        #     return self.right.get_max()
 
     # Call the function `fn` on the value of each node
     def for_each(self, fn):
-        pass
+        fn(self.value)
+        if self.left:
+            self.left.for_each(fn)
+        if self.right:
+            self.right.for_each(fn)
 
     # Part 2 -----------------------
 

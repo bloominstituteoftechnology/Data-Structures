9560b8dd325952670750dd017d505478f6b29ef7
Ryan Sizemore <58410246+mastaryan@users.noreply.github.com>
2020-06-16 23:08:14 -0400

    Stack.py completed and Data_Structures_Questions for stack and queue answered

--- Data_Structures_Questions.md
+++ Data_Structures_Questions.md
@@ -2,31 +2,31 @@ Answer the following questions for each of the data structures you implemented a
 
 ## Stack
 
-1. What is the runtime complexity of `push` using a list?
+1. What is the runtime complexity of `push` using a list? O(1)
 
-2. What is the runtime complexity of `push` using a linked list?
+2. What is the runtime complexity of `push` using a linked list? O(1)
 
-3. What is the runtime complexity of `pop` using a list?
+3. What is the runtime complexity of `pop` using a list? O(1)
 
-4. What is the runtime complexity of `pop` using a linked list?
+4. What is the runtime complexity of `pop` using a linked list? O(n)
 
-5. What is the runtime complexity of `len` using a list?
+5. What is the runtime complexity of `len` using a list? O(1)
 
-6. What is the runtime complexity of `len` using a linked list?
+6. What is the runtime complexity of `len` using a linked list? O(1)
 
 ## Queue
 
-1. What is the runtime complexity of `enqueue` using a list?
+1. What is the runtime complexity of `enqueue` using a list? O(1)
 
-2. What is the runtime complexity of `enqueue` using a linked list?
+2. What is the runtime complexity of `enqueue` using a linked list? O(1)
 
-3. What is the runtime complexity of `dequeue` using a list?
+3. What is the runtime complexity of `dequeue` using a list? O(1)
 
-4. What is the runtime complexity of `dequeue` using a linked list?
+4. What is the runtime complexity of `dequeue` using a linked list? O(1)
 
-5. What is the runtime complexity of `len` using a list?
+5. What is the runtime complexity of `len` using a list? O(1)
 
-6. What is the runtime complexity of `len` using a linked list?
+6. What is the runtime complexity of `len` using a linked list? O(1)
 
 ## Doubly Linked List
 
--- stack/stack.py
+++ stack/stack.py
@@ -10,16 +10,72 @@ return elements in Last In First Out order.
 3. What is the difference between using an array vs. a linked list when 
    implementing a Stack?
 """
+
+#Linked List Implementation
+class Node:
+    def __init__(self, data):
+       self.data = data
+       self.next = None
+
 class Stack:
     def __init__(self):
+        # Array Implementation
+        # self.storage = []
+
+        # Linked List Implementation
         self.size = 0
-        # self.storage = ?
+        self.head = None
+        self.tail = None
 
     def __len__(self):
-        pass
+        # Array Implementation
+        # return len(self.storage)
+
+        # Linked List Implementation
+        # Start at the head and add one to the count until you reach the tail (.next is None)
+        return self.size
 
     def push(self, value):
-        pass
+        # Array Implementation
+        # self.storage.append(value)
+
+        # Linked List Implementation
+        new_node = Node(value)
+        # If the list is empty, set the head node and it's .next value to be the value
+        if self.head is None:
+            self.head = new_node
+            self.head.next = new_node
+            self.tail = new_node
+        # Otherwise, if there are nodes, change the current tail's .next value from None to the value
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+
+        self.size += 1
 
     def pop(self):
-        pass
+        # Array Implementation
+        # if len(self.storage) == 0:
+        #     return None
+        # else:
+        #     value = self.storage.pop()
+        #     return value
+        
+        # Linked List Implementation
+        if self.head is None:
+            return None
+        elif self.head.next is None:
+            removed = self.head
+            self.head = None
+            self.size -= 1
+            return removed.data
+        else:
+            new_last = self.head
+            # While there is at least two elements after the current Node, keep moving through the loop since it isn't the next to last Node
+            while new_last.next.next:
+                new_last = new_last.next
+
+            removed = new_last.next
+            new_last.next = None
+            self.size -= 1
+            return removed.data

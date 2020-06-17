18d671b8923f47881b9c16c38e5c5287cf4e26cf
Michael Lowe <mrlowememphis@yahoo.com>
2020-06-17 18:22:04 -0500

    stack implemented using list array

--- stack/stack.py
+++ stack/stack.py
@@ -14,12 +14,19 @@ class Stack:
     def __init__(self):
         self.size = 0
         # self.storage = ?
+        self.storage = []
 
     def __len__(self):
-        pass
+        return self.size
 
     def push(self, value):
-        pass
+        self.storage.append(value)
+        self.size += 1
 
     def pop(self):
-        pass
+        if self.size == 0:
+            return 
+        else:
+             self.size =-1
+             return self.storage.pop (-1)
+            

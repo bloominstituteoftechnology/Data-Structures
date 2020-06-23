eabcb72d9092da5f6fd5c37e366318e5d8c3ac15
rfamilara <techknohauz.com>
2020-06-23 07:39:13 -0500

    Part 1 BST Project -Completed -Tested Automation successfully

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -17,20 +17,68 @@ class BSTNode:
 
     # Insert the given value into the tree
     def insert(self, value):
-        pass
+        # value is less than the root go left
+        if value < self.value:
+            # if there is no value on the left side
+            if not self.left:
+                # value becomes the root of the new subtree
+                self.left = BSTNode(value)
+            else:
+                # insert the the value
+                self.left.insert(value)
+
+        else:
+            # go right
+            # if there is no value on the right side
+            if not self.right:
+                # value becomes the root of the new subtree
+                self.right = BSTNode(value)
+            else:
+                # insert the the value
+                self.right.insert(value)
 
     # Return True if the tree contains the value
     # False if it does not
     def contains(self, target):
-        pass
+        # if root equals target
+        if self.value == target:
+            # return True
+            return True
+    # Check on the left side
+        if self.left:
+            # if left side contains target
+            if self.left.contains(target):
+                # return True
+                return True
+    # Check on the right side
+        if self.right:
+            # if right side conatins target
+            if self.right.contains(target):
+                # return True
+                return True
+    # otherwise, return False
+        return False
 
     # Return the maximum value found in the tree
     def get_max(self):
-        pass
+        # check if there is no node on the right
+        if not self.right:
+            # return self.value
+            return self.value
+        # keep moving to the right for as long as you can
+        # then return the node at the end of it all
+        return self.right.get_max()
 
     # Call the function `fn` on the value of each node
     def for_each(self, fn):
-        pass
+        fn(self.value)
+
+        if self.left:
+            self.left.for_each(fn)
+
+        if self.right:
+            self.right.for_each(fn)    
+
 
     # Part 2 -----------------------
 

39addb705ad26aa41681ce7e61d099de926a1a3b
Joseph Thompson <joe.thompson838@gmail.com>
2020-07-01 15:00:54 -0400

    completed stack with use of singly linked list

---
+++ singly_linked_list/singly_linked_list.py
@@ -0,0 +1,106 @@
+class Node:
+    def __init__(self, data, next=None):
+        self.data = data
+        self.next = next
+
+    def __str__(self):
+        return (f'{self.data}')
+
+    def get_value(self):
+        return self.data
+
+    def get_next(self):
+        return self.next
+
+    def set_next(self, updated_next):
+        self.next = updated_next
+
+
+# node = Node(1)
+# node.set_next(Node(2))
+# node.get_next().set_next(Node(3))
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def len(self):
+        count = 0
+        current_node = self.storage.get_head()
+        while True:
+            if current_node is None:
+                return 0
+            else:
+                count += 1
+                current_node = current_node.get_next()
+        return count
+
+    def get_tail(self):
+        return self.tail
+
+    def get_head(self):
+        return self.head
+
+    def add_to_tail(self, data):
+        new_node = Node(data)
+
+        if not self.head and not self.tail:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.set_next(new_node)
+            self.tail = new_node
+
+    def add_to_head(self, data):
+        new_node = Node(data)
+
+        if self.head is None and self.tail is None:
+            self.head = new_node
+            self.tail = new_node
+
+        else:
+            new_node.set_next(self.head)
+            self.head = new_node
+
+    def remove_head(self):
+        if self.head is None:
+            return None
+        old_head = self.head
+
+        if self.head is self.tail:
+            self.head = None
+            self.tail = None
+        else:
+            self.head = old_head.get_next()
+
+        return old_head.get_value()
+
+    def contains(self, data):
+        if self.head is None:
+            return False
+
+        current_node = self.head
+
+        while current_node is not None:
+            if current_node.data == data:
+                return True
+
+            current_node = current_node.get_next()
+        return False
+
+    def get_max(self):
+        if self.head is None:
+            return None
+
+        max = self.head.get_value()
+        current_node = self.head
+
+        while current_node is not None:
+            if current_node.get_value() > max:
+                max = current_node.get_value()
+
+            else:
+                current_node = current_node.get_next()
+
+        return max
--- singly_linked_list/test_singly_linked_list.py
+++ singly_linked_list/test_singly_linked_list.py
@@ -1,17 +1,18 @@
 import unittest
 from singly_linked_list import LinkedList
 
+
 class LinkedListTests(unittest.TestCase):
     def setUp(self):
         self.list = LinkedList()
 
     def test_add_to_tail(self):
         self.list.add_to_tail(1)
-        self.assertEqual(self.list.tail.value, 1)
-        self.assertEqual(self.list.head.value, 1)
+        self.assertEqual(self.list.tail.data, 1)
+        self.assertEqual(self.list.head.data, 1)
         self.list.add_to_tail(2)
-        self.assertEqual(self.list.tail.value, 2)
-        self.assertEqual(self.list.head.value, 1)
+        self.assertEqual(self.list.tail.data, 2)
+        self.assertEqual(self.list.head.data, 1)
 
     def test_contains(self):
         self.list.add_to_tail(1)
@@ -30,8 +31,8 @@ class LinkedListTests(unittest.TestCase):
         self.assertEqual(self.list.remove_head(), 20)
         self.assertFalse(self.list.contains(20))
 
-        self.list.add_to_tail(10)    
-        self.assertEqual(self.list.remove_head(), 10)    
+        self.list.add_to_tail(10)
+        self.assertEqual(self.list.remove_head(), 10)
         self.assertIsNone(self.list.head)
         self.assertIsNone(self.list.tail)
         self.assertIsNone(self.list.remove_head())
@@ -44,6 +45,7 @@ class LinkedListTests(unittest.TestCase):
         self.assertEqual(self.list.get_max(), 100)
         self.list.add_to_tail(101)
         self.assertEqual(self.list.get_max(), 101)
+
 
 if __name__ == '__main__':
     unittest.main()

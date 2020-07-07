14b675438f9c6e4726a5c8dc489309bbcc689fcc
VodeniZeko <salexy.es@gmail.com>
2020-07-07 15:56:51 -0700

    queue-aray_solution

--- queue/queue.py
+++ queue/queue.py
@@ -20,13 +20,18 @@ Stretch: What if you could only use instances of your Stack class to implement t
 class Queue:
     def __init__(self):
         self.size = 0
-        # self.storage = ?
+        self.storage = []
 
     def __len__(self):
-        pass
+        return self.size
 
     def enqueue(self, value):
-        pass
+        self.storage.append(value)
+        self.size += 1
 
     def dequeue(self):
-        pass
+        if self.size > 0:
+            self.size -= 1
+            return self.storage.pop(0)
+        else:
+            return None

4a6199f0ebc46919264610e6f0a9ab4b799d9636
Sean Chen <skypemaster007@gmail.com>
2020-07-09 16:48:35 -0700

    Remove unused DLL ListNode methods

--- doubly_linked_list/doubly_linked_list.py
+++ doubly_linked_list/doubly_linked_list.py
@@ -1,40 +1,17 @@
-"""Each ListNode holds a reference to its previous node
-as well as its next node in the List."""
+"""
+Each ListNode holds a reference to its previous node
+as well as its next node in the List.
+"""
 class ListNode:
     def __init__(self, value, prev=None, next=None):
-        self.value = value
         self.prev = prev
+        self.value = value
         self.next = next
-
-    """Wrap the given value in a ListNode and insert it
-    after this node. Note that this node could already
-    have a next node it is point to."""
-    def insert_after(self, value):
-        current_next = self.next
-        self.next = ListNode(value, self, current_next)
-        if current_next:
-            current_next.prev = self.next
-
-    """Wrap the given value in a ListNode and insert it
-    before this node. Note that this node could already
-    have a previous node it is point to."""
-    def insert_before(self, value):
-        current_prev = self.prev
-        self.prev = ListNode(value, current_prev, self)
-        if current_prev:
-            current_prev.next = self.prev
-
-    """Rearranges this ListNode's previous and next pointers
-    accordingly, effectively deleting this ListNode."""
-    def delete(self):
-        if self.prev:
-            self.prev.next = self.next
-        if self.next:
-            self.next.prev = self.prev
-
-
-"""Our doubly-linked list class. It holds references to
-the list's head and tail nodes."""
+            
+"""
+Our doubly-linked list class. It holds references to 
+the list's head and tail nodes.
+"""
 class DoublyLinkedList:
     def __init__(self, node=None):
         self.head = node
@@ -43,46 +20,63 @@ class DoublyLinkedList:
 
     def __len__(self):
         return self.length
-
-    """Wraps the given value in a ListNode and inserts it 
+    
+    """
+    Wraps the given value in a ListNode and inserts it 
     as the new head of the list. Don't forget to handle 
-    the old head node's previous pointer accordingly."""
+    the old head node's previous pointer accordingly.
+    """
     def add_to_head(self, value):
         pass
-
-    """Removes the List's current head node, making the
+        
+    """
+    Removes the List's current head node, making the
     current head's next node the new head of the List.
-    Returns the value of the removed Node."""
+    Returns the value of the removed Node.
+    """
     def remove_from_head(self):
         pass
-
-    """Wraps the given value in a ListNode and inserts it 
+            
+    """
+    Wraps the given value in a ListNode and inserts it 
     as the new tail of the list. Don't forget to handle 
-    the old tail node's next pointer accordingly."""
+    the old tail node's next pointer accordingly.
+    """
     def add_to_tail(self, value):
         pass
-
-    """Removes the List's current tail node, making the 
+            
+    """
+    Removes the List's current tail node, making the 
     current tail's previous node the new tail of the List.
-    Returns the value of the removed Node."""
+    Returns the value of the removed Node.
+    """
     def remove_from_tail(self):
         pass
-
-    """Removes the input node from its current spot in the 
-    List and inserts it as the new head node of the List."""
+            
+    """
+    Removes the input node from its current spot in the 
+    List and inserts it as the new head node of the List.
+    """
     def move_to_front(self, node):
         pass
-
-    """Removes the input node from its current spot in the 
-    List and inserts it as the new tail node of the List."""
+        
+    """
+    Removes the input node from its current spot in the 
+    List and inserts it as the new tail node of the List.
+    """
     def move_to_end(self, node):
         pass
 
-    """Removes a node from the list and handles cases where
-    the node was the head or the tail"""
+    """
+    Deletes the input node from the List, preserving the 
+    order of the other elements of the List.
+    """
     def delete(self, node):
         pass
-        
-    """Returns the highest value currently in the list"""
+
+    """
+    Finds and returns the maximum value of all the nodes 
+    in the List.
+    """
     def get_max(self):
         pass
--- doubly_linked_list/test_doubly_linked_list.py
+++ doubly_linked_list/test_doubly_linked_list.py
@@ -2,7 +2,6 @@ import unittest
 from doubly_linked_list import ListNode
 from doubly_linked_list import DoublyLinkedList
 
-
 class DoublyLinkedListTests(unittest.TestCase):
     def setUp(self):
         self.node = ListNode(1)
@@ -58,25 +57,6 @@ class DoublyLinkedListTests(unittest.TestCase):
         self.assertEqual(self.dll.tail.value, 20)
         self.assertEqual(len(self.dll), 3)
 
-    def test_node_delete(self):
-        node_1 = ListNode(3)
-        node_2 = ListNode(4)
-        node_3 = ListNode(5)
-
-        node_1.next = node_2
-        node_2.next = node_3
-        node_2.prev = node_1
-        node_3.prev = node_2
-
-        node_2.delete()
-
-        self.assertEqual(node_1.next, node_3)
-        self.assertEqual(node_3.prev, node_1)
-
-    def test_node_insert_before(self):
-        self.node.insert_before(0)
-        self.assertEqual(self.node.prev.value, 0)
-
     def test_list_add_to_head(self):
         self.assertEqual(self.dll.head.value, 1)
 
@@ -84,10 +64,6 @@ class DoublyLinkedListTests(unittest.TestCase):
         self.assertEqual(self.dll.head.value, 10)
         self.assertEqual(self.dll.head.next.value, 1)
         self.assertEqual(len(self.dll), 2)
-
-    def test_node_insert_after(self):
-        self.node.insert_after(2)
-        self.assertEqual(self.node.next.value, 2)
 
     def test_list_move_to_end(self):
         self.dll.add_to_head(40)
@@ -149,7 +125,6 @@ class DoublyLinkedListTests(unittest.TestCase):
         self.assertEqual(self.dll.get_max(), 100)
         self.dll.add_to_tail(101)
         self.assertEqual(self.dll.get_max(), 101)
-
 
 if __name__ == '__main__':
     unittest.main()

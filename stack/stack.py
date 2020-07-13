3cddd697e3f5a31d4d50f5b4a0a891685308e6b2
wesley.moody <wgm3190@gmail.com>
2020-07-13 16:25:57 -0500

    good to go on queue tests passed

--- stack/stack.py
+++ stack/stack.py
@@ -3,12 +3,12 @@ A stack is a data structure whose primary purpose is to store and
 return elements in Last In First Out order. 
 
 1. Implement the Stack class using an array as the underlying storage structure.
-   Make sure the Stack tests pass.
+    Make sure the Stack tests pass.
 2. Re-implement the Stack class, this time using the linked list implementation
-   as the underlying storage structure.
-   Make sure the Stack tests pass.
+    as the underlying storage structure.
+    Make sure the Stack tests pass.
 3. What is the difference between using an array vs. a linked list when 
-   implementing a Stack?
+    implementing a Stack?
 """
 class Stack:
     def __init__(self):

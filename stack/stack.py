c57dcf68f1ac6ba2c68ef44c4790175c64377c5c
Aaron Watkins <61791690+ekselan@users.noreply.github.com>
2020-07-13 15:55:51 -0400

    Array implementation for Stack class, beign testing

--- stack/stack.py
+++ stack/stack.py
@@ -1,3 +1,7 @@
+# Implement the Stack and Queue classes using built-in Python lists 
+# and the Node and LinkedList classes you created during the 
+# Module 1 Guided Project.
+
 """
 A stack is a data structure whose primary purpose is to store and
 return elements in Last In First Out order. 
@@ -10,16 +14,46 @@ return elements in Last In First Out order.
 3. What is the difference between using an array vs. a linked list when 
    implementing a Stack?
 """
+
+# Should have the methods: push, pop, and len.
+
+# push adds an item to the top of the stack.
+# pop removes and returns the element at the top of the stack
+# len returns the number of elements in the stack.
+
+# Using built-in python lists
 class Stack:
     def __init__(self):
         self.size = 0
-        # self.storage = ?
+        self.storage = []
 
     def __len__(self):
-        pass
+        return len(self.storage)
 
     def push(self, value):
-        pass
+        """Adds an item to the top of the stack"""
+        self.storage = [value] + self.storage
+        return self.storage
 
     def pop(self):
-        pass
+        """Removes and returns the element at the top of the stack"""
+        top = self.storage[0]
+        self.storage.remove(top)
+        new_top = self.storage[0]
+        return new_top
+
+
+# # Using Node and LinkedList classes
+# class Stack:
+#     def __init__(self):
+#         self.size = 0
+#         # self.storage = ?
+
+#     def __len__(self):
+#         pass
+
+#     def push(self, value):
+#         pass
+
+#     def pop(self):
+#         pass

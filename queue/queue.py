d41b220a488563df1cdc20a596e82d4ef57df8c4
Aaron Watkins <61791690+ekselan@users.noreply.github.com>
2020-07-13 18:36:13 -0400

    Begin work on Queue class

--- queue/queue.py
+++ queue/queue.py
@@ -1,3 +1,7 @@
+# Implement the Stack and Queue classes using built-in Python lists 
+# and the Node and LinkedList classes you created during the 
+# Module 1 Guided Project.
+
 """
 A queue is a data structure whose primary purpose is to store and
 return elements in First In First Out order. 
@@ -13,16 +17,47 @@ return elements in First In First Out order.
 Stretch: What if you could only use instances of your Stack class to implement the Queue?
          What would that look like? How many Stacks would you need? Try it!
 """
+
+# Has the methods: enqueue, dequeue, and len.
+# enqueue adds an element to the back of the queue.
+# dequeue removes and returns the element at the front of the queue.
+# len returns the number of elements in the queue.
+
+# Array implementation of Queue class
 class Queue:
+    """Array implementation of Queue class"""
     def __init__(self):
         self.size = 0
         # self.storage = ?
     
     def __len__(self):
+        """Returns the number of elements in the queue"""
         pass
 
     def enqueue(self, value):
+        """Adds an element to the back of the queue"""
         pass
 
     def dequeue(self):
+        """Removes and returns the element at the front of the queue"""
         pass
+
+
+# # OOP implementation of Queue class
+# class Queue:
+#     """OOP implementation of Queue class using Node and LinkedList classes"""
+#     def __init__(self):
+#         self.size = 0
+#         # self.storage = ?
+    
+#     def __len__(self):
+#         """Returns the number of elements in the queue"""
+#         pass
+
+#     def enqueue(self, value):
+#         """Adds an element to the back of the queue"""
+#         pass
+
+#     def dequeue(self):
+#         """Removes and returns the element at the front of the queue"""
+#         pass

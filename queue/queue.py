31f8537100280fc0d1a1499e9ba19901c7523e85
Kelli Scheuble <github@scheuble.dev>
2020-07-13 14:26:27 -0700

    Queue with linked lists. MVP done

--- queue/queue.py
+++ queue/queue.py
@@ -15,7 +15,7 @@ return elements in First In First Out order.
 Stretch: What if you could only use instances of your Stack class to implement the Queue?
          What would that look like? How many Stacks would you need? Try it!
 """
-class Queue:
+class ListQueue:
     def __init__(self):
         self.size = 0
         self.storage = list()
@@ -32,3 +32,21 @@ class Queue:
             return None
         self.size -= 1
         return self.storage.pop(0)
+
+class Queue:
+    def __init__(self):
+        self.size = 0
+        self.storage = LinkedList()
+
+    def __len__(self):
+        return self.size
+
+    def enqueue(self, value):
+        self.size += 1
+        self.storage.add_to_tail(value)
+
+    def dequeue(self):
+        if self.size == 0:
+            return None
+        self.size -= 1
+        return self.storage.remove_head()

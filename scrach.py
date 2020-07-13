44817b345b3686e204f3f837f58e3ef586820efc
Matthew Bolton <matthewwbolton@gmail.com>
2020-07-13 18:34:20 -0400

    -commit changes

--- scrach.py
+++ scrach.py
@@ -8,26 +8,32 @@ for item in a:
     print(item)
 
 
-Print out each element of the following array on a separate line, but this time the input array can contain arrays nested to an arbitrarily deep level:
+# Print out each element of the following array on a separate line, but this time the input array can contain arrays nested to an arbitrarily deep level:
 a = ['Bob', 'Slack', ['reddit', '89', 101, ['alacritty', '(brackets)', 5, 375]], 0, [
     '{slice, owned}'], 22]
-For the above input, the expected output is:
-Bob
-Slack
-reddit
-89
-101
-alacritty
-(brackets)
-5
-375
-0
-{slice, owned}
-22
-You may use whatever programming language you'd like.
-Verbalize your thought process as much as possible before writing any code. Run through the UPER problem solving framework while going through your thought process.
+# For the above input, the expected output is:
+# Bob
+# Slack
+# reddit
+# 89
+# 101
+# alacritty
+# (brackets)
+# 5
+# 375
+# 0
+# {slice, owned}
+# 22
+# You may use whatever programming language you'd like.
+# Verbalize your thought process as much as possible before writing any code. Run through the UPER problem solving framework while going through your thought process.
 
 
-result = []
+def print_nested_list(input):
+    if type(input) == list:
+        for item in input:
+            print_nested_list(item)
+    else:
+        print(input)
 
-for item in a:
+
+print_nested_list(a)

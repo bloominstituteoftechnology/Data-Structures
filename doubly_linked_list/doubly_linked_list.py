d55a03300c6e0c1a79bb50bb6e8b35d8a73cd28d
Nick <ptnguyen1901@gmail.com>
2020-07-14 17:58:55 -0400

    delete

--- doubly_linked_list/doubly_linked_list.py
+++ doubly_linked_list/doubly_linked_list.py
@@ -10,6 +10,35 @@ class ListNode:
         self.value = value
         self.next = next
 
+    """Wrap the given value in a ListNode and insert it
+        after this node. Note that this node could already
+        have a next node it is point to."""
+
+    def insert_after(self, value):
+        current_next = self.next
+        self.next = ListNode(value, self, current_next)
+        if current_next:
+            current_next.prev = self.next
+
+    """Wrap the given value in a ListNode and insert it
+    before this node. Note that this node could already
+    have a previous node it is point to."""
+
+    def insert_before(self, value):
+        current_prev = self.prev
+        self.prev = ListNode(value, current_prev, self)
+        if current_prev:
+            current_prev.next = self.prev
+
+    """Rearranges this ListNode's previous and next pointers
+    accordingly, effectively deleting this ListNode."""
+
+    def delete(self):
+        if self.prev:
+            self.prev.next = self.next
+        if self.next:
+            self.next.prev = self.prev
+
 
 """
 Our doubly-linked list class. It holds references to 
@@ -18,7 +47,7 @@ the list's head and tail nodes.
 
 
 class DoublyLinkedList:
-    def __init__(self, node = None):
+    def __init__(self, node=None):
         self.head = node
         self.tail = node
         self.length = 1 if node is not None else 0
@@ -28,18 +57,16 @@ class DoublyLinkedList:
 
     def add_to_head(self, value):
 
-        new_node: ListNode = ListNode(value)
-        new_node.next: ListNode = self.head
-        new_node.prev: ListNode = None
-
-        self.length += 1
-        if self.head is not None:
+        new_node = ListNode(value)
+        if self.head and self.tail:
+            new_node.next = self.head
             self.head.prev = new_node
-
             self.head = new_node
         else:
             self.head = new_node
             self.tail = new_node
+
+        self.length += 1
 
     def remove_from_head(self):
 
@@ -61,24 +88,19 @@ class DoublyLinkedList:
             self.head = target_to_delete.next
             self.length -= 1
 
-
             return target_to_delete.value
-
 
     def add_to_tail(self, value):
         new_node = ListNode(value)
-        new_node.prev: ListNode = self.tail
-        new_node.next: ListNode = None
 
-        self.length += 1
-
-        if self.head is not None:
-            self.head.next = new_node
+        if self.tail and self.head:
+            new_node.prev = self.tail
+            self.tail.next = new_node
             self.tail = new_node
         else:
             self.head = new_node
             self.tail = new_node
-
+        self.length += 1
 
     def remove_from_tail(self):
         tail_to_delete = self.tail
@@ -93,36 +115,46 @@ class DoublyLinkedList:
             self.length -= 1
             self.tail = tail_to_delete.prev
 
-
             return tail_to_delete.value
-
-
-
 
     def move_to_front(self, node):
         pass
 
-
     def move_to_end(self, node):
         pass
 
-
     def delete(self, node):
-        pass
+        if self.head == self.tail:
+            self.head = None
+            self.tail = None
+        elif node == self.tail:
+            self.tail = node.prev
+            node.delete()
+        elif node == self.head:
+            self.head = node.next
+            node.delete()
+        else:
+            node.delete()
+        self.length -= 1
 
 
     def get_max(self):
-        pass
+        if self.head is None:
+            return None
+        current = self.head
+        current_max = current.value
+        while current.next is not None:
+            current_max = max(current_max, current.next.value)
+            current = current.next
+        return current_max
 
 
-dd = DoublyLinkedList()
+dd = DoublyLinkedList(ListNode(1))
 
-dd.add_to_tail(2123)
-dd.add_to_tail(2323)
-dd.add_to_tail(22)
-dd.add_to_tail(323)
-print(dd.remove_from_tail())
-print(dd.tail.next)
+print(dd.get_max())
+dd.add_to_tail(100)
 
+print(dd.tail.value)
+dd.add_to_tail(55)
 
-
+print(dd.get_max())

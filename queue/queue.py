6e4a654bb8a62564558db8b103054fd6bfcb3ccb
Christine Fletcher <chrismis79@gmail.com>
2020-07-14 09:55:10 -0500

    queue import working! and tests passing using singly_linked_list

--- queue/queue.py
+++ queue/queue.py
@@ -13,8 +13,7 @@ return elements in First In First Out order.
 Stretch: What if you could only use instances of your Stack class to implement the Queue?
          What would that look like? How many Stacks would you need? Try it!
 """
-import os, sys
-sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
+
 from singly_linked_list import LinkedList
 
 class Queue:
@@ -26,7 +25,7 @@ class Queue:
         return self.size
     
     def enqueue(self, value):
-        self.storage.add_to_end(value)
+        self.storage.add_to_tail(value)
         self.size += 1
 
     def dequeue(self):

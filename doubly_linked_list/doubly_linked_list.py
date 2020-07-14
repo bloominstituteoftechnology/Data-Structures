24f73a1751ddede2c8a261588da7b0ba0a25a7e2
jesseleeruiz <jesseleeruiz@gmail.com>
2020-07-14 09:27:39 -1000

    Stubbed out add_to_head, remove_from_head, add_to_tail and remove_from_tail methods.

--- doubly_linked_list/doubly_linked_list.py
+++ doubly_linked_list/doubly_linked_list.py
@@ -7,6 +7,10 @@ class ListNode:
         self.prev = prev
         self.value = value
         self.next = next
+
+    def delete(self):
+        self.value = None
+        pass
             
 """
 Our doubly-linked list class. It holds references to 
@@ -27,7 +31,27 @@ class DoublyLinkedList:
     the old head node's previous pointer accordingly.
     """
     def add_to_head(self, value):
-        pass
+        # Create instance of ListNode with value
+        new_node = ListNode(value)
+
+        # Increment the DLL Length Attribute
+        self.length += 1
+
+        # Check if DLL is empty
+            # If it is empty: 
+                # Set head and tail to the new node instance
+        if self == None:
+            self.head = new_node
+            self.tail = new_node
+
+            # If not:
+                # Set new nodes next to current head
+                # Set head's previous to new node
+                # Set head to new node head
+        else:
+            new_node.next = self.head
+            self.prev = new_node
+            self.head = new_node.value
         
     """
     Removes the List's current head node, making the
@@ -35,7 +59,30 @@ class DoublyLinkedList:
     Returns the value of the removed Node.
     """
     def remove_from_head(self):
-        pass
+        # Store the Value of the Head
+        removed = self.head
+        # Decrement Length
+        self.length -= 1
+        # Delete the Head
+        removed.delete()
+            # If current Head's next is not None
+                # Set next node's previous value to None
+                # Set head to current Head's next value
+        if removed.next != None:
+            new_head_node = removed.next
+            new_head_node.prev = None
+            removed.next = None
+            removed = new_head_node.head
+
+            # Else if Head is None
+                # Set Head to None
+                # Set Tail to None
+        else:
+            removed = None
+            self.tail = None
+
+        # Return the Value
+        return removed
             
     """
     Wraps the given value in a ListNode and inserts it 
@@ -43,7 +90,24 @@ class DoublyLinkedList:
     the old tail node's next pointer accordingly.
     """
     def add_to_tail(self, value):
-        pass
+        # Create instance of ListNode with value
+        new_node = ListNode(value)
+        # Increment the DLL Length Attribute
+        self.length += 1
+
+        # Check if DLL is empty
+            # If it is empty: 
+                # Set head and tail to the new node instance
+        if self == None:
+            self.head = new_node
+            self.tail = new_node
+
+        # If DLL is NOT empty:
+            # Set new node's previous to the current tail
+            # Set Tail's Next to new node
+            # Set Tail to New node
+        else:
+            new_node.prev = self.tail
             
     """
     Removes the List's current tail node, making the 
@@ -51,7 +115,28 @@ class DoublyLinkedList:
     Returns the value of the removed Node.
     """
     def remove_from_tail(self):
-        pass
+        # Store the Value of the Tail
+        removed = self.tail
+        # Decrement Length
+        self.length -= 1
+        # Delete the Tail
+        removed.delete()
+            # If current Tail's previous is not None
+                # Set head node's next value to None
+                # Set tail to current Head's next value
+        if removed.prev != None:
+            head_node = removed.prev
+            head_node.next = removed
+
+            # Else if tail.prev is None
+                # Set Head to None
+                # Set Tail to None
+        else:
+            removed = None
+            self.head - None
+
+        # Return the Value
+        return removed
             
     """
     Removes the input node from its current spot in the 

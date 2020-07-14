4c65d8e8b441c48107ed64ce047dc517fea9897d
Teague <teaguehannam@gmail.com>
2020-07-14 12:30:54 -0600

    Finished day 1

--- queue/queue.py
+++ queue/queue.py
@@ -1,6 +1,5 @@
 """
-A queue is a data structure whose primary purpose is to store and
-return elements in First In First Out order. 
+Queue is a Data structure
 
 1. Implement the Queue class using an array as the underlying storage structure.
    Make sure the Queue tests pass.
@@ -13,16 +12,21 @@ return elements in First In First Out order.
 Stretch: What if you could only use instances of your Stack class to implement the Queue?
          What would that look like? How many Stacks would you need? Try it!
 """
+
+
 class Queue:
     def __init__(self):
         self.size = 0
         # self.storage = ?
-    
+        self.storage = []
     def __len__(self):
-        pass
+        return len(self.storage)
 
     def enqueue(self, value):
-        pass
+        self.storage.append(value)
 
     def dequeue(self):
-        pass
+        if len(self.storage)>0:
+            return self.storage.pop(0)
+        else:
+            return None
--- stack/stack.py
+++ stack/stack.py
@@ -1,25 +1,27 @@
 """
-A stack is a data structure whose primary purpose is to store and
-return elements in Last In First Out order. 
+~ Stack is a Data Structure ~
 
 1. Implement the Stack class using an array as the underlying storage structure.
-   Make sure the Stack tests pass.
 2. Re-implement the Stack class, this time using the linked list implementation
    as the underlying storage structure.
-   Make sure the Stack tests pass.
 3. What is the difference between using an array vs. a linked list when 
    implementing a Stack?
 """
+
+
 class Stack:
     def __init__(self):
         self.size = 0
-        # self.storage = ?
+        self.storage = []
 
     def __len__(self):
-        pass
+        return len(self.storage)
 
     def push(self, value):
-        pass
+        self.storage.append(value)
 
     def pop(self):
-        pass
+        if len(self)>0:
+            return self.storage.pop()
+        else:
+            return None

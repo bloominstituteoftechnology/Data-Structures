df94af7da4655e4cab7468987e053532235ffb1a
Reagan Barrington <reagan@barringtonfamily.net>
2020-07-14 11:57:29 -0400

    mvp

--- stack/stack.py
+++ stack/stack.py
@@ -9,51 +9,51 @@ return elements in Last In First Out order.
    """
 
 
-# class Stack:
-#     def __init__(self):
-#         self.size = 0
-#         self.storage = []
-
-#     def __len__(self):
-#         return self.size
-
-#     def push(self, value):
-#         self.size = self.size + 1
-#         self.storage.append(value)
-#         return value
-
-#     def pop(self):
-#         if self.size == 0:
-#             return None
-#         else:
-#             self.size = self.size - 1
-#             value = self.storage.pop()
-#             return value
-            
-"""
-2. Re-implement the Stack class, this time using the linked list implementation
-   as the underlying storage structure.
-"""
-
 class Stack:
     def __init__(self):
         self.size = 0
-        self.storage = LinkedList()
+        self.storage = []
 
     def __len__(self):
         return self.size
 
     def push(self, value):
         self.size = self.size + 1
-        self.storage.add_to_tail(value)
+        self.storage.append(value)
+        return value
 
     def pop(self):
         if self.size == 0:
             return None
         else:
-            value = self.storage.remove_head()
             self.size = self.size - 1
+            value = self.storage.pop()
             return value
+            
+"""
+2. Re-implement the Stack class, this time using the linked list implementation
+   as the underlying storage structure.
+"""
+
+# class Stack:
+#     def __init__(self):
+#         self.size = 0
+#         self.storage = LinkedList()
+
+#     def __len__(self):
+#         return self.size
+
+#     def push(self, value):
+#         self.size = self.size + 1
+#         self.storage.add_to_tail(value)
+
+#     def pop(self):
+#         if self.size == 0:
+#             return None
+#         else:
+#             value = self.storage.remove_tail()
+#             self.size = self.size - 1
+#             return value
 
 """            
 3. What is the difference between using an array vs. a linked list when 

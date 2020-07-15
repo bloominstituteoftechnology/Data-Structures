122b43b1a20e471e222364baf8fdb08116bcbbe2
shengjiyang <samswank@gmail.com>
2020-07-15 10:18:54 -0600

    added insert to bst

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -17,7 +17,16 @@ class BSTNode:
 
     # Insert the given value into the tree
     def insert(self, value):
-        pass
+        # inserting value into root-less tree
+        if not self.value:
+            self.value = value
+        # general case
+        elif value < self.value:
+            self.left = value
+
+        elif value > self.value:
+            self.right = value
+
 
     # Return True if the tree contains the value
     # False if it does not

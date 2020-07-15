03a4ddd2da9639f36a7e41a0b05afff9234e9269
jesseleeruiz <jesseleeruiz@gmail.com>
2020-07-15 09:50:42 -1000

    Added some stuff to doubly file from morning lecture. Started on BST assignment. Insert and Contains methods given this morning. Just finished get_max method with tests passing.

--- doubly_linked_list/doubly_linked_list.py
+++ doubly_linked_list/doubly_linked_list.py
@@ -8,6 +8,13 @@ class ListNode:
         self.prev = prev
         self.value = value
         self.next = next
+
+    def delete_list_node(self):
+        if self.prev:
+            self.prev.next = self.next
+        if self.next:
+            self.next.prev = self.prev
+        gc.collect()
             
 """
 Our doubly-linked list class. It holds references to 
@@ -60,6 +67,11 @@ class DoublyLinkedList:
     Returns the value of the removed Node.
     """
     def remove_from_head(self):
+        """
+        value = self.head.value
+        self.delete(self.head)
+        return value
+        """
         # Store the Value of the Head
         removed = self.head.value
         # Decrement Length
@@ -76,6 +88,17 @@ class DoublyLinkedList:
     the old tail node's next pointer accordingly.
     """
     def add_to_tail(self, value):
+        """
+        new_node = ListNode(value, None, None)
+        self.length += 1
+        if not self.tail and not self.head:
+            self.tail = new_node
+            self.head = new_node
+        else:
+            new_node.prev = self.tail
+            self.tail.next = new_node
+            self.tail = new_node
+        """
         # Create instance of ListNode with value
         new_node = ListNode(value)
 
@@ -105,6 +128,11 @@ class DoublyLinkedList:
     Returns the value of the removed Node.
     """
     def remove_from_tail(self):
+        """
+        value = self.tail.value
+        self.delete(self.tail)
+        return value
+        """
         # Store the Value of the Tail
         removed = self.tail.value
         # Decrement Length
@@ -133,48 +161,53 @@ class DoublyLinkedList:
     List and inserts it as the new head node of the List.
     """
     def move_to_front(self, node):
-        # Case 1 if this node is the tail
-        current = self.head
-
-        current = node
-        node.prev = None
-        node.next = current.prev
-
-        # if node == self.tail:
-        #     current = node
-        #     node.prev = None
-        #     current = self.tail
-        #     current.prev = node.next
-        #     current.next = None
-
-        # # Case 2 if this node is not the tail
-        # else:
-        #     current = node
-        #     node.prev = None
-        #     current.next = tail.prev
-        #     current.prev = node.next
-        
+        if node is self.head:
+            return
+        value = node.value
+        if node is self.tail:
+            self.remove_from_tail()
+        else:
+            node.delete_list_node()
+            self.length -= 1
+        self.add_to_head(value)
     """
     Removes the input node from its current spot in the 
     List and inserts it as the new tail node of the List.
     """
     def move_to_end(self, node):
-        current = self.tail
-
-        current = node
-        node.prev = current.next
-        node.next = None
+        if node is self.tail:
+            return
+        if node is self.head:
+            self.remove_from_head()
+            self.add_to_tail(node.value)
+        else:
+            node.delete_list_node()
+            self.length -= 1
+            self.add_to_tail(node.value)
 
     """
     Deletes the input node from the List, preserving the 
     order of the other elements of the List.
     """
     def delete(self, node):
-        previous_node = node.prev
-        next_node = node.next
-        previous_node = next_node
-        node = None
-        # previous_node, next_node, previous_node.next, node = node.prev, node.next, next_node.prev, None
+        """
+        if not self.head and not self.tail:
+            return
+        self.length -= 1
+        ## Checking if DLL is length of 1 ##
+        if self.head is self.tail: ## is because checking reference
+            self.head = None
+            self.tail = None
+        elif self.head == node:
+            self.head = node.next
+            node.delete()
+        elif self.tail == node:
+            self.tail = node.prev
+            node.delete()
+        else:
+            node.delete()
+        """
+        node.delete_list_node()
 
     """
     Finds and returns the maximum value of all the nodes 

4cecb05716ff4c49aa563f981b0b9d212755c426
Christine Fletcher <chrismis79@gmail.com>
2020-07-15 22:49:08 -0500

    binary search tree pt1 MVP complete

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -9,28 +9,113 @@ This part of the project comprises two days:
 2. Implement the `in_order_print`, `bft_print`, and `dft_print` methods
    on the BSTNode class.
 """
+# class BSTNode:
+#     def __init__(self, value):
+#         self.value = value
+#         self.left = None
+#         self.right = None
+
+#     # Insert the given value into the tree
+#     def insert(self, value):
+#         #First: Case 1 Value is less thean self.value --> goes left
+#         if value < self.value:
+#             #if no left child, insert value here
+#             if self.left is None:
+#                 self.left = BSTNode(value)
+              
+                
+#             #else repeat the process on left subtree
+#             #self.left.insert(value) --> recursion
+#             else:
+#                   self.left.insert(value)
+                
+                
+
+#         #Case2 value is greater than self.value --> goes right
+#         #Case3 value is equal to self.value --> usally goes right (in solution d/t tests goes right)
+        
+#         #First: Case 1 Value is less thean self.value --> goes left
+#         elif value >= self.value:
+#             #if no left child, insert value here
+#             if self.right is not None:
+#                 self.right.insert(value)
+                
+#             #else repeat the process on left subtree
+#             #self.left.insert(value) --> recursion
+#             else:
+#                 self.left = BSTNode(value)
+                
+#     # Return True if the tree contains the value
+#     # False if it does not
+#     def contains(self, target):
+#         # Case 1: self.value is equal to the target
+#         if self.value == target:
+#             return True
+#         #Case 2: If target is less than self.value
+#         if target < self.value:
+#             #if self.left is none it isn't in the tree
+#             if self.left is None:
+#                 return False
+#             else:
+#                 return self.left.contains(target) #Recursive call --> must return on contains, not needed on insert
+#         #Case 3: otherwise
+#         else:
+#             if self.right is None:
+#                 return False
+#             else:
+#                 return self.right.contains(target)
+
 class BSTNode:
     def __init__(self, value):
         self.value = value
         self.left = None
         self.right = None
-
-    # Insert the given value into the tree
+    
     def insert(self, value):
-        pass
+        if value < self.value:
+            if self.left is None:
+                self.left = BSTNode(value)
+            else:
+                self.left.insert(value)
+        else:
+            if self.right is None:
+                self.right = BSTNode(value)
+            else:
+                self.right.insert(value)
+    #Return True if the tree contains the value
+    #False if is doesn't
 
-    # Return True if the tree contains the value
-    # False if it does not
     def contains(self, target):
-        pass
+        if self.value == target:
+            return True
+        elif target < self.value:
+            if self.left is None:
+                return False
+            else:
+                return self.left.contains(target)
+        else:
+            if self.right is None:
+                return False
+            else:
+                return self.right.contains(target)
+
 
     # Return the maximum value found in the tree
     def get_max(self):
-        pass
+        if self.right is None:
+            return self.value
+        else:
+            return self.right.get_max()
 
     # Call the function `fn` on the value of each node
     def for_each(self, fn):
-        pass
+            fn(self.value)
+            if self.right is not None:
+                self.right.for_each(fn)
+            if self.left is not None:
+                self.left.for_each(fn)
+            return
+       
 
     # Part 2 -----------------------
 

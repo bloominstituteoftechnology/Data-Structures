05cb6f5efb4d6e6259087c5816ba73cb55e229a4
Rob LeCheminant <roblecheminant@gmail.com>
2020-07-21 21:12:53 -0600

    adding add to head and tail methods

--- singly_linked_list/singly_linked_list.py
+++ singly_linked_list/singly_linked_list.py
@@ -14,18 +14,30 @@ class Node:
         self.next_node = new_next
 
 class LinkedList:
-    def __init__(self, head, tail):
-        self.head = head_node
-        self.tail = tail_node
+    def __init__(self, head, tail, length):
+        self.head = None
+        self.tail = None
+        self.length = 0
 
-    def add_to_head():
+    def add_to_head(self, value):
+        new_node = Node(value, self.head)
+        self.head = new_node
+        if self.length == 0:
+            self.tail = new_node
+        self.lenth += 1
+
+    def add_to_tail(self, value):
+        new_node = Node(value)
+        if self.head is None and self.tail is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.set_next(new_node)
+            self.tail = new_node
+        self.length += 1
+
+    def remove_head(self, head):
         pass
 
-    def remove_head():
-        pass
-
-    def add_to_tail():
-        pass
-
-    def remove_tail():
+    def remove_tail(self):
         pass

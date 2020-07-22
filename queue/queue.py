aa2b796eb358b9ee0048128b50d21463599e4705
Alex Shillingford <alex.shillingford@icloud.com>
2020-07-21 21:00:52 -0600

    Fill out Queue class methods; test_queue.py tests all pass.

--- queue/queue.py
+++ queue/queue.py
@@ -16,13 +16,18 @@ Stretch: What if you could only use instances of your Stack class to implement t
 class Queue:
     def __init__(self):
         self.size = 0
-        # self.storage = ?
+        self.storage = list()
     
     def __len__(self):
-        pass
+        return self.size
 
     def enqueue(self, value):
-        pass
+        self.storage.append(value)
+        self.size += 1
 
     def dequeue(self):
-        pass
+        if self.size > 0:
+            self.size -= 1
+            return self.storage.pop(0)
+        else:
+            pass

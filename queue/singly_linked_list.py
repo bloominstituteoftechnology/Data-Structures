b8344849688bf4cdb6fa220fb53b839eb8efbb0c
John Kouris <jdkouris@gmail.com>
2020-07-22 15:23:40 -0700

    Added LL files to folders to import

---
+++ queue/singly_linked_list.py
@@ -0,0 +1,100 @@
+class Node:
+    def __init__(self, value=None, next_node=None):
+        # the value at this linked list node
+        self.value = value
+        # reference to the next node in the list
+        self.next_node = next_node
+
+    def get_value(self):
+        return self.value
+
+    def get_next(self):
+        return self.next_node
+
+    def set_next(self, new_next):
+        # set this node's next_node reference to the passed in node
+        self.next_node = new_next
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.length = 0
+    
+    def add_to_head(self, value):
+        new_node = Node(value, self.head)
+        self.head = new_node
+        if self.length == 0:    # if self.head is None and self.tail is None:
+            self.tail = new_node
+        self.length += 1
+
+    def add_to_tail(self, value):
+        new_node = Node(value)
+        if self.head is None and self.tail is None:
+            self.head = new_node
+        else:
+            self.tail.set_next(new_node)
+        self.tail = new_node
+        self.length += 1
+
+    def remove_head(self):
+        # empty LL
+        if self.head is None:
+            return None
+
+        # LL with 1 Node
+        elif self.head == self.tail:
+            value = self.head.get_value()
+            self.head = None
+            self.tail = None
+            self.length -= 1
+            return value
+
+        # LL with 2+ Nodes
+        else:
+            value = self.head.get_value()
+            self.head = self.head.get_next()
+            self.length -= 1
+            return value
+
+    def remove_tail(self):
+        # empty LL
+        if self.head is None:
+            return None
+
+        # LL with 1 Node
+        elif self.head == self.tail:
+            value = self.head.get_value()
+            self.head = None
+            self.tail = None
+            return value
+
+        # LL with 2+ Nodes
+        current = self.head
+        while current.get_next() is not self.tail:
+            current = current.get_next()
+        value = self.tail.get_value()
+        self.tail = current
+        return value
+            
+    def contains(self, value):
+        if not self.head:
+            return False
+        current = self.head
+        while current:
+            if current.get_value() == value:
+                return True
+            current = current.get_next()
+        return False
+
+    def get_max(self):
+        if self.head is None:
+            return None
+        # iterate through all the elements
+        cur_node = self.head
+        cur_max = self.head.get_value()
+        while cur_node is not None:
+            if cur_node.get_value() > cur_max:
+                cur_max = cur_node.get_value()
+            cur_node = cur_node.get_next()
+        return cur_max

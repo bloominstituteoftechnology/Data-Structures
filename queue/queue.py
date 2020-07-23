6fe44b8611b42a11925d826b780a3acc1de7b844
David <davi86m@gmail.com>
2020-07-22 23:50:03 -0400

    Queue - Built ins

--- queue/queue.py
+++ queue/queue.py
@@ -14,15 +14,19 @@ Stretch: What if you could only use instances of your Stack class to implement t
          What would that look like? How many Stacks would you need? Try it!
 """
 class Queue:
-    def __init__(self):
+    def __init__(self, storage = []):
         self.size = 0
-        # self.storage = ?
+        self.storage = storage
     
     def __len__(self):
-        pass
+        return self.size
 
     def enqueue(self, value):
-        pass
+        self.storage.append(value)
+        self.size += 1 
 
     def dequeue(self):
-        pass
+        if not self.storage:
+            return None
+        self.size -= 1    
+        return self.storage.pop(0)

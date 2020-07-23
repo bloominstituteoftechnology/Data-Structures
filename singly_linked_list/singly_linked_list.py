2856e806796e12a03d71738345c40a212cee02c3
SMoore3773 <Smooresculpture@gmail.com>
2020-07-22 22:04:57 -0400

    initial commit

---
+++ singly_linked_list/singly_linked_list.py
@@ -0,0 +1,75 @@
+class Node:
+    def __init__(self, value=None, next_node=None):
+        self.value = value
+        self.next_node = next_node
+
+    def get_value(self):
+        return self.value
+
+    def get_next(self):
+        return self.next_node
+
+    def set_next(self, new_next):
+        self.next_node = new_next
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def add_to_tail(self, value):
+        new_node = Node(value, None)
+
+        if not self.head:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.set_next(new_node)
+            self.tail = new_node
+
+    def remove_head(self):
+        if not self.head:
+            return None
+        if not self.head.get_next():
+            head = self.head
+            self.head = None
+            self.tail = None
+            return head.get_value()
+        value = self.head.get_value()
+        self.head = self.head.get_next()
+        return value
+
+    def contains(self, value):
+        if not self.head:
+            return False
+        current = self.head
+
+        while current:
+            if current.get_value() == value:
+                return True
+            current = current.get_next()
+
+        return False
+
+    def get_max(self):
+        initial = self.head
+        maximum = initial.get_value()
+        while self.head is not None:
+            if maximum < initial.get_value():
+                maximum = initial.get_value()
+            initial = initial.next_node()
+        return maximum
+
+
+        # current = self.head
+        # maximum = self.head.get_value()
+        # if self.head is None:
+        #     return False
+        # else:
+        #     while(True):
+        #         if maximum < current.value:
+        #             maximum = current.value
+        #     current = current.next_node
+        #     if(current == self.head):
+        #         return current

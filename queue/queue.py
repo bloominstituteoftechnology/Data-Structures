7fdce9934c13ed5c57b10bf609bdb852c4006765
Seth Nelson <seth.nelson@outlook.com>
2020-07-22 20:39:30 -0700

    almost finished queue.py, need to work out as few bugs tomorrow

--- queue/queue.py
+++ queue/queue.py
@@ -19,19 +19,19 @@ Stretch: What if you could only use instances of your Stack class to implement t
 Queue class with array storage structure.
 '''
 
-class Queue:
-    def __init__(self):
-        self.size = 0
-        # self.storage = ?
+# class Queue:
+#     def __init__(self):
+#         self.size = 0
+#         # self.storage = ?
     
-    def __len__(self):
-        pass
+#     def __len__(self):
+#         pass
 
-    def enqueue(self, value):
-        pass
+#     def enqueue(self, value):
+#         pass
 
-    def dequeue(self):
-        pass
+#     def dequeue(self):
+#         pass
 
 
 '''
@@ -40,6 +40,47 @@ Queue class with list storage structure.
 
 class Queue:
     class Node:
-        def __init__(self, element, _next):
+        def __init__(self, element):
             self.element = element
-            self._next = _next
+            self.front = None
+    
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.size = 0
+    
+    def __len__(self):
+        return self.size
+    
+    def is_empty(self):
+        return self.size == 0
+    
+    def enqueue(self, element):
+        new_node = self.Node(element)
+        
+        if self.tail.is_empty():
+            self.tail.front = new_node
+        else:
+            self.head = new_node
+        self.tail = new_node
+        self.size += 1
+    
+    def dequeue(self):
+        if self:
+            self.head = self.head.front
+        else:
+            return None
+    
+    def first(self):
+        if self.is_empty():
+            return None
+        else:
+            return self.head.element
+            
+            
+s = Queue()
+s.enqueue(1)
+s.enqueue('two')
+s.enqueue(3)
+s.enqueue('four')
+print(s.first())

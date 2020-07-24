c82cd5f6133a482027b221b9e3d2c1f455e02266
tyler broadway <tbroadway96@gmail.com>
2020-07-23 19:23:17 -0500

    Working on stack & queue

--- queue/queue.py
+++ queue/queue.py
@@ -13,6 +13,7 @@ return elements in First In First Out order.
 Stretch: What if you could only use instances of your Stack class to implement the Queue?
          What would that look like? How many Stacks would you need? Try it!
 """
+
 class Queue:
     def __init__(self):
         self.size = 0
--- stack/stack.py
+++ stack/stack.py
@@ -10,6 +10,8 @@ return elements in Last In First Out order.
 3. What is the difference between using an array vs. a linked list when 
    implementing a Stack?
 """
+from singly_linked_list import LinkedList
+
 class Stack:
     def __init__(self):
         self.size = 0

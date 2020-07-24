a75ee1aee69814e5aca7a50b9840a00dcbae5f32
Sal B Amer <iosdevsalman@gmail.com>
2020-07-23 19:12:12 -0700

    updated ArrayStack

--- stack/stack.py
+++ stack/stack.py
@@ -12,27 +12,43 @@ return elements in Last In First Out order.
 """
 #  LIFO Order  
 # You can only push to add a new element to the top of the stack, pop to remove the element from the top, and peek at the top element without popping it off.
-# class Stack:
-#     def __init__(self):
-#         self.size = 0
-#         self.storage = []
+class ArrayStack:
+    def __init__(self):
+        self.storage = []
 
-#     def __len__(self):
-#         return len(self.storage)
+    def __len__(self):
+        return len(self.storage)
 
-#     def push(self, value):
-#         self.storage.append(value)
+    def push(self, value):
+        self.storage.append(value)
 
-#     def pop(self): #remove last one
-#         if self.storage != []:
-#             return self.storage.pop()
-#         else:
-#             return None
+    def pop(self): #remove last one
+        if len(self.storage) == 0:
+            return None
+        else:
+            return self.storage.pop()
 
 #  Do the same with  linked list
 # A linked list is a sequence of data items, just like an array. But where an array allocates a big block of memory to store the objects, the elements in a linked list are totally separate objects in memory and are connected through links:
 #use Node instead of Stack
+# class Stack:
+#     def __init__(self):
+#         self.size = 0
+#         self.items = LinkedList()
+
 class Stack:
     def __init__(self):
         self.size = 0
-        self.items = LinkedList()
+        self.storage = []
+
+    def __len__(self):
+        return len(self.storage)
+
+    def push(self, value):
+        self.storage.append(value)
+
+    def pop(self): #remove last one
+        if self.storage != []:
+            return self.storage.pop()
+        else:
+            return None

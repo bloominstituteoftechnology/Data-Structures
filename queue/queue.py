b811af2ada82b62a718c3f0abbccebd28db2426b
Jessie <jessie.griffin@icloud.com>
2020-07-25 12:19:42 -0400

    Queue with linked list complete

--- queue/queue.py
+++ queue/queue.py
@@ -1,3 +1,6 @@
+import sys
+sys.path.append('../singly_linked_list')
+from singly_linked_list import LinkedList
 """
 A queue is a data structure whose primary purpose is to store and
 return elements in First In First Out order. 
@@ -13,7 +16,7 @@ return elements in First In First Out order.
 Stretch: What if you could only use instances of your Stack class to implement the Queue?
          What would that look like? How many Stacks would you need? Try it!
 """
-class Queue:
+class ArrayQueue:
     def __init__(self):
         self.size = 0
         self.storage = []
@@ -22,9 +25,28 @@ class Queue:
         return len(self.storage)
 
     def enqueue(self, value):
-        self.storage.insert(0, value)
+        self.storage.append(value)
 
     def dequeue(self):
         if len(self.storage) != 0:
-            return self.storage.pop()
+            return self.storage.pop(0)
         return None
+
+
+class SLLQueue:
+    def __init__(self):
+        self.size = 0
+        self.storage = LinkedList()
+
+    def __len__(self):
+        return self.size
+
+    def enqueue(self, value):
+        self.storage.add_to_tail(value)
+        self.size += 1
+
+    def dequeue(self):
+        if self.size == 0:
+            return None
+        self.size -= 1
+        return self.storage.remove_head()
--- queue/test_queue.py
+++ queue/test_queue.py
@@ -1,9 +1,9 @@
 import unittest
-from queue import Queue
+from queue import SLLQueue
 
 class QueueTests(unittest.TestCase):
     def setUp(self):
-        self.q = Queue()
+        self.q = SLLQueue()
 
     def test_len_returns_0_for_empty_queue(self):
         self.assertEqual(len(self.q), 0)

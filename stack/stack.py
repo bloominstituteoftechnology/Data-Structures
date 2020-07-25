bffd575a1a9707b1b09ce519e051b0236a12a0d2
Jessie <jessie.griffin@icloud.com>
2020-07-25 12:19:55 -0400

    Stack with linked list complete

--- stack/stack.py
+++ stack/stack.py
@@ -1,5 +1,5 @@
-from linked_list import LinkedList
-from linked_list import Node
+from singly_linked_list import LinkedList
+
 """
 A stack is a data structure whose primary purpose is to store and
 return elements in Last In First Out order. 
@@ -12,35 +12,27 @@ return elements in Last In First Out order.
 3. What is the difference between using an array vs. a linked list when 
    implementing a Stack?
 """
-"""
-class Stack:
+
+class LinkedListStack:
     def __init__(self):
         self.size = 0
-        self.storage = []
+        self.storage = LinkedList()
 
     def __len__(self):
-        return len(self.storage)
+        return self.size
 
     def push(self, value):
-    #    self.storage.unshift(value)
-        self.storage.insert(0, value)
+        self.storage.add_to_head(value)
+        self.size += 1
 
     def pop(self):
-        if len(self.storage) != 0:
-            del self.storage[0]
+        if self.size == 0:
+            return None
+        self.size -= 1
+        return self.storage.remove_head()
 
-stack = Stack()
-stack.push(9)
-stack.push(10)
-stack.push(105)
-for i in stack.storage:
-    print(i)
-stack.pop()
-for i in stack.storage:
-    print(i)
 """
-
-class Stack:
+class AlsoArrayStack:
     def __init__(self):
         self.size = 0
         self.storage = []
@@ -55,7 +47,6 @@ class Stack:
         if len(self.storage) != 0:
             return self.storage.pop(0)
 
-
 stack = Stack()
 stack.push(9)
 stack.push(10)
@@ -65,3 +56,21 @@ for i in stack.storage:
 stack.pop()
 for i in stack.storage:
     print(i)
+"""
+
+"""
+class ArrayStack:
+    def __init__(self):
+        self.size = 0
+        self.storage = []
+
+    def __len__(self):
+        return len(self.storage)
+
+    def push(self, value):
+        self.storage.append(value)
+
+    def pop(self):
+        if len(self.storage) != 0:
+            return self.storage.pop()
+"""

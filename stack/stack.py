4167920da5d79eca1e3c691704f201cf930fed3e
Veto Ramirez <vetorobert3@gmail.com>
2020-07-24 19:03:58 -0700

    started on stacks, queues and doubly link lists.

--- stack/stack.py
+++ stack/stack.py
@@ -10,28 +10,42 @@ return elements in Last In First Out order.
 3. What is the difference between using an array vs. a linked list when 
    implementing a Stack?
 """
-class Node:
-    def __int__(self, value=None, next_node=None):
-        self.value = value
-        self.next_node = next_node
 
-class Stack:
+class ArrayStack:
     def __init__(self):
         self.size = 0
         self.storage = []
 
     def __len__(self):
-        return 
+        return len(self.storage)
 
     def push(self, value): # add to tail
-        new_node = Node(value, None)
-
-        if not self.head:
-            self.head = new_node
-            self.tail = new_node
-        else:
-            self.tail.set_next(new_node)
-            self.tail = new_node
+        self.storage.append(value)
 
     def pop(self): # remove from tail
-        
+        if len(self.storage) == 0:
+            return None
+        return self.storage.pop()  
+
+
+import sys
+sys.path.append('../singly_linked_list')
+from singly_linked_list import LinkedList
+
+class LLStack: # with a linked list
+    def __init__(self):
+        self.size = 0
+        self.storage = LinkedList()
+
+    def __len__(self):
+        return self.size
+
+    def push(self, value):
+        self.storage.add_to_head(value)
+        self.size += 1
+
+    def pop(self):
+        if self.size == 0:
+            return None
+        self.size -= 1
+        return self.storage.remove_head()  

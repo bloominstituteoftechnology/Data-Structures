937f3b7c2cff8c938b1fad08c3601ac9f1510f29
quan005 <ralphealfreeman@gmail.com>
2020-07-25 10:35:03 -0700

    Module 2 MVP

--- doubly_linked_list/doubly_linked_list.py
+++ doubly_linked_list/doubly_linked_list.py
@@ -27,7 +27,15 @@ class DoublyLinkedList:
     the old head node's previous pointer accordingly.
     """
     def add_to_head(self, value):
-        pass
+        new_node = ListNode(value)
+        
+        if self.length == 0:
+            self.tail = new_node
+        else:
+            new_node.next = self.head
+            self.head.prev = new_node
+        self.head = new_node
+        self.length += 1
         
     """
     Removes the List's current head node, making the
@@ -35,7 +43,20 @@ class DoublyLinkedList:
     Returns the value of the removed Node.
     """
     def remove_from_head(self):
-        pass
+        if self.length == 0:
+            return None
+        elif self.length == 1:
+            value = self.head.value
+            self.head = None
+            self.tail = None
+            self.length -= 1
+            return value
+        else:
+            value = self.head.value
+            self.head.next.prev = None
+            self.head = self.head.next
+            self.length -= 1
+            return value
             
     """
     Wraps the given value in a ListNode and inserts it 
@@ -43,7 +64,15 @@ class DoublyLinkedList:
     the old tail node's next pointer accordingly.
     """
     def add_to_tail(self, value):
-        pass
+        new_node = ListNode(value)
+
+        if self.length == 0:
+            self.head = new_node
+        else:
+            new_node.prev = self.tail
+            self.tail.next = new_node
+        self.tail = new_node
+        self.length += 1
             
     """
     Removes the List's current tail node, making the 
@@ -51,32 +80,76 @@ class DoublyLinkedList:
     Returns the value of the removed Node.
     """
     def remove_from_tail(self):
-        pass
+        if self.length == 0:
+            return None
+        elif self.length == 1:
+            value = self.tail.value
+            self.tail = None
+            self.head = None
+            self.length -= 1
+            return value
+        else:
+            value = self.tail
+            self.tail.prev.next = None
+            self.tail = self.tail.prev
+            self.length -= 1
+            return value
             
     """
     Removes the input node from its current spot in the 
     List and inserts it as the new head node of the List.
     """
     def move_to_front(self, node):
-        pass
+        if self.length == 0:
+            return 'There is nothing to move'
+        else:
+            self.add_to_head(node.value)
+            self.delete(node)
         
     """
     Removes the input node from its current spot in the 
     List and inserts it as the new tail node of the List.
     """
     def move_to_end(self, node):
-        pass
+        if self.length == 0:
+            return 'There is nothing to move'
+        else:
+            self.add_to_tail(node.value)
+            self.delete(node)
 
     """
     Deletes the input node from the List, preserving the 
     order of the other elements of the List.
     """
     def delete(self, node):
-        pass
+        if node == self.head:
+            self.remove_from_head()
+        elif node == self.tail:
+            self.remove_from_tail()
+        else:
+            node.prev.next = node.next
+            node.next.prev = node.prev
+            self.length -= 1
 
     """
     Finds and returns the maximum value of all the nodes 
     in the List.
     """
     def get_max(self):
-        pass
+        if self.length == 0:
+            return None
+        elif self.length == 1:
+            return self.head.value
+        else:
+            n = self.head
+            j = n.next
+            max = self.head.value
+            while n.next is not None:
+                if j.value > n.value:
+                    max = j.value
+                elif n.value > j.value:
+                    max = n.value
+                
+                n = n.next
+                j = n.next
+            return max

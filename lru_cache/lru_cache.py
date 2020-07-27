bf82d94cd9d5f6e7d331af8c61bcc1c854ee933f
David Wright <davidtwright89@gmail.com>
2020-07-27 02:09:24 -0400

    Implemented LRUCache

--- lru_cache/lru_cache.py
+++ lru_cache/lru_cache.py
@@ -1,3 +1,39 @@
+# import sys
+# sys.path.append('doubly_linked_list')
+# from doubly_linked_list import DoublyLinkedList # pylint: disable=import-error
+
+class Node:
+    def __init__(self, key, value):
+        self.key = key
+        self.value = value
+        self.prev = None
+        self.next = None
+
+    def get_next(self):
+        return self.next
+    
+    def get_prev(self):
+        return self.prev
+    
+    def get_key(self):
+        return self.key
+    
+    def get_value(self):
+        return self.value
+    
+    def set_key(self, new_key):
+        self.key = new_key
+    
+    def set_value(self, new_value):
+        self.value = new_value
+    
+    def set_next(self, new_next):
+        self.next = new_next
+    
+    def set_prev(self, new_prev):
+        self.prev = new_prev
+
+
 class LRUCache:
     """
     Our LRUCache class keeps track of the max number of nodes it
@@ -7,8 +43,12 @@ class LRUCache:
     to every node stored in the cache.
     """
     def __init__(self, limit=10):
-        pass
-
+        self.limit = limit
+        self.size = 0
+        self.head = None
+        self.tail = None
+        self.storage_dict = {}
+    
     """
     Retrieves the value associated with the given key. Also
     needs to move the key-value pair to the end of the order
@@ -17,7 +57,12 @@ class LRUCache:
     key-value pair doesn't exist in the cache.
     """
     def get(self, key):
-        pass
+        if key not in self.storage_dict:
+            return None
+
+        node = self.storage_dict[key]
+        self.move_to_front(node)
+        return node.get_value()
 
     """
     Adds the given key-value pair to the cache. The newly-
@@ -30,4 +75,86 @@ class LRUCache:
     the newly-specified value.
     """
     def set(self, key, value):
-        pass
+        if key in self.storage_dict:
+            node = self.storage_dict[key]
+            node.set_value(value)
+            self.storage_dict[key] = node
+            self.move_to_front(node)
+            return
+
+        if self.size >= self.limit:
+            del self.storage_dict[self.tail.get_key()]
+            self.remove_from_tail()
+        
+        new_node = Node(key, value)
+        self.storage_dict[key] = new_node
+        self.add_to_head(new_node)
+    
+    ######### Helper Methods #########
+    
+    def isEmpty(self):
+        return self.size == 0
+
+    def __str__(self):
+        list_str = ""
+        curr = self.head
+        while curr is not None:
+            if curr is self.head:
+                list_str += f"{curr.get_value()}"
+            else:
+                list_str += f" -> {curr.get_value()}"
+            curr = curr.get_next()
+        return f"Size = {self.size}, {{{list_str}}}\n"
+
+    def delete(self, node):
+        if node is self.head:
+            self.remove_from_head()
+        elif node is self.tail:
+            self.remove_from_tail()
+        else:
+            self.size -= 1
+            prev_node = node.get_prev()
+            next_node = node.get_next()
+            prev_node.set_next(next_node)
+            next_node.set_prev(prev_node)
+    
+    def add_to_head(self, node):
+        self.size += 1
+        node.set_prev(None)
+        node.set_next(self.head)
+        if self.head:
+            self.head.set_prev(node)
+        else:
+            self.tail = node
+        self.head = node
+    
+    def remove_from_head(self):
+        if self.isEmpty():
+            return None
+        
+        self.size -= 1
+        removed_value = self.head.get_value()
+        self.head = self.head.get_next()
+        if self.head:
+            self.head.set_prev(None)
+        else:
+            self.tail = None
+        return removed_value
+
+    def remove_from_tail(self):
+        if self.isEmpty():
+            return None
+        
+        self.size -= 1
+        removed_value = self.tail.get_value()
+        self.tail = self.tail.get_prev()
+        if self.tail:
+            self.tail.set_next(None)
+        else:
+            self.head = None
+        return removed_value
+    
+    def move_to_front(self, node):
+        if node is not self.head:
+            self.delete(node)
+            self.add_to_head(node)

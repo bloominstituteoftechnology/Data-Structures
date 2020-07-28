d1d5d686964d5ab0af7ef434ef335e556f5f4a93
Richard Olson <8571182+richardOlson@users.noreply.github.com>
2020-07-27 21:54:15 -0600

    work on assignment

--- data_structures/binary_search_tree/binary_search_tree.py
+++ data_structures/binary_search_tree/binary_search_tree.py
@@ -60,28 +60,88 @@ class BSTNode:
 
     # Return the maximum value found in the tree
     def get_max(self):
-        pass
+        # using recursion to find the max
+        # This means that I will need to traverse the whole tree
+        # base case
+        val = None
+
+        if self == None:
+            return  # might need to change this 
+
+        theMax = self.__our_max(self.left.get_max(), self.right.get_max())
+
+        return max(theMax, self.value)
+        
+
+        
+        
 
     # Call the function `fn` on the value of each node
     def for_each(self, fn):
-        pass
+        # will go through each of the Nodes in depth form
+        if self == None:
+            return
+        # doing the left side of the tree first
+        self.for_each(fn)
+
+        # running the funtion on the value of the node
+        fn(self.value)
+
+        # going donw the right side
+        self.for_each(fn)
 
     # Part 2 -----------------------
 
     # Print all the values in order from low to high
     # Hint:  Use a recursive, depth first traversal
     def in_order_print(self):
-        pass
+        # going through the list in recursion
+        if self == None:
+            return 
+
+        self.left.in_order_print()
+        print(self.value)
+        self.right.in_order_print()
 
     # Print the value of every node, starting with the given node,
     # in an iterative breadth first traversal
     def bft_print(self):
-        pass
+        # making it to implement a queue
+        # using the implementation of the queue
+        # that we have made to keep track of the nodes we visit
+        from data_structures.queue.queue import Queue
+
+        thequeue = Queue()
+        # adding everything to the queue
+        root = self
+        thequeue.enqueue(root)
+
+        #looping through the queue
+        while thequeue.size > 0:
+            currentNode = thequeue.dequeue()
+             # will then add the left and the right children if 
+             # they are present
+            if currentNode.left != None:
+                 # adding to the queue
+                 thequeue.enqueue(currentNode.left)
+            if currentNode.right != None:
+                # adding to the queue
+                thequeue.enqueue(currentNode.right)
+
+            print(currentNode.value)
+            
+
+
+
 
     # Print the value of every node, starting with the given node,
     # in an iterative depth first traversal
     def dft_print(self):
-        pass
+        # using a depth first traversal that is iterative
+        # importing and using the stack 
+        node = self
+        while node != None:
+            # doing an inner loop that will go into the left side
 
     # Stretch Goals -------------------------
     # Note: Research may be required
@@ -94,11 +154,20 @@ class BSTNode:
     def post_order_dft(self):
         pass
 
+    # This is a function that I created to do the comparison with a none 
+    def __our_max(self, val1, val2):
+        if val1 == None:
+            return val2
+        if val2 == None:
+            return val1
+        else:
+            return max(val1, val2)
+
 """
 This code is necessary for testing the `print` methods
 """
-bst = BinarySearchTree(1)
-
+#bst = BinarySearchTree(1)
+bst = BSTNode(1)
 bst.insert(8)
 bst.insert(5)
 bst.insert(7)

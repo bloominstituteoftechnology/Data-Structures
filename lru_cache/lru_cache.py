9ada8a24eb4089ca18dd0c637463106ad4b3094f
thomas <t_huynh927@yahoo.com>
2020-07-27 20:30:41 -0700

    did lru cache

--- lru_cache/lru_cache.py
+++ lru_cache/lru_cache.py
@@ -1,3 +1,7 @@
+import os, sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from doubly_linked_list.doubly_linked_list import DoublyLinkedList
+
 class LRUCache:
     """
     Our LRUCache class keeps track of the max number of nodes it
@@ -7,8 +11,9 @@ class LRUCache:
     to every node stored in the cache.
     """
     def __init__(self, limit=10):
-        pass
-
+        self.cache = {}
+        self.limit = limit
+        self.priority = DoublyLinkedList()
     """
     Retrieves the value associated with the given key. Also
     needs to move the key-value pair to the end of the order
@@ -17,7 +22,10 @@ class LRUCache:
     key-value pair doesn't exist in the cache.
     """
     def get(self, key):
-        pass
+        if key in self.cache:
+            self.priority.move_to_front(self.cache[key]["priority"])
+            return self.cache[key]["value"]
+        return None
 
     """
     Adds the given key-value pair to the cache. The newly-
@@ -30,4 +38,32 @@ class LRUCache:
     the newly-specified value.
     """
     def set(self, key, value):
-        pass
+        if key in self.cache or len(self.cache) < self.limit:
+            if key in self.cache:
+                self.priority.move_to_front(self.cache[key]["priority"])
+            else:
+                self.priority.add_to_head(key)
+            self.cache[key] = {
+                "value": value,
+                "priority": self.priority.head   
+            }
+        else:
+            last = self.priority.remove_from_tail()
+            del self.cache[last]
+            self.cache[key] = {
+                "value": value,
+                "priority": self.priority.head
+            }
+            self.priority.add_to_head(key)
+
+
+lru = LRUCache(3)
+lru.set("a",1)
+lru.set("b", 2)
+lru.set("c", 3)
+lru.set("d", 4)
+lru.set("c", 5)
+
+print(lru.cache)
+lru.get("b")
+print(lru.priority)

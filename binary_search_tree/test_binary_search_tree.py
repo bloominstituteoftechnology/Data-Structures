593ea328d0cafd6ad3cfe8fe997e4628fb2ca08b
Rabah Babaci <4rabah@gmail.com>
2020-07-28 19:55:41 -0700

    remove unnecessary passed arguments to methods in test_print_traversal()

--- binary_search_tree/test_binary_search_tree.py
+++ binary_search_tree/test_binary_search_tree.py
@@ -77,30 +77,30 @@ class BinarySearchTreeTests(unittest.TestCase):
         self.bst.insert(4)
         self.bst.insert(2)
 
-        self.bst.in_order_print(self.bst)
+        self.bst.in_order_print()
 
         output = sys.stdout.getvalue()
         self.assertEqual(output, "1\n2\n3\n4\n5\n6\n7\n8\n")
 
         sys.stdout = io.StringIO()
-        self.bst.bft_print(self.bst)
+        self.bst.bft_print()
         output = sys.stdout.getvalue()
         self.assertTrue(output == "1\n8\n5\n3\n7\n2\n4\n6\n" or
                         output == "1\n8\n5\n7\n3\n6\n4\n2\n")
 
         sys.stdout = io.StringIO()
-        self.bst.dft_print(self.bst)
+        self.bst.dft_print()
         output = sys.stdout.getvalue()
         self.assertTrue(output == "1\n8\n5\n7\n6\n3\n4\n2\n" or
                         output == "1\n8\n5\n3\n2\n4\n7\n6\n")
 
         sys.stdout = io.StringIO()
-        self.bst.pre_order_dft(self.bst)
+        self.bst.pre_order_dft()
         output = sys.stdout.getvalue()
         self.assertEqual(output, "1\n8\n5\n3\n2\n4\n7\n6\n")
 
         sys.stdout = io.StringIO()
-        self.bst.post_order_dft(self.bst)
+        self.bst.post_order_dft()
         output = sys.stdout.getvalue()
         self.assertEqual(output, "2\n4\n3\n6\n7\n5\n8\n1\n")
 

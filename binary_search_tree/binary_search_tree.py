1981aab37d3ff117f0e9caad296a4adc65e3062e
Sal B Amer <iosdevsalman@gmail.com>
2020-07-30 05:10:29 -0700

    setup dft_print

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -33,18 +33,12 @@ class BSTNode:
     # Return True if the tree contains the value
     # False if it does not
     def contains(self, target):
-        #check wheather curr node matches target
-        if self.value == target:
+        if target == self.value:
             return True
-        if self.value >= target:
-            if not self.left:
-                return False
-            else:
-                return = self.left.contains(target)
-        if self.value < target:
-            if not self.right:
-                return False
-            return = self.right.contains(target)
+        elif target < self.value:
+            return self.left.contains(target) if self.left else False
+        else:
+            return self.right.contains(target) if self.right else False
 
 
     # Return the maximum value found in the tree
@@ -52,7 +46,7 @@ class BSTNode:
         # The largest value will always be on the right of the current node
         #  if we can go right lets find the largest number by calling get_max on the right subtree
         #  of we cannot go right retrun the current value
-        if self.right = None:
+        if self.right is None:
             return self.value
         max_val = self.right.get_max()
         return max_val
@@ -68,22 +62,44 @@ class BSTNode:
         if self.right:
             self.right.for_each(fn)
 
-    # Part 2 -----------------------
+    # Part 2 ----------------------- Tree Traversal 
 
     # Print all the values in order from low to high
     # Hint:  Use a recursive, depth first traversal
     def in_order_print(self):
-        pass
+        if not self:
+            return
+        if self.left:
+            self.left.in_order_print()
+        print(self.value)
+        if self.right:
+            self.right.in_order_print()
 
     # Print the value of every node, starting with the given node,
     # in an iterative breadth first traversal
-    def bft_print(self):
-        pass
+    def bft_print(self, node):
+        queue = Queue()
+        queue.put(node)
+        while not queue.empty():
+            node = queue.get()
+            print(node.value)
+            if node.left:
+                queue.put(node.left)
+            if node.right:
+                queue.put(node.right)
 
     # Print the value of every node, starting with the given node,
     # in an iterative depth first traversal
-    def dft_print(self):
-        pass
+    def dft_print(self, node):
+        stack = Stack()
+        stack.push(node)
+        while not stack.isEmpty():
+            node = stack.pop()
+            print(node.value)
+            if node.right:
+                stack.push(node.right)
+            if node.left:
+                stack.push(node.left)
 
     # Stretch Goals -------------------------
     # Note: Research may be required
@@ -99,7 +115,7 @@ class BSTNode:
 """
 This code is necessary for testing the `print` methods
 """
-bst = BinarySearchTree(1)
+bst = BSTNode(1)
 
 bst.insert(8)
 bst.insert(5)
@@ -109,13 +125,13 @@ bst.insert(3)
 bst.insert(4)
 bst.insert(2)
 
-bst.bft_print()
-bst.dft_print()
+# bst.bft_print(bst)
+# bst.dft_print(bst)
 
-print("elegant methods")
-print("pre order")
-bst.pre_order_dft()
-print("in order")
-bst.in_order_dft()
-print("post order")
-bst.post_order_dft()  
+# print("elegant methods")
+# print("pre order")
+# bst.pre_order_dft()
+# print("in order")
+# bst.in_order_dft()
+# print("post order")
+# bst.post_order_dft()

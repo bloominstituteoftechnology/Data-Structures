8288c85c5f3c3f4406fff7d90964c7ec43820a07
Seth Nelson <seth.nelson@outlook.com>
2020-07-30 16:38:17 -0700

    got most of the BST code written. Still a few failing tests, and need to complete the dft_print and bft_print code

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -9,6 +9,8 @@ This part of the project comprises two days:
 2. Implement the `in_order_print`, `bft_print`, and `dft_print` methods
    on the BSTNode class.
 """
+
+
 class BSTNode:
     def __init__(self, value):
         self.value = value
@@ -20,24 +22,31 @@ class BSTNode:
         # Take the current value of our node (self.value)
         # Compare to the new value we want to insert
         
-        # if self
-        
+        if self < self.value:
         # if new value < self.value
+            if not self.left:
             # if self.left is already taken by a node
+                if not self.left:
+                    self.left = BSTNode(value)
                 # make that node call insert()
+                else:
+                    self.left.insert(value)
             # set the left child to the new node with new value
             
-        # if new value >= self.value
-            # if self.right is already taken by a node
-                # make that node call insert()
-            # set the right child to the new node with new value
+                # the new value is greater than the current node
+                # go right
+            else:
+                if not self.right:
+                    self.right = BSTNode(value)
+                else:
+                    self.right.insert(value)
             
         pass
 
     # Return True if the tree contains the value
     # False if it does not
     def contains(self, target):
-        if current self.value == target
+        if self.value == target:
             return True
         # compare the target to current value
         # if current value >= target
@@ -60,27 +69,51 @@ class BSTNode:
 
     # Return the maximum value found in the tree
     def get_max(self):
-        pass
+        # if we can go right, go right
+        # return when we can't go right anymore
+        if not self.right:
+            return self.value
+        max_value = self.right.get_max()
+
+        return max_value
 
     # Call the function `fn` on the value of each node
     def for_each(self, fn):
-        pass
+        if self == None:
+            return
+
+        # call the function
+        fn(self.value)
+
+        # if left is not None, go left
+        if self.left:
+            self.left.for_each(fn)
+
+        # if right is not None, go right
+        if self.right:
+            self.right.for_each(fn)
 
     # Part 2 -----------------------
 
     # Print all the values in order from low to high
     # Hint:  Use a recursive, depth first traversal
-    def in_order_print(self):
-        pass
+    def in_order_print(self, node):
+        if node.left:
+            self.in_order_print(node.left)
+
+        print(node.value)
+
+        if node.right:
+            self.in_order_print(node.right)
 
     # Print the value of every node, starting with the given node,
     # in an iterative breadth first traversal
-    def bft_print(self):
+    def bft_print(self, node):
         pass
 
     # Print the value of every node, starting with the given node,
     # in an iterative depth first traversal
-    def dft_print(self):
+    def dft_print(self, node):
         pass
 
     # Stretch Goals -------------------------

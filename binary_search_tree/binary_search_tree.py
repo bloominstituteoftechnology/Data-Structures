6aa6db49e5e30334a80319a0acfff1baa2f488dd
Veto Ramirez <vetorobert3@gmail.com>
2020-07-30 18:10:48 -0700

    push unseved saves

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -1,3 +1,6 @@
+from collections import deque
+
+
 """
 Binary search trees are a data structure that enforce an ordering over 
 the data they store. That ordering in turn makes it a lot more efficient 
@@ -52,23 +55,62 @@ class BSTNode:
 
     # Return the maximum value found in the tree
     def get_max(self):
-        pass
+        #U: traverse BST to find the global max
+        #1. check you input --> is there a node here
+        #2. declare max variable == self.value
+        #3. iterate through tree until we hit Null
+        #4. update max value
+        #5. move to the right
+
+        if not self:
+            return None
+        max_value = self.value
+        current = self
+        while current:
+            if current.value > max_value:
+                max_value = current.value
+            current = current.right
+        return max_value
+
+        # recursive solution
+        #base case: no right node available
+        # recursive step: pass right sub tree to get_max
+        if not self.right:
+            return self.value
+        return self.right.get_max()
 
     # Call the function `fn` on the value of each node
     def for_each(self, fn):
-        pass
+        #U: apply fn to each node of tree
+        #1. apply fn to root
+        #2. call for_each on left and right subtrees
+
+        if not self:
+            return
+        fn(self.value)
+        if self.left:
+            self.left.for_each(fn)
+        if self.right:
+            self.right.for_each(fn)
 
     # Part 2 -----------------------
 
     # Print all the values in order from low to high
     # Hint:  Use a recursive, depth first traversal
     def in_order_print(self):
-        pass
+        if not self:
+            return
+        if self.left:
+            self.left.in_order_print()
+
+        print(self.value)
+
+        if self.right.in_order_print()
 
     # Print the value of every node, starting with the given node,
     # in an iterative breadth first traversal
     def bft_print(self):
-        pass
+        
 
     # Print the value of every node, starting with the given node,
     # in an iterative depth first traversal

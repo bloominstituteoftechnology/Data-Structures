4ac9c83ec2e6886e062f50ac989b2dea52816bdd
Sean Chen <skypemaster007@gmail.com>
2020-08-10 08:40:54 -0700

    Update test_singly_linked_list.py

--- singly_linked_list/test_singly_linked_list.py
+++ singly_linked_list/test_singly_linked_list.py
@@ -36,14 +36,19 @@ class LinkedListTests(unittest.TestCase):
         self.assertIsNone(self.list.tail)
         self.assertIsNone(self.list.remove_head())
 
-    def test_get_max(self):
-        self.assertIsNone(self.list.get_max())
-        self.list.add_to_tail(100)
-        self.assertEqual(self.list.get_max(), 100)
-        self.list.add_to_tail(55)
-        self.assertEqual(self.list.get_max(), 100)
-        self.list.add_to_tail(101)
-        self.assertEqual(self.list.get_max(), 101)
+    def test_remove_tail(self):
+        self.list.add_to_tail(30)
+        self.list.add_to_tail(40)
+        self.assertEqual(self.list.remove_tail(), 40)
+        self.assertFalse(self.list.contains(40))
+        self.assertEqual(self.list.remove_tail(), 30)
+        self.assertFalse(self.list.contains(30))
+
+        self.list.add_to_tail(100)    
+        self.assertEqual(self.list.remove_tail(), 100)    
+        self.assertIsNone(self.list.head)
+        self.assertIsNone(self.list.tail)
+        self.assertIsNone(self.list.remove_tail())
 
 if __name__ == '__main__':
     unittest.main()

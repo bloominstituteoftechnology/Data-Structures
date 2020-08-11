2c2a5cf2e03d1968fd49d4a965c0fdeb07419a4d
Edward Blanciak <blanciak.edward2@gmail.com>
2020-08-10 22:28:00 -0400

    stack complete with LL

--- stack/stack.py
+++ stack/stack.py
@@ -10,45 +10,69 @@ return elements in Last In First Out order.
 3. What is the difference between using an array vs. a linked list when 
    implementing a Stack?
 """
+
+import sys
+
+sys.path.append('../singly_linked_list/')
 from singly_linked_list import LinkedList
+
+
+# class Stack:
+#     def __init__(self):
+#         # Each time an item from the stack gets removed or added, decrement/increment the self.size
+#         self.size = 0
+#         self.storage = []
+#
+#     def __len__(self):
+#         # Return length of self.storage
+#         return len(self.storage)
+#
+#     def push(self, value):
+#         # Increment self.size
+#         self.size += 1
+#         # Adds the incoming value to the top of the stack / storage list
+#         return self.storage.append(value)
+#
+#     def pop(self):
+#         # Check if storage is not empty
+#         if len(self.storage) != 0:
+#             # Decrement self.size
+#             self.size -= 1
+#             # Removes last item of self.storage
+#             return self.storage.pop()
+#
+#
 
 class Stack:
     def __init__(self):
-        # Each time an item from the stack gets removed or added, decrement/increment the self.size
         self.size = 0
-        self.storage = []
+        self.storage = LinkedList()
 
     def __len__(self):
-        # Return length of self.storage
-        return len(self.storage)
+        # Return the length of the SLL
+        return self.size
 
     def push(self, value):
-        # Increment self.size
         self.size += 1
-        # Adds the incoming value to the top of the stack / storage list
-        return self.storage.append(value)
-
-        # FIXME
-        # Should I be returning the appended value here?
+        # Invoke add_to_tail
+        self.storage.add_to_tail(value)
 
     def pop(self):
-        # Check if storage is not empty
-        if len(self.storage) != 0:
-            # Decrement self.size
+        if self.size > 0:
             self.size -= 1
-            # Removes last item of self.storage
-            return self.storage.pop()
+            # Invoke remove_tail
+            return self.storage.remove_tail()
 
 
 # Creates a new stack
 s1 = Stack()
 
 # Successfully prints the size
-print(f'Size: {s1.size}')
+print(f'Size: {s1.__len__()}')
 # Successfully adds item
 s1.push(1)
 # Successfully prints the size when incremented
-print(f'Size: {s1.size}')
+print(f'Size: {s1.__len__()}')
 s1.push(2)
 s1.push(3)
 print(s1.storage)
@@ -56,22 +80,4 @@ print(s1.storage)
 s1.pop()
 print(s1.storage)
 # Successfully prints the size when decremented
-print(f'Size: {s1.size}')
-
-class StackLL:
-    def __init__(self):
-        self.size = 0
-        self.storage = LinkedList()
-
-    def __len__(self):
-        # Return the length of the SLL
-        pass
-
-    def push(self, value):
-        # Invoke add_to_tail
-
-        pass
-
-    def pop(self):
-        pass
-
+print(f'Size: {s1.__len__()}')

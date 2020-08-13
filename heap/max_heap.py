9a75e462597089f660d5deed5f0ebfe39f200e7c
tatianaportsova <tt.sova@gmail.com>
2020-08-13 17:20:43 -0400

    Completed the max_heap stretch goal

--- heap/max_heap.py
+++ heap/max_heap.py
@@ -29,11 +29,7 @@ class Heap:
         self._bubble_up(last)
 
     def delete(self):
-        if self.size == 2:
-          self.size -=1
-          max = self.storage.pop(0)
-          self._sift_down(0)
-        elif self.size > 2:
+        if self.size > 1:
           max = self.storage.pop(0)
           self.storage[0], self.storage[len(self.storage)-1] = self.storage[len(self.storage)-1], self.storage[0]
           self.size -=1
@@ -50,26 +46,26 @@ class Heap:
         return self.size
 
     def _bubble_up(self, index):
-        parent = index // 2
-        if index <= 0:
-          return
-        else:
-          if self.storage[index] > self.storage[parent]:
-            self.storage[index], self.storage[parent] = self.storage[parent],self.storage[index]
-            self._bubble_up(parent)
+        while index > 0:
+            parent = (index - 1) // 2
+            if self.storage[index] > self.storage[parent]:
+                self.storage[index], self.storage[parent] = self.storage[parent], self.storage[index]
+                index = parent
+            else:
+                break
 
     def _sift_down(self, index):
-        left_child = index * 2
-        right_child = index * 2 + 1
-        largest = index
-        if left_child < (len(self.storage)-1) and self.storage[left_child] > self.storage[index]:
-          largest = left_child
-          self.storage[index], self.storage[left_child] = self.storage[left_child],self.storage[index]
-          self._sift_down(largest)
-        if right_child < (len(self.storage)-1) and self.storage[right_child] > self.storage[index]:
-          largest = right_child
-          self.storage[index], self.storage[right_child] = self.storage[right_child], self.storage[index]
-          self._sift_down(largest)
+        next = index + 1
+        while index < (len(self.storage)-1):
+            if next <= (len(self.storage)-1) and self.storage[next] <= self.storage[index]:
+              next += 1
+              self._sift_down(next)
+            elif next <= (len(self.storage)-1) and self.storage[next] > self.storage[index]:
+                self.storage[index], self.storage[next] = self.storage[next],self.storage[index]
+            #   self._sift_down(next)
+                self._sift_down(self.storage[0])
+            else:
+                break
 
 # class Heap:
 #     def __init__(self):
@@ -123,3 +119,4 @@ class Heap:
 #           largest = right_child
 #           self.storage[index], self.storage[right_child] = self.storage[right_child], self.storage[index]
 #           self._sift_down(largest)
+

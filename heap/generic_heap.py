6b1146f5290e36d363440539c82863e6ee736464
tatianaportsova <tt.sova@gmail.com>
2020-08-17 21:26:14 -0400

    Completed the 'Generic_heap' algorithm

--- heap/generic_heap.py
+++ heap/generic_heap.py
@@ -1,22 +1,79 @@
+# python3 heap/test_generic_heap.py -v
+
 class Heap:
-    def __init__(self, comparator):
+    def __init__(self, comparator=None):
         self.storage = []
         self.comparator = comparator
+        self.size = 0
 
     def insert(self, value):
-        pass
+        self.storage.append(value)
+        self.size += 1
+        last = len(self.storage)-1
+        self._bubble_up(last)
 
     def delete(self):
-        pass
+        if self.size > 1:
+            max = self.storage.pop(0)
+            self.storage[0], self.storage[len(self.storage)-1] = self.storage[len(self.storage)-1], self.storage[0]
+            self.size -=1
+            self._sift_down(0)
+            return max
+        else:
+            max = self.storage.pop(0)
+            self.size -=1
+            return max
 
     def get_priority(self):
-        pass
+        if self.comparator == None:
+            return self.storage[0]
+        else:
+            return self.storage[0]
 
     def get_size(self):
-        pass
+        return self.size
 
     def _bubble_up(self, index):
-        pass
+        if self.comparator == None:
+            while index > 0:
+                parent = (index - 1) // 2
+                if self.storage[index] > self.storage[parent]:
+                    self.storage[index], self.storage[parent] = self.storage[parent], self.storage[index]
+                    index = parent
+                else:
+                    break
+        else:
+           while index > 0:
+                parent = (index - 1) // 2
+                if self.storage[index] < self.storage[parent]:
+                    self.storage[index], self.storage[parent] = self.storage[parent], self.storage[index]
+                    index = parent
+                else:
+                    break
 
     def _sift_down(self, index):
-        pass
+        if self.comparator == None:
+            next = index + 1
+            while index < (len(self.storage)-1):
+                if next <= (len(self.storage)-1) and self.storage[next] <= self.storage[index]:
+                  next += 1
+                  self._sift_down(next)
+                elif next <= (len(self.storage)-1) and self.storage[next] > self.storage[index]:
+                    self.storage[index], self.storage[next] = self.storage[next],self.storage[index]
+                    self._sift_down(next)
+                    self._sift_down(self.storage[0])
+                else:
+                    break
+        else:
+            next = index + 1
+            while index < (len(self.storage)-1):
+                if next <= (len(self.storage)-1) and self.storage[next] >= self.storage[index]:
+                  next += 1
+                  self._sift_down(next)
+                elif next <= (len(self.storage)-1) and self.storage[next] < self.storage[index]:
+                    self.storage[index], self.storage[next] = self.storage[next],self.storage[index]
+                    self._sift_down(next)
+                    self._sift_down(self.storage[0])
+                else:
+                    break
+ 

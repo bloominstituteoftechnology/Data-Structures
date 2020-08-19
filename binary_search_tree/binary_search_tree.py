45a73b2da3f4efece8b17f243ae2fc68d6b6a59b
Ryan Sizemore <58410246+mastaryan@users.noreply.github.com>
2020-08-19 16:42:49 -0400

    Initial commit- new branch Ryan_D-Sizemore

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -2,7 +2,6 @@
 Binary search trees are a data structure that enforce an ordering over 
 the data they store. That ordering in turn makes it a lot more efficient 
 at searching for a particular piece of data in the tree. 
-
 This part of the project comprises two days:
 1. Implement the methods `insert`, `contains`, `get_max`, and `for_each`
    on the BSTNode class.
@@ -17,116 +16,45 @@ class BSTNode:
 
     # Insert the given value into the tree
     def insert(self, value):
-        if value < self.value:
-            if self.left:
-                self.left.insert(value)
-            else:
-                self.left = BSTNode(value)
-        else:
-            if self.right:
-                self.right.insert(value)
-            else:
-                self.right = BSTNode(value)
-        
+        pass
 
     # Return True if the tree contains the value
     # False if it does not
-    
     def contains(self, target):
-        if target == self.value:
-            return True
-        
-        if target >= self.value:
-            if self.right is None:
-                return False
-        else:
-            return self.right.contains(target)
-        
-        if target < self.value:
-            if self.left is None:
-                return False
-        else:
-            return self.left.contains(target)
-        
+        pass
 
     # Return the maximum value found in the tree
     def get_max(self):
-        if self.right is None:
-            return self.value
-        else:
-            return self.right.get_max()
+        pass
 
     # Call the function `fn` on the value of each node
     def for_each(self, fn):
-        fn(self.value)
-        if self.right is not None:
-            self.right.for_each(fn)
-        if self.left is not None:
-            self.left.for_each(fn)
+        pass
 
     # Part 2 -----------------------
 
     # Print all the values in order from low to high
     # Hint:  Use a recursive, depth first traversal
     def in_order_print(self, node):
-        if node is None:
-            return
-
-        node.in_order_print(node.left)
-        print(node.value)
-        node.in_order_print(node.right)
+        pass
 
     # Print the value of every node, starting with the given node,
     # in an iterative breadth first traversal
     def bft_print(self, node):
-        if node is None:
-            return
-
-        current = [node]
-        while current:
-            next_level = []
-            for node in current:
-                print(node.value)
-                if node.left:
-                    next_level.append(node.left)
-                if node.right:
-                    next_level.append(node.right)
-
-            current = next_level  
+        pass
 
     # Print the value of every node, starting with the given node,
     # in an iterative depth first traversal
     def dft_print(self, node):
-        if node is None:
-            return
-        
-        current = [node]
-        while current:
-            curr_node = current.pop()
-            print(curr_node.value)
-
-            if curr_node.right:
-                current.append(curr_node.right)
-            if curr_node.left:
-                current.append(curr_node.left)
+        pass
 
     # Stretch Goals -------------------------
     # Note: Research may be required
 
     # Print Pre-order recursive DFT
     def pre_order_dft(self, node):
-        if node is None:
-            return
-        
-        print(node.value)    
-        node.pre_order_dft(node.left)
-        node.pre_order_dft(node.right)
+        pass
 
     # Print Post-order recursive DFT
     def post_order_dft(self, node):
-        if node is None:
-            return
-            
-        node.post_order_dft(node.left)
-        node.post_order_dft(node.right)
-        print(node.value)
+        pass

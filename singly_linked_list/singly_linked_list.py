175f8563c5515f57cdb20bc5ae7003233de8fbe9
Ariunbold <ariun1118@gmail.com>
2020-08-22 16:48:28 -0400

    fixed all the failed tests

--- singly_linked_list/singly_linked_list.py
+++ singly_linked_list/singly_linked_list.py
@@ -1,36 +1,85 @@
 
 class Node:
-    def __init__(self, value):
+    def __init__(self, value=None, next_node=None):
+        # the value at this linked list node
         self.value = value
-        self.next = None
+        # reference to the next node in the list
+        self.next_node = next_node
+
+    def get_value(self):
+        return self.value
+
+    def get_next(self):
+        return self.next_node
+
+    def set_next(self, new_next):
+        # set this node's next_node reference to the passed in node
+        self.next_node = new_next
 
 
 class LinkedList:
     def __init__(self):
         self.head = None
         self.tail = None
-
-    def traverse_list(self):
-        if self.head is None:
-            print("List is empty")
-            return
-        else:
-            n = self.head
-            while n is not None:
-                print(n.value, " ")
-                n = n.next
+        self.length = 0
 
     def add_to_head(self, value):
         new_node = Node(value)
-        new_node.next = self.head
         self.head = new_node
+        # if self.head is None and self.tail is None:
+        if self.length == 0:    
+            self.tail = new_node
+        self.length += 1
 
     def add_to_tail(self, value):
         new_node = Node(value)
-        if self.head is None:
+        if self.head is None and self.tail is None:
             self.head = new_node
-            return
-        n = self.head
-        while n.next is not None:
-            n = n.next
-        n.next = new_node
+        else:
+            self.tail.set_next(new_node)
+        self.tail = new_node
+        self.length += 1
+
+    def remove_head(self):
+        # empty LL
+        if self.head is None:
+            return None
+
+        # LL with 1 Node
+        elif self.head == self.tail:
+            value = self.head.get_value()
+            self.head = None
+            self.tail = None
+            self.length -= 1
+            return value
+
+        # LL with 2+ Nodes
+        else:
+            value = self.head.get_value()
+            self.head = self.head.get_next()
+            self.length -= 1
+            return value
+
+    def remove_tail(self):
+        # empty LL
+        if self.head is None:
+            return None
+
+        # LL with 1 Node
+        elif self.head == self.tail:
+            value = self.head.get_value()
+            self.head = None
+            self.tail = None
+            self.length -= 1
+            return value
+
+        # LL with 2+ Nodes
+        else:
+            current = self.head
+            while current.get_next() is not self.tail:
+                current = current.get_next()
+            value = self.tail.get_value()
+            current.set_next(None)
+            self.tail = current
+            self.length -= 1
+            return value

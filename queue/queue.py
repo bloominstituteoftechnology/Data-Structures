82af50a4430837c0a58eae2163b1fc0505101510
connor holly <connor-holly11@gmail.com>
2020-08-23 21:26:30 -0400

    mvp

--- queue/queue.py
+++ queue/queue.py
@@ -17,15 +17,37 @@ Stretch: What if you could only use instances of your Stack class to implement t
 class Queue:
     def __init__(self):
         self.size = 0
-        # self.storage = ?
+        self.storage = []
 
-        self.s
-    
     def __len__(self):
-        pass
+        return len(self.storage)
 
     def enqueue(self, value):
-        pass
+        self.storage.insert(0,value)
 
     def dequeue(self):
-        pass
+      if self.__len__() is 0:
+         return None
+      else:
+         return self.storage.pop()
+
+# from singly_linked_list.singly_linked_list import LinkedList
+# class Queue:
+#    def __init__(self):
+#       self.size = 0
+#       self.storage = LinkedList()
+   
+#    def __len__(self):
+#       return self.size
+
+#    def enqueue(self, value):
+#       self.storage.add_to_head(value)
+#       self.size += 1
+
+#    def dequeue(self):
+#       if self.__len__() is 0:
+#          return None
+#       else:
+#          front = self.storage.remove_from_tail()
+#          self.size -= 1
+#          return front.value

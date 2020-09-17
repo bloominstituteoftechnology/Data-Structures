dcf7c2c363562666ec72c72a48ecf27a9b46f009
Veto Ramirez <vetorobert3@gmail.com>
2020-09-16 20:23:00 -0700

    Working on SLL

--- singly_linked_list/singly_linked_list.py
+++ singly_linked_list/singly_linked_list.py
@@ -63,4 +63,13 @@ class LinkedList:
 
         """middle node: How do you find and return the middle node of a singly linked list in one pass? You do not have access to the length of the list. If the list is even, you should return the first of the two "middle" nodes. You may not store the nodes in another data structure."""
 
+
+class Node:
+    def __init__(self, value, next_node = None):
+        self.value = value
+        self.next_node = next_node
+
+    
+
+
         

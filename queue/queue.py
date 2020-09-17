163329e11225a0496638d0e30a6067f5db4d5255
Alexander Thompson <53663334+alext8900@users.noreply.github.com>
2020-09-17 01:04:34 -0500

    Added second queue method with linked list, added stack and second stack method, did the linked list

--- queue/queue.py
+++ queue/queue.py
@@ -13,13 +13,15 @@ return elements in First In First Out order.
 Stretch: What if you could only use instances of your Stack class to implement the Queue?
          What would that look like? How many Stacks would you need? Try it!
 """
+
+"""
 class Queue:
     def __init__(self):
         self.size = 0
         self.storage = []
-    
+
     def __len__(self):
-        return len(self.storage):
+        return len(self.storage)
 
     def enqueue(self, value):
         self.storage.append(value)
@@ -30,4 +32,27 @@ class Queue:
             del self.storage[0]
         else:
             item = None
-		return item
+        return item
+
+"""
+from singly_linked_list import LinkedList
+
+class Queue:
+    def __init__(self):
+        self.size = 0
+        self.storage = LinkedList()
+    
+    def __len__(self):
+        return self.size
+
+    def enqueue(self, value):
+        self.storage.add_to_tail(value)
+        self.size += 1
+
+
+    def dequeue(self):
+        if self.size == 0:
+            return None
+        self.size -= 1
+        return self.storage.remove_head()   
+        

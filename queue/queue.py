d66228bada6788f032b73004c2a7455c6c03da51
Chad Parker <chad@chadparker.com>
2020-09-16 21:05:48 -0700

    Stretch: Implement Queue with Stacks, tests pass!

--- queue/queue.py
+++ queue/queue.py
@@ -1,6 +1,8 @@
 import sys
 sys.path.append('../singly_linked_list/')
+sys.path.append('../stack/')
 from singly_linked_list import LinkedList
+from stack import Stack
 
 """
 A queue is a data structure whose primary purpose is to store and
@@ -33,19 +35,46 @@ Stretch: What if you could only use instances of your Stack class to implement t
 #             return None
 #         return self.storage.pop(0)
 
+# class Queue:
+#     def __init__(self):
+#         self.size = 0
+#         self.storage = LinkedList()
+    
+#     def __len__(self):
+#         return self.size
+
+#     def enqueue(self, value):
+#         self.size += 1
+#         self.storage.add_to_tail(value)
+
+#     def dequeue(self):
+#         if self.size > 0:
+#             self.size -= 1
+#         return self.storage.remove_head()
+
 class Queue:
     def __init__(self):
         self.size = 0
-        self.storage = LinkedList()
+        # 2 stacks required
+        self.push_stack = Stack()
+        self.pop_stack = Stack()
     
     def __len__(self):
         return self.size
 
     def enqueue(self, value):
         self.size += 1
-        self.storage.add_to_tail(value)
+        self.push_stack.push(value)
 
     def dequeue(self):
         if self.size > 0:
             self.size -= 1
-        return self.storage.remove_head()
+        # re-stack to reverse order
+        while self.push_stack.size > 0:
+            self.pop_stack.push(self.push_stack.pop())
+        # pop off one
+        value = self.pop_stack.pop()
+        # restack to original stack, original order
+        while self.pop_stack.size > 0:
+            self.push_stack.push(self.pop_stack.pop())
+        return value

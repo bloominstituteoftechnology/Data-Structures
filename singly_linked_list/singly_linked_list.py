8b248a9929b4f2ce0f7023d375175c1edcac88cb
lex-marie790 <lexi.marie790@gmail.com>
2020-09-16 21:40:59 -0700

    completed

---
+++ singly_linked_list/singly_linked_list.py
@@ -0,0 +1,56 @@
+class Node:
+    def __init__(self, value, next=None):
+        self.value = value
+        self.next = next
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def add_to_tail(self, value):
+        newNode = Node(value, None)
+        if self.head is None:
+            self.head = newNode
+            self.tail = newNode
+
+        else:
+            oldTail = self.tail
+            oldTail.next = newNode
+            self.tail = newNode
+
+    def remove_head(self):
+        if self.head is None:
+            return None
+        else:
+            if self.head == self.tail:
+                oldHead = self.head
+                self.head = None
+                self.tail = None
+                return oldHead.value
+            else:
+                currentHead = self.head
+                self.head = currentHead.next
+                return currentHead.value
+
+    def remove_tail(self):
+        if self.head == None:
+            return None
+
+        elif self.head == self.tail:
+            oldTail = self.tail
+            self.head = None
+            self.tail = None
+            return oldTail.value
+
+        else:
+            current = self.head
+            oldTail = self.tail
+            while current.next is not self.tail:
+                current = current.next
+
+            removedValue = self.tail.value
+            self.tail = None
+            self.tail = current
+            return removedValue
--- stack/stack.py
+++ stack/stack.py
@@ -1,71 +1,18 @@
-class Node: 
-    def __init__(self,value):
-        self.value = value
-        self.next = None
-    
-    def get_value(self):
-        return self.value
+"""
+A stack is a data structure whose primary purpose is to store and
+return elements in Last In First Out order. 
+1. Implement the Stack class using an array as the underlying storage structure.
+   Make sure the Stack tests pass.
+2. Re-implement the Stack class, this time using the linked list implementation
+   as the underlying storage structure.
+   Make sure the Stack tests pass.
+3. What is the difference between using an array vs. a linked list when 
+   implementing a Stack?
+"""
 
-    def get_next(self):
-        return self.next
-
-    def set_next(self, new_text): 
-        self.next = new_text
-
-class LinkedList:
-    def __init__(self):
-        self.head = None
-        self.tail = None
-
-    def add_to_tail(self,value):
-        new_node = Node(value)
-
-        if self.tail is None and self.tail is None: 
-            self.head = new_node
-            self.tail = new_node
-
-        else:
-            self.tail.set_next(new_node)
-            self.tail = new_node
-
-    def remove_head(self):
-        if self.head is None and self.tail is None:
-            return
-
-        if not self.head.get_next():
-            head = self.head
-            self.head = head
-            self.tail = None
-        value = self.head.get_value()
-        self.head = self.head.get_next()
-        return value
-
-    def remove_tail(self):
-        if self.head is None and self.tail is None:
-            return 
-
-        if self.head == self.tail:
-            val = self.head.get_value()
-            self.head = None
-            self.tail = None
-            return val
-
-        else: 
-            val = self.tail.get_value()
-            current = self.head
-
-        while current.get_next() is not self.tail:
-            current = current.get_next()
-
-
-        self.tail = current
-        self.tail.set_next(None)
-        self.tail = current
-        return val
-
-    def length(self):
-        return len(self)
-
+import sys
+sys.path.append('../singly_linked_list/')
+from singly_linked_list import LinkedList
 
 class Stack:
     def __init__(self):

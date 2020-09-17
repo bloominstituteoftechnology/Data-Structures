8fc974ecb35145fd4653365712a8a5ebeae956af
rfamilara <techknohauz.com>
2020-09-16 23:22:42 -0500

    Completed queue.py

--- queue/queue.py
+++ queue/queue.py
@@ -13,16 +13,28 @@ return elements in First In First Out order.
 Stretch: What if you could only use instances of your Stack class to implement the Queue?
          What would that look like? How many Stacks would you need? Try it!
 """
+from doubly_linked_list import DoublyLinkedList
+import sys
+sys.path.append('../doubly_linked_list')
+
 class Queue:
     def __init__(self):
         self.size = 0
-        # self.storage = ?
+        self.storage = DoublyLinkedList()
     
     def __len__(self):
-        pass
+        return self.size        
 
     def enqueue(self, value):
-        pass
+        self.storage.add_to_tail(value)
+        self.size += 1
 
     def dequeue(self):
-        pass
+        if self.storage.head == None:
+            return None
+
+        else:
+            # we need to decrement/decrease size by 1
+            self.size -= 1
+            # use remove_from_head() to delete the first node/the head
+            return self.storage.remove_from_head()    

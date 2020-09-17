95b81646b9b713b21cc6864f2645f7429396c6a4
SMoore3773 <Smooresculpture@gmail.com>
2020-09-16 23:37:45 -0400

    format stack

--- queue/stack.py
+++ queue/stack.py
@@ -7,7 +7,7 @@ return elements in Last In First Out order.
 2. Re-implement the Stack class, this time using the linked list implementation
    as the underlying storage structure.
    Make sure the Stack tests pass.
-3. What is the difference between using an array vs. a linked list when 
+3. What is the difference between using an array vs. a linked list when
    implementing a Stack?
 """
 
@@ -16,6 +16,7 @@ from singly_linked_list import LinkedList
 stack = LinkedList()
 # arr = []
 
+
 class Stack:
     def __init__(self):
         self.size = 0
@@ -23,18 +24,16 @@ class Stack:
 
     def __len__(self):
         return self.size
-        
 
     def push(self, value):
         stack.add_to_head(value)
         self.size += 1
         # arr.append(value)
-        
 
     def pop(self):
         if self.size == 0:
             return None
-        self.size -=1
+        self.size -= 1
         return stack.remove_head()
 
         # end = len(arr)

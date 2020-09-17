52cf3c37df74490dd5b9f929c137b4ae252f4cc3
Mohd Raza <noorinfotechinc@gmail.com>
2020-09-16 22:13:29 -0500

    starting project

--- stack/stack.py
+++ stack/stack.py
@@ -10,6 +10,9 @@ return elements in Last In First Out order.
 3. What is the difference between using an array vs. a linked list when 
    implementing a Stack?
 """
+from './../singly_linked_list/singly_linked_list' import Node
+
+
 class Stack:
     def __init__(self):
         self.size = 0

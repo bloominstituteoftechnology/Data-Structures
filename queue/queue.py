37175fe722b5fe96da94de85e6153a574965d428
bsherwood9 <36565536+bsherwood9@users.noreply.github.com>
2020-09-16 23:19:33 -0600

    Updaated this file.

--- queue/queue.py
+++ queue/queue.py
@@ -10,7 +10,7 @@ return elements in First In First Out order.
 3. What is the difference between using an array vs. a linked list when
    implementing a Queue?
 
-   
+
 
 Stretch: What if you could only use instances of your Stack class to implement the Queue?
          What would that look like? How many Stacks would you need? Try it!

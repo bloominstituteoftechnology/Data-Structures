c40a5a51cd6be99fb2275cfdf66c0585ab93b231
Veto Ramirez <vetorobert3@gmail.com>
2020-09-17 17:56:51 -0700

    Working on bugs for linked kist

--- singly_linked_list/singly_linked_list.py
+++ singly_linked_list/singly_linked_list.py
@@ -1,4 +1,4 @@
-class Node:
+class Node1:
     def __init__(self, value=None, next_node=None):
         self.value = value
         self.next_node = next_node
@@ -12,7 +12,7 @@ class Node:
     def set_next(self, new_next):
         self.next_node = new_next
 
-class LinkedList:
+class LinkedList1:
     def __init__(self):
         self.head = None
         self.tail = None
@@ -69,7 +69,81 @@ class Node:
         self.value = value
         self.next_node = next_node
 
+    def get_value(self):
+        return self.value
     
+    def get_next(self):
+        return self.next_node
 
+    def set_next(self, new_next):
+        self.next_node = new_next
 
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def add_to_tail(self, value):
+        new_node = Node(value)
+
+        if self.head is None and self.tail is None:
+            self.head = new_node
+            self.tail = new_node
+
+        else:
+            self.tail.set_next(new_node)
+            self.tail = new_node
+
+    def remove_tail(self):
+        if self.head is None and self.tail is None:
+            return None
+
+        if self.head == self.tail:
+            value = self.tail.get_value()
+
+            self.head = None
+            self.tail = None
+
+            return value
+
+        else:
+            value = self.tail.get_value()
+            current_node = self.head
+
+            while current_node.get_next() != self.tail:
+                current_node = current_node.get_next()
+
+            self.tail = current_node
+            self.tail.set_next(None)
+            return value
+
+    def remove_head(self):
+        if self.head is None and self.tail is None:
+            return None
         
+        if self.head == self.tail:
+            value = self.head.get_value()
+
+            self.head = None
+            self.tail = None
+            return value
+        else:
+            value = self.head.get_value()
+            self.head = self.head.get_next()
+            return value
+
+        def contains(self, value):
+            if not self.head:
+                return False
+  
+        # get a reference to the node we're currently at; update this as we traverse the list
+        current = self.head
+        # check to see if we're at a valid node 
+        while current:
+        # return True if the current value we're looking at matches our target value
+            if current.get_value() == value:
+                return True
+        # update our current node to the current node's next node
+        current = current.get_next()
+        # if we've gotten here, then the target node isn't in our list
+        return False

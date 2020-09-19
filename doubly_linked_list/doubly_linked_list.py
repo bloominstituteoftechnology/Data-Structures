93ee0d65d0b289a9e8b5dccd3f99b8e27d51cc6c
Claudia Maciel <clc_80@yahoo.com>
2020-09-18 20:07:28 -0700

    Same error

--- doubly_linked_list/doubly_linked_list.py
+++ doubly_linked_list/doubly_linked_list.py
@@ -26,8 +26,13 @@ class ListNode:
     def delete(self):
         if self.prev:
             self.prev.next = self.next
+        else:
+            return None
+
         if self.next:
             self.next.prev = self.prev
+        else:
+            return None
 
 """
 Our doubly-linked list class. It holds references to 
@@ -129,8 +134,12 @@ class DoublyLinkedList:
         # Check to see if list is empty
         if self.head is None and self.tail is None:
             return None
+            
+        # decrement by 1
+        self.length -= 1
+
         # Check to see if there is only one node 
-        elif self.head == self.tail:
+        if self.head == self.tail:
             self.head = None
             self.tail = None
         # Check to see if it's the head node
@@ -143,9 +152,6 @@ class DoublyLinkedList:
             node.delete()
         else: 
             node.delete()
-
-        # decrement by 1
-        self.length -= 1
 
         
 

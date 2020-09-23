1f4956cc45699f08cf9efff9458ff6898805c963
Veto Ramirez <vetorobert3@gmail.com>
2020-09-22 19:56:17 -0700

    Added notes from lecture

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -152,6 +152,61 @@ class BSTNode:
         pass
 
 """
+Redo code from flex
+"""
+
+class BSTNode:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+    # Insert the given value into the tree
+    def insert(self, value):
+        pass
+
+    # Return True if the tree contains the value
+    # False if it does not
+    def contains(self, target):
+        pass
+
+    # Return the maximum value found in the tree
+    def get_max(self):
+        pass
+
+    # Call the function `fn` on the value of each node
+    def for_each(self, fn):
+        pass
+
+    # Part 2 -----------------------
+
+    # Print all the values in order from low to high
+    # Hint:  Use a recursive, depth first traversal
+    def in_order_print(self):
+        pass
+
+    # Print the value of every node, starting with the given node,
+    # in an iterative breadth first traversal
+    def bft_print(self):
+        pass
+
+    # Print the value of every node, starting with the given node,
+    # in an iterative depth first traversal
+    def dft_print(self):
+        pass
+
+    # Stretch Goals -------------------------
+    # Note: Research may be required
+
+    # Print Pre-order recursive DFT
+    def pre_order_dft(self):
+        pass
+
+    # Print Post-order recursive DFT
+    def post_order_dft(self):
+        pass
+
+"""
 This code is necessary for testing the `print` methods
 """
 bst = BinarySearchTree(1)

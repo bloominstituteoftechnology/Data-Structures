ff0c20b8dfd6f608293bd04df9ea1ecb4233ba4d
lex-marie790 <lexi.marie790@gmail.com>
2020-09-22 22:12:16 -0700

    doubly linked list completed

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -17,51 +17,38 @@ class BSTNode:
 
     # Insert the given value into the tree
     def insert(self, value):
-        # take the current value of our node (self.value)
-        # compare to the new value we want to insert
-        # if new value < self.value
+
         if self.value > value:
-            # if self.left is already taken by a node
             if self.left != None:
-                # make that (left) node, call insert
                 self.left.insert(value)
             else:
-                # set the left to the new node with the new value
                 self.left = BSTNode(value)
 
-        # elif new value >= self.value
         elif self.value <= value:
-            # if self.right is already taken by node
             if self.right != None:
-                # make that (right) node call insert
                 self.right.insert(value)
-            # set the right child to the new node with new value
             else:
                 self.right = BSTNode(value)
 
+    # Return True if the tree contains the value
+    # False if it does not
     def contains(self, target):
         if self.value == target:
             return True
-        # compare the target to current value
-        # if current value < target
-        # found = False
+
         if self.value > target:
-            # check the left substree
-            # if you cannot go left, return False
             if self.left is None:
                 return False
             found = self.left.contains(target)
 
-        # elif current value >= target
+
         if self.value <= target:
-            # check if right subtree contains target
-            # if you cannot go right, return False
             if self.right is None:
                 return False
             found = self.right.contains(target)
 
         return found
-
+        
     # Return the maximum value found in the tree
     def get_max(self):
         current = self
@@ -124,13 +111,13 @@ bst.insert(3)
 bst.insert(4)
 bst.insert(2)
 
-# bst.bft_print()
-# bst.dft_print()
+bst.bft_print()
+bst.dft_print()
 
-# print("elegant methods")
-# print("pre order")
-# bst.pre_order_dft()
-# print("in order")
-# bst.in_order_dft()
-# print("post order")
-# bst.post_order_dft()  
+print("elegant methods")
+print("pre order")
+bst.pre_order_dft()
+print("in order")
+bst.in_order_dft()
+print("post order")
+bst.post_order_dft()  

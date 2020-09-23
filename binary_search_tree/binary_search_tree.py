830ebab462da48da4316dd0f3b494e3db27c936d
Claudia Maciel <clc_80@yahoo.com>
2020-09-22 18:23:14 -0700

    Added insert method to BST

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -17,19 +17,79 @@ class BSTNode:
 
     # Insert the given value into the tree
     def insert(self, value):
-        pass
+        # check if the new nodes value is less than the current nodes value
+        if value < self.value:
+            # if there is no left child already here
+            if self.left != None:
+                # add the new node to the left
+                # create a BSTNode and encapsulate the value in it then set it to the left
+                self.left = BSTNode(value)
+            # otherwise call insert on the left node
+            else:
+              self.left.insert(value)
+        # otherwise (the new nodes value is greaterthan or equal to the current node value)
+        else:
+            # if there is no right child already here
+            if self.right != None:
+                # add the new node to the right
+                # create a BSTNode and encapsulate the value in it then set it to the right
+                self.right = BSTNode(value)
+            # otherwise call insert on the right node
+            else:
+                self.right.insert(value)
 
     # Return True if the tree contains the value
     # False if it does not
     def contains(self, target):
+        # if the value of the current node matches the target
+            # return True
+
+        # check if the target is less than the current nodes value
+            # if there is no left child already here
+                # return False
+            # otherwise
+                # return a call of contains on the left child passing in the target value
+        # otherwise (the target is greater than the current nodes value)
+            # if there is no right child already here
+                # return False
+            # otherwise
+                # return a call of contains on the right child passing in the target value
         pass
 
     # Return the maximum value found in the tree
     def get_max(self):
+        # check for an empty tree
+            # return None
+
+        # ----------------------------------------------
+        # recursive approach
+        # check if there is no node to the right
+            # return the nodes value
+        # return a call to get max on the right child
+        # -----------------------------------------------
+
+        # iterative aproach
+
+        # initialise the max value
+
+        # get a ref to the current node
+
+        # loop while there is still a current node
+            # if the current value is greater than the max value, update the max value
+            # move on to the next right node
+        
+        # return the max value
         pass
 
     # Call the function `fn` on the value of each node
     def for_each(self, fn):
+        # call the function passing in the current nodes value
+
+        # if there is a node to the left
+            # call the function on the left value
+        
+        # if there is a node to the right
+            # call the function on the right node
         pass
 
     # Part 2 -----------------------
@@ -80,6 +140,6 @@ print("elegant methods")
 print("pre order")
 bst.pre_order_dft()
 print("in order")
-bst.in_order_dft()
+#bst.in_order_dft()
 print("post order")
 bst.post_order_dft()  

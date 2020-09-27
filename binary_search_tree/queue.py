f2b7ebccd9fd72a7ad129fe6a10db238826a3100
Cristina Altreche <c.altreche@gmail.com>
2020-09-27 17:34:50 -0400

    removed singly linked list from binary folder. Not needed

--- binary_search_tree/queue.py
+++ binary_search_tree/queue.py
@@ -13,7 +13,6 @@ return elements in First In First Out order.
 Stretch: What if you could only use instances of your Stack class to implement the Queue?
          What would that look like? How many Stacks would you need? Try it!
 """
-from singly_linked_list import LinkedList
 
 
 class Queue:
